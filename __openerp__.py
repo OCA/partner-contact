1b0dfd03bbb160dd5539f95503d3e2aca31cfe12
Gerhard Könighofer <gerhard.koenighofer@swing-system.com>
2012-04-21 11:09:27 +0200

    __openerp__.py modernized

--- __openerp__.py
+++ __openerp__.py
@@ -1,23 +1,23 @@
-{
-	"name" : "Partner Identification Numbers",
-	"version" : "0.2",
-	"author"  : "ChriCar Beteiligungs- und Beratungs GmbH" ,
-	"website" : "http://www.chricar.at/ChriCar",
-	"description" : """This module allows to manage all sort of identification numbers
-	and certificates which are assigend to a partner and vary from country to country
-	* Commercial register
-	* VAT ID (eventually)
-	* fiscal ID's
-	* membership numbers
-	* ....
+{ "name"         : "Partner Identification Numbers"
+, "version"      : "0.2"
+, "author"       : "ChriCar Beteiligungs- und Beratungs GmbH" 
+, "website"      : "http://www.chricar.at/ChriCar"
+, "description"  : """This module allows to manage all sort of identification numbers
+and certificates which are assigned to a partner and vary from country to country
 
-	""",
-	"category" : "Generic Modules/Others",
-	"depends" : ["base"],
-	"init_xml" : [],
-	"demo_xml" : ["partner_id_number_demo.xml"],
-	"update_xml" : ["partner_id_number_view.xml","security/ir.model.access.csv"],
-        #"update_xml" : ["partner_id_number_view.xml","security/ir.model.access.csv"],
-	"active": False,
-	"installable": True
+ * Commercial register
+ * VAT ID (eventually)
+ * fiscal ID's
+ * membership numbers
+ * ...
+
+"""
+, "category"     : "Generic Modules/Others"
+, "depends"      : ["base"]
+, "init_xml"     : []
+, "demo_xml"     : ["partner_id_number_demo.xml"]
+, "update_xml"   : ["partner_id_number_view.xml", "security/ir.model.access.csv"]
+#, "update_xml"   : ["partner_id_number_view.xml","security/ir.model.access.csv"]
+, "auto_install" : False
+, "installable"  : True
 }
