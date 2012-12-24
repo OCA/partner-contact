e30eb5096cf22414704d580eed7cda940b4d17d4
ferdinand <office@chricar.at>
2012-12-24 08:11:34 +0100

    [FIX] change keyword demo_xml to demo

--- __openerp__.py
+++ __openerp__.py
@@ -15,7 +15,7 @@ and certificates which are assigned to a partner and vary from country to countr
 , "category"     : "Generic Modules/Others"
 , "depends"      : ["base"]
 , "init_xml"     : []
-, "demo_xml"     : ["partner_id_number_demo.xml"]
+, "demo"         : ["partner_id_number_demo.xml"]
 , "update_xml"   : ["partner_id_number_view.xml", "security/ir.model.access.csv"]
 #, "update_xml"   : ["partner_id_number_view.xml","security/ir.model.access.csv"]
 , "auto_install" : False
