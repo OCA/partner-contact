0079040a016e5cc5168c097f51675bce6edf7295
Vincent Renaville <vincent.renaville@camptocamp.com>
2013-05-21 15:27:17 +0200

    [FIX] search

--- partner_firstname/partner.py
+++ partner_firstname/partner.py
@@ -33,7 +33,7 @@ class ResPartner(Model):
     def _compute_name_custom(self, cursor, uid, ids, fname, arg, context=None):
         res = {}
         for rec in self.read(cursor, uid, ids, ['firstname', 'lastname']):
-            name = rec['lastname'] if rec['lastname'] else u""  + (u" " + rec['firstname'] if rec['firstname'] else u"")
+            name = '%s %s'%(rec['lastname'] if rec['lastname'] else u"",rec['firstname'] if rec['firstname'] else u"")
             res[rec['id']] = name
         return res
 
