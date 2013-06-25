173dda1494036025de0cae0120eb1dd3665c2bb4
unknown <nicolas.bessi@camptocamp.com>
2013-06-25 10:29:42 +0200

    [MV] rename better_zip to base_location

---
+++ base_location/partner.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Nicolas Bessi. Copyright Camptocamp SA
+#    Contributor: Pedro Manuel Baeza <pedro.baeza@serviciosbaeza.com>
+#                 Ignacio Ibeas <ignacio@acysos.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from openerp.osv import orm, fields
+
+
+class ResPartner(orm.Model):
+    _inherit = "res.partner"
+    _columns = {'zip_id': fields.many2one('res.better.zip', 'City/Location')}
+
+    def onchange_zip_id(self, cursor, uid, ids, zip_id, context=None):
+        if not zip_id:
+            return {}
+        if isinstance(zip_id, list):
+            zip_id = zip_id[0]
+        bzip = self.pool['res.better.zip'].browse(cursor, uid, zip_id, context=context)
+        return {'value': {'zip': bzip.name,
+                          'city': bzip.city,
+                          'country_id': bzip.country_id.id if bzip.country_id else False,
+                          'state_id': bzip.state_id.id if bzip.state_id else False,
+                          }
+                }
---
+++ base_location/state.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Nicolas Bessi. Copyright Camptocamp SA
+#    Contributor: Pedro Manuel Baeza <pedro.baeza@serviciosbaeza.com>
+#                 Ignacio Ibeas <ignacio@acysos.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from openerp.osv import orm, fields
+
+
+class ResCountryState(orm.Model):
+
+    _inherit = 'res.country.state'
+
+    _columns = {'better_zip_ids': fields.one2many('res.better.zip', 'state_id', 'Cities')}
