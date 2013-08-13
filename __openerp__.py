db93c865ab679c31da9c601bf044e6043b786e26
ferdinand <office@chrcar.at>
2013-08-13 06:53:21 +0200

    [FIX] unify author name

--- __openerp__.py
+++ __openerp__.py
@@ -1,6 +1,6 @@
 { "name"         : "Partner Identification Numbers"
 , "version"      : "0.2"
-, "author"       : "ChriCar Beteiligungs- und Beratungs GmbH"
+, "author"       : "ChriCar Beteiligungs- und Beratungs- GmbH"
 , "website"      : "http://www.chricar.at/ChriCar"
 , "description"  : """This module allows to manage all sort of identification numbers
 and certificates which are assigned to a partner and vary from country to country
