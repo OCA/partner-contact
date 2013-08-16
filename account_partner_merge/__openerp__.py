18e9def5d9e9c53a2600e2fcd77abc1c5fd143d5
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2013-08-16 16:15:40 +0200

    [ADD] account_partner_merge module

---
+++ account_partner_merge/__openerp__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Yannick Vaucher
+#    Copyright 2013 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{'name' : 'Account Partner Merge',
+ 'version' : '1.0',
+ 'category': 'Hidden',
+ 'description': """Update invoice commercial_partner_id""",
+ 'author' : 'Camptocamp',
+ 'maintainer': 'Camptocamp',
+ 'website': 'http://www.camptocamp.com/',
+ 'depends' : ['account_report_company', 'base_partner_merge'],
+ 'data': [],
+ 'test': [],
+ 'installable': True,
+ 'auto_install': True,
+ 'application': False,
+ }
