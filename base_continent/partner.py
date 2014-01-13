ae875f10378ec35fc2bb500b19c730614ecb963a
Romain Deheele <romain.deheele@camptocamp.com>
2014-01-13 17:34:23 +0100

    [ADD] add continent management addon

---
+++ base_continent/partner.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Romain Deheele
+#    Copyright 2014 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv.orm import Model
+from openerp.osv import fields
+
+
+class Partner(Model):
+    _inherit = 'res.partner'
+    _columns = {
+        'continent_id': fields.related('country_id', 'continent_id',
+                                       type='many2one',
+                                       relation='res.continent',
+                                       string='Continent',
+                                       readonly=True, store=True),
+    }
