1d3ac0e75e6ebc839f749339890420559551596c
EL HADJI DEM <elhadji.dem@savoirfairelinux.com>
2014-01-30 11:41:08 -0500

    [ADD] Add passport and passport_partner modules: It allows to manage several passports for each contact

---
+++ passport/res_passport.py
@@ -0,0 +1,45 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This module copyright (C) 2013 Savoir-faire Linux
+#    (<http://www.savoirfairelinux.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv import fields, orm
+
+
+class res_passport(orm.Model):
+    _description = 'Passport'
+    _name = 'res.passport'
+    _columns = {
+        'name': fields.char('Owner name', size=256, select=True,
+                            help='Owner name (As printed into the passport).'),
+        'number': fields.char('Passport No', size=50,
+                              help='Passport number.'),
+        'country_id': fields.many2one('res.country', 'Delivery country',
+                                      help="Delivery country."),
+        'expiration_date': fields.date('Expiration date',
+                                       help="Expiration date."),
+        'birth_date': fields.date('Birth Date', help="Birth Date."),
+        'gender': fields.selection([('male', 'Male'),
+                                    ('female', 'Female')],
+                                   'Gender',
+                                   help="Gender."),
+    }
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
