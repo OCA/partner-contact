191a82dc53519102458b9a3ae99ae81c54c3f97c
ferdinand <office@chrcar.at>
2014-02-13 22:26:58 +0400

    [REF] v8 description

--- __openerp__.py
+++ __openerp__.py
@@ -1,16 +1,16 @@
 { 'sequence': 500,
- "name"         : "Partner Identification Numbers"
+"name"         : "Partner Identification Numbers"
 , "version"      : "0.2"
 , "author"       : "ChriCar Beteiligungs- und Beratungs- GmbH"
 , "website"      : "http://www.chricar.at/ChriCar"
 , "description"  : """This module allows to manage all sort of identification numbers
 and certificates which are assigned to a partner and vary from country to country
 
- * Commercial register
- * VAT ID (eventually)
- * fiscal ID's
- * membership numbers
- * ...
+    * Commercial register
+    * VAT ID (eventually)
+    * fiscal ID's
+    * membership numbers
+    * ...
 
 """
 , "category"     : "Generic Modules/Others"
