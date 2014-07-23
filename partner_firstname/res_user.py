fde8de711d229d1604f82404092b96e7b5ccea85
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2014-07-23 19:54:06 +0200

    [ADD] partner_firstname for v8 because it's working as is

---
+++ partner_firstname/res_user.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Nicolas Bessi. Copyright Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from openerp.osv import orm
+from openerp.tools.translate import _
+
+
+class ResUsers(orm.Model):
+
+    _inherit = 'res.users'
+
+    def copy_data(self, cr, uid, _id, default=None, context=None):
+        """
+        Avoid to replicate the firstname into the name when duplicating a user
+        """
+        default = default or {}
+        if not default.get('lastname'):
+            default = default.copy()
+            default['lastname'] = (
+                _('%s (copy)') % self.read(
+                    cr, uid, [_id], ['lastname'], context=context
+                )[0]['lastname']
+            )
+            if default.get('name'):
+                del(default['name'])
+        return super(ResUsers, self).copy_data(
+            cr, uid, _id, default, context=context)
