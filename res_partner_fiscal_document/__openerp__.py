fffb17e3ed8563a6e782655c97f785d5ac685bcf
blaggacao <david@elaleman.co>
2014-08-08 06:06:49 -0500

    -> PEP8 styling 2nd

--- res_partner_fiscal_document/__openerp__.py
+++ res_partner_fiscal_document/__openerp__.py
@@ -32,9 +32,11 @@
 {
     'name': 'Partner Authentication with Fiscal Document',
     'description': """
-* Keep track of due legal identification of partners with a corresponing document.
+* Keep track of due legal identification of partners with a \
+corresponing document.
 * Register additional document types within the GUI.
-* Easy hook for custom copy and validation/formatting methods (res_partner_document.py)
+* Easy hook for custom copy and validation/formatting methods \
+(res_partner_document.py)
 """,
     'category': 'Localization',
     'license': 'AGPL-3',
--- res_partner_fiscal_document/res_partner_idtype.py
+++ res_partner_fiscal_document/res_partner_idtype.py
@@ -20,7 +20,7 @@
 #
 ##############################################################################
 
-from openerp import models, fields, api, _
+from openerp import models, fields # , api, _
 
 
 class ResPartnerIDtype(models.Model):
