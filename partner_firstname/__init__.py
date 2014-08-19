7d623563bb6f0330b5870c01e736b8f337a78179
Sandy Carter <bwrsandman@gmail.com>
2014-08-19 08:35:31 -0400

    Fix pep8 (line length)
    Fix pylint errors

--- partner_firstname/__init__.py
+++ partner_firstname/__init__.py
@@ -17,5 +17,6 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+
 from . import partner
 from . import res_user
