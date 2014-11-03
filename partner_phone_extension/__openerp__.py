347a50e6b76a13c92377637bcafdd546bd99ab6c
El Hadji Dem <elhadji.dem@savoirfairelinux.com>
2014-11-03 14:00:31 -0500

    [ADD] Partner_phone_number_extension module
    Add a phone number extension field

---
+++ partner_phone_extension/__openerp__.py
@@ -0,0 +1,45 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This module copyright (C) 2013-2014 Savoir-faire Linux
+#    (<http://www.savoirfairelinux.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Partner Phone Number Extension',
+    'version': '1.1',
+    'category': 'Customer Relationship Management',
+    'summary': 'Partner Phone Number Extension',
+    'description': """
+Partner Phone Number Extension
+==============================
+
+This module allows to add a phone number extension field in partner form view.
+
+Contributors
+------------
+* El Hadji Dem (elhadji.dem@savoirfairelinux.com)
+""",
+    'author': 'Savoir-faire Linux',
+    'website': 'www.savoirfairelinux.com',
+    'license': 'AGPL-3',
+    'data': [
+        'res_partner_view.xml',
+    ],
+    'installable': True,
+}
