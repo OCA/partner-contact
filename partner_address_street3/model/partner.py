4f3dc75bc7c2d6c73eed82e83f6cf8482de709bc
jbeficent <jordi.ballester@eficent.com>
2014-12-05 18:31:04 +0100

    Add module 'partner_address_street_3'. Backported from 8.0

---
+++ partner_address_street3/model/partner.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Nicolas Bessi
+#    Copyright 2014 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from openerp.osv import orm, fields
+from openerp.addons.base.res import res_partner
+
+ADDRESS_FIELDS = ('street', 'street2', 'street3', 'zip', 'city', 'state_id', 'country_id')
+
+class res_partner(orm.Model):
+    """Add third field in address"""
+
+    _inherit = "res.partner"
+    _columns = {
+        'street3': fields.char('Street 3'),
+    }
+
+    def _address_fields(self, cr, uid, context=None):
+        """ Returns the list of address fields that are synced from the parent
+        when the `use_parent_address` flag is set. """
+        return list(ADDRESS_FIELDS)
+
+    def _commercial_fields(self, cr, uid, context=None):
+        fields = super(res_partner, self
+                       )._commercial_fields(cr, uid, context=context)
+        fields.append('street3')
+        return fields
+
+
+class res_country(orm.Model):
+    """Override default adresses formatting of coutries"""
+
+    _inherit = 'res.country'
+
+    _defaults = {
+        'address_format': ("%(street)s\n%(street2)s\n%(street3)s\n"
+                           "%(city)s %(state_code)s %(zip)s\n"
+                           "%(country_name)s"),
+    }
+
+
