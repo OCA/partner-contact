9c82d94f73edb1f87f4dff7b947c52dd5dc32250
Alejandro Santana <alejandrosantana@anubia.es>
2015-01-19 00:35:12 +0100

    [IMP] base_location: Updated to v8 syntax and uses. Added filters in some views. Added two columns in respective tree views.

--- base_location/__openerp__.py
+++ base_location/__openerp__.py
@@ -4,6 +4,7 @@
 #    Author: Nicolas Bessi. Copyright Camptocamp SA
 #    Contributor: Pedro Manuel Baeza <pedro.baeza@serviciosbaeza.com>
 #                 Ignacio Ibeas <ignacio@acysos.com>
+#                 Alejandro Santana <alejandrosantana@anubia.es>
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -19,19 +20,21 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-{'name': 'Location management (aka Better ZIP)',
- 'version': '0.3.1',
- 'depends': ['base'],
- 'author': 'Camptocamp',
- 'description': """
-Introduces a better zip/npa management system.
-It enables zip/city auto-completion on partners""",
- 'website': 'http://www.camptocamp.com',
- 'data': ['better_zip_view.xml',
-          'state_view.xml',
-          'company_view.xml',
-          'partner_view.xml',
-          'security/ir.model.access.csv'],
- 'installable': True,
- 'active': False,
- }
+{
+    'name': 'Location management (aka Better ZIP)',
+    'version': '0.3.2',
+    'depends': ['base'],
+    'author': 'Camptocamp',
+    'description': '''
+    Introduces a better zip/npa management system.
+    It enables zip/city auto-completion on partners.''',
+    'website': 'http://www.camptocamp.com',
+    'data': ['views/better_zip.xml',
+             'views/state.xml',
+             'views/res_country.xml',
+             'views/company.xml',
+             'views/partner.xml',
+             'security/ir.model.access.csv'],
+    'installable': True,
+    'active': False,
+}
