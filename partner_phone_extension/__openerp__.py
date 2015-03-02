154f49873744975ed518ea67207dc86b5a0eec7a
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2015-03-02 17:26:29 +0100

    Add OCA as author of OCA addons
    In order to get visibility on https://www.odoo.com/apps the OCA board has
    decided to add the OCA as author of all the addons maintained as part of the
    association.

--- partner_phone_extension/__openerp__.py
+++ partner_phone_extension/__openerp__.py
@@ -35,7 +35,7 @@ Contributors
 ------------
 * El Hadji Dem (elhadji.dem@savoirfairelinux.com)
 """,
-    'author': 'Savoir-faire Linux',
+    'author': "Savoir-faire Linux,Odoo Community Association (OCA)",
     'website': 'www.savoirfairelinux.com',
     'license': 'AGPL-3',
     'data': [
