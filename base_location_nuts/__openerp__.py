a3853a0903d35bb236038295ba67b09e4b3a7f07
Antonio Espinosa <antonioea@antiun.com>
2015-03-31 00:51:36 +0200

    [ADD] NUTS regions as a base addon

---
+++ base_location_nuts/__openerp__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# Python source code encoding : https://www.python.org/dev/peps/pep-0263/
+##############################################################################
+#
+#    OpenERP, Odoo Source Management Solution
+#    Copyright (c) 2015 Antiun Ingeniería S.L. (http://www.antiun.com)
+#                       Antonio Espinosa <antonioea@antiun.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'NUTS Regions',
+    'category': 'Localisation/Europe',
+    'version': '8.0.1.0.0',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'views/res_partner_nuts_view.xml',
+        'views/res_partner_view.xml',
+        'wizard/nuts_import_view.xml',
+        'security/ir.model.access.csv',
+    ],
+    'author': 'Antiun Ingeniería S.L., '
+              'Odoo Community Association (OCA)',
+    'website': 'http://www.antiun.com',
+    'license': 'AGPL-3',
+    'installable': True,
+}
