5b423f7a44d0a07b51d47679e7cb9c5af5486f19
Joao Alfredo Gama Batista <joao.gama@savoirfairelinux.com>
2015-05-06 11:16:42 -0400

    [FIX] Fix copyright

--- res_company_logo_secondary/res_company.py
+++ res_company_logo_secondary/res_company.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Author: Nicolas Bessi. Copyright Camptocamp SA
+#    Odoo, Open Source Management Solution
+#    This module copyright (C) 2015 Savoir-faire Linux
+#    (<http://www.savoirfairelinux.com>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
