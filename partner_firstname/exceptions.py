f0680bd6aa890823dfe9b52388b7b7dc73b7592b
Jairo Llopis <j.llopis@grupoesoc.es>
2015-05-15 13:53:22 +0200

    Better partner_firstname. Merged rebase.
    Fix wrong naming.
    
    Remove subclassing of exception, since there is only one.
    
    Rename exception according to PEP8.
    
    Reduce tests' redundancy.
    
    Add test for whitespace trimming.
    
    Use unicode for code & tests.
    
    Fix wrong comments.
    
    Split _name_inverse adding _name_clean_inverse.
    
    Conflicts:
    	partner_firstname/models.py
    
    Remove license header in XML files.
    
    Preserve old view names.
    
    Credits to last translator.
    
    Increase coverage.
    
    Add args to exception to display the correct message in the UI.
    
    Execute _firstname_install when installing, and log it.
    
    Rename methods to follow guidelines.
    
    Better docstrings.
    
    Workaround https://github.com/odoo/odoo/issues/6324.
    
    Fix users not being able to create companies.
    
    This happened because the invert method was not being called when using
    the UI, and because lastname & firstname fields were required while
    hidden.
    
    Add new tests and fix the resulting bugs.
    
    New tests, some fail.
    
    Add docs to test modules.
    
    Fix recursive onchange misbehavior & tests.
    
    Fix UI problem when lastname was u"".
    
    It should be `False` to avoid `required` errors.
    
    Use new() to create onchange records. Reduce redundancy in tests.
    
    Tests work with `mail` module installed now.
    
    Sometimes, the only way is to just skip them.

---
+++ partner_firstname/exceptions.py
@@ -0,0 +1,27 @@
+# -*- encoding: utf-8 -*-
+
+# Odoo, Open Source Management Solution
+# Copyright (C) 2014-2015  Grupo ESOC <www.grupoesoc.es>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from openerp import _, exceptions
+
+
+class EmptyNamesError(exceptions.ValidationError):
+    def __init__(self, record, value=_("No name is set.")):
+        self.record = record
+        self._value = value
+        self.name = _("Error(s) with partner %d's name.") % record.id
+        self.args = (self.name, value)
--- partner_firstname/tests/__init__.py
+++ partner_firstname/tests/__init__.py
@@ -28,4 +28,4 @@
 #
 ##############################################################################
 
-from . import test_partner_firstname
+from . import test_empty, test_name, test_onchange
