19565a548cb77d97de1c416c43fd242cddb3ca1f
Jairo Llopis <j.llopis@grupoesoc.es>
2015-05-15 13:59:59 +0200

    Add module partner_lastname2.

---
+++ partner_second_lastname/tests/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# © 2015 Grupo ESOC Ingeniería de Servicios, S.L.U.
+
+from . import test_name, test_onchange
+from openerp.addons.partner_firstname.tests import test_empty
