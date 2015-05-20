6998c6d75f2505c85ea443a25d34e518b91935b8
Jairo Llopis <j.llopis@grupoesoc.es>
2015-05-20 10:18:23 +0200

    Create partner_contact_personal_information_page.
    Really I'm just renaming partner_contact_base.
    The new name is more self-explanatory.

---
+++ partner_contact_personal_information_page/__openerp__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+# Odoo, Open Source Management Solution
+# Copyright (C) 2014-2015  Grupo ESOC <www.grupoesoc.es>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+{
+    "name": "Personal information page for contacts",
+    "summary": "Add a page to contacts form to put personal information",
+    "version": "1.0.0",
+    "author": "Odoo Community Association (OCA)",
+    "category": "Customer Relationship Management",
+    "website": "https://odoo-community.org/",
+    "depends": [
+        "base"
+    ],
+    "data": [
+        "views/res_partner.xml",
+    ],
+}
