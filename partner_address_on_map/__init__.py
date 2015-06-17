4e478e91852e9da3122dbf32b8fbb41f24ffb708
Alexis de Lattre <alexis.delattre@akretion.com>
2015-06-18 00:19:30 +0200

    Add module partner_address_on_map

---
+++ partner_address_on_map/__init__.py
@@ -0,0 +1,3 @@
+# -*- encoding: utf-8 -*-
+
+from . import partner_address_on_map
---
+++ partner_address_on_map/partner_address_on_map.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Partner Address on Map module for OpenERP
+#    Copyright (C) 2015 Akretion (http://www.akretion.com/)
+#    @author: Alexis de Lattre <alexis.delattre@akretion.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import models, fields, api, _
+from openerp.exceptions import Warning
+
+
+class MapUrl(models.Model):
+    _name = 'map.url'
+    _description = 'Map System'
+
+    name = fields.Char(string='Map Provider', required=True)
+    url = fields.Char(string='URL', required=True)
+
+
+class ResUsers(models.Model):
+    _inherit = 'res.users'
+
+    # begin with context_ to allow user to change it by himself
+    context_map_url_id = fields.Many2one(
+        'map.url', string='Map Provider')
+    # TODO: Add default
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    @api.multi
+    def open_map(self):
+        if not self.env.user.context_map_url_id:
+            raise Warning(_(
+                'Missing Map Provider: you should set it in your Preferences.'))
+        url = self.env.user.context_map_url_id.url
+        if self.street:
+            url += self.street
+        if self.city:
+            url += ' ' + self.city
+        if self.state_id:
+            url += ' ' + self.state_id.name
+        if self.country_id:
+            url += ' ' + self.country_id.name
+        return {
+            'type': 'ir.actions.act_url',
+            'url': url,
+            'targer': 'new',
+            }
