002e5409e23be0ccbc550fd09521e5cb05e69dac
Jairo Llopis <j.llopis@grupoesoc.es>
2015-07-03 09:18:01 +0200

    Only check if OCA_RUNBOT exists, not its value.

--- base_contact/__init__.py
+++ base_contact/__init__.py
@@ -27,6 +27,6 @@ import os
 _logger = logging.getLogger(__name__ + ".deprecated")
 
 # Skip warnings on runbots
-_method = _logger.info if os.environ.get("OCA_RUNBOT") else _logger.warning
+_method = _logger.info if "OCA_RUNBOT" in os.environ else _logger.warning
 _method("This module is DEPRECATED. See %s/README.rst.",
         os.path.dirname(__file__))
