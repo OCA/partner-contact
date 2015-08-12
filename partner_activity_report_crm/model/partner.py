530765fb341a8493e39d2ec8bfd996d58f9f693c
vrenaville <vincent.renaville@camptocamp.com>
2015-08-12 13:47:47 +0200

    [ADD] activity on crm for partner

---
+++ partner_activity_report_crm/model/partner.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Vincent Renaville, Damien Crier
+#    Copyright 2014-2015 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import models, api
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    @api.multi
+    def get_partner_crm_informations(self):
+        crm_obj = self.env['crm.lead']
+        lead_partners = crm_obj.search([('partner_id', '=', self.id)])
+        return lead_partners
