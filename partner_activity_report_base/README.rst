23a0e7e905d4ec69b4433c8cbf33cf9f032cdaa8
vrenaville <vincent.renaville@camptocamp.com>
2015-08-12 11:36:23 +0200

    [ADD] partner activity base report

---
+++ partner_activity_report_base/README.rst
@@ -0,0 +1,45 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+    :alt: License: AGPL-3
+
+Partner Activity Details Base
+=============================
+
+This module give a report with basic information on the partner
+---------------------------------------------------------------
+
+* Display adresses and contact of the partner
+
+
+Bug Tracker
+===========
+
+Bugs are tracked on `GitHub Issues <https://github.com/OCA/partner-contact/issues>`_.
+In case of trouble, please check there if your issue has already been reported.
+If you spotted it first, help us smashing it by providing a detailed and welcomed feedback
+`here <https://github.com/OCA/partner-contact/issues/new?body=module:%20partner_activity_report%0Aversion:%208.0%0A%0A**Steps%20to%20reproduce**%0A-%20...%0A%0A**Current%20behavior**%0A%0A**Expected%20behavior**>`_.
+
+
+Credits
+=======
+
+Contributors
+------------
+
+* Vincent Renaville <vincent.renaville@camptocamp.com>
+* Damien Crier <damien.crier@camptocamp.com>
+
+Maintainer
+----------
+
+.. image:: https://odoo-community.org/logo.png
+   :alt: Odoo Community Association
+   :target: https://odoo-community.org
+
+This module is maintained by the OCA.
+
+OCA, or the Odoo Community Association, is a nonprofit organization whose
+mission is to support the collaborative development of Odoo features and
+promote its widespread use.
+
+To contribute to this module, please visit http://odoo-community.org.
+
---
+++ partner_activity_report_base/wizard/partner_activity_selection.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Vincent Renaville,Damien Crier
+#    Copyright 2014-2015 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import models, fields, api, _
+
+
+class partner_print_activity(models.TransientModel):
+    _name = 'partner.print.activity'
+    _description = 'Partner printing activity'
+
+    @api.model
+    def _get_partner_ids(self):
+        context = self.env.context
+        print str(context)
+        if context.get('active_model') != 'res.partner':
+            return False
+        return context.get('active_ids', False)
+
+    partner_information = fields.Boolean(string='Print partner address',
+                                         default=True)
+    partner_address_information = fields.Boolean(
+        string='Print partner contacts',
+        default=True)
+
+    partner_ids = fields.Many2many(comodel_name='res.partner',
+                                   string='Partner ids',
+                                   default=_get_partner_ids)
+    user_id = fields.Many2one(comodel_name='res.users', string='User',
+                              required=False, default=1)
+
+    @api.multi
+    def print_report(self):
+        self.ensure_one()
+        if not self.partner_ids:
+            raise api.Warning(_('No partner selected'))
+        report_name = 'partner_activity_report_base.report_partner_activity_qweb'
+        report_obj = self.env['report'].with_context(
+            active_ids=self.id
+            )
+        return report_obj.get_action(self, report_name)
