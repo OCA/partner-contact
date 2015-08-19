1dbd3934ff40d77df8192ec76d4b8fa668d83d5d
Apptuate <support@idncommunications.com>
2015-08-19 12:12:03 -0500

    fixed indent to pep8

--- partner_relations/model/res_partner_relation_all.py
+++ partner_relations/model/res_partner_relation_all.py
@@ -167,9 +167,8 @@ class ResPartnerRelationAll(models.AbstractModel):
         domain = []
         if self.type_selection_id.partner_category_other is not False:
             domain.append(
-                ('category_id', 'in',
-                self.type_selection_id.partner_category_other.id)
-            )
+                         ('category_id', 'in',
+                         self.type_selection_id.partner_category_other.id))
 
         if self.type_selection_id.contact_type_other is not False:
             is_company = self.type_selection_id.contact_type_other == 'c'
