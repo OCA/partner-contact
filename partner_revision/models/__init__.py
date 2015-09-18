856dd90f6b179d004f498e3070624f2fe6f5de90
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2015-09-18 16:45:20 +0200

    Rename addon according to the new term: partner_changeset

--- partner_revision/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import res_partner
-from . import res_partner_revision
-from . import revision_field_rule
--- partner_revision/tests/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import test_revision_flow
-from . import test_revision_field_type
-from . import test_revision_origin
