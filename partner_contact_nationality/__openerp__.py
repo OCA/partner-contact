2b46f8f31d45f17071ee640e119ff44a93e86ec4
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2015-10-09 10:01:52 +0200

    [UPD] prefix versions with 8.0

--- partner_contact_nationality/__openerp__.py
+++ partner_contact_nationality/__openerp__.py
@@ -18,7 +18,7 @@
 
 {
     "name": "Contact's nationality",
-    "version": "1.0.0",
+    "version": "8.0.1.0.0",
     "author": "Odoo Community Association (OCA)",
     "category": "Customer Relationship Management",
     "website": "https://odoo-community.org/",
