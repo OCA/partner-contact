b3fee1d5f912c9a3a82ee94fa583614f80bc8dfa
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2015-10-09 10:01:52 +0200

    [UPD] prefix versions with 8.0

--- base_partner_merge/__openerp__.py
+++ base_partner_merge/__openerp__.py
@@ -2,7 +2,7 @@
     'name': 'Base Partner Merge',
     'author': "OpenERP S.A.,Odoo Community Association (OCA)",
     'category': 'Generic Modules/Base',
-    'version': '0.1',
+    'version': '8.0.0.1.0',
     'description': """
 backport module, to be removed when we switch to saas2 on the private servers
 """,
