81400b31f72f707aaf2548e39eba3d4121e1d0af
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2015-10-09 10:01:52 +0200

    [UPD] prefix versions with 8.0

--- portal_partner_merge/__openerp__.py
+++ portal_partner_merge/__openerp__.py
@@ -19,7 +19,7 @@
 #
 ##############################################################################
 {'name': 'Portal Partner Merge',
- 'version': '1.0',
+ 'version': '8.0.1.0.0',
  'category': 'Hidden',
  'description': """
 Link module for base_partner_merge which extract portal dependency
