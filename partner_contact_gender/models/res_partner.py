242d87c01c09291ed59ab91d817ed02da03d507e
Richard deMeester <richard@willowit.com.au>
2015-11-20 10:57:25 +1100

    [9.0] Upgrade Partner Gender module
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

---
+++ partner_contact_gender/models/res_partner.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2014-2015  Grupo ESOC <www.grupoesoc.es>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    gender = fields.Selection([('male', 'Male'),
+                               ('female', 'Female'),
+                               ('other', 'Other')
+                               ])
