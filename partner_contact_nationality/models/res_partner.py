d5d8693ecb47f27c4c0f835b4fd63afe955a6d31
Richard deMeester <richard@willowit.com.au>
2015-11-23 09:59:19 +1100

    [9.0] Port partner_contact_nationality.

---
+++ partner_contact_nationality/models/res_partner.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2014-2015  Grupo ESOC <www.grupoesoc.es>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    nationality_id = fields.Many2one("res.country", "Nationality")
