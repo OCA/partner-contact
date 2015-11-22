db1dd083b23dc2b2e33d0d6c3233bdd344a67fa2
Richard deMeester <richard@willowit.com.au>
2015-11-23 09:41:18 +1100

    Changes as requested.

--- partner_contact_gender/models/res_partner.py
+++ partner_contact_gender/models/res_partner.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
+# Copyright (C) 2014-2015  Grupo ESOC <www.grupoesoc.es>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import fields, models, _, api
+from openerp import fields, models
 
 
 class ResPartner(models.Model):
