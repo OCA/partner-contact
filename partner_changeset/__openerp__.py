e1d99f47a14aee67ccea6e7b6d8570b5cd809608
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2015-11-30 09:56:55 +0100

    Adapt for inclusion in OCA

--- partner_changeset/__openerp__.py
+++ partner_changeset/__openerp__.py
@@ -1,27 +1,10 @@
 # -*- coding: utf-8 -*-
-#
-#
-#    Authors: Guewen Baconnier
-#    Copyright 2015 Camptocamp SA
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#
+# © 2015 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {'name': 'Partner Changesets',
- 'version': '1.0',
- 'author': 'Camptocamp',
+ 'version': '8.0.1.0.0',
+ 'author': 'Camptocamp, Odoo Community Association (OCA)',
  'license': 'AGPL-3',
  'category': 'Sales Management',
  'depends': ['base',
@@ -36,7 +19,5 @@
           ],
  'demo': ['demo/changeset_field_rule.xml',
           ],
- 'test': [],
  'installable': True,
- 'auto_install': False,
  }
