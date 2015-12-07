2ae6d63cf10e8f5fcab51c97ad033f411c0dc176
Javier Iniesta <javieria@antiun.com>
2015-12-07 15:34:19 +0100

    partner_sector

---
+++ partner_sector/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# © 2015 Antiun Ingenieria S.L. - Javier Iniesta
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import res_partner
+from . import res_partner_sector
