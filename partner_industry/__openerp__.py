51f70505860f14802d7de492ffcca030d566342a
Julien Thewys <julien.thewys@gmail.com>
2015-12-15 08:21:46 +0100

    [REF] partner_industry: cosmetic fit

--- partner_industry/__openerp__.py
+++ partner_industry/__openerp__.py
@@ -1,31 +1,11 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Author: Jacques-Etienne Baudoux <je@bcim.be>
-#    Copyright 2015 BCIM sprl
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
+# © 2015 Jacques-Etienne Baudoux <je@bcim.be>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Partner Industry Sector',
-    'version': '1.0',
+    'version': '8.0.1.0',
     'author': "BCIM,Odoo Community Association (OCA)",
-    'maintainer': 'BCIM',
     'category': 'Sales Management',
-    'complexity': 'easy',
     'depends': ['base'],
     'website': 'http://www.bcim.be',
     'data': [
