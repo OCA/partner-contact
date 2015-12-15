e30d13ea351ff2dfd6cb56ac561d5eff3c5ecc3a
Fekete Mihai <mihai.fekete@forbiom.eu>
2015-12-14 21:13:18 -0800

    Update code in tests, fix tests flake8.

--- partner_create_by_vat/tests/test_partner_create_by_vat.py
+++ partner_create_by_vat/tests/test_partner_create_by_vat.py
@@ -18,13 +18,14 @@ class TestPartnerCreatebyVAT(TransactionCase):
         self.partner1_id.button_get_partner_data()
 
         # Check if the datas fetch correspond with the datas from VIES.
-        self.assertTrue(self.partner1_id.name == 'SA ODOO')
-        self.assertTrue(
-            unicode(self.partner1_id.street) == u'Chaussée De Namur 40 1367 '
-                                                 'Ramillies')
-        self.assertTrue(self.partner1_id.country_id.name == 'Belgium')
-        self.assertTrue(self.partner1_id.vat == 'BE0477472701')
-        self.assertTrue(self.partner1_id.vat_subjected == True)
+        self.assertEqual(self.partner1_id.name, 'SA ODOO')
+        self.assertEqual(
+            unicode(self.partner1_id.street),
+            u'Chaussée De Namur 40 1367 Ramillies'
+        )
+        self.assertEqual(self.partner1_id.country_id.name, 'Belgium')
+        self.assertEqual(self.partner1_id.vat, 'BE0477472701')
+        self.assertEqual(self.partner1_id.vat_subjected, True)
 
     def test_create_from_vat(self):
         # Create an partner with VAT number in name field
@@ -35,10 +36,11 @@ class TestPartnerCreatebyVAT(TransactionCase):
         self.partner2_id.button_get_partner_data()
 
         # Check if the datas fetch correspond with the datas from VIES.
-        self.assertTrue(self.partner2_id.name == 'SA ODOO')
-        self.assertTrue(
-            unicode(self.partner2_id.street) == u'Chaussée De Namur 40 1367 '
-                                                 'Ramillies')
-        self.assertTrue(self.partner2_id.country_id.name == 'Belgium')
-        self.assertTrue(self.partner2_id.vat == 'BE0477472701')
-        self.assertTrue(self.partner2_id.vat_subjected == True)
+        self.assertEqual(self.partner2_id.name, 'SA ODOO')
+        self.assertEqual(
+            unicode(self.partner2_id.street),
+            u'Chaussée De Namur 40 1367 Ramillies'
+        )
+        self.assertEqual(self.partner2_id.country_id.name, 'Belgium')
+        self.assertEqual(self.partner2_id.vat, 'BE0477472701')
+        self.assertEqual(self.partner2_id.vat_subjected, True)
