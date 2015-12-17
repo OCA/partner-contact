a0b7c29fc441d09ddbaeed06b920b1f66efc20aa
Jairo Llopis <yajo.sk8@gmail.com>
2015-12-17 17:39:34 +0100

    Prepare for OCA.
    - Shorter copyright & license headers.
    - Remove all CRM functionality & dependencies.
    - Move fields to Sales & Purchases tab.
    - Remove pot.
    - Probably something more.

--- partner_employee_amount/models/res_partner.py
+++ partner_employee_amount/models/res_partner.py
@@ -1,12 +1,25 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in root directory
-##############################################################################
+# -*- coding: utf-8 -*-
+# © 2015 Antiun Ingeniería S.L. - Antonio Espinosa
+# © 2015 Antiun Ingeniería S.L. - Jairo Llopis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 from openerp import models, fields
 
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    employees_range = fields.Many2one(comodel_name='crm.employees_range')
-    employees_number = fields.Integer()
+    employee_amount = fields.Integer(
+        oldname="employees_number")
+    employee_amount_range_id = fields.Many2one(
+        "res.partner.employee_amount_range",
+        "Employee amount range",
+        help="Range of this partner depending on the employee amount.",
+        oldname="employees_range")
+
+
+class ResPartnerEmployeeAmountRange(models.Model):
+    _name = "res.partner.employee_amount_range"
+    _description = "Partner employee amount range"
+
+    name = fields.Char(required=True, translate=True)
