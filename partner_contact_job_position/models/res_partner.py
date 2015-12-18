e3241d32b62df4a47dbaab23c8abcd11bb75788b
Jairo Llopis <yajo.sk8@gmail.com>
2015-12-18 17:46:52 +0100

    Prepare for OCA:
    - Renamed crm_job_position to partner_contact_job_position.
    - Drop all CRM dependencies.
    - Improve README.
    - Add form view.
    - Rename one model.
    - Rename some fields.
    - Reorder module.
    - Remove pot.
    - Update translation.
    - Use new-style short license and copyright headers.

---
+++ partner_contact_job_position/models/res_partner.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+#    Copyright (c) 2014 Serv. Tecnol. Avanzados (http://www.serviciosbaeza.com)
+#                       Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
+#    Copyright (c) 2015 Antiun Ingeniería S.L. (http://www.antiun.com)
+#                       Antonio Espinosa <antonioea@antiun.com>
+# © 2015 Antiun Ingeniería S.L. - Jairo Llopis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, fields
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    job_position_id = fields.Many2one(
+        "res.partner.job_position",
+        "Categorized job position",
+        oldname="job_position")
+
+
+class ResPartnerJobPosition(models.Model):
+    _name = "res.partner.job_position"
+    _order = "parent_left"
+    _parent_order = "name"
+    _parent_store = True
+    _description = "Job position"
+
+    name = fields.Char(required=True, translate=True)
+    parent_id = fields.Many2one(
+        "res.partner.job_position",
+        "Parent")
+    child_ids = fields.One2many(
+        "res.partner.job_position",
+        "parent_id",
+        "Children",
+        oldname="children")
+    parent_left = fields.Integer(index=True)
+    parent_right = fields.Integer(index=True)
