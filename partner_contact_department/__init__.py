6074665b3a20ccff1f80b2bf3ec2ff644a7a7056
Jairo Llopis <yajo.sk8@gmail.com>
2015-12-21 17:25:01 +0100

    Rename to partner_contact_department.

---
+++ partner_contact_department/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+#    Copyright (c) 2014 Serv. Tecnol. Avanzados (http://www.serviciosbaeza.com)
+#                       Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
+#    Copyright (c) 2015 Antiun Ingeniería S.L. (http://www.antiun.com)
+#                       Antonio Espinosa <antonioea@antiun.com>
+# © 2015 Antiun Ingeniería S.L. - Jairo Llopis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import models
