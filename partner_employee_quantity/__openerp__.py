2b5201ed588d3dd423285b1aefa7b366fc656d86
Jairo Llopis <yajo.sk8@gmail.com>
2015-12-21 16:09:33 +0100

    Rename to partner_employee_quantity.

---
+++ partner_employee_quantity/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# © 2015 Antiun Ingeniería S.L. - Antonio Espinosa
+# © 2015 Antiun Ingeniería S.L. - Jairo Llopis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Employee quantity in partners',
+    'summary': 'Know how many employees a partner has',
+    'version': '8.0.1.0.0',
+    'category': 'Customer Relationship Management',
+    'author': 'Antiun Ingeniería S.L.',
+    'website': 'http://www.antiun.com',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/res_partner_employee_quantity_range_view.xml',
+        'views/res_partner_view.xml',
+    ],
+    "installable": True,
+}
