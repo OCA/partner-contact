71cc802387e93e26ce3afbdddba9eeba1b8678e9
Jairo Llopis <yajo.sk8@gmail.com>
2015-12-21 17:25:01 +0100

    Rename to partner_contact_department.

---
+++ partner_contact_department/models/res_partner.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+#    Copyright (c) 2014 Serv. Tecnol. Avanzados (http://www.serviciosbaeza.com)
+#                       Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
+#    Copyright (c) 2015 Antiun Ingeniería S.L. (http://www.antiun.com)
+#                       Antonio Espinosa <antonioea@antiun.com>
+# © 2015 Antiun Ingeniería S.L. - Jairo Llopis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, fields
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    department_id = fields.Many2one(
+        "res.partner.department",
+        "Department",
+        oldname="department")
+
+
+class ResPartnerDepartment(models.Model):
+    _name = 'res.partner.department'
+    _order = "parent_left"
+    _parent_order = "name"
+    _parent_store = True
+    _description = "Department"
+
+    name = fields.Char(required=True, translate=True)
+    parent_id = fields.Many2one(
+        "res.partner.department",
+        "Parent department")
+    child_ids = fields.One2many(
+        "res.partner.department",
+        "parent_id",
+        "Child departments",
+        oldname="children")
+    parent_left = fields.Integer(index=True)
+    parent_right = fields.Integer(index=True)
