9708977b6568225ccee7eb89449ea43e26ff5cad
Jairo Llopis <yajo.sk8@gmail.com>
2015-12-29 16:29:29 +0100

    Update base_location_nuts to 8.0.2.0.0.
    This makes NUTS labels and levels to be stored in the res.country object.
    Now creating l10n submodules is a piece of cake!
    
    Relational fields now follow guidelines on naming. Old name attribute used
    for backwards compatibility wherever needed.
    
    Also some methods have been renamed, and refactored to be smarter. Most cases
    l10n modules will just need to fill the res.contry table, and regions and
    substates domains will work out of the box. In case you still need to
    overwrite any method, splitting in smaller methods makes it easier too.
    
    Oh! And no need for recursive dictionary updates.
    
    Return dict() to make it easier for submodules to add domains.
    
    Fix KeyError: 'substate_id_level'.

--- base_location_nuts/models/__init__.py
+++ base_location_nuts/models/__init__.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in root directory
-##############################################################################
+# © 2015 Antiun Ingeniería S.L. - Antonio Espinosa
+# © 2015 Antiun Ingeniería S.L. - Jairo Llopis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import res_partner_nuts
-from . import res_partner
+from . import (
+    res_country,
+    res_partner_nuts,
+    res_partner,
+)
