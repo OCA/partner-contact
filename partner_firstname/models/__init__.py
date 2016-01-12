dc5ce8629719f5f4f1856b8d580bab25942b6f0c
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2016-01-12 16:35:28 +0100

    [PORT] partner_firstname to 9.0 - add missing authors - use reduced license header - move models in models directory - adapt views - show firstname, lastname only in edit mode - use company_type in views instead of is_company - adapt constraint on contacts
    [PORT][9.0] partner_firstname - remove hack to be able to edit user view. Fixed in odoo/odoo#cf63d4d277ef1ba02ff4ebcdae8583332a1775b1
    
    [PORT][9.0] partner_firstname - Format of string in __openerp__.py
    
    [PORT][9.0] Adapt tests to take new constraint raising IntegrityError
    
    partner_firstname: Name is not mandatory if partner is an address
    
    [FIX] Ensure default values are computed for res.users
    
    Add test for shipping address with empty name
    
    partner_firstname: fix user creation
    User name field is required what is stoping to create a new user
    
    Fix user form with firstname and lastname asking for a mandatory name

--- partner_firstname/models/__init__.py
+++ partner_firstname/models/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
 # © 2013 Nicolas Bessi (Camptocamp SA)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from . import models
+from . import res_partner
+from . import res_user
--- partner_firstname/tests/__init__.py
+++ partner_firstname/tests/__init__.py
@@ -1,33 +1,6 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-# Authors: Nemry Jonathan
-# Copyright (c) 2014 Acsone SA/NV (http://www.acsone.eu)
-#       2015:       Grupo ESOC <www.grupoesoc.es>
-# All Rights Reserved
-#
-# WARNING: This program as such is intended to be used by professional
-# programmers who take the whole responsibility of assessing all potential
-# consequences resulting from its eventual inadequacies and bugs.
-# End users who are looking for a ready-to-use solution with commercial
-# guarantees and support are strongly advised to contact a Free Software
-# Service Company.
-#
-# This program is Free Software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-#
-##############################################################################
+# © 2014 Nemry Jonathan (Acsone SA/NV) (http://www.acsone.eu)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import (
     test_create,
@@ -35,5 +8,6 @@ from . import (
     test_delete,
     test_empty,
     test_name,
-    test_onchange
+    test_onchange,
+    test_user_onchange
 )
