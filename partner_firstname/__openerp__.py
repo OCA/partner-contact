cbd8c4190acbb67a847d7ac3f6e2593cc398b1fe
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2016-01-12 16:35:28 +0100

    [PORT] partner_firstname to 9.0 - add missing authors - use reduced license header - move models in models directory - adapt views - show firstname, lastname only in edit mode - use company_type in views instead of is_company - adapt constraint on contacts
    [PORT][9.0] partner_firstname - remove hack to be able to edit user view. Fixed in odoo/odoo#cf63d4d277ef1ba02ff4ebcdae8583332a1775b1
    
    [PORT][9.0] partner_firstname - Format of string in __openerp__.py
    
    [PORT][9.0] Adapt tests to take new constraint raising IntegrityError
    
    partner_firstname: Name is not mandatory if partner is an address
    
    [FIX] Ensure default values are computed for res.users
    
    Add test for shipping address with empty name
    
    partner_firstname: fix user creation
    User name field is required what is stoping to create a new user
    
    Fix user form with firstname and lastname asking for a mandatory name

--- partner_firstname/__openerp__.py
+++ partner_firstname/__openerp__.py
@@ -1,31 +1,17 @@
 # -*- coding: utf-8 -*-
-
-#    Author: Nicolas Bessi. Copyright Camptocamp SA
-#    Copyright (C)
-#       2014:       Agile Business Group (<http://www.agilebg.com>)
-#       2015:       Grupo ESOC <www.grupoesoc.es>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# © 2013 Nicolas Bessi (Camptocamp SA)
+# © 2014 Agile Business Group (<http://www.agilebg.com>)
+# © 2015 Grupo ESOC (<http://www.grupoesoc.es>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Partner first name and last name',
     'summary': "Split first name and last name for non company partners",
-    'version': '8.0.2.1.0',
-    "author": "Camptocamp, "
+    'version': '9.0.1.0.0',
+    'author': "Camptocamp, "
               "Grupo ESOC Ingeniería de Servicios, "
               "Odoo Community Association (OCA)",
-    "license": "AGPL-3",
+    'license': "AGPL-3",
     'maintainer': 'Camptocamp, Acsone',
     'category': 'Extra Tools',
     'website':
@@ -39,6 +25,6 @@
     'demo': [],
     'test': [],
     'auto_install': False,
-    'installable': False,
+    'installable': True,
     'images': []
 }
