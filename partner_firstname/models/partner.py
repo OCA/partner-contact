efb51adf4d1f3799f76672e763d5ca8d14251a79
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2016-01-12 16:35:28 +0100

    [PORT] partner_firstname to 9.0 - add missing authors - use reduced license header - move models in models directory - adapt views - show firstname, lastname only in edit mode - use company_type in views instead of is_company - adapt constraint on contacts

--- partner_firstname/models/partner.py
+++ partner_firstname/models/partner.py
@@ -1,26 +1,11 @@
 # -*- coding: utf-8 -*-
-
-#    Author: Nicolas Bessi. Copyright Camptocamp SA
-#    Copyright (C)
-#       2014:       Agile Business Group (<http://www.agilebg.com>)
-#       2015:       Grupo ESOC <www.grupoesoc.es>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+# © 2013 Nicolas Bessi (Camptocamp SA)
+# © 2014 Agile Business Group (<http://www.agilebg.com>)
+# © 2015 Grupo ESOC (<http://www.grupoesoc.es>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 import logging
 from openerp import api, fields, models
-from . import exceptions
+from .. import exceptions
 
 
 _logger = logging.getLogger(__name__)
@@ -202,3 +187,12 @@ class ResPartner(models.Model):
         # Force calculations there
         records._inverse_name()
         _logger.info("%d partners updated installing module.", len(records))
+
+    # As name is computed after sql_constraint check, we modify it to accept
+    # to write when at least one name field is filled
+    _sql_constraints = [
+        ('check_name',
+         "CHECK( (type='contact' AND (name IS NOT NULL OR"
+         " firstname IS NOT NULL OR lastname IS NOT NULL)) or"
+         " (type!='contact') )", 'Contacts require a name.'),
+    ]
