2cf6b986153bf249f038897f8781b59d09cbc883
Denis Leemann <denis.leemann@camptocamp.com>
2016-01-18 13:18:40 +0100

    Port of Birthdate

--- partner_contact_birthdate/__openerp__.py
+++ partner_contact_birthdate/__openerp__.py
@@ -18,15 +18,15 @@
 
 {
     "name": "Contact's birthdate",
-    "version": "8.0.1.0.0",
+    "version": "9.0.1.0.0",
     "author": "Odoo Community Association (OCA)",
     "category": "Customer Relationship Management",
     "website": "https://odoo-community.org/",
     "depends": [
-        "partner_contact_personal_information_page",
+        "base",
     ],
     "data": [
         "views/res_partner.xml",
     ],
-    'installable': False,
+    'installable': True,
 }
