cb0a5800d158c8ae601eec8447c892ed1b8dcce3
Denis Leemann <denis.leemann@camptocamp.com>
2016-01-18 13:18:40 +0100

    Port of Birthdate

--- partner_contact_birthdate/models/__init__.py
+++ partner_contact_birthdate/models/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2014-2015  Grupo ESOC <www.grupoesoc.es>
+# Copyright 2014-2015  Grupo ESOC <www.grupoesoc.es>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from . import models
+
+from . import res_partner
