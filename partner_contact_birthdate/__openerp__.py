aeae84383128af17f35bd77e5f7e650b7d664368
Denis Leemann <denis.leemann@camptocamp.com>
2016-01-18 11:12:09 +0100

    Fix multiple tabs "contact information"
    Port of Birthdate

--- partner_contact_birthdate/__openerp__.py
+++ partner_contact_birthdate/__openerp__.py
@@ -1,25 +1,12 @@
 # -*- coding: utf-8 -*-
-
-# Odoo, Open Source Management Solution
-# Copyright (C) 2014-2015  Grupo ESOC <www.grupoesoc.es>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# Copyright 2014-2015  Grupo ESOC <www.grupoesoc.es>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Contact's birthdate",
-    "version": "8.0.1.0.0",
-    "author": "Odoo Community Association (OCA)",
+    "version": "9.0.1.0.0",
+    "author": "Jairo Llopis,"
+              "Odoo Community Association (OCA)",
     "category": "Customer Relationship Management",
     "website": "https://odoo-community.org/",
     "depends": [
@@ -28,4 +15,5 @@
     "data": [
         "views/res_partner.xml",
     ],
+    'installable': True,
 }
