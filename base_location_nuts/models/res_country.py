860e42c804feb8e336ca4bd9289c7d1fd4f84b4e
Antonio Espinosa <antespi@gmail.com>
2016-02-01 17:58:21 +0100

    Use separate and explicit fields for each NUTS level

--- base_location_nuts/models/res_country.py
+++ base_location_nuts/models/res_country.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# © 2015 Antiun Ingeniería S.L. - Antonio Espinosa
 # © 2015 Antiun Ingeniería S.L. - Jairo Llopis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
@@ -6,24 +7,7 @@ from openerp import models, fields
 
 
 class ResCountry(models.Model):
-    """Add labels corresponding to each country.
-
-    These stay empty in this base module, and should be filled by l10n ones.
-    """
     _inherit = "res.country"
 
-    state_label = fields.Char(
-        translate=True,
-        help="Label for the state NUTS category.")
-    substate_label = fields.Char(
-        translate=True,
-        help="Label for the substate NUTS category.")
-    region_label = fields.Char(
-        translate=True,
-        help="Label for the region NUTS category.")
     state_level = fields.Integer(
         help="Level for the state NUTS category.")
-    substate_level = fields.Integer(
-        help="Level for the substate NUTS category.")
-    region_level = fields.Integer(
-        help="Level for the region NUTS category.")
