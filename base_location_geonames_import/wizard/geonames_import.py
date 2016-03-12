3ed9efef6f6330dedd3c83cab11adeeb93c7c89e
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-03-12 09:20:05 +0100

    [FIX] base_location_geonames_import: Don't alias Warning

--- base_location_geonames_import/wizard/geonames_import.py
+++ base_location_geonames_import/wizard/geonames_import.py
@@ -5,7 +5,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp import models, fields, api, _
-from openerp.exceptions import Warning as UserError
+from openerp.exceptions import UserError
 import requests
 import tempfile
 import StringIO
