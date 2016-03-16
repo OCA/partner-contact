6d6a1c9462829db23c018678f47ba9950f2376a9
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2016-03-16 10:37:25 +0100

    partner_identification: several fix/imp
    * [IMP] improve module description
    * [IMP] Remove useless comments
    * [FIX] Complete incomplete sentence
    * [IMP] Replace field 'state' by 'status' in res_partner_id_number
    * [IMP] Add new field 'Place of Issuance'
    * [FIX] Readme formatting
    * [IMP] status is now a selection field
    * [IMP] use method to provide the default value for validation_code
    * [IMP] Add help texts
    * [FIX] Add missing constrains on category_id in res_partner.id_number
            The number must be validated also when we change the category

--- partner_identification/models/res_partner_id_category.py
+++ partner_identification/models/res_partner_id_category.py
@@ -20,19 +20,26 @@ class ResPartnerIdCategory(models.Model):
     _name = "res.partner.id_category"
     _order = "name"
 
-    code = fields.Char(string="Code", size=16, required=True)
-    name = fields.Char(string="ID name", required=True, translate=True)
+    def _default_validation_code(self):
+        return _("\n# Python code. Use failed = True to specify that the id "
+                 "number is not valid.\n"
+                 "# You can use the following variables :\n"
+                 "#  - self: browse_record of the current ID Category "
+                 "browse_record\n"
+                 "#  - id_number: browse_record of ID number to validate")
+
+    code = fields.Char(
+        string="Code", size=16, required=True,
+        help="Abbreviation or acronym of this ID type. For example, "
+             "'driver_license'")
+    name = fields.Char(
+        string="ID name", required=True, translate=True,
+        help="Name of this ID type. For example, 'Driver License'")
     active = fields.Boolean(string="Active", default=True)
     validation_code = fields.Text(
         'Python validation code',
         help="Python code called to validate an id number.",
-        default="""
-# Python code. Use failed = True to .
-# You can use the following variables :
-#  - self: browse_record of the current ID Category browse_record
-#  - id_number: browse_record of ID number to validte
-"""
-        )
+        default=_default_validation_code)
 
     @api.multi
     def _validation_eval_context(self, id_number):
