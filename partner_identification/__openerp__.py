4f31656ab91c98acab30b0905cfd74e128bab685
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2016-03-16 10:37:25 +0100

    partner_identification: several fix/imp
    * [IMP] improve module description
    * [IMP] Remove useless comments
    * [FIX] Complete incomplete sentence
    * [IMP] Replace field 'state' by 'status' in res_partner_id_number
    * [IMP] Add new field 'Place of Issuance'
    * [FIX] Readme formatting
    * [IMP] status is now a selection field
    * [IMP] use method to provide the default value for validation_code
    * [IMP] Add help texts
    * [FIX] Add missing constrains on category_id in res_partner.id_number
            The number must be validated also when we change the category

--- partner_identification/__openerp__.py
+++ partner_identification/__openerp__.py
@@ -9,18 +9,15 @@
 # © 2016 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    # Addon information
     'name': 'Partner Identification Numbers',
     'category': 'Customer Relationship Management',
     'version': '9.0.1.0.0',
-    # Views templates, pages, menus, options and snippets
     'data': [
         'views/res_partner_id_category_view.xml',
         'views/res_partner_id_number_view.xml',
         'views/res_partner_view.xml',
         'security/ir.model.access.csv',
     ],
-    # Your information
     'author': 'ChriCar Beteiligungs- und Beratungs- GmbH, '
               'Antiun Ingeniería S.L.',
               'Camptocamp,'
@@ -29,5 +26,4 @@
     'website': 'https://odoo-community.org/',
     'license': 'AGPL-3',
     'installable': True,
-
 }
