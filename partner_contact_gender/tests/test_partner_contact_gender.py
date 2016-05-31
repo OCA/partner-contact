184f6d1c99bab855dcb0be806a6a593cd8382826
Holger Brunn <hbrunn@therp.nl>
2016-05-31 18:42:17 +0200

    [FIX] review fixes

--- partner_contact_gender/tests/test_partner_contact_gender.py
+++ partner_contact_gender/tests/test_partner_contact_gender.py
@@ -5,6 +5,14 @@ from openerp.tests.common import TransactionCase
 
 
 class TestPartnerContactGender(TransactionCase):
+    def setUp(self):
+        super(TestPartnerContactGender, self).setUp()
+        self.testpartner = self.env['res.partner'].create({
+            'name': 'test',
+            'title': self.env.ref('base.res_partner_title_madam').id,
+        })
+
     def test_partner_contact_gender(self):
         from ..hooks import post_init_hook
         post_init_hook(self.cr, None)
+        self.assertEqual(self.testpartner.gender, 'female')
