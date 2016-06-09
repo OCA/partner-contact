7a1144eb8da5c50b6be85ee5ed43fbda0280f0a9
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-06-09 10:17:12 +0200

    [ADD] crm_deduplicate_by_ref
    =================================
    Deduplicate Contacts by reference
    =================================
    
    This module extends the criteria to match duplicated contacts using the field
    reference.
    
    Usage
    =====
    
    To use this module, you need to:
    
    * Go to *Sales > Tools > Deduplicate Contacts*.
    * Mark "Reference" in the section "Search duplicates based on duplicated data
      in".
    * This criteria will be used for deduplicating.

---
+++ partner_deduplicate_by_ref/tests/test_crm_deduplicate_by_ref.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp.tests import common
+from openerp.tools.safe_eval import safe_eval
+
+
+class TestDeduplicateByRef(common.TransactionCase):
+    def setUp(self):
+        super(TestDeduplicateByRef, self).setUp()
+        self.partner_1 = self.env['res.partner'].create({
+            'name': 'Partner 1',
+            'ref': '123456',
+            'email': 'test@deduplicate.com',
+        })
+        self.partner_2 = self.env['res.partner'].create({
+            'name': 'Partner 2',
+            'ref': '123456',
+            'email': 'test@deduplicate.com',
+        })
+
+    def test_deduplicate_by_ref(self):
+        wizard = self.env['base.partner.merge.automatic.wizard'].create({
+            'group_by_ref': True,
+        })
+        wizard.start_process_cb()
+        found_match = False
+        for line in wizard.line_ids:
+            match_ids = safe_eval(line.aggr_ids)
+            if (self.partner_1.id in match_ids and
+                    self.partner_2.id in match_ids):
+                found_match = True
+                break
+        self.assertTrue(found_match)
+
+    def test_deduplicate_by_ref_and_is_company(self):
+        wizard = self.env['base.partner.merge.automatic.wizard'].create({
+            'group_by_ref': True,
+            'group_by_email': True,
+        })
+        wizard.start_process_cb()
+        found_match = False
+        for line in wizard.line_ids:
+            match_ids = safe_eval(line.aggr_ids)
+            if (self.partner_1.id in match_ids and
+                    self.partner_2.id in match_ids):
+                found_match = True
+                break
+        self.assertTrue(found_match)
