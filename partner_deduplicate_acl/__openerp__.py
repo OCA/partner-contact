68912d8b639ddf3a43c76ff6e6d94609b9cc69e2
jbeficent <jordi.ballester@eficent.com>
2016-07-13 16:54:06 +0200

    only users beloning to group 'Manually' should be able to merge partners from the partner list views (action 'Merge automatically') Use <record id="crm.action_partner_merge"... to change only affected field.

--- partner_deduplicate_acl/__openerp__.py
+++ partner_deduplicate_acl/__openerp__.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
-# © 2016 Antiun Ingeniería S.L. - Jairo Llopis
+# © 2016 Tecnativa, S.L. - Jairo Llopis
+# © 2016 Tecnativa, S.L. - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Deduplicate Contacts ACL",
     "summary": "Contact deduplication with fine-grained permission control",
-    "version": "8.0.1.0.0",
+    "version": "9.0.1.0.0",
     "category": "Tools",
-    "website": "http://www.antiun.com",
-    "author": "Antiun Ingeniería S.L., Odoo Community Association (OCA)",
+    "website": "http://www.tecnativa.com",
+    "author": "Tecnativa, "
+              "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
@@ -17,6 +19,7 @@
     "data": [
         "security/crm_deduplicate_acl_security.xml",
         "wizards/partner_merge_view.xml",
+        "views/base_partner_merge_view.xml",
     ],
     "images": [
         "images/perms.png",
