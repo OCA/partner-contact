10ab1facffa9673eba08aa9b4cb52b60f937cced
Antonio Espinosa <antespi@gmail.com>
2016-07-22 17:14:51 +0200

    Remove unused imports

--- partner_firstname/tests/test_access_check.py
+++ partner_firstname/tests/test_access_check.py
@@ -2,7 +2,7 @@
 # Copyright 2016 Antonio Espinosa <antonio.espinosa@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp.tests.common import HttpCase, TransactionCase
+from openerp.tests.common import TransactionCase
 from openerp.exceptions import AccessError
 
 
