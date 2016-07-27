1fdaec679aedf0fa9bba84159cec1f0b0d301545
leemannd <denis.leemann@camptocamp.com>
2016-07-27 11:24:54 +0200

    Update __openerp__.py

--- partner_contact_birthdate/__openerp__.py
+++ partner_contact_birthdate/__openerp__.py
@@ -6,7 +6,7 @@
     "name": "Contact's birthdate",
     "version": "9.0.1.0.0",
     "author": "Jairo Llopis",
-              "doo Community Association (OCA)",
+              "Odoo Community Association (OCA)",
     "category": "Customer Relationship Management",
     "website": "https://odoo-community.org/",
     "depends": [
