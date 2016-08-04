7a13984876995ce5f78956e8225f658172d31b17
Carlos Incaser <carlos@incaser.es>
2016-08-05 00:42:54 +0200

    [NEW] partner_sale_risk: New module from split partner_financial_risk

---
+++ partner_sale_risk/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# © 2016 Carlos Dauden <carlos.dauden@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Partner Sale Risk',
+    'summary': 'Manage partner risk in sales orders',
+    'version': '9.0.1.0.0',
+    'category': 'Sales Management',
+    'license': 'AGPL-3',
+    'author': 'Tecnativa, Odoo Community Association (OCA)',
+    'website': 'https://www.tecnativa.com',
+    'depends': ['sale', 'partner_financial_risk'],
+    'data': [
+        'views/res_partner_view.xml',
+        'views/sale_view.xml',
+        'wizard/sale_order_risk_exceeded_view.xml',
+    ],
+    'installable': True,
+}
