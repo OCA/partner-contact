c8497d05476974fd145a9762bcb5eecb099d02f8
Andhitia Rama <andhitia.r@gmail.com>
2016-08-28 16:11:00 +0700

    [ADD][8.0] base_contact_readonly

---
+++ base_contact_readonly/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Readonly Address Book",
+    "version": "8.0.1.0.0",
+    "category": "Base",
+    "website": "https://opensynergy-indonesia.com/",
+    "author": "OpenSynergy Indonesia, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "contacts",
+    ],
+    "data": [
+        "views/res_partner_views.xml",
+    ],
+}
