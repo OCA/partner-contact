3e0d05a970d1d3d5c6e2844a3f4563805d5976dc
Sylvain GARANCHER <sylvain.garancher@syleam.fr>
2016-10-06 23:51:40 +0200

    [MIG] Migrate partner_firstname to v10

--- partner_firstname/models/res_user.py
+++ partner_firstname/models/res_user.py
@@ -4,7 +4,7 @@
 # © 2015 Grupo ESOC (<http://www.grupoesoc.es>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 import logging
-from openerp import api, models
+from odoo import api, models
 
 
 _logger = logging.getLogger(__name__)
