582eff0610a2c754aba7109a5da1dda28b7579dd
Sylvain GARANCHER <sylvain.garancher@syleam.fr>
2016-10-06 23:51:40 +0200

    [MIG] Migrate partner_firstname to v10

--- partner_firstname/tests/base.py
+++ partner_firstname/tests/base.py
@@ -2,7 +2,7 @@
 # © 2014 Nemry Jonathan (Acsone SA/NV) (http://www.acsone.eu)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp.tests.common import TransactionCase
+from odoo.tests.common import TransactionCase
 from .. import exceptions as ex
 
 
