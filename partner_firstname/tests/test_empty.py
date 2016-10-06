ab0760bf44ee9d01717980ee4c4ee53f578b93ce
Sylvain GARANCHER <sylvain.garancher@syleam.fr>
2016-10-06 23:51:40 +0200

    [MIG] Migrate partner_firstname to v10

--- partner_firstname/tests/test_empty.py
+++ partner_firstname/tests/test_empty.py
@@ -6,7 +6,7 @@
 
 To have more accurate results, remove the ``mail`` module before testing.
 """
-from openerp.tests.common import TransactionCase
+from odoo.tests.common import TransactionCase
 from .base import MailInstalled
 from .. import exceptions as ex
 
