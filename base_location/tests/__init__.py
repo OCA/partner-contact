1ddd8a2d9ccf2f7d2e26e097465d8663f452add5
Francesco Apruzzese <cescoap@gmail.com>
2016-10-19 10:57:33 +0200

    [MIG] base_location: Migrated to 10.0
    * Headers shortened
    * Move cities management to settings

--- base_location/tests/__init__.py
+++ base_location/tests/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2015 Yannick Vaucher (Camptocamp)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright 2015 Yannick Vaucher, Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 from . import test_completion
