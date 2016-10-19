b046df78ee4e718c660d6e3d6cea9b4d3cc7aca5
Francesco Apruzzese <cescoap@gmail.com>
2016-10-19 10:57:33 +0200

    [MIG] base_location: Migrated to 10.0
    * Headers shortened
    * Move cities management to settings

--- base_location/models/company.py
+++ base_location/models/company.py
@@ -1,33 +1,14 @@
 # -*- coding: utf-8 -*-
-#
-#
-#    Author: Nicolas Bessi. Copyright Camptocamp SA
-#    Contributor: Pedro Manuel Baeza <pedro.baeza@serviciosbaeza.com>
-#                 Ignacio Ibeas <ignacio@acysos.com>
-#                 Alejandro Santana <alejandrosantana@anubia.es>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#
-from openerp import models, fields, api
+# Copyright 2016 Nicolas Bessi, Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields, api
 
 
 class ResCompany(models.Model):
 
     _inherit = 'res.company'
 
-    @api.one
     @api.onchange('better_zip_id')
     def on_change_city(self):
         if self.better_zip_id:
@@ -39,6 +20,5 @@ class ResCompany(models.Model):
     better_zip_id = fields.Many2one(
         'res.better.zip',
         string='Location',
-        select=1,
         help='Use the city name or the zip code to search the location',
     )
