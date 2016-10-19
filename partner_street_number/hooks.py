ad5b87092074685d7e585c5f1ebe838858f8d939
Andrea <a.stirpe@onestein.nl>
2016-10-19 18:24:57 +0200

    post_init_hook moved to separate file

--- partner_street_number/hooks.py
+++ partner_street_number/hooks.py
@@ -19,5 +19,23 @@
 #
 ##############################################################################
 
-from . import models
-from .hooks import post_init_hook
+import logging
+from odoo import SUPERUSER_ID, api
+
+
+def post_init_hook(cr, registry):
+    """
+    Post-install script. Because 'street' is now a stored function field, we
+    should be able to retrieve its values from the cursor. We use those to
+    fill the new name/number fields using the street field's inverse function,
+    which does a basic street name/number split.
+    """
+    logging.getLogger('odoo.addons.partner_street_number').info(
+        'Migrating existing street names')
+
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    partners = env['res.partner'].with_context(active_test=False).search([
+        ('street', '!=', False),
+        ('street_name', '=', False)
+    ])
+    partners._write_street()
