2dff973a78c21715e2d9fd0bb8f57325225052fc
Alexis de Lattre <alexis.delattre@akretion.com>
2016-10-24 09:36:11 +0200

    Port base_location_geonames_import to v10
    Add option to put city name all upper case

--- base_location_geonames_import/tests/test_base_location_geonames_import.py
+++ base_location_geonames_import/tests/test_base_location_geonames_import.py
@@ -2,7 +2,7 @@
 # © 2016 Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp.tests import common
+from odoo.tests import common
 
 
 class TestBaseLocationGeonamesImport(common.TransactionCase):
