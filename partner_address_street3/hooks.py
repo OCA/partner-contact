1f0a1ba56da999aee73c75d1272f5a2e7e74354c
Alexis de Lattre <alexis@via.ecp.fr>
2016-11-02 07:20:54 +0100

    [10.0] Port partner_address_street3 (#318)
    Add %(street3)s in 'address_format' of existing countries

---
+++ partner_address_street3/hooks.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Sodexis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+def post_init_hook(cr, registry):
+    """ Add street3 to address format """
+    query = """
+        UPDATE res_country
+        SET address_format = replace(
+        address_format,
+        E'%(street2)s\n',
+        E'%(street2)s\n%(street3)s\n'
+        )
+    """
+    cr.execute(query)
+
+
+def uninstall_hook(cr, registry):
+    """ Remove street3 from address format """
+    # Remove %(street3)s\n from address_format
+    query = """
+        UPDATE res_country
+        SET address_format = replace(
+        address_format,
+        E'%(street3)s\n',
+        ''
+        )
+    """
+    cr.execute(query)
+
+    # Remove %(street3)s from address_format
+    query = """
+        UPDATE res_country
+        SET address_format = replace(
+        address_format,
+        E'%(street3)s',
+        ''
+        )
+    """
+    cr.execute(query)
--- partner_address_street3/tests/test_street_3.py
+++ partner_address_street3/tests/test_street_3.py
@@ -1,88 +1,36 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Author: Nicolas Bessi
-#    Copyright 2014 Camptocamp SA
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-import openerp.tests.common as test_common
+# © 2014-2016 Camptocamp SA
+# @author: Nicolas Bessi
+# © 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo.tests.common import TransactionCase
 
 
-class TestStreet3(test_common.TransactionCase):
+class TestStreet3(TransactionCase):
 
     def test_partner(self):
-        part_model = self.registry('res.partner')
-        country_model = self.registry('res.country')
-        country_id = country_model.create(
-            self.cr,
-            self.uid,
-            {
-                'name': 'Donut Land',
-                'code': 'DNL',
-            }
-        )
+        # Test address_format has been updated on existing countries
+        us_country = self.env.ref('base.us')
+        self.assertTrue('%(street3)s' in us_country.address_format)
 
-        self.assertTrue(country_id)
-
-        create_data = {
+        homer = self.env['res.partner'].create({
             'name': 'Homer Simpson',
             'city': 'Springfield',
             'street': '742 Evergreen Terrace',
             'street2': 'Donut Lane',
             'street3': 'Tho',
-            'country_id': country_id,
-            'is_company': True
-        }
+            'country_id': us_country.id,
+            })
 
-        homer_id = part_model.create(
-            self.cr,
-            self.uid,
-            create_data
-        )
-
-        homer = part_model.browse(
-            self.cr,
-            self.uid,
-            homer_id,
-        )
-
-        self.assertEqual(
-            homer.country_id.address_format,
-            ("%(street)s\n%(street2)s\n%(street3)s\n"
-             "%(city)s %(state_code)s %(zip)s\n"
-             "%(country_name)s")
-        )
-
-        create_data = {
+        # test synchro of street3 on create
+        bart = self.env['res.partner'].create({
             'name': 'Bart Simpson',
-            'is_company': False,
             'parent_id': homer.id,
-            'use_parent_address': True
-        }
+            'type': 'contact',
+            })
+        self.assertEquals(bart.street3, 'Tho')
 
-        bart_id = part_model.create(
-            self.cr,
-            self.uid,
-            create_data
-        )
-
-        bart = part_model.browse(
-            self.cr,
-            self.uid,
-            bart_id,
-        )
-
-        self.assertTrue(bart.street3, 'Tho')
+        # test synchro of street3 on write
+        homer.write({'street3': 'in OCA we trust'})
+        self.assertEquals(bart.street3, 'in OCA we trust')
