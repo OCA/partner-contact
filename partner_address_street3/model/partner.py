1f82e6a5a38d979b970c2cdc75502712d481a756
Alexis de Lattre <alexis@via.ecp.fr>
2016-11-02 07:20:54 +0100

    [10.0] Port partner_address_street3 (#318)
    Add %(street3)s in 'address_format' of existing countries
    
    OCA Transbot updated translations from Transifex

--- partner_address_street3/model/partner.py
+++ partner_address_street3/model/partner.py
@@ -1,30 +1,15 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Author: Nicolas Bessi
-#    Copyright 2014-2015 Camptocamp SA
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-from openerp import models, fields, api
+# © 2014-2016 Camptocamp SA
+# @author: Nicolas Bessi
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields, api
 
 
 class ResPartner(models.Model):
     """Add third field in address"""
-
     _inherit = "res.partner"
+
     street3 = fields.Char('Street 3')
 
     @api.model
@@ -32,17 +17,3 @@ class ResPartner(models.Model):
         fields = super(ResPartner, self)._address_fields()
         fields.append('street3')
         return fields
-
-
-class res_country(models.Model):
-    """Override default adresses formatting of countries"""
-
-    _inherit = 'res.country'
-
-    address_format = fields.Text(
-        default=(
-            "%(street)s\n%(street2)s\n%(street3)s\n"
-            "%(city)s %(state_code)s %(zip)s\n"
-            "%(country_name)s"
-        )
-    )
