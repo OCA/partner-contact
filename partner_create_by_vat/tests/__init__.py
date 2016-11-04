1ebb3cc6654d69765eae2b74b83eaa8e78840c06
Fekete Mihai <mihai.fekete@forestandbiomass.ro>
2016-11-03 23:58:30 -0700

    [[MIG] partner_create_by_vat: Migrated to 10.0.

---
+++ partner_create_by_vat/tests/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# ©  2015 Forest and Biomass Services Romania
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import test_partner_create_by_vat
