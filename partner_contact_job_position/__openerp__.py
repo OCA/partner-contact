03bab9c32b2efaf2223c0ac2a08692f679f5c046
cubells <vicent@vcubells.net>
2016-11-07 13:16:46 +0100

    [MIG] partner_contact_job_position
        * Changed icon
        * Change author and web
        * Check recursion

--- partner_contact_job_position/__openerp__.py
+++ partner_contact_job_position/__openerp__.py
@@ -1,19 +1,15 @@
 # -*- coding: utf-8 -*-
-#    Copyright (c) 2014 Serv. Tecnol. Avanzados (http://www.serviciosbaeza.com)
-#                       Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
-#    Copyright (c) 2015 Antiun Ingeniería S.L. (http://www.antiun.com)
-#                       Antonio Espinosa <antonioea@antiun.com>
-# © 2015 Antiun Ingeniería S.L. - Jairo Llopis
+# © 2014-2016 Tecnativa S.L. - Jairo Llopis
+# © 2016 Tecnativa S.L. - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Partner job position",
     "summary": "Categorize job positions for contacts",
-    "version": "8.0.1.0.0",
+    "version": "9.0.1.0.0",
     'category': 'Customer Relationship Management',
-    "website": "http://www.antiun.com",
-    'author': 'Serv. Tecnolog. Avanzados - Pedro M. Baeza, '
-              'Antiun Ingeniería S.L., '
+    "website": "http://www.tecnativa.com",
+    'author': 'Tecnativa, '
               "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
