84840192cfbc4d1786ea46334ca04ed9268fc343
cubells <vicent@vcubells.net>
2016-11-08 20:09:15 +0100

    [MIG] partner_sector: Migration to 9.0

--- partner_sector/__openerp__.py
+++ partner_sector/__openerp__.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 # © 2015 Antiun Ingenieria S.L. - Javier Iniesta
+# © 2016 Tecnativa S.L. - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Partner Sector",
     "summary": "Add partner sectors",
-    "version": "8.0.1.0.0",
+    "version": "9.0.1.0.0",
     "category": "Customer Relationship Management",
-    "website": "http://www.antiun.com",
-    "author": "Antiun Ingeniería S.L., Odoo Community Association (OCA)",
+    "website": "http://www.tecnativa.com",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
