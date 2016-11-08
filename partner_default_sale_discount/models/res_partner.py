b67c1f42f83447c6e45ee63fa2738aa9deedff8a
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-11-08 14:37:28 +0100

    [ADD] partner_default_sale_discount (#322)
    * [ADD] partner_default_sale_discount
    
    Default sales discount per partner
    ==================================
    
    This module allows to define at partner level a default discount to be applied
    on sales orders as the default one. As the field is a company dependent one,
    you can configure a different value for the partner for each of your companies.
    
    Configuration
    =============
    
    Enabling the use of discounts in Odoo:
    
    * Go to *Sales > Configuration > Settings*
    * On the group "Quotations & Sales", select the option "Allow discounts on
      sales order lines" for the "Discount" section.
    
    Setting a default sales discount:
    
    * Then, go to *Sales > Customers*.
    * Create or modify a customer.
    * Go to "Sales & Purchases" page.
    * In the "Sale" section, there's a field called "Default sales discount (%)"
      where you can fill the default discount percentage.
    * This value can only be filled at company partners, not for contacts.
    
    Usage
    =====
    
    To use this module, you need to:
    
    * Go to Sales > Quotations.
    * Create a new quotation.
    * Select a partner with a default discount applied.
    * Create an order line, and you will see this discount as default in the line.
    
    Known issues / Roadmap
    ======================
    
    * This module doesn't work with *product_visible_discount*, as it overwrites
      the discount field with its own value.

---
+++ partner_default_sale_discount/models/res_partner.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    default_sale_discount = fields.Float(
+        string="Default sales discount (%)", company_dependent=True,
+    )
---
+++ partner_default_sale_discount/tests/test_partner_default_sale_discount.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp.tests import common
+from lxml import etree
+
+
+class TestPartnerDefaultSaleDiscount(common.SavepointCase):
+    def test_fields_view_get(self):
+        res = self.env['sale.order'].fields_view_get(
+            view_id=self.env.ref('sale.view_order_form').id, view_type='form')
+        eview = etree.fromstring(res['arch'])
+        xml_order_line = eview.xpath("//field[@name='order_line']")
+        self.assertTrue(xml_order_line)
+        self.assertIn(
+            "'default_discount': default_sale_discount,",
+            xml_order_line[0].get('context', '{}'))
