41f8c1d555bd7aaccc20c6a21fc9a206fea58498
invitu <invitu@users.noreply.github.com>
2016-11-11 16:08:15 -1000

    [MIG] Migration partner_contact_birthdate partner_contact_gender part… (#315)
    [MIG] Migration partner_contact_birthdate partner_contact_gender partner_contact_personal_information_page to v10

--- partner_contact_birthdate/__init__.py
+++ partner_contact_birthdate/__init__.py
@@ -2,3 +2,4 @@
 # Copyright (C) 2014-2015  Grupo ESOC <www.grupoesoc.es>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from . import models
+from .hooks import post_init_hook
