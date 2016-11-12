d9325e88fc62e6c79811fce66f16f6ac3d063578
cubells <vicent@vcubells.net>
2016-11-12 17:20:05 +0100

    partner_contact_department
        * Add tests
        * Other minor changes

--- partner_contact_department/__init__.py
+++ partner_contact_department/__init__.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
-#    Copyright (c) 2014 Serv. Tecnol. Avanzados (http://www.serviciosbaeza.com)
-#                       Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
-#    Copyright (c) 2015 Antiun Ingeniería S.L. (http://www.antiun.com)
-#                       Antonio Espinosa <antonioea@antiun.com>
-# © 2015 Antiun Ingeniería S.L. - Jairo Llopis
+# © 2014-2015 Tecnativa S.L. - Jairo Llopis
+# © 2016 Tecnativa S.L. - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import models
