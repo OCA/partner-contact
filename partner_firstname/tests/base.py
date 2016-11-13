af53e49e012b0fa115e21a6fb9199b6965b2b1ca
Dave Lasley <dave@laslabs.com>
2016-11-13 15:53:24 -0800

    [MIG] partner_firstname: Upgrade to v10 * Prefer odoo to openerp & remove data tags * Move exceptions file out of models * Fix string selection in xpath * Rename res_users and add import * Fix api mismatches * Add missing param * Alphabetize imports * Remove readonly from computed field * Remove deprecated api.one * Add Tecnativa * Fix icon * Fix singleton issue * Add depends to compute

--- partner_firstname/tests/base.py
+++ partner_firstname/tests/base.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo.tests.common import TransactionCase
-from ..models import exceptions as ex
+from .. import exceptions as ex
 
 
 class MailInstalled():
