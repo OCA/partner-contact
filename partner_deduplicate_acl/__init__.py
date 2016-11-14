5cf568bf47ae91bb36c99795b12e54abc80a799c
cubells <vicent@vcubells.net>
2016-11-14 09:22:26 +0100

    crm_deduplicate_acl

--- partner_deduplicate_acl/__init__.py
+++ partner_deduplicate_acl/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-# © 2016 Antiun Ingeniería S.L. - Jairo Llopis
+# © 2016 Tecnativa, S.L. - Jairo Llopis
+# © 2016 Tecnativa, S.L. - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import wizards
