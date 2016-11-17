ea43c4151243aa7f9ea89389e0279c7c654bef83
Maxime Chambreuil <mchambreuil@ursainfosystems.com>
2016-11-17 11:45:27 -0600

    [ADD] partner_contact_personal_info

---
+++ partner_contact_personal_info/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Ursa Information Systems <http://ursainfosystems.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Partner Contact Personal Information",
+    "summary": "Provide weight, height.",
+    "version": "9.0.1.0.0",
+    "category": "Uncategorized",
+    "website": "https://odoo-community.org/",
+    "author": "Ursa Information Systems, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "product",
+        "partner_contact_birthdate",
+        "partner_contact_gender",
+    ],
+    "data": [
+        "views/res_partner_view.xml",
+    ],
+}
