d96a540ca34d4ed3b529f0a069fde3de456e2b1a
Maxime Chambreuil <mchambreuil@ursainfosystems.com>
2016-11-17 19:39:36 -0600

    [ADD] partner_contact_nutrition

---
+++ partner_contact_nutrition/models/res_partner.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Ursa Information Systems <http://ursainfosystems.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+from openerp import fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    caloric_intake = fields.Float("Calories")
+    caloric_intake_uom = fields.Many2one("product.uom", "Calories UoM")
+    carbohydrate_intake = fields.Float("Carbohydrate")
+    carbohydrate_intake_uom = fields.Many2one("product.uom", "Carbohydrate UoM")
+    fat_intake = fields.Float("Fat")
+    fat_intake_uom = fields.Many2one("product.uom", "Fat UoM")
+    protein_intake = fields.Float("Protein")
+    protein_intake_uom = fields.Many2one("product.uom", "Protein UoM")
