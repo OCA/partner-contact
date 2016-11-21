2dbabf8003b4f3157a98189234de808aabd1ad86
Maxime Chambreuil <mchambreuil@ursainfosystems.com>
2016-11-21 10:30:09 -0600

    [IMP] Move uom domain in the model

--- partner_contact_nutrition/models/res_partner.py
+++ partner_contact_nutrition/models/res_partner.py
@@ -10,11 +10,27 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     caloric_intake = fields.Float("Calories")
-    caloric_intake_uom = fields.Many2one("product.uom", "Calories UoM")
+    caloric_intake_uom = fields.Many2one("product.uom", "Calories UoM",
+                                         domain="[('category_id', '=', "
+                                                "self.env.ref('product_uom.\
+                                                product_category_energy').id)]"
+                                         )
     carbohydrate_intake = fields.Float("Carbohydrate")
     carbohydrate_intake_uom = fields.Many2one("product.uom",
-                                              "Carbohydrate UoM")
+                                              "Carbohydrate UoM",
+                                              domain="[('category_id', '=', "
+                                                     "self.env.ref('product.\
+                                                     product_uom_categ_kgm').id)]" # noqa
+                                              )
     fat_intake = fields.Float("Fat")
-    fat_intake_uom = fields.Many2one("product.uom", "Fat UoM")
+    fat_intake_uom = fields.Many2one("product.uom", "Fat UoM",
+                                     domain="[('category_id', '=', "
+                                            "self.env.ref('product.\
+                                            product_uom_categ_kgm').id)]"
+                                     )
     protein_intake = fields.Float("Protein")
-    protein_intake_uom = fields.Many2one("product.uom", "Protein UoM")
+    protein_intake_uom = fields.Many2one("product.uom", "Protein UoM",
+                                         domain="[('category_id', '=', "
+                                                "self.env.ref('product.\
+                                                product_uom_categ_kgm').id)]"
+                                         )
