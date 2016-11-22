78cedc690db86e19e88db9685fa63f597643c1c0
Maxime Chambreuil <mchambreuil@ursainfosystems.com>
2016-11-22 10:08:25 -0600

    [IMP] Remove data tag and use Health category

--- partner_contact_nutrition_diet/__openerp__.py
+++ partner_contact_nutrition_diet/__openerp__.py
@@ -5,7 +5,7 @@
     "name": "Partner Contact Nutrition Diet",
     "summary": "Set the nutrition diet of your contacts",
     "version": "9.0.1.0.0",
-    "category": "Uncategorized",
+    "category": "Health",
     "website": "http://ursainfosystems.com",
     "author": "Ursa Information Systems, Odoo Community Association (OCA)",
     "license": "AGPL-3",
