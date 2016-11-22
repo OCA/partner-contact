6a15d457af7cb863c0cdfd7d5aba97d516f6c312
Maxime Chambreuil <mchambreuil@ursainfosystems.com>
2016-11-22 11:37:08 -0600

    [ADD] partner_contact_nutrition_diet (#339)
    * [ADD] partner_contact_nutrition_diet

---
+++ partner_contact_nutrition_diet/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Ursa Information Systems <http://ursainfosystems.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import (
+    nutrition_diet,
+    res_partner,
+)
