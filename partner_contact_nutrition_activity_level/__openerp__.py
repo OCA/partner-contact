f293f356fe605b27fbbf5b4e00839fd2c53584a3
Maxime Chambreuil <mchambreuil@ursainfosystems.com>
2016-11-23 11:04:17 -0600

    partner_contact_nutrition_activity_level (#342)
    * [ADD] partner_contact_nutrition_activity_level

---
+++ partner_contact_nutrition_activity_level/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Ursa Information Systems <http://ursainfosystems.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Partner Contact Activity Level",
+    "summary": "Set the activity level of your contacts",
+    "version": "9.0.1.0.0",
+    "category": "Health",
+    "website": "http://ursainfosystems.com",
+    "author": "Ursa Information Systems, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "partner_contact_nutrition",
+    ],
+    "data": [
+        "views/res_partner_view.xml",
+    ],
+}
