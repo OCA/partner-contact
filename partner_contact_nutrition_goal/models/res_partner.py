4a6412760f77fffdfce26770471af64448c79c5b
Maxime Chambreuil <mchambreuil@ursainfosystems.com>
2016-11-23 15:39:37 -0600

    [ADD] partner_contact_nutrition_goal (#338)
    * [ADD] partner_contact_nutrition_goal

---
+++ partner_contact_nutrition_goal/models/res_partner.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Ursa Information Systems <http://ursainfosystems.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+from openerp import fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    weight_goal = fields.Selection([('lose', 'Lose weight'),
+                                    ('maintain', 'Maintain weight'),
+                                    ('gain', 'Gain weight')], "Goal")
