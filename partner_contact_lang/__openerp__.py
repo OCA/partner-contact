f2574fb40bae1da29c58c4895804ff57a9e60e53
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-11-24 23:52:04 +0100

    partner_contact_lang: Manage language in contacts (#302)
    ===========================
    Manage language in contacts
    ===========================
    
    Odoo by default propagate language field to the created contacts from their
    form, but it doesn't allow to change it once created.
    
    This module fills this gap, and also provides other facilities for the
    contact language management:
    
    * Put the language of the parent company when the contact doesn't have a
      language and this parent company is assigned.
    * When the company changes the language, it fills with the same language all
      the contacts that don't have any.
    
    Usage
    =====
    
    Go to any partner that is a company and has contacts. Click on one contact
    and you will be able to edit the language.

---
+++ partner_contact_lang/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# © 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Manage language in contacts',
+    'version': '8.0.1.0.0',
+    'category': 'Partner Management',
+    'license': 'AGPL-3',
+    'author': 'Tecnativa,'
+              'Odoo Community Association (OCA)',
+    'website': 'https://www.tecnativa.com',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'views/res_partner_view.xml',
+    ],
+    'installable': True
+}
