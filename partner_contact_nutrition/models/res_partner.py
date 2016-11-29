4da07d1f7b968dee1c5873769580f0c35295b916
Maxime Chambreuil <mchambreuil@ursainfosystems.com>
2016-11-29 15:20:35 -0600

    domain (#345)

--- partner_contact_nutrition/models/res_partner.py
+++ partner_contact_nutrition/models/res_partner.py
@@ -10,27 +10,30 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     caloric_intake = fields.Float("Calories")
-    caloric_intake_uom = fields.Many2one("product.uom", "Calories UoM",
-                                         domain="[('category_id', '=', "
-                                                "self.env.ref('product_uom.\
-                                                product_category_energy').id)]"
-                                         )
+    caloric_intake_uom = fields.Many2one(
+        "product.uom", "Calories UoM",
+        domain=lambda self: [('category_id', '=',
+                              self.env.ref(
+                                  'product_uom.product_category_energy').id)]
+    )
     carbohydrate_intake = fields.Float("Carbohydrate")
-    carbohydrate_intake_uom = fields.Many2one("product.uom",
-                                              "Carbohydrate UoM",
-                                              domain="[('category_id', '=', "
-                                                     "self.env.ref('product.\
-                                                     product_uom_categ_kgm').id)]" # noqa
-                                              )
+    carbohydrate_intake_uom = fields.Many2one(
+        "product.uom", "Carbohydrate UoM",
+        domain=lambda self: [('category_id', '=',
+                              self.env.ref('product.product_uom_categ_kgm').id)
+                             ]
+    )
     fat_intake = fields.Float("Fat")
-    fat_intake_uom = fields.Many2one("product.uom", "Fat UoM",
-                                     domain="[('category_id', '=', "
-                                            "self.env.ref('product.\
-                                            product_uom_categ_kgm').id)]"
-                                     )
+    fat_intake_uom = fields.Many2one(
+        "product.uom", "Fat UoM",
+        domain=lambda self: [('category_id', '=',
+                              self.env.ref('product.product_uom_categ_kgm').id)
+                             ]
+    )
     protein_intake = fields.Float("Protein")
-    protein_intake_uom = fields.Many2one("product.uom", "Protein UoM",
-                                         domain="[('category_id', '=', "
-                                                "self.env.ref('product.\
-                                                product_uom_categ_kgm').id)]"
-                                         )
+    protein_intake_uom = fields.Many2one(
+        "product.uom", "Protein UoM",
+        domain=lambda self: [('category_id', '=',
+                              self.env.ref('product.product_uom_categ_kgm').id)
+                             ]
+    )
