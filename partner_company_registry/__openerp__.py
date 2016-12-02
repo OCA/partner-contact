053bed918287e5bd9cca64a9298ab5b897175811
aschenkels-ictstudio <a.schenkels@ictstudio.eu>
2016-12-02 14:03:09 +0100

    [ADD] Company Registry Module

---
+++ partner_company_registry/__openerp__.py
@@ -0,0 +1,36 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2015 ICTSTUDIO (<http://www.ictstudio.eu>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Partner Company Registry',
+    'version': '8.0.1.0.0',
+    'category': 'CRM',
+    'description': """Partner Company Registry
+    Adds a Partner Registry field to the partner object.
+    """,
+    'author': 'ICTSTUDIO, André Schenkels',
+    'license': 'AGPL-3',
+    'website': 'http://www.ictstudio.eu',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'view/res_partner.xml',
+    ],
+}
