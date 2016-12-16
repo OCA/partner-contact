04120e61f090a0cee34ae599cced958166334980
cubells <vicent@vcubells.net>
2016-12-16 05:34:15 +0100

    base_partner_sequence module

--- base_partner_sequence/__init__.py
+++ base_partner_sequence/__init__.py
@@ -1,3 +1,8 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
+# Copyright 2004-2009 Tiny SPRL (<http://tiny.be>).
+# Copyright 2013 initOS GmbH & Co. KG (<http://www.initos.com>).
+# Copyright 2016 Tecnativa - Vicent Cubells
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 from . import models
 from . import tests
