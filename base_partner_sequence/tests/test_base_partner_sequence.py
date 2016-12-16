68c802be1dfeb05aec6a7ee6b144db4542b64ea0
cubells <vicent@vcubells.net>
2016-12-16 05:34:15 +0100

    base_partner_sequence module

--- base_partner_sequence/tests/test_base_partner_sequence.py
+++ base_partner_sequence/tests/test_base_partner_sequence.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-# © 2015 ACSONE SA/NV (<http://acsone.eu>).
+# Copyright 2015 ACSONE SA/NV (<http://acsone.eu>).
+# Copyright 2016 Tecnativa - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 import openerp.tests.common as common
@@ -7,13 +8,28 @@ import openerp.tests.common as common
 
 class TestBasePartnerSequence(common.TransactionCase):
 
-    def test_ref_sequence_on_partner(self):
-        res_partner = self.env['res.partner']
-        partner = res_partner.create({
+    def setUp(self):
+        super(TestBasePartnerSequence, self).setUp()
+
+        self.res_partner = self.env['res.partner']
+        self.partner = self.res_partner.create({
             'name': "test1",
             'email': "test@test.com"})
-        self.assertTrue(partner.ref, "A partner has always a ref.")
 
-        copy = partner.copy()
+    def test_ref_sequence_on_partner(self):
+        # Test sequence on creating partner and copying it
+        self.assertTrue(self.partner.ref, "A partner has always a ref.")
+
+        copy = self.partner.copy()
         self.assertTrue(copy.ref, "A partner with ref created by copy "
                         "has a ref by default.")
+
+    def test_ref_sequence_on_contact(self):
+        # Test if sequence doesn't increase on creating a contact child
+        contact = self.res_partner.create({
+            'name': "contact1",
+            'email': "contact@contact.com",
+            'parent_id': self.partner.id})
+        self.assertEqual(
+            self.partner.ref, contact.ref, "All it's ok as sequence doesn't "
+                                           "increase.")
