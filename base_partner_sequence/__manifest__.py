bd9254def7c44526f044d08a5649330adb0f8bc5
Akim Juillerat <akim.juillerat@camptocamp.com>
2016-12-19 11:45:34 +0100

    base_partner_sequence: Migration to 10.0

--- base_partner_sequence/__manifest__.py
+++ base_partner_sequence/__manifest__.py
@@ -2,15 +2,16 @@
 # Copyright 2004-2009 Tiny SPRL (<http://tiny.be>).
 # Copyright 2013 initOS GmbH & Co. KG (<http://www.initos.com>).
 # Copyright 2016 Tecnativa - Vicent Cubells
+# Copyright 2016 Camptocamp - Akim Juillerat (<http://www.camptocamp.com>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
 {
-    "name": "Add a sequence on customers' code",
-    "version": "9.0.0.1.0",
     "author": "Tiny/initOS GmbH & Co. KG,"
               "ACSONE SA/NV,"
               "Tecnativa, "
-              "Odoo Community Association (OCA)",
+              "Odoo Community Association (OCA), "
+              "Camptocamp, ",
+    "name": "Add a sequence on customers' code",
+    "version": "10.0.1.0.0",
     "category": "Generic Modules/Base",
     "website": "http://www.initos.com",
     "depends": [
