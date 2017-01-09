6dd260ad0437d3a01edef83ba94186118b74698c
cubells <vicent@vcubells.net>
2017-01-09 22:47:03 +0100

    crm_deduplicate_filter module - Updated README - updated views - Fix dependence

--- partner_deduplicate_filter/__openerp__.py
+++ partner_deduplicate_filter/__openerp__.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 # Copyright 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# Copyright 2017 Vicent Cubells <vicent.cubells@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Exclude records from the deduplication",
-    "version": "8.0.1.0.0",
+    "version": "9.0.1.0.0",
     "category": "Tools",
     "website": "https://www.tecnativa.com",
     "author": "Tecnativa, "
@@ -13,6 +14,7 @@
     "installable": True,
     "depends": [
         "crm",
+        "crm_deduplicate_acl",
     ],
     "data": [
         'wizards/partner_merge_view.xml',
