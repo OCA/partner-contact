6785be2913f96fc551cbb18c22d5bc94b5f07980
Ted Salmon <tsalmon@laslabs.com>
2017-01-11 12:45:47 -0800

    [ADD] partner_password_reset: Create module * Create module that allows you to send the password reset emails from the Partner view to any associated `res.users`

---
+++ partner_password_reset/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 LasLabs Inc.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Partner User Password Reset",
+    "summary": "Add Action to allow resetting of a Partner's associated user "
+               "password from within the partner view.",
+    "version": "9.0.1.0.0",
+    "category": "Customer Relationship Management",
+    "website": "https://laslabs.com/",
+    "author": "LasLabs, "
+              "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    'depends': [
+        'auth_signup',
+    ],
+    'data': [
+        'views/res_partner_view.xml',
+    ],
+}
---
+++ partner_password_reset/models/res_partner.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 LasLabs Inc.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import api, models
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    @api.multi
+    def action_reset_user_password(self):
+        self.mapped('user_ids').action_reset_password()
