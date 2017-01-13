1d603b18df8336c5700ea266b015a1155cff69aa
cubells <vicent@vcubells.net>
2017-01-13 19:34:51 +0100

    [9.0][ADD] partner_ref_unique module
    - Added OCA icon

--- partner_ref_unique/__openerp__.py
+++ partner_ref_unique/__openerp__.py
@@ -1,30 +1,24 @@
 # -*- coding: utf-8 -*-
-# © 2016 Antiun Ingenieria S.L. - Antonio Espinosa
+# Copyright 2016 Antiun Ingenieria S.L. - Antonio Espinosa
+# Copyright 2017 Tecnativa - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Partner unique reference",
     "summary": "Add an unique constraint to partner ref field",
-    "version": "8.0.1.0.0",
+    "version": "9.0.1.0.0",
     "category": "Customer Relationship Management",
     "website": "http://www.antiun.com",
-    "author": "Antiun Ingeniería S.L.",
+    "author": "Antiun Ingeniería S.L., "
+              "Tecnativa, "
+              "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
-    # 'auto_install':False,
-    "external_dependencies": {
-        "python": [],
-        "bin": [],
-    },
     "depends": [
         "base",
     ],
     "data": [
         "views/res_company_view.xml",
     ],
-    "demo": [
-    ],
-    "qweb": [
-    ]
 }
