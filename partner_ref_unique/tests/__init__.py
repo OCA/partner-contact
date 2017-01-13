8c4705c1380a89443f87dd615071cbf106d5e112
cubells <vicent@vcubells.net>
2017-01-13 19:34:51 +0100

    [9.0][ADD] partner_ref_unique module
    - Added OCA icon

--- partner_ref_unique/tests/__init__.py
+++ partner_ref_unique/tests/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 Antiun Ingenieria S.L. - Antonio Espinosa
+# Copyright 2017 Tecnativa - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import models
+from . import test_res_partner_ref
