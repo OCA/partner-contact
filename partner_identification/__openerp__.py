87e267a76445630e509688f7db830cd92b3e02ef
Dave Lasley <dave@laslabs.com>
2017-01-24 09:55:18 -0800

    [IMP] partner_identification: Add context override (#373)
    Allow for context override of validations using ``id_no_validate``

--- partner_identification/__openerp__.py
+++ partner_identification/__openerp__.py
@@ -11,7 +11,7 @@
 {
     'name': 'Partner Identification Numbers',
     'category': 'Customer Relationship Management',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.0.1',
     'depends': [
         'sales_team',
     ],
