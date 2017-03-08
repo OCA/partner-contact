f8423ab6b2570860c5740cca130b44cd93ba2f45
Nicolas JEUDY <nicolas@sudokeys.com>
2017-03-08 17:50:53 +0100

    [MIG] 10.0 partner_contact_in_several_companies (#347)
    * [MIG] inital work to 10.0
    
    * [FIX] restore computed field
    
    - select=1 is not usable anymore ...
    
    * [FIX] convert _fields_sync to new api
    
    * [FIX] restore form . import statement
    
    * [FIX] readme with last template.
    
    * [FIX] import statement.
    
    * [FIX] check all tests
    
    * [FIX] Flake8 errors fixes
    
    * [FIX] flake 8
    
    * [FIX] wrong obj call self -> self.contact_id
    
    * [FIX] typo
    
    * [FIX] flake 8
    
    * [FIX] add index on contact_type
    
    * [fix] use in statement on test
    
    * [FIX] update base action with new context
    
    * [FIX] test fixes
    
    * [FIX] test work
    
    * [FIX] flake 8 over indent
    
    * [FIX] fixes on lastest comments
    
    - better readability
    - remove <data></data> tags
    
    * [FIX] model that same pattern for _fields_sync
    
    - add test ensure_once()
    
    * [FIX] remove last data xml tags
    
    * [FIX] fix action test
    
    * [FIX] typo in import statement
    
    * [FIX] remove logger from tests
    
    * [FIX] new flake8 compute method name for OCA
    
    * [FIX] minor typo
    
    * [FIX] partner_contact_in_several_companies: remove contributor key in manifest
    
    * [FIX] partner_contact_in_several_companies: residual onchange on partner view
    
    * Revert "[FIX] partner_contact_in_several_companies: remove contributor key in manifest"
    
    This reverts commit 56e511a9a7e1dd20cf99578a340a00301a98ba85.
    
    * [FIX] partner_contact_personal_information_page: clean manifest and authors
    
    * [FIX] partner_contact_in_several_companies: required = True not needed

--- partner_contact_in_several_companies/models/ir_actions.py
+++ partner_contact_in_several_companies/models/ir_actions.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, api
+from odoo import api, models
 
 
 class IRActionsWindow(models.Model):
