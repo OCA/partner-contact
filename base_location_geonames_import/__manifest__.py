e26b2ce1b48402093929d461efe957bdcfcf736a
Dave Lasley <dave@laslabs.com>
2017-04-23 17:00:18 -0700

    [10.0][IMP] base_location(geonames): Add Lat/Long (#405)
    * [IMP] base_location: Add lat & long to `better.zip`
    * Add latitude and longitude columns to `better.zip`
    
    * [IMP] base_location_geonames_import: Add lat/long
    * Add support for latitude & longitude to genomes importer

--- base_location_geonames_import/__manifest__.py
+++ base_location_geonames_import/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'Base Location Geonames Import',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.0.1',
     'category': 'Partner Management',
     'license': 'AGPL-3',
     'summary': 'Import better zip entries from Geonames',
