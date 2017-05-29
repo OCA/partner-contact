5721d0ee7e9b8d099d0f174cda851d4b8a004f8d
David Vidal <chienandalu@gmail.com>
2017-05-29 18:20:58 +0200

    [MIG] partner_employee_quantity: Migration to 10.0

--- partner_employee_quantity/__manifest__.py
+++ partner_employee_quantity/__manifest__.py
@@ -1,19 +1,22 @@
 # -*- coding: utf-8 -*-
-# © 2015 Antiun Ingeniería S.L. - Antonio Espinosa
-# © 2015 Antiun Ingeniería S.L. - Jairo Llopis
+# Copyright 2015 Antonio Espinosa <antonio.espinosa@tecnativa.com>
+# Copyright 2015 Jairo Llopis <jairo.llopis@tecnativa.com>
+# Copyright 2017 David Vidal <david.vidal@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Employee quantity in partners',
     'summary': 'Know how many employees a partner has',
-    'version': '8.0.1.0.0',
+    'version': '10.0.1.0.0',
     'category': 'Customer Relationship Management',
-    "author": "Antiun Ingeniería S.L., Odoo Community Association (OCA)",
+    "author": 'Antiun Ingeniería S.L., '
+              'Tecnativa, '
+              'Odoo Community Association (OCA)',
     "license": "AGPL-3",
     "application": False,
-    'website': 'http://www.antiun.com',
+    'website': 'http://www.tecnativa.com',
     'depends': [
-        'base',
+        'sale',
     ],
     'data': [
         'security/ir.model.access.csv',
