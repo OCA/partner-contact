dde73b02b773a33b40118679fc96ec96af26271b
Dave Lasley <dave@laslabs.com>
2017-05-30 02:16:20 -0700

    [10.0][IMP] partner_identification: Add field computation and inverses (#419)
    * [IMP] partner_identification: Add field computation and inverses
    * Add methods to allow for computation and inverse of an ID field of a specific category type
    
    * [IMP] partner_identification: Add search option

--- partner_identification/models/res_partner_id_category.py
+++ partner_identification/models/res_partner_id_category.py
@@ -10,10 +10,9 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 
-from openerp import api, models, fields
-from openerp.exceptions import ValidationError, UserError
-from openerp.tools.safe_eval import safe_eval
-from openerp.tools.translate import _
+from odoo import api, models, fields, _
+from odoo.exceptions import ValidationError, UserError
+from odoo.tools.safe_eval import safe_eval
 
 
 class ResPartnerIdCategory(models.Model):
