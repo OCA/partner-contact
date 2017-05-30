f14faafa9aac7562f956ff2638f6171b76d8307c
David Vidal <chienandalu@gmail.com>
2017-05-30 10:05:10 +0200

    [MIG] partner_capital: Migration to 10.0

--- partner_capital/models/res_partner.py
+++ partner_capital/models/res_partner.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-# © 2015 Antiun Ingeniería S.L. - Antonio Espinosa
-# © 2015 Antiun Ingeniería S.L. - Jairo Llopis
+# Copyright 2015 Antonio Espinosa <antonio.espinosa@tecnativa.com>
+# Copyright 2015 Jairo Llopis <jairo.llopis@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields
+from odoo import fields, models
 
 
 class ResPartner(models.Model):
