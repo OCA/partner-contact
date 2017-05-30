efa6c486b95048a59dee406f2ea4b8539e3b97af
tarteo <d.sluijk@onestein.nl>
2017-05-30 16:28:38 +0200

    [ADD] partner_coc

---
+++ partner_coc/models/res_partner.py
@@ -0,0 +1,18 @@
+from odoo import models, fields
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    coc_registration_number = fields.Char(
+        name='Chamber Of Commerce Registration Number',
+        compute=lambda s: s._compute_identification(
+            'coc_registration_number', 'coc',
+        ),
+        inverse=lambda s: s._inverse_identification(
+            'coc_registration_number', 'coc',
+        ),
+        search=lambda s, *a: s._search_identification(
+            'coc_registration_number', 'coc', *a
+        )
+    )
