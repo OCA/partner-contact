2811dd283aaa41b5832059cb31a81f047480ddd3
David Vidal <chienandalu@gmail.com>
2017-05-31 18:55:33 +0200

    [MIG] base_location_nuts: Migration to 10.0

--- base_location_nuts/__manifest__.py
+++ base_location_nuts/__manifest__.py
@@ -1,32 +1,15 @@
 # -*- coding: utf-8 -*-
-# Python source code encoding : https://www.python.org/dev/peps/pep-0263/
-##############################################################################
-#
-#    OpenERP, Odoo Source Management Solution
-#    Copyright (c) 2015 Antiun Ingeniería S.L. (http://www.antiun.com)
-#                       Antonio Espinosa <antonioea@antiun.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2015 Antonio Espinosa <antonio.espinosa@tecnativa.com>
+# Copyright 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
+# Copyright 2017 David Vidal <david.vidal@tecnativa.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'NUTS Regions',
     'category': 'Localisation/Europe',
-    'version': '8.0.2.0.0',
+    'version': '10.0.1.0.0',
     'depends': [
-        'base',
+        'sales_team',
     ],
     'data': [
         'views/res_country_view.xml',
@@ -38,9 +21,9 @@
     'images': [
         'images/new_fields.png',
     ],
-    'author': 'Antiun Ingeniería S.L., '
+    'author': 'Tecnativa, '
               'Odoo Community Association (OCA)',
-    'website': 'http://www.antiun.com',
+    'website': 'https://www.tecnativa.com',
     'license': 'AGPL-3',
-    'installable': False,
+    'installable': True,
 }
--- base_location_nuts/models/__init__.py
+++ base_location_nuts/models/__init__.py
@@ -1,10 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2015 Antiun Ingeniería S.L. - Antonio Espinosa
-# © 2015 Antiun Ingeniería S.L. - Jairo Llopis
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import (
-    res_country,
-    res_partner_nuts,
-    res_partner,
-)
+from . import res_country
+from . import res_partner
+from . import res_partner_nuts
