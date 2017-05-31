7e5b14e41344dd0284368b2b86a14a890c660dda
Carlos Dauden <carlos.dauden@tecnativa.com>
2017-05-31 16:02:45 +0200

    [9.0] [ADD] partner_stock_risk (#300)
    * [9.0][ADD] partner_stock_risk: New module
    
    * [9.0][WIP] partner_stock_risk: New module
    
    * [9.0][WIP] partner_stock_risk: Improve code
    
    * [9.0][FIX] partner_stock_risk: Fix test
    
    * [9.0][IMP] partner_stock_risk: Translate

---
+++ partner_stock_risk/tests/test_partner_stock_risk.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# © 2016 Carlos Dauden <carlos.dauden@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp.tests.common import TransactionCase
+
+
+class TestPartnerStocklRisk(TransactionCase):
+    def setUp(self):
+        super(TestPartnerStocklRisk, self).setUp()
+        self.partner = self.env['res.partner'].create({
+            'name': 'Partner test',
+            'customer': True,
+        })
+        self.product = self.env.ref('product.product_product_36')
+        self.quant = self.env['stock.quant'].create({
+            'qty': 100,
+            'location_id': self.env.ref('stock.stock_location_stock').id,
+            'product_id': self.product.id,
+        })
+        self.picking = self.env['stock.picking'].create({
+            'picking_type_id': self.env.ref('stock.picking_type_out').id,
+            'location_id': self.env.ref('stock.stock_location_stock').id,
+            'location_dest_id':
+                self.env.ref('stock.stock_location_customers').id,
+        })
+        self.move = self.env['stock.move'].create({
+            'name': '/',
+            'picking_id': self.picking.id,
+            'product_uom': self.product.uom_id.id,
+            'location_id': self.env.ref('stock.stock_location_stock').id,
+            'location_dest_id':
+                self.env.ref('stock.stock_location_customers').id,
+            'product_id': self.product.id,
+        })
+
+    def test_stock_move(self):
+        self.move.action_done()
