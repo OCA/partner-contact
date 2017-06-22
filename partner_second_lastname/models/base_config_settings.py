77b103d175bae7874196abe38ecacab50f86b96e
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-06-22 02:31:00 +0200

    partner_second_lastname: Migration to 10.0

--- partner_second_lastname/models/base_config_settings.py
+++ partner_second_lastname/models/base_config_settings.py
@@ -2,7 +2,7 @@
 # © 2015 Antiun Ingenieria S.L. - Antonio Espinosa
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, api
+from odoo import models, api
 
 
 class BaseConfigSettings(models.TransientModel):
--- partner_second_lastname/tests/__init__.py
+++ partner_second_lastname/tests/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-# © 2015 Grupo ESOC Ingeniería de Servicios, S.L.U.
 
-from . import test_name, test_onchange
-from openerp.addons.partner_firstname.tests import test_empty
+from . import test_name
+from . import test_onchange
+from . import test_config
+from odoo.addons.partner_firstname.tests import test_empty
