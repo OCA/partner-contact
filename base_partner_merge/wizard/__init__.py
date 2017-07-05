07f2aa08a81a9729268cf6a17a8c5540b4e4d2eb
Alan Ramos <alan.ramos@jarsa.com.mx>
2017-07-05 10:18:00 -0500

    [FIX] Migrate base_partner_merge to V10

---
+++ base_partner_merge/wizard/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Camptocamp SA
+# Copyright 2017 Jarsa Sistemas
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from . import base_partner_merge
+from . import validate_email
