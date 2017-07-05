6150eaa4a2f486bced58a4974b463ad8957b733d
Alan Ramos <alan.ramos@jarsa.com.mx>
2017-07-05 10:18:00 -0500

    [FIX] Migrate base_partner_merge to V10

--- base_partner_merge/__init__.py
+++ base_partner_merge/__init__.py
@@ -1,2 +1,5 @@
-from . import base_partner_merge
-from . import validate_email
+# -*- coding: utf-8 -*-
+# Copyright 2016 Camptocamp SA
+# Copyright 2017 Jarsa Sistemas
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from . import wizard
--- base_partner_merge/validate_email.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-# © 2016 Camptocamp SA
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from openerp.addons.crm.validate_email import *  # noqa
