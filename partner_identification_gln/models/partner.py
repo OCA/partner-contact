71dd6c4a82e1c1f7b748f8cfd0850a9bb6a8c0cf
gilles <meyomesse.gilles@gmail.com>
2017-07-06 18:07:43 +0200

    [MIG] Partner Identification Gln v10

--- partner_identification_gln/models/partner.py
+++ partner_identification_gln/models/partner.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
-# ©  2016 ACSONE SA/NV (<http://acsone.eu>)
+# Copyright 2016-2017 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 import logging
-from openerp import api, models
+
+from odoo import api, models
+
 _logger = logging.getLogger(__name__)
 
 try:
