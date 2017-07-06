fe9f77570565f00255d8e15e7e21c5024c77d591
gilles <meyomesse.gilles@gmail.com>
2017-07-06 18:07:43 +0200

    [MIG] Partner Identification Gln v10

--- partner_identification_gln/tests/test_gln.py
+++ partner_identification_gln/tests/test_gln.py
@@ -2,35 +2,37 @@
 # Copyright 2016 Acsone S.A.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp.exceptions import ValidationError
-from openerp.tests.common import TransactionCase
+from odoo.exceptions import ValidationError
+from odoo.tests.common import SavepointCase
 
 
-class TestGLN(TransactionCase):
-    def setUp(self):
-        super(TestGLN, self).setUp()
-        self.partner = self.env['res.partner'].create({'name': 'TestGLN'})
-        self.partner2 = self.env['res.partner'].create({'name': 'TestGLN2'})
-        pc = self.env.ref('partner_identification_gln.'
-                          'partner_identification_gln_number_category')
-        self.partner_id_category = pc
+class TestGLN(SavepointCase):
 
-        pc_gcp = self.env.ref('partner_identification_gln.'
-                              'partner_identification_gcp_number_category')
-        self.partner_id_gcp_category = pc_gcp
+    def setUp(cls):
+        super(TestGLN, cls).setUp()
+        cls.partner = cls.env['res.partner'].create({'name': 'TestGLN'})
+        cls.partner2 = cls.env['res.partner'].create({'name': 'TestGLN2'})
+        pc = cls.env.ref('partner_identification_gln.'
+                         'partner_identification_gln_number_category')
+        cls.partner_id_category = pc
+
+        pc_gcp = cls.env.ref('partner_identification_gln.'
+                             'partner_identification_gcp_number_category')
+        cls.partner_id_gcp_category = pc_gcp
 
     def test_gln(self):
         # Good GLN
-        vals = {'name': '5450534001717',
+        vals = {'name': '5450534005852',
                 'category_id': self.partner_id_category.id
                 }
+
         self.partner.write({'id_numbers': [(0, 0, vals)]})
         id_number = self.partner.id_numbers[0]
 
-        self.assertEqual(id_number.name, '5450534001717')
+        self.assertEqual(id_number.name, '5450534005852')
 
         # Duplicate GLN
-        vals = {'name': '5450534001717',
+        vals = {'name': '5450534005852',
                 'category_id': self.partner_id_category.id
                 }
 
@@ -43,6 +45,10 @@ class TestGLN(TransactionCase):
                 }
         with self.assertRaises(ValidationError):
             self.partner.write({'id_numbers': [(0, 0, vals)]})
+
+    def test_gln_with_no_number(self):
+        self.assertFalse(
+            self.partner_id_category.validate_res_partner_gln(id_number=None))
 
     def test_gcp(self):
         # Good GLN
@@ -68,3 +74,8 @@ class TestGLN(TransactionCase):
                 }
         with self.assertRaises(ValidationError):
             self.partner.write({'id_numbers': [(0, 0, vals)]})
+
+    def test_gcp_with_no_number(self):
+        self.assertFalse(
+            self.partner_id_gcp_category.validate_res_partner_gln(
+                id_number=None))
