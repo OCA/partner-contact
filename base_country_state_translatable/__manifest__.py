477f72ee358b7e4b900037fe8eb8230be72666be
rami-wafaie <eng.rami.wafaie@gmail.com>
2017-08-25 12:47:25 +0200

    base_country_state_translatable Module (#461)

---
+++ base_country_state_translatable/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# © initOS GmbH 2017
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Translate Country States",
+    "version": "10.0.1.0.0",
+    "depends": ['base',
+                ],
+    'application': False,
+    'author': 'initOS GmbH, Odoo Community Association (OCA)',
+    "category": "Localisation/Europe",
+    'license': 'AGPL-3',
+    'data': [],
+    'installable': True,
+    'auto_install': False,
+}
