38fdf950c1c01a4bfc2c1c32634aa2e5f341baa8
Carlos Dauden <carlos.dauden@tecnativa.com>
2017-09-27 20:03:35 +0200

    [9.0][IMP] partner_payment_return_risk: Unify with base method and split amount

--- partner_payment_return_risk/models/res_partner.py
+++ partner_payment_return_risk/models/res_partner.py
@@ -13,23 +13,30 @@ class ResPartner(models.Model):
     risk_payment_return_limit = fields.Monetary(
         string='Limit Payments Returns', help='Set 0 if it is not locked')
     risk_payment_return = fields.Monetary(
-        compute='_compute_risk_payment_return', store=True,
+        compute='_compute_risk_invoice', store=True,
         string='Total Returned Invoices',
         help='Total returned invoices in Open state')
 
-    @api.multi
-    @api.depends('invoice_ids.state', 'invoice_ids.returned_payment')
-    def _compute_risk_payment_return(self):
-        AccountInvoice = self.env['account.invoice']
-        for partner in self:
-            partner.risk_payment_return = AccountInvoice.read_group(
-                [('partner_id', '=', partner.id),
-                 ('returned_payment', '=', True),
-                 ('state', '=', 'open'),
-                 ],
-                ['residual'],
-                []
-            )[0]['residual']
+    @api.model
+    def _risk_invoice_domain_list(self):
+        res = super(ResPartner, self)._risk_invoice_domain_list()
+        for reg in res:
+            reg['domain'].append(('returned_payment', '=', False))
+        res.append({
+            'risk_field': 'risk_payment_return',
+            'domain': [('returned_payment', '=', True)],
+            'amount_field': 'residual_signed'
+        })
+        return res
+
+    @api.model
+    def _risk_invoice_depends_fields(self):
+        res = super(ResPartner, self)._risk_invoice_depends_fields()
+        res.extend([
+            'invoice_ids.returned_payment',
+            'child_ids.invoice_ids.returned_payment',
+        ])
+        return res
 
     @api.model
     def _risk_field_list(self):
@@ -37,3 +44,14 @@ class ResPartner(models.Model):
         res.append(('risk_payment_return', 'risk_payment_return_limit',
                     'risk_payment_return_include'))
         return res
+
+    # TODO: Avoid overwrite function
+    @api.multi
+    @api.depends('credit', 'risk_invoice_open', 'risk_invoice_unpaid',
+                 'child_ids.credit', 'child_ids.risk_invoice_open',
+                 'child_ids.risk_invoice_unpaid')
+    def _compute_risk_account_amount(self):
+        for partner in self.filtered(lambda x: x.customer and not x.parent_id):
+            partner.risk_account_amount = (
+                partner.credit - partner.risk_invoice_open -
+                partner.risk_invoice_unpaid - partner.risk_payment_return)
