5fbf1e30682235f6e666729422159a88d9fabd50
Carlos Dauden <carlos.dauden@tecnativa.com>
2017-10-09 00:50:24 +0200

    [9.0][FIX] partner_financial_risk: Invoice refunds add risk. (#476)
    * [9.0][FIX] partner_financial_risk: Invoice refunds add risk. Date_maturity change
    
    * [9.0][FIX] partner_financial_risk: Recompute when invoice_unpaid_margin change
    
    * [9.0][IMP] partner_financial_risk: Improve heritability
    
    * [9.0][IMP] partner_payment_return_risk: Unify with base method and split amount
    
    * [9.0][IMP] partner_financial_risk: Improve code. Include archived contacts
    
    * [9.0][WIP] partner_financial_risk: Different model in read_group list
    
    * [9.0][WIP] partner_financial_risk: Invoices amount from account.move.line
    
    * [9.0][WIP] partner_financial_risk: Change "©" to Copyright
    
    * [9.0][IMP] partner_financial_risk: invoice_unpaid_margin in depends
    
    * [9.0][IMP] partner_financial_risk: Descriptions and translation

--- partner_payment_return_risk/models/res_partner.py
+++ partner_payment_return_risk/models/res_partner.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2016 Carlos Dauden <carlos.dauden@tecnativa.com>
+# Copyright 2016 Carlos Dauden <carlos.dauden@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp import api, fields, models
@@ -9,27 +9,43 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     risk_payment_return_include = fields.Boolean(
-        string='Include Payments Returns', help='Full risk computation')
+        string='Include Payments Returns',
+        help='Full risk computation.\n'
+             'Residual amount of move lines not reconciled with returned '
+             'lines related.')
     risk_payment_return_limit = fields.Monetary(
         string='Limit Payments Returns', help='Set 0 if it is not locked')
     risk_payment_return = fields.Monetary(
-        compute='_compute_risk_payment_return', store=True,
-        string='Total Returned Invoices',
-        help='Total returned invoices in Open state')
+        compute='_compute_risk_account_amount', store=True,
+        string='Total Payments Returns',
+        help='Residual amount of move lines not reconciled with returned '
+             'lines related.')
+
+    @api.model
+    def _risk_account_groups(self):
+        res = super(ResPartner, self)._risk_account_groups()
+        res['open']['domain'] += [
+            ('partial_reconcile_returned_ids', '=', False),
+        ]
+        res['unpaid']['domain'] += [
+            ('partial_reconcile_returned_ids', '=', False),
+        ]
+        res['returned'] = {
+            'domain': [('reconciled', '=', False),
+                       ('account_id.internal_type', '=', 'receivable'),
+                       ('partial_reconcile_returned_ids', '!=', False)],
+            'fields': ['partner_id', 'account_id', 'amount_residual'],
+            'group_by': ['partner_id', 'account_id']
+        }
+        return res
 
     @api.multi
-    @api.depends('invoice_ids.state', 'invoice_ids.returned_payment')
-    def _compute_risk_payment_return(self):
-        AccountInvoice = self.env['account.invoice']
-        for partner in self:
-            partner.risk_payment_return = AccountInvoice.read_group(
-                [('partner_id', '=', partner.id),
-                 ('returned_payment', '=', True),
-                 ('state', '=', 'open'),
-                 ],
-                ['residual'],
-                []
-            )[0]['residual']
+    def _prepare_risk_account_vals(self, groups):
+        vals = super(ResPartner, self)._prepare_risk_account_vals(groups)
+        vals['risk_payment_return'] = sum(
+            reg['amount_residual'] for reg in groups['returned']['read_group']
+            if reg['partner_id'][0] == self.id)
+        return vals
 
     @api.model
     def _risk_field_list(self):
