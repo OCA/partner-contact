8c1d1b33bc42b50ce3cd375033c6c2d692a8de2f
Enric Tobella <etobella@creublanca.es>
2017-10-09 13:09:47 +0200

    [MIG] partner_firstname

--- partner_firstname/models/base_config_settings.py
+++ partner_firstname/models/base_config_settings.py
@@ -3,12 +3,12 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 import logging
-from openerp import api, fields, models
+from odoo import api, fields, models
 _logger = logging.getLogger(__name__)
 
 
-class BaseConfigSettings(models.TransientModel):
-    _inherit = 'base.config.settings'
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
 
     partner_names_order = fields.Selection(
         string="Partner names order",
@@ -32,18 +32,20 @@ class BaseConfigSettings(models.TransientModel):
         return self.env['res.partner']._names_order_default()
 
     @api.model
-    def get_default_partner_names_order(self, fields):
-        return {
-            'partner_names_order': self.env['ir.config_parameter'].get_param(
-                'partner_names_order', self._partner_names_order_default(),
-            ),
-        }
+    def get_values(self):
+        res = super(ResConfigSettings, self).get_values()
+        partner_names_order = self.env['ir.config_parameter'].sudo().get_param(
+            'partner_names_order',
+            default=self._partner_names_order_default()
+        )
+        res.update(partner_names_order=partner_names_order)
+        return res
 
     @api.multi
     @api.depends('partner_names_order')
     def _compute_names_order_changed(self):
-        current = self.env['ir.config_parameter'].get_param(
-            'partner_names_order', self._partner_names_order_default(),
+        current = self.env['ir.config_parameter'].sudo().get_param(
+            'partner_names_order', default=self._partner_names_order_default()
         )
         for record in self:
             record.partner_names_order_changed = bool(
@@ -53,12 +55,14 @@ class BaseConfigSettings(models.TransientModel):
     @api.multi
     @api.onchange('partner_names_order')
     def _onchange_partner_names_order(self):
-        self.partner_names_order_changed = self._compute_names_order_changed()
+        self._compute_names_order_changed()
 
     @api.multi
-    def set_partner_names_order(self):
-        self.env['ir.config_parameter'].set_param(
-            'partner_names_order', self.partner_names_order)
+    def set_values(self):
+        super(ResConfigSettings, self).set_values()
+        self.env['ir.config_parameter'].sudo().set_param(
+            'partner_names_order', self.partner_names_order
+        )
 
     @api.multi
     def _partners_for_recalculating(self):
