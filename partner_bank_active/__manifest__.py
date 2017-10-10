366d8eaa5073c0932a69506b3899f98ed000556f
Benjamin Willig <benjamin.willig@acsone.eu>
2017-10-10 15:57:13 +0200

    [ADD] Module which allow to deactivate partner bank accounts

---
+++ partner_bank_active/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Partner Bank Active',
+    'summary': "Allow to deactivate a partner bank account",
+    'version': '10.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'ACSONE SA/NV, Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/partner-contact',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'views/res_partner_bank.xml',
+    ],
+}
