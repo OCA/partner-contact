d97c43afc35e19f2b2c3310c340920a2fc2bc3bb
Ronald Portier <ronald@therp.nl>
2017-10-11 02:23:55 +0200

    [IMP] Correct search criteria in hooks.

--- partner_multi_relation_contact/hooks.py
+++ partner_multi_relation_contact/hooks.py
@@ -9,7 +9,7 @@ def post_init_hook(cr, registry):
     partner_model = env['res.partner']
     # Contact relations have to be created for contacts:
     partners = partner_model.with_context(active_test=False).search([
-        ('parent_id', '=', True),
+        ('parent_id', '!=', False),
         ('is_company', '=',  False),
         ('type', '=',  'contact'),
     ])
