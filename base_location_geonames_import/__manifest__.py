d118410876dc3706f769ef1aa1135331d87201ec
Jordi Ballester <jordi.ballester@eficent.com>
2017-10-13 14:08:15 +0200

    migration to 11.0

--- base_location_geonames_import/__manifest__.py
+++ base_location_geonames_import/__manifest__.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
-# © 2014-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
-# © 2014 Lorenzo Battistini <lorenzo.battistini@agilebg.com>
-# © 2016 Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright 2014-2016 Akretion (Alexis de Lattre
+#                     <alexis.delattre@akretion.com>)
+# Copyright 2014 Lorenzo Battistini <lorenzo.battistini@agilebg.com>
+# Copyright 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+#                <contact@eficent.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Base Location Geonames Import',
-    'version': '10.0.1.0.1',
+    'version': '11.0.1.0.1',
     'category': 'Partner Management',
     'license': 'AGPL-3',
     'summary': 'Import better zip entries from Geonames',
@@ -17,7 +20,7 @@
               'Odoo Community Association (OCA)',
     'website': 'http://www.akretion.com',
     'depends': ['base_location'],
-    'external_dependencies': {'python': ['requests', 'unicodecsv']},
+    'external_dependencies': {'python': ['requests']},
     'data': [
         'wizard/geonames_import_view.xml',
         ],
