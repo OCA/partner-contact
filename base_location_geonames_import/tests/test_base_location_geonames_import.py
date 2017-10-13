bb1ca1ecf7e2fbf4127ad0b021597d1b9cb782a4
Jordi Ballester <jordi.ballester@eficent.com>
2017-10-13 14:08:15 +0200

    migration to 11.0

--- base_location_geonames_import/tests/test_base_location_geonames_import.py
+++ base_location_geonames_import/tests/test_base_location_geonames_import.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-# © 2016 Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo.tests import common
 
@@ -10,6 +10,7 @@ class TestBaseLocationGeonamesImport(common.SavepointCase):
     def setUpClass(cls):
         super(TestBaseLocationGeonamesImport, cls).setUpClass()
         cls.country = cls.env.ref('base.mc')
+        cls.country.enforce_cities = True
         cls.wizard = cls.env['better.zip.geonames.import'].create({
             'country_id': cls.country.id,
         })
@@ -27,12 +28,25 @@ class TestBaseLocationGeonamesImport(common.SavepointCase):
             ('country_id', '=', self.country.id)
         ])
         self.assertEqual(zip_count, max_import)
+
+        # Look if there are imported cities
+        city_count = self.env['res.city'].search_count([
+            ('country_id', '=', self.country.id)
+        ])
+        self.assertTrue(city_count)
+
         # Reimport again to see that there's no duplicates
         self.wizard.with_context(max_import=max_import).run_import()
         state_count2 = self.env['res.country.state'].search_count([
             ('country_id', '=', self.country.id)
         ])
         self.assertEqual(state_count, state_count2)
+
+        city_count2 = self.env['res.city'].search_count([
+            ('country_id', '=', self.country.id)
+        ])
+        self.assertEqual(city_count, city_count2)
+
         zip_count = self.env['res.better.zip'].search_count([
             ('country_id', '=', self.country.id)
         ])
@@ -46,6 +60,13 @@ class TestBaseLocationGeonamesImport(common.SavepointCase):
         self.wizard.run_import()
         self.assertFalse(zip_entry.exists())
 
+        city_entry = self.env['res.city'].create({
+            'name': 'Test city',
+            'country_id': self.country.id,
+        })
+        self.wizard.run_import()
+        self.assertFalse(city_entry.exists())
+
     def test_import_title(self):
         self.wizard.letter_case = 'title'
         self.wizard.with_context(max_import=1).run_import()
@@ -54,6 +75,11 @@ class TestBaseLocationGeonamesImport(common.SavepointCase):
         )
         self.assertEqual(zip.city, zip.city.title())
 
+        city = self.env['res.city'].search(
+            [('country_id', '=', self.country.id)], limit=1
+        )
+        self.assertEqual(city.name, city.name.title())
+
     def test_import_upper(self):
         self.wizard.letter_case = 'upper'
         self.wizard.with_context(max_import=1).run_import()
@@ -61,3 +87,8 @@ class TestBaseLocationGeonamesImport(common.SavepointCase):
             [('country_id', '=', self.country.id)], limit=1
         )
         self.assertEqual(zip.city, zip.city.upper())
+
+        city = self.env['res.city'].search(
+            [('country_id', '=', self.country.id)], limit=1
+        )
+        self.assertEqual(city.name, city.name.upper())
