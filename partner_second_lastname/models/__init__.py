b2223c8c6845c27b2c35acf67f3358e3216415fc
Jordi Ballester <jordi.ballester@eficent.com>
2017-10-13 16:26:14 +0200

    migration to 11.0

--- partner_second_lastname/models/__init__.py
+++ partner_second_lastname/models/__init__.py
@@ -2,5 +2,5 @@
 # © 2015 Antiun Ingenieria S.L. - Antonio Espinosa
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import base_config_settings
+from . import res_config_settings
 from . import res_partner
