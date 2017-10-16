39b216bd1b1dacc1ead36ade2ed1b4e00544c4ed
Ronald Portier <ronald@therp.nl>
2017-10-16 12:39:37 +0200

    [IMP] Make partner_multi_relation more extendable.

--- partner_multi_relation/models/res_partner_relation_type_selection.py
+++ partner_multi_relation/models/res_partner_relation_type_selection.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2014-2016 Therp BV <http://therp.nl>
+# Copyright 2013-2017 Therp BV <http://therp.nl>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 """
 For the model defined here _auto is set to False to prevent creating a
@@ -18,11 +18,6 @@ from psycopg2.extensions import AsIs
 from openerp import api, fields, models
 from openerp.tools import drop_view_if_exists
 
-from .res_partner_relation_type import ResPartnerRelationType
-
-
-PADDING = 10
-
 
 class ResPartnerRelationTypeSelection(models.Model):
     """Virtual relation types"""
@@ -33,13 +28,20 @@ class ResPartnerRelationTypeSelection(models.Model):
     _log_access = False
     _order = 'name asc'
 
+    @api.model
+    def get_partner_types(self):
+        """Partner types are defined by model res.partner.relation.type."""
+        # pylint: disable=no-self-use
+        rprt_model = self.env['res.partner.relation.type']
+        return rprt_model.get_partner_types()
+
     type_id = fields.Many2one(
         comodel_name='res.partner.relation.type',
         string='Type',
     )
     name = fields.Char('Name')
     contact_type_this = fields.Selection(
-        selection=ResPartnerRelationType.get_partner_types.im_func,
+        selection='get_partner_types',
         string='Current record\'s partner type',
     )
     is_inverse = fields.Boolean(
@@ -47,7 +49,7 @@ class ResPartnerRelationTypeSelection(models.Model):
         help="Inverse relations are from right to left partner.",
     )
     contact_type_other = fields.Selection(
-        selection=ResPartnerRelationType.get_partner_types.im_func,
+        selection='get_partner_types',
         string='Other record\'s partner type',
     )
     partner_category_this = fields.Many2one(
@@ -65,43 +67,68 @@ class ResPartnerRelationTypeSelection(models.Model):
         string='Symmetric',
     )
 
+    def _get_additional_view_fields(self):
+        """Allow inherit models to add fields to view.
+
+        If fields are added, the resulting string must have each field
+        prepended by a comma, like so:
+            return ', typ.allow_self, typ.left_partner_category'
+        """
+        return ''
+
+    def _get_additional_tables(self):
+        """Allow inherit models to add tables (JOIN's) to view.
+
+        Example:
+            return 'JOIN type_extention ext ON (bas.type_id = ext.id)'
+        """
+        return ''
+
     @api.model_cr_context
     def _auto_init(self):
         cr = self._cr
         drop_view_if_exists(cr, self._table)
         cr.execute(
-            """CREATE OR REPLACE VIEW %(table)s AS
-            SELECT
-                id * %(padding)s AS id,
-                id AS type_id,
-                name AS name,
-                False AS is_inverse,
-                contact_type_left AS contact_type_this,
-                contact_type_right AS contact_type_other,
-                partner_category_left AS partner_category_this,
-                partner_category_right AS partner_category_other,
-                allow_self,
-                is_symmetric
-            FROM %(underlying_table)s
-            UNION SELECT
-                id * %(padding)s + 1,
-                id,
-                name_inverse,
-                True,
-                contact_type_right,
-                contact_type_left,
-                partner_category_right,
-                partner_category_left,
-                allow_self,
-                is_symmetric
-             FROM %(underlying_table)s
-             WHERE not is_symmetric
+            """\
+CREATE OR REPLACE VIEW %(table)s AS
+     WITH selection_type AS (
+ SELECT
+     id * 2 AS id,
+     id AS type_id,
+     name AS name,
+     False AS is_inverse,
+     contact_type_left AS contact_type_this,
+     contact_type_right AS contact_type_other,
+     partner_category_left AS partner_category_this,
+     partner_category_right AS partner_category_other
+ FROM %(underlying_table)s
+ UNION SELECT
+     (id * 2) + 1,
+     id,
+     name_inverse,
+     True,
+     contact_type_right,
+     contact_type_left,
+     partner_category_right,
+     partner_category_left
+     FROM %(underlying_table)s
+     WHERE not is_symmetric
+ )
+ SELECT
+     bas.*,
+     typ.allow_self,
+     typ.is_symmetric
+     %(additional_view_fields)s
+ FROM selection_type bas
+ JOIN res_partner_relation_type typ ON (bas.type_id = typ.id)
+     %(additional_tables)s
             """,
-            {
-                'table': AsIs(self._table),
-                'padding': PADDING,
-                'underlying_table': AsIs('res_partner_relation_type'),
-            })
+            {'table': AsIs(self._table),
+             'underlying_table': AsIs('res_partner_relation_type'),
+             'additional_view_fields':
+                AsIs(self._get_additional_view_fields()),
+             'additional_tables':
+                 AsIs(self._get_additional_tables())})
         return super(ResPartnerRelationTypeSelection, self)._auto_init()
 
     @api.multi
@@ -111,18 +138,14 @@ class ResPartnerRelationTypeSelection(models.Model):
             (this.id,
              this.is_inverse and this.type_id.name_inverse or
              this.type_id.display_name)
-            for this in self
-        ]
+            for this in self]
 
     @api.model
     def name_search(self, name='', args=None, operator='ilike', limit=100):
         """Search for name or inverse name in underlying model."""
         # pylint: disable=no-value-for-parameter
         return self.search(
-            [
-                '|',
-                ('type_id.name', operator, name),
-                ('type_id.name_inverse', operator, name),
-            ] + (args or []),
-            limit=limit
-        ).name_get()
+            ['|',
+             ('type_id.name', operator, name),
+             ('type_id.name_inverse', operator, name)] + (args or []),
+            limit=limit).name_get()
