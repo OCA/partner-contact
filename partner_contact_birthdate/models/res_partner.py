a5da70fff51916823588c0b75319fab099441946
Jean-Paul Robineau <jean-paul.robineau@apertoso.be>
2017-10-31 11:42:55 +0100

    [MIG] partner_contact_birthdate: Migration to 11.0

--- partner_contact_birthdate/models/res_partner.py
+++ partner_contact_birthdate/models/res_partner.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2014-2015  Grupo ESOC <www.grupoesoc.es>
+# © 2017-Apertoso N.V. (<http://www.apertoso.be>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from odoo import api, fields, models
 import logging
