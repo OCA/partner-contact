2d274073d34cccb9c0d7ea86d3c11033432da4ff
Jean-Paul Robineau <jean-paul.robineau@apertoso.be>
2017-10-31 11:42:55 +0100

    [MIG] partner_contact_birthdate: Migration to 11.0

--- partner_contact_birthdate/models/__init__.py
+++ partner_contact_birthdate/models/__init__.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
 # Copyright 2014-2015  Grupo ESOC <www.grupoesoc.es>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# © 2017-Apertoso N.V. (<http://www.apertoso.be>)
+#  License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import res_partner
