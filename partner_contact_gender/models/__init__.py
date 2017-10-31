7cfdc45d46cc24998c6cb838971e273c9ec73de8
Jean-Paul Robineau <jean-paul.robineau@apertoso.be>
2017-10-31 11:53:26 +0100

    [MIG] partner_contact_gender: Migration to 11.0

--- partner_contact_gender/models/__init__.py
+++ partner_contact_gender/models/__init__.py
@@ -1,5 +1,2 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2014-2015  Grupo ESOC <www.grupoesoc.es>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from . import res_partner
