8decd6ace67e155f285515a9682bd5f37ed7971b
Jean-Paul Robineau <jean-paul.robineau@apertoso.be>
2017-10-31 11:53:26 +0100

    [MIG] partner_contact_gender: Migration to 11.0
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>
    
    [UPD] Update partner_contact_gender.pot
    
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

--- partner_contact_gender/__init__.py
+++ partner_contact_gender/__init__.py
@@ -1,6 +1,3 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2014-2015  Grupo ESOC <www.grupoesoc.es>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from . import models
 from .hooks import post_init_hook
