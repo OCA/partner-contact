a6f720c51bf8df428f31c02ebbbf2319c64e6c7e
Dave Burkholder <dave@thinkwelldesigns.com>
2017-11-20 04:21:47 -0500

    [10.0][FIX] Added is_company field for the domain constraint (#520)

--- partner_contact_job_position/__manifest__.py
+++ partner_contact_job_position/__manifest__.py
@@ -8,7 +8,7 @@
 {
     "name": "Partner job position",
     "summary": "Categorize job positions for contacts",
-    "version": "10.0.1.0.0",
+    "version": "10.0.1.0.1",
     'category': 'Customer Relationship Management',
     "website": "http://www.antiun.com",
     'author': 'Serv. Tecnolog. Avanzados - Pedro M. Baeza, '
