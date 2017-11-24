dd79ed35903bf7e55d1da59b3b0dd24d348c5ac4
denislour <denislour@gmail.com>
2017-11-24 16:34:29 +0700

    Add the copyrights.

--- partner_email_check/__manifest__.py
+++ partner_email_check/__manifest__.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
+# Copyright 2017 Komit <http://komit-consulting.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 {
     'name': 'Email Format Checker',
     'version': '10.0.1.0.0',
     'summary': 'Validate email address field',
-    'author': 'Odoo Community Association (OCA)',
-    'website': 'http://www.komit-consulting.com'
-    'category' 'Tools',
+    'author': "Komit, Odoo Community Association (OCA)",
+    'website': 'http://komit-consulting.com',
+    'category': 'Tools',
     'depends': ['base'],
     'installable': True,
     'application': False,
--- partner_email_check/tests/test_partner_email_check.py
+++ partner_email_check/tests/test_partner_email_check.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+# Copyright 2017 Komit <http://komit-consulting.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 from odoo.exceptions import ValidationError
 from odoo.tests.common import TransactionCase
 
