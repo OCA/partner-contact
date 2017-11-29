7bee24ddf72ed853d631ce57c27677b83a374e99
Simone Orsi <simahawk@gmail.com>
2017-11-29 15:13:08 +0100

    [11.0][MIG] partner_identification

--- partner_identification/models/res_partner.py
+++ partner_identification/models/res_partner.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 #
-# © 2004-2010 Tiny SPRL http://tiny.be
-# © 2010-2012 ChriCar Beteiligungs- und Beratungs- GmbH
+# Copyright 2004-2010 Tiny SPRL http://tiny.be
+# Copyright 2010-2012 ChriCar Beteiligungs- und Beratungs- GmbH
 #             http://www.camptocamp.at
-# © 2015 Antiun Ingenieria, SL (Madrid, Spain)
+# Copyright 2015 Antiun Ingenieria, SL (Madrid, Spain)
 #        http://www.antiun.com
 #        Antonio Espinosa <antonioea@antiun.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
