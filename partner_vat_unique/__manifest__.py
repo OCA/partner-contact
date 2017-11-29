5cafb656ccf591e01c5f8c61c6c4a6dfcbea147d
Ismael Calvo <ismael.calvo@gmail.com>
2017-11-29 12:23:18 +0100

    [ADD] partner_vat_unique, check unique VAT number

---
+++ partner_vat_unique/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Grant Thornton Spain - Ismael Calvo <ismael.calvo@es.gt.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Partner VAT Unique",
+    "version": "10.0.1.0.0",
+    "category": "Customer Relationship Management",
+    "website": "https://github.com/OCA/partner-contact",
+    "author": "Grant Thornton S.L.P, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": True,
+    "installable": True,
+    "depends": ["base"],
+}
