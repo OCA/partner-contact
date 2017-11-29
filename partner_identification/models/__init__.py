21668007e8eaaddc8ccfb97cd4e56dafa4e168c0
Simone Orsi <simahawk@gmail.com>
2017-11-29 16:11:17 +0100

    partner_identification: fix/imp tests on res.partner

--- partner_identification/models/__init__.py
+++ partner_identification/models/__init__.py
@@ -1,6 +1,3 @@
-# -*- coding: utf-8 -*-
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
 from . import res_partner_id_number
 from . import res_partner_id_category
 from . import res_partner
