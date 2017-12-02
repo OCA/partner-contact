4150faac9584bbeb46db297f8bc94b4b597c04fb
Nicolas JEUDY <nicolas@sudokeys.com>
2017-12-02 11:50:28 +0100

    [MIG] V11 migration
    - fax field removed from res.partner
    - 2to3 on all py files
    - add contributor and update README
    - use hasclass in place of @class in xpath expr
    - improve tests
    
    [FIX] typo and clean README

--- partner_contact_in_several_companies/__manifest__.py
+++ partner_contact_in_several_companies/__manifest__.py
@@ -4,14 +4,11 @@
 {
     "name": "Contacts in several partners",
     "summary": "Allow to have one contact in several partners",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "category": "Customer Relationship Management",
-    "website": "https://odoo-community.org/",
-    "author": "Odoo Community Association (OCA),Odoo SA",
+    "website": "https://github.com/OCA/partner-contact",
+    "author": "Nicolas JEUDY,Odoo Community Association (OCA),Odoo SA",
     "license": "AGPL-3",
-    'application': False,
-    'installable': True,
-    'auto_install': False,
     "depends": [
         "base",
         "partner_contact_personal_information_page",
