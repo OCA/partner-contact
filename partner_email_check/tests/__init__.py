946700031405d118b17fd4bdfcfd81184033ce18
denislour <denislour@gmail.com>
2017-12-18 18:31:01 +0700

    Add new module partner-email-check (#496)
    * Add new module partner-email-check
    Validate email input

---
+++ partner_email_check/tests/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Komit <http://komit-consulting.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import test_partner_email_check
