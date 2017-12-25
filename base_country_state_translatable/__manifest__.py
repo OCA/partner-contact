3d688b086055fa5e1027549c0cbdad53daa4353e
Yoshi Tashiro <tashiro@quartile.co>
2017-12-25 16:09:19 +0000

    [MIG] base_country_state_translatable: Migration to 11.0
    remove unnecessary lines, minor adjustments

--- base_country_state_translatable/__manifest__.py
+++ base_country_state_translatable/__manifest__.py
@@ -1,17 +1,13 @@
-# -*- coding: utf-8 -*-
 # © initOS GmbH 2017
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Translate Country States",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "depends": ['base',
                 ],
-    'application': False,
     'author': 'initOS GmbH, Odoo Community Association (OCA)',
-    "category": "Localisation/Europe",
+    "category": "Localisation",
     'license': 'AGPL-3',
-    'data': [],
     'installable': True,
-    'auto_install': False,
 }
