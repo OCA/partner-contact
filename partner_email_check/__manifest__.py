9f2dc259c865d710e2d4b0d6226c8d5343fd2ed5
huy <huynguyenpuf>
2018-01-04 10:00:10 +0700

    Migrate module partner_email_check to version 11.0

--- partner_email_check/__manifest__.py
+++ partner_email_check/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Komit <http://komit-consulting.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Email Format Checker',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'summary': 'Validate email address field',
     'author': "Komit, Odoo Community Association (OCA)",
     'website': 'http://komit-consulting.com',
