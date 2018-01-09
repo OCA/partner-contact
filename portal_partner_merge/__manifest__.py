2b4fabc57ed8219a40d38c03450db1bdc0850e97
Jay Vora(SerpentCS) <jay.vora@serpentcs.com>
2018-01-09 19:33:25 +0530

    [MIG] Migrated portal_partner_merge to 11.0

--- portal_partner_merge/__manifest__.py
+++ portal_partner_merge/__manifest__.py
@@ -1,36 +1,15 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Author: Yannick Vaucher
-#    Copyright 2013 Camptocamp SA
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# See LICENSE file for full copyright and licensing details.
 {'name': 'Portal Partner Merge',
- 'version': '8.0.1.0.0',
+ 'version': '11.0.1.0.0',
  'category': 'Hidden',
- 'description': """
-Link module for base_partner_merge which extract portal dependency
- """,
  'author': "Camptocamp,Odoo Community Association (OCA)",
  'maintainer': 'Camptocamp',
+ 'license': 'AGPL-3',
  'website': 'http://www.camptocamp.com/',
  'depends': ['portal', 'base_partner_merge'],
  'data': [],
  'test': [],
- 'installable': False,
- 'auto_install': True,
+ 'installable': True,
+ 'auto_install': False,
  'application': False,
  }
