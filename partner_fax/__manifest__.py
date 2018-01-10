fad51a3811a434cbb0f11a9eaf4ceef60e4bf203
Francesco Apruzzese <cescoap@gmail.com>
2018-01-10 15:52:10 +0100

    [ADD] partner_fax

---
+++ partner_fax/__manifest__.py
@@ -0,0 +1,20 @@
+# © 2018 Apruzzese Francesco <f.apruzzese@apuliasoftware.it>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Partner fax',
+    'category': 'Extra Tools',
+    'summary': 'Add fax number on partner',
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Francesco Apruzzese, '
+              'Odoo Community Association (OCA)',
+    'website': 'https://odoo-community.org/',
+    'depends': [
+        'base_setup'
+        ],
+    'data': [
+        'views/res_partner.xml',
+        ],
+    'installable': True,
+}
