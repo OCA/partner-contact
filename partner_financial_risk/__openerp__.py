a0fa8591110d867fd1438078e3ef4caebe0abd89
Jairo Llopis <Yajo@users.noreply.github.com>
2018-02-05 09:56:52 +0000

    [IMP] Increase version number
    Done as requested in https://github.com/OCA/partner-contact/pull/530#issuecomment-361909132, to reflect those changes

--- partner_financial_risk/__openerp__.py
+++ partner_financial_risk/__openerp__.py
@@ -5,7 +5,7 @@
 {
     'name': 'Partner Financial Risk',
     'summary': 'Manage partner risk',
-    'version': '9.0.2.0.1',
+    'version': '9.0.2.1.0',
     'category': 'Sales Management',
     'license': 'AGPL-3',
     'author': 'Tecnativa, Odoo Community Association (OCA)',
