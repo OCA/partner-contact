d284e22171e8216e97636e5b5c852d62e12a60d0
Alexis de Lattre <alexis.delattre@akretion.com>
2018-02-20 00:28:10 +0100

    Update to latest OCA coding conventions

--- partner_disable_gravatar/models/partner.py
+++ partner_disable_gravatar/models/partner.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2018 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2018 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import models
