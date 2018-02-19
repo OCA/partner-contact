f9ecfde734d6b8b166c50c594280b94906588c4e
Alexis de Lattre <alexis.delattre@akretion.com>
2018-02-20 00:28:10 +0100

    Update to latest OCA coding conventions

--- partner_disable_gravatar/__manifest__.py
+++ partner_disable_gravatar/__manifest__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2018 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2018 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
@@ -9,7 +9,7 @@
     'license': 'AGPL-3',
     'summary': 'Disable automatic connection to gravatar.com',
     'author': 'Akretion,Odoo Community Association (OCA)',
-    'website': 'https://odoo-community.org',
+    'website': 'https://github.com/OCA/partner-contact',
     'depends': ['base'],
     'installable': True,
 }
