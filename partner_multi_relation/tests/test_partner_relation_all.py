3ddad44d07c1749a8daa7144748b6aba5cdc157a
David Dufresne <dufresne.david.2@gmail.com>
2018-02-26 17:07:23 -0500

    [MIG] partner_multi_relation: Migration to 11.0
    * Update README
    * Python 2to3
    * Move relation menus items under contacts
    * Fix date comparison
    * Fix cache invalidate on relation write
    * Bump module version
    * Remove test_relation_type_unlink_dberror
    
      This test is not required because it tests a standard behavior of SQL.
      If a foreign key column is not nullable, then the foreign row can not be deleted.
    
    * Remove utf8 encoding comment
    * Add missing dependency for partner_multi_relation
    * Add missing active_test in view action

--- partner_multi_relation/tests/test_partner_relation_all.py
+++ partner_multi_relation/tests/test_partner_relation_all.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016-2017 Therp BV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from openerp.exceptions import ValidationError
--- partner_multi_relation/tests/test_partner_search.py
+++ partner_multi_relation/tests/test_partner_search.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Camptocamp SA
 # Copyright 2016 Therp BV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
