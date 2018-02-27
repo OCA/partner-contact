209488efe93433e57192eacc331b78bf8da5dcfd
Huy Ly <HuyLy@Super.local>
2018-02-27 14:22:54 +0700

    [MIG] partner_address_street3 Migration to 11.0

--- partner_address_street3/model/country.py
+++ partner_address_street3/model/country.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
 # © 2014-2016 Camptocamp SA
 # @author: Nicolas Bessi
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ResCountry(models.Model):
