d3dcf9ae0e8e3ce8494c4ac70a5eb78d2c531f67
Ernesto Tejeda <ernesto.tejeda87@gmail.com>
2018-04-12 17:20:04 -0400

    [MIG] crm_deduplicate_filter: Migration to 11.0
    [UPD] Update crm_deduplicate_filter.pot

--- partner_deduplicate_filter/__manifest__.py
+++ partner_deduplicate_filter/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # Copyright 2017 Vicent Cubells <vicent.cubells@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Exclude records from the deduplication",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "category": "Tools",
     "website": "https://www.tecnativa.com",
     "author": "Tecnativa, "
