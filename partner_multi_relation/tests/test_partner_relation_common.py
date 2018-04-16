2403e8d6b4fef9e3b4068c2824e4d56dae4b8898
David Dufresne <dufresne.david.2@gmail.com>
2018-04-16 15:52:24 -0400

    [MIG] partner_multi_relation: Continue migration to 11.0
    * Bug Fix
    * Enable additional columns on res_partner_relation_all.
    
      Enable to add additional columns from res_partner_relation in the sql view of res_partner_relation_all.
    
    * Only require inverse_name when relation type is not symmetric
    * Remove deprecated @api.one and replace openerp with odoo
    * Add website to manifest
    * Prevent disabling allow_self if reflexive relations exist
    * Enable deleting/ending invalid relations after deactivating reflexivity.
    * Handle case of invalid future reflexive relations

--- partner_multi_relation/tests/test_partner_relation_common.py
+++ partner_multi_relation/tests/test_partner_relation_common.py
@@ -1,6 +1,6 @@
 # Copyright 2016 Therp BV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from openerp.tests import common
+from odoo.tests import common
 
 
 class TestPartnerRelationCommon(common.TransactionCase):
