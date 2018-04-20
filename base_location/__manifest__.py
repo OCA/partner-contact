76e77c0020886c3819fc389893d8f05b1430746d
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-04-20 06:48:13 +0200

    [IMP] base_location: Include onchange for state
    Incredibly not included in Odoo core.

--- base_location/__manifest__.py
+++ base_location/__manifest__.py
@@ -1,9 +1,10 @@
 # Copyright 2016 Nicolas Bessi, Camptocamp SA
+# Copyright 2018 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Location management (aka Better ZIP)',
-    'version': '11.0.1.0.0',
+    'version': '11.0.1.0.1',
     'depends': [
         'base_address_city'
     ],
