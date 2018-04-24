1a029bc93feebc5de65c409bd14a1cc6c24427a4
Raúl Martín <raul.martin@braintec-group.com>
2018-04-24 08:12:59 +0200

    [MIG][11.0] Migration of partner_affiliate to v11 (#579)
    * [MIG] Migration of partner_affiliate to v11
    
    * Fixing use of @class and do not inheriting address from parent company
    
    * Improving legibility and details of fields
    
    * Fixing travis errors
    
    remove obsolete .pot files [ci skip]
    
    [UPD] Update partner_affiliate.pot

---
+++ partner_affiliate/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2012 Camptocamp SA - Yannick Vaucher
+# Copyright 2017 Tecnativa - Vicent Cubells
+# Copyright 2018 brain-tec AG - Raul Martin
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Partner Affiliates',
+    'version': '11.0.1.0.0',
+    'author': "Camptocamp, "
+              "Tecnativa, "
+              "brain-tec AG, "
+              "Odoo Community Association (OCA)",
+    'website': 'http://www.camptocamp.com',
+    'category': 'CRM',
+    'license': 'AGPL-3',
+    'installable': True,
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'views/res_partner_view.xml',
+    ],
+}
