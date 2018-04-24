1323261afcc8ec03750d51cc6f6d4498fc4a040b
Raúl Martín <raul.martin@braintec-group.com>
2018-04-24 08:12:59 +0200

    [MIG][11.0] Migration of partner_affiliate to v11 (#579)
    * [MIG] Migration of partner_affiliate to v11
    
    * Fixing use of @class and do not inheriting address from parent company
    
    * Improving legibility and details of fields
    
    * Fixing travis errors
    
    remove obsolete .pot files [ci skip]
    
    [UPD] Update partner_affiliate.pot

---
+++ partner_affiliate/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright 2012 Camptocamp SA - Yannick Vaucher
+# Copyright 2018 brain-tec AG - Raul Martin
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import models
