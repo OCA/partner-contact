886405b3a51bdee2f906cc3558494a8202c5b5af
David <david.vidal@tecnativa.com>
2018-07-12 15:49:32 +0200

    [MIG] partner_phonecall_schedule: Migration to 11.0

--- partner_phonecall_schedule/__manifest__.py
+++ partner_phonecall_schedule/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Jairo Llopis <jairo.llopis@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Partner phonecalls schedule",
     "summary": "Track the time and days your partners expect phone calls",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "category": "Customer Relationship Management",
     "website": "https://github.com/OCA/crm",
     "author": "Tecnativa, Odoo Community Association (OCA)",
