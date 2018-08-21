270521bffc933f6c8206fb58592ff499752cbdc6
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2018-08-21 15:34:09 +0530

    Search Partner by Phone & Mobile & Email

---
+++ partner_phone_search/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2018 - TODAY Serpent Consulting Services Pvt. Ltd.
+#   (<http://www.serpentcs.com>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Search Partner Phone/Mobile/Email',
+    'version': '11.0.1.0.0',
+    'category': 'Extra Tools',
+    'summary': 'Partner Search by Phone/Mobile/Email',
+    'author': "Serpent Consulting Services Pvt. Ltd.,"
+                "Odoo Community Association (OCA)",
+    'website': 'https://github.com/OCA/partner-contact',
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
