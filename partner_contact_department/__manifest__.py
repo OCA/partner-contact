a1b082d9206abe1a6f05d7abb74ca8ba37db971a
mreficent <miquel.raich@eficent.com>
2018-08-29 12:40:20 +0200

    partner_contact_department: Migration to 11.0

--- partner_contact_department/__manifest__.py
+++ partner_contact_department/__manifest__.py
@@ -1,13 +1,12 @@
-# -*- coding: utf-8 -*-
 # Copyright 2014-2015 Tecnativa S.L. - Jairo Llopis
 # Copyright 2016 Tecnativa S.L. - Vicent Cubells
 # Copyright 2017 Tecnativa S.L. - David Vidal
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Contact department',
+    'name': 'Partner Contact Department',
     "summary": "Assign contacts to departments",
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Customer Relationship Management',
     'author': 'Tecnativa, '
               'Odoo Community Association (OCA)',
@@ -15,7 +14,7 @@
     'website': 'http://www.tecnativa.com',
     "application": False,
     'depends': [
-        'sales_team',
+        'contacts',
     ],
     'data': [
         'security/ir.model.access.csv',
