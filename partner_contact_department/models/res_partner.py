ac1340d03e43f0dc7aef3577adf1eb1b5bae4561
mreficent <miquel.raich@eficent.com>
2018-08-29 12:40:20 +0200

    partner_contact_department: Migration to 11.0

--- partner_contact_department/models/res_partner.py
+++ partner_contact_department/models/res_partner.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
 # © 2014-2015 Tecnativa S.L. - Jairo Llopis
 # © 2016 Tecnativa S.L. - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields, api, _
+from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError
 
 
