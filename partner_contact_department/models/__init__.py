9b3474e0dd2296c498472a85ecfa3517d4d9e6b1
mreficent <miquel.raich@eficent.com>
2018-08-29 12:40:20 +0200

    partner_contact_department: Migration to 11.0

--- partner_contact_department/models/__init__.py
+++ partner_contact_department/models/__init__.py
@@ -1,6 +1,3 @@
-# -*- coding: utf-8 -*-
-# © 2014-2015 Tecnativa S.L. - Jairo Llopis
-# © 2016 Tecnativa S.L. - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import res_partner
---
+++ partner_contact_department/readme/USAGE.rst
@@ -0,0 +1,4 @@
+To use this module, you need to:
+
+* Go to any partner's form.
+* You will find the new *Department* field below *Job Position*.
