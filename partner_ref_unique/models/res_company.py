6220d0009c865013eae035c8734b1b05a84cab51
Cris Martín <cristina.marrod@gmail.com>
2018-09-17 10:40:23 +0200

    [11.0][MIG] partner_ref_unique: Migrate to v11

--- partner_ref_unique/models/res_company.py
+++ partner_ref_unique/models/res_company.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
-# Copyright 2016 Antiun Ingenieria S.L. - Antonio Espinosa
+# Copyright 2016 Antonio Espinosa
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields
+from odoo import fields, models
 
 
 class ResCompany(models.Model):
