0839f0c8d3ad9170dc759d282508fd8bb8196545
Cris Martín <cristina.marrod@gmail.com>
2018-09-17 10:40:23 +0200

    [11.0][MIG] partner_ref_unique: Migrate to v11

--- partner_ref_unique/models/__init__.py
+++ partner_ref_unique/models/__init__.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-# Copyright 2016 Antiun Ingenieria S.L. - Antonio Espinosa
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import res_company
---
+++ partner_ref_unique/readme/CONTRIBUTORS.rst
@@ -0,0 +1,10 @@
+
+
+* `Tecnativa <https://www.tecnativa.com>`__:
+
+  * Antonio Espinosa
+  * Pedro M. Baeza
+  * Rafael Blasco
+  * Jairo Llopis
+  * Vicent Cubells
+  * Cristina Martín
