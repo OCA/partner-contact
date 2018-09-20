b9ef4b589c77bbccf371bf17cdb8feb57ece4f15
SimoRubi <simone.rubino@agilebg.com>
2018-09-20 18:14:02 +0200

    [UPD] base_location_nuts: update based on flag

--- base_location_nuts/__manifest__.py
+++ base_location_nuts/__manifest__.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Copyright 2015 Antonio Espinosa <antonio.espinosa@tecnativa.com>
 # Copyright 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
 # Copyright 2017 David Vidal <david.vidal@tecnativa.com>
@@ -6,7 +7,7 @@
 {
     'name': 'NUTS Regions',
     'category': 'Localisation/Europe',
-    'version': '11.0.1.0.0',
+    'version': '11.0.1.1.0',
     'depends': [
         'contacts',
     ],
@@ -21,6 +22,7 @@
         'images/new_fields.png',
     ],
     'author': 'Tecnativa, '
+              'Agile Business Group, '
               'Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/partner-contact/',
     'license': 'AGPL-3',
