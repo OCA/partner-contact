7a744ec693d400af3f1b5c114be723e47f8fa87c
David <david.vidal@tecnativa.com>
2018-09-26 13:20:56 +0200

    [MIG] crm_deduplicate_by_ref: Migration to 11.0

--- partner_deduplicate_by_ref/__manifest__.py
+++ partner_deduplicate_by_ref/__manifest__.py
@@ -1,18 +1,15 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
 {
     "name": "Deduplicate Contacts by reference",
-    "version": "9.0.1.0.0",
+    "version": "11.0.1.0.0",
     "category": "Tools",
-    "website": "https://www.tecnativa.com",
+    "website": "https://www.github.com/OCA/crm",
     "author": "Tecnativa, "
               "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "installable": True,
     "depends": [
-        "crm",
         "crm_deduplicate_acl",
     ],
     "data": [
