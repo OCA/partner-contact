37402d1a6ca376bedabc4e2d6976960615414e28
David <david.vidal@tecnativa.com>
2018-09-27 18:11:00 +0200

    [ADD] portal_partner_select_all: New module

---
+++ portal_partner_select_all/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Tecnativa S.L. - David Vidal
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Portal Partner Select All',
+    'version': '10.0.1.0.1',
+    'category': 'Custom',
+    'author': 'Tecnativa,'
+              'Odoo Community Association (OCA)',
+    'website': 'https://www.github.com/OCA/partner-contact',
+    "license": "AGPL-3",
+    'depends': [
+        'portal',
+    ],
+    'data': [
+        'wizard/portal_wizard.xml',
+    ],
+    'installable': True,
+}
