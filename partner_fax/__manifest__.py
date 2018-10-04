fc4db63be4a1a3fd8dfd198d611fe71c7fbe9c3d
Aitor Bouzas <bouzasnaveira@gmail.com>
2018-10-04 17:19:35 +0200

    [MIG] partner_fax: Migration to 12.0
    Code improvements

--- partner_fax/__manifest__.py
+++ partner_fax/__manifest__.py
@@ -1,15 +1,14 @@
-# © 2018 Apruzzese Francesco <f.apruzzese@apuliasoftware.it>
+# Copyright 2018 Apruzzese Francesco <f.apruzzese@apuliasoftware.it>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Partner fax',
     'category': 'Extra Tools',
     'summary': 'Add fax number on partner',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'license': 'AGPL-3',
-    'author': 'Francesco Apruzzese, '
-              'Odoo Community Association (OCA)',
-    'website': 'https://odoo-community.org/',
+    'author':  'Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/partner-contact',
     'depends': [
         'base_setup'
         ],
