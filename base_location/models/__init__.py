f24dab823a09913df3a94489956c6ef5a957115d
Aitor Bouzas <bouzasnaveira@gmail.com>
2018-10-08 17:04:28 +0200

    [MIG] base_location: Migration to 12.0
    This module has now been refactored to be more consistent with what base_address_city offers to the location management.
    Added dependency to contacts so that I could change the menu location for cities / zip management.
    
    Now, every res.city record has a relation One2many to res.city.zip (old res.better.zip). This way, every zip has a realted city too.
    Zips can be searched through city code, zip or city name (same as before).
    
    Modified tests and deleted not needed tests.
    
    Added sql contraints so that zips and cities are unique within it's country / state / city.

--- base_location/models/__init__.py
+++ base_location/models/__init__.py
@@ -1,7 +1,6 @@
-# Copyright 2016 Nicolas Bessi, Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import better_zip
-from . import partner
-from . import state
-from . import company
+from . import res_city_zip
+from . import res_partner
+from . import res_company
+from . import res_city
