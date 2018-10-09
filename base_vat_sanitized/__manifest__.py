2981b30999d303a1b1434e2a9e85c16a8c79f95a
Andrea <a.stirpe@onestein.nl>
2018-10-09 09:43:17 +0200

    [12.0][MIG] base_vat_sanitized
    [UPD] README.rst
    
    [UPD] Update base_vat_sanitized.pot
    
    Translated using Weblate (Portuguese (Brazil))
    
    Currently translated at 66.7% (2 of 3 strings)
    
    Translation: partner-contact-12.0/partner-contact-12.0-base_vat_sanitized
    Translate-URL: https://translation.odoo-community.org/projects/partner-contact-12-0/partner-contact-12-0-base_vat_sanitized/pt_BR/
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: partner-contact-12.0/partner-contact-12.0-base_vat_sanitized
    Translate-URL: https://translation.odoo-community.org/projects/partner-contact-12-0/partner-contact-12-0-base_vat_sanitized/

--- base_vat_sanitized/__manifest__.py
+++ base_vat_sanitized/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'Base VAT Sanitized',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Hidden/Dependency',
     'license': 'AGPL-3',
     'summary': 'Adds field sanitized_vat on partners',
---
+++ base_vat_sanitized/readme/CONTRIBUTORS.rst
@@ -0,0 +1,2 @@
+* Alexis de Lattre <alexis.delattre@akretion.com>
+* Andrea Stirpe <a.stirpe@onestein.nl>
