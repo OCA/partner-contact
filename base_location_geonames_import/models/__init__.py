3c3416e9568dc587f22d7dc923bd85fd255c280c
Aitor Bouzas <bouzasnaveira@gmail.com>
2018-10-16 16:13:56 +0200

    [MIG] base_location_geonames_import: Migration to 12.0 Generated new readme. Adapted the module due to the refactoring of base_location. Adapted tests.

--- base_location_geonames_import/models/__init__.py
+++ base_location_geonames_import/models/__init__.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 Franco Tampieri, Freelancer http://franco.tampieri.info
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import res_country
