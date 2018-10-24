0428071c5f91731440f21a27a971240f8ab5ff47
tarteo <d.sluijk@onestein.nl>
2018-10-24 15:46:33 +0200

    [MIG] partner_affiliate: Migration to 12.0
    [IMP] partner_affiliate: Readme
    
    [IMP] Small readability improvements
    
    [FIX] Remove copyright from init files
    
    [UPD] README.rst
    
    [UPD] Update partner_affiliate.pot
    
    Update translation files
    
    Updated by Update PO files to match POT (msgmerge) hook in Weblate.
    
    [UPD] Update partner_affiliate.pot
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: partner-contact-12.0/partner-contact-12.0-partner_affiliate
    Translate-URL: https://translation.odoo-community.org/projects/partner-contact-12-0/partner-contact-12-0-partner_affiliate/

--- partner_affiliate/__manifest__.py
+++ partner_affiliate/__manifest__.py
@@ -1,17 +1,17 @@
-# -*- coding: utf-8 -*-
 # Copyright 2012 Camptocamp SA - Yannick Vaucher
 # Copyright 2017 Tecnativa - Vicent Cubells
 # Copyright 2018 brain-tec AG - Raul Martin
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Partner Affiliates',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'author': "Camptocamp, "
               "Tecnativa, "
               "brain-tec AG, "
+              "Onestein, "
               "Odoo Community Association (OCA)",
-    'website': 'http://www.camptocamp.com',
-    'category': 'CRM',
+    'website': 'https://github.com/OCA/partner-contact',
+    'category': 'Customer Relationship Management',
     'license': 'AGPL-3',
     'installable': True,
     'depends': [
@@ -20,4 +20,5 @@
     'data': [
         'views/res_partner_view.xml',
     ],
+    'development_status': 'Production/Stable',
 }
