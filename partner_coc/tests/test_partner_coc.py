2a293b4deabb4f78ffe06e364821fc6d294b18f6
tarteo <d.sluijk@onestein.nl>
2018-10-24 12:44:26 +0200

    [MIG] partner_coc: Migration to 12.0

--- partner_coc/tests/test_partner_coc.py
+++ partner_coc/tests/test_partner_coc.py
@@ -1,7 +1,7 @@
 # Copyright 2018 Onestein (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from openerp.tests.common import TransactionCase
+from odoo.tests.common import TransactionCase
 
 
 class TestPartnerCoC(TransactionCase):
