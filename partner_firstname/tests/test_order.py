bd2b255921ce9f23ae642f9398c691879b4f71c7
Graeme Gellatly <gdgellatly@gmail.com>
2018-10-30 00:59:38 +1300

    Migrate to V12

--- partner_firstname/tests/test_order.py
+++ partner_firstname/tests/test_order.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# © 2015 Antiun Ingenieria S.L. - Antonio Espinosa
+# Copyright 2015 Antiun Ingenieria S.L. - Antonio Espinosa
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo.tests.common import TransactionCase
