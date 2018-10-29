96761e137843b2832ed557471d31a05b6e5464f2
Graeme Gellatly <gdgellatly@gmail.com>
2018-10-30 00:59:38 +1300

    Migrate to V12

--- partner_firstname/tests/test_delete.py
+++ partner_firstname/tests/test_delete.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# © 2015 Grupo ESOC
+# Copyright 2015 Grupo ESOC
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo.tests.common import TransactionCase
