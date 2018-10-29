2ef4e85057ff1f41e67b96c4d1a4d67e620061f5
Graeme Gellatly <gdgellatly@gmail.com>
2018-10-30 00:59:38 +1300

    Migrate to V12

--- partner_firstname/__init__.py
+++ partner_firstname/__init__.py
@@ -1,6 +1,2 @@
-# -*- coding: utf-8 -*-
-# © 2013 Nicolas Bessi (Camptocamp SA)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
 from . import models
 from .hooks import post_init_hook
