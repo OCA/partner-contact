09220aeb1c399f464c8cead6a855d465cbeecba6
David <dajuayen@gmail.com>
2018-11-02 12:55:32 +0100

    [REM] Rename module.

--- partner_children_navigation/__manifest__.py
+++ partner_children_navigation/__manifest__.py
@@ -2,14 +2,12 @@
 # © 2018 David Juaneda <djuaneda@sdi.es>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': "SDi-CRM: nav to contacts",
+    'name': "Navigate to a partner's contacts",
     'version': '11.0.1.0.0',
     'development_status': 'Beta',
     'category': 'Partners',
     'website': 'https://github.com/OCA/social',
     'author': 'SDi, David Juaneda, Odoo Community Association (OCA)',
-    'summary': """  
-        Add navigation from customers to its contacts.""",
     'license': 'AGPL-3',
     'depends': [
         'contacts',
@@ -17,5 +15,5 @@
     'data': [
         'views/inherit_res_partner_views.xml',
     ],
-    'installable':True,
+    'installable': True,
 }
