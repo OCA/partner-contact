eadb06f55ab19e01c0e21bffd10b0dc207ee773d
ernesto <ernesto.tejeda87@gmail.com>
2018-11-12 19:41:50 -0500

    [MIG] partner_external_map: Migration to 12.0

--- partner_external_map/__manifest__.py
+++ partner_external_map/__manifest__.py
@@ -1,11 +1,11 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015-2016 Akretion - Alexis de Lattre
 # Copyright 2016-2017 Tecnativa - Pedro M. Baeza
+# Copyright 2018 Tecnativa - Ernesto Tejeda
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Partner External Maps',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Extra Tools',
     'license': 'AGPL-3',
     'summary': 'Add Map and Map Routing buttons on partner form to '
--- partner_external_map/models/res_users.py
+++ partner_external_map/models/res_users.py
@@ -1,6 +1,6 @@
-# -*- coding: utf-8 -*-
-# © 2015-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
-# © 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# Copyright 2015 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import models, fields, api
