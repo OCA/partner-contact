545ed23f7a6332afbe49e3f62286ec5c30cd4294
Ronald Portier <ronald@therp.nl>
2018-11-17 00:10:15 +0100

    [IMP] New module partner_multi_relation_tabs.

---
+++ partner_multi_relation_tabs/models/res_partner_relation_type_selection.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# Copyright 2013-2017 Therp BV <http://therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openerp import fields, models
+
+
+class ResPartnerRelationTypeSelection(models.Model):
+    """Virtual relation types"""
+    _inherit = 'res.partner.relation.type.selection'
+
+    tab_id = fields.Many2one(
+        comodel_name='res.partner.tab',
+        string='Show relation on tab',
+        readonly=True,
+    )
+
+    def _get_additional_view_fields(self):
+        """Add tab_id to fields in view."""
+        return ','.join([
+            super(ResPartnerRelationTypeSelection, self)
+            ._get_additional_view_fields(),
+            "CASE"
+            "    WHEN NOT bas.is_inverse"
+            "    THEN lefttab.id"
+            "    ELSE righttab.id"
+            " END as tab_id"])
+
+    def _get_additional_tables(self):
+        """Add two links to res_partner_tab."""
+        return ' '.join([
+            super(ResPartnerRelationTypeSelection, self)
+            ._get_additional_tables(),
+            "LEFT OUTER JOIN res_partner_tab lefttab"
+            " ON typ.tab_left_id = lefttab.id",
+            "LEFT OUTER JOIN res_partner_tab righttab"
+            " ON typ.tab_right_id = righttab.id"])
