ed5fa7e1d2c0077b1537e2e3552de7769ca039f5
Ronald Portier <ronald@therp.nl>
2018-11-24 18:46:35 +0100

    [IMP] partner_multi_relation_tabs. Forward to 100% coverage.

--- partner_multi_relation_tabs/models/res_partner.py
+++ partner_multi_relation_tabs/models/res_partner.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2014-2018 Therp BV <https://therp.nl>
+# Copyright 2014-2018 Therp BV <https://therp.nl>.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 # pylint: disable=no-member
 import logging
