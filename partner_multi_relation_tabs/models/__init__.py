6bda18d46ae997383f11d768969e709e1e75621f
Ronald Portier <ronald@therp.nl>
2018-11-26 07:58:14 +0100

    [IMP] partner_multi_relation_tabs. 11.0 migration.

--- partner_multi_relation_tabs/models/__init__.py
+++ partner_multi_relation_tabs/models/__init__.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-# Copyright 2014-2017 Therp BV <https://therp.nl>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from . import res_partner_tab
 from . import res_partner_relation_type
