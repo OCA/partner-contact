1129743935e5caba931a5d98910b5451192729d8
Quentin Groulard <quentin.groulard@acsone.eu>
2018-12-10 17:25:52 +0100

    [MIG] Base Partner Sequence: Migration to 12.0
    Bump module version to 12.0.1.0.0
    
    Use README by fragment system
    
    Remove copyrights
    
    Update website
    
    Add development_status

--- base_partner_sequence/__manifest__.py
+++ base_partner_sequence/__manifest__.py
@@ -4,15 +4,18 @@
 # Copyright 2016 Camptocamp - Akim Juillerat (<http://www.camptocamp.com>).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    "author": "Tiny/initOS GmbH & Co. KG,"
-              "ACSONE SA/NV,"
-              "Tecnativa, "
-              "Odoo Community Association (OCA), "
-              "Camptocamp, ",
+    "author": (
+        "Tiny/initOS GmbH & Co. KG,"
+        "ACSONE SA/NV,"
+        "Tecnativa,"
+        "Camptocamp,"
+        "Odoo Community Association (OCA)"
+    ),
     "name": "Add a sequence on customers' code",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
+    "development_status": "Stable",
     "category": "Generic Modules/Base",
-    "website": "http://www.initos.com",
+    "website": "https://github.com/OCA/partner-contact",
     "depends": [
         'base',
     ],
