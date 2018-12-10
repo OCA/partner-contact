7a977966f7a2796533c4356b54bdc90a7db6645e
Quentin Groulard <quentin.groulard@acsone.eu>
2018-12-10 17:25:52 +0100

    Base Partner Sequence: Migration to 12.0
    Bump module version to 12.0.1.0.0
    
    Use README by fragment system
    
    Remove copyrights
    
    Update website
    
    Add development_status

--- base_partner_sequence/tests/test_base_partner_sequence.py
+++ base_partner_sequence/tests/test_base_partner_sequence.py
@@ -3,7 +3,7 @@
 # Copyright 2016 Tecnativa - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-import openerp.tests.common as common
+import odoo.tests.common as common
 
 
 class TestBasePartnerSequence(common.TransactionCase):
