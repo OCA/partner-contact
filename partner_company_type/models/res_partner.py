7f9975c0d2c15c58e14691685e9e6931e1cf356a
Gilles Meyomesse <gilles.meyomesse@acsone.eu>
2018-12-14 19:45:23 +0100

    [12.0][MIG] partner company type

--- partner_company_type/models/res_partner.py
+++ partner_company_type/models/res_partner.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 ACSONE SA/NV
+# Copyright 2017-2018 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
@@ -9,6 +8,6 @@ class ResPartner(models.Model):
     _inherit = "res.partner"
 
     partner_company_type_id = fields.Many2one(
-        'res.partner.company.type',
-        string='Company Type'
+        comodel_name='res.partner.company.type',
+        string='Legal Form',
     )
