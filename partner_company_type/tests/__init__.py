8b355860fc55d1a8a7ba669ccccf99678bd6eb27
Gilles Meyomesse <gilles.meyomesse@acsone.eu>
2018-12-14 19:45:23 +0100

    [12.0][MIG] partner company type

--- partner_company_type/tests/__init__.py
+++ partner_company_type/tests/__init__.py
@@ -1,4 +1,1 @@
-# -*- coding: utf-8 -*-
-# Copyright 2016 ACSONE SA/NV
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from . import test_company_type
+from . import test_res_partner_company_type
