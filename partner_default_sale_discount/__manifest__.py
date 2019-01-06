b8a791337dc776e9280f49c17987c8deec649b08
Laurent Bélorgey <lb@lalieutenante.com>
2019-01-06 11:21:48 +0100

    [10.0][MIG] partner_default_sale_discount: migrate to 10.0

--- partner_default_sale_discount/__manifest__.py
+++ partner_default_sale_discount/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'Default sales discount per partner',
-    'version': '9.0.1.0.0',
+    'version': '10.0.1.0.0',
     'category': 'Partner Management',
     'license': 'AGPL-3',
     'author': 'Tecnativa,'
--- partner_default_sale_discount/models/res_partner.py
+++ partner_default_sale_discount/models/res_partner.py
@@ -2,7 +2,7 @@
 # Copyright 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import fields, models
+from odoo import fields, models
 
 
 class ResPartner(models.Model):
