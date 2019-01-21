c16d27cb60696b77914e7e7523800df4afb3d058
Nikul-Chaudhary <nikulchaudhary2112@gmail.com>
2019-01-21 23:43:45 +0530

    [MIG] partner_second_lastname v11 to v12

--- partner_second_lastname/README.rst
+++ partner_second_lastname/README.rst
@@ -83,6 +83,7 @@ Contributors
 * Jairo Llopis <jairo.llopis@tecnativa.com>.
 * Antonio Espinosa.
 * Pedro M. Baeza <pedro.baeza@tecnativa.com>.
+* Nikul Chaudhary <nikulchaudhary2112@gmail.com>
 
 Maintainer
 ----------
--- partner_second_lastname/__init__.py
+++ partner_second_lastname/__init__.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
+# Copyright 2015 Grupo ESOC Ingeniería de Servicios, S.L.U.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-# © 2015 Grupo ESOC Ingeniería de Servicios, S.L.U.
 
 from . import models
