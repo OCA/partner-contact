a2ee48d743f76108fbc615e4fa6ff6152663e663
Alexis de Lattre <alexis.delattre@akretion.com>
2019-01-31 23:44:37 +0100

    partner_disable_gravatar from v10 to v12

--- partner_disable_gravatar/models/partner.py
+++ partner_disable_gravatar/models/partner.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
-# Copyright 2018 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2018-2019 Akretion France (https://akretion.com/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import models
