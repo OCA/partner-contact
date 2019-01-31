abbef42f8c535b66d2f4b9606457acfe4c911b5c
Alexis de Lattre <alexis.delattre@akretion.com>
2019-01-31 23:44:37 +0100

    [MIG] partner_disable_gravatar from v10 to v12

--- partner_disable_gravatar/__manifest__.py
+++ partner_disable_gravatar/__manifest__.py
@@ -1,10 +1,10 @@
-# -*- coding: utf-8 -*-
-# Copyright 2018 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2018-2019 Akretion France (https://akretion.com/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Partner Disable Gravatar',
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Tools',
     'license': 'AGPL-3',
     'summary': 'Disable automatic connection to gravatar.com',
