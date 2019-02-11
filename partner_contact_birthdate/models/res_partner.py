6d440422bd764207c3b560b9272a0cd938741f52
Olivier-LAURENT <olivier.laurent@acsone.eu>
2019-02-11 18:49:03 +0100

    [CHG] remove useless birthdate field; changes after review; fix pylint warning; move birthdate field on the main area of the form

--- partner_contact_birthdate/models/res_partner.py
+++ partner_contact_birthdate/models/res_partner.py
@@ -1,33 +1,11 @@
 # Copyright (C) 2014-2015  Grupo ESOC <www.grupoesoc.es>
 # © 2017-Apertoso N.V. (<http://www.apertoso.be>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from odoo import api, fields, models
-import logging
-
-
-_logger = logging.getLogger(__name__)
+from odoo import fields, models
 
 
 class ResPartner(models.Model):
     """Partner with birth date in date format."""
     _inherit = "res.partner"
 
-    # New birthdate field in date format
     birthdate_date = fields.Date("Birthdate")
-
-    # Make the old Char field to reflect the new Date field
-    birthdate = fields.Char(
-        compute="_birthdate_compute",
-        inverse="_birthdate_inverse",
-        store=True)
-
-    @api.one
-    @api.depends("birthdate_date")
-    def _birthdate_compute(self):
-        """Store a string of the new date in the old field."""
-        self.birthdate = self.birthdate_date
-
-    @api.one
-    def _birthdate_inverse(self):
-        """Convert the old Char date to the new Date format."""
-        self.birthdate_date = self.birthdate
