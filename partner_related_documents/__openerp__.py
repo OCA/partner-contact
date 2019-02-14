0306f377a8f84fa1f9a0f038ce6232d28f05547d
fwyaime <f.wyaime@gmail.com>
2019-02-14 16:42:39 +0100

    [ADD] button for partner related documents in partner form view

---
+++ partner_related_documents/__openerp__.py
@@ -0,0 +1,21 @@
+{
+    'name': "Partner Related Document",
+    'version': '9.0.1.0',
+    'depends': [
+        "website_sign",
+    ],
+    'author': "François Wyaime, AbAKUS it-solutions SARL",
+    'website': "http://www.abakusitsolutions.eu",
+    'category': 'Partner',
+    'description': 
+    """
+Partner Related Document
+
+This module provides access to documents through the contact form view.
+
+This module has been developed by François Wyaime @ AbAKUS it-solution.    
+ """,
+    "data": [
+        'views/res_partner_related_docs_view.xml'
+    ],
+}
