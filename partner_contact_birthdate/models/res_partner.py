9ba79699e19e6a8b2730052735c227bad14a08db
Oihane Crucelaegui <oihanecruce@gmail.com>
2019-03-12 11:55:05 +0100

    [MIG] partner_contact_birthdate: Migration to 12.0
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>
    
    [UPD] Update partner_contact_birthdate.pot
    
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

--- partner_contact_birthdate/models/res_partner.py
+++ partner_contact_birthdate/models/res_partner.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2014-2015  Grupo ESOC <www.grupoesoc.es>
-# © 2017-Apertoso N.V. (<http://www.apertoso.be>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright 2017-Apertoso N.V. (<http://www.apertoso.be>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from odoo import fields, models
 
 
---
+++ partner_contact_birthdate/readme/INSTALLATION.rst
@@ -0,0 +1,5 @@
+To install this module, you need to:
+
+* Install the OCA repository `partner-contact <https://github.com/OCA/partner-contact>`_.
+* Update your modules list.
+* Search and install this module.
