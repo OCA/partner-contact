5f8347e81cbaad27b4bc313c48510ec806d9e57b
Oihane Crucelaegui <oihanecruce@gmail.com>
2019-03-12 11:55:05 +0100

    [MIG] partner_contact_birthdate: Migration to 12.0
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>
    
    [UPD] Update partner_contact_birthdate.pot
    
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

--- partner_contact_birthdate/__init__.py
+++ partner_contact_birthdate/__init__.py
@@ -1,4 +1,1 @@
-# Copyright (C) 2014-2015  Grupo ESOC <www.grupoesoc.es>
-# © 2017-Apertoso N.V. (<http://www.apertoso.be>)
-#  License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from . import models
--- partner_contact_birthdate/models/__init__.py
+++ partner_contact_birthdate/models/__init__.py
@@ -1,5 +1,1 @@
-# Copyright 2014-2015  Grupo ESOC <www.grupoesoc.es>
-# © 2017-Apertoso N.V. (<http://www.apertoso.be>)
-#  License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
 from . import res_partner
