af507c76dc8af126c1286dc7f16c3d41a9ac132c
robin.keunen <robin@keunen.net>
2019-03-22 16:12:53 +0100

    [ADD] partner_email_unique: new module

---
+++ partner_email_unique/models/res_partner.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Coop IT Easy SCRL fs
+#   Robin Keunen <robin@coopiteasy.be>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, api, _
+from openerp.exceptions import ValidationError
+from openerp.tools import config
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    @api.multi
+    @api.constrains('email')
+    def _check_email(self):
+        if config['test_enable'] and not self.env.context.get(
+                'test_partner_email_unique'):
+            return
+
+        for partner in self:
+            domain = [
+                ('id', '!=', partner.id),
+                ('email', '=', partner.email),
+            ]
+            other_partners = self.search(domain)
+
+            # active_test is False when called from
+            # base.partner.merge.automatic.wizard
+            if other_partners and self.env.context.get("active_test", True):
+                raise ValidationError(
+                    _("This email is already set to partner '%s'")
+                    % other_partners[0].display_name)
---
+++ partner_email_unique/readme/USAGE.rst
@@ -0,0 +1,3 @@
+To use this module, you need to:
+
+* Try to create two partners with the same email.
