51856cca4a7bb36cd25b34798f5914df787c0f4e
Pedro M. Baeza <pedro.baeza@gmail.com>
2019-03-22 16:07:41 +0100

    [MIG] partner_deduplicate_acl: Complete migration
    Now there's an argument for controlling extra checks

--- partner_deduplicate_acl/tests/test_partner_deduplicate_acl.py
+++ partner_deduplicate_acl/tests/test_partner_deduplicate_acl.py
@@ -1,12 +1,13 @@
+# Copyright 2017-2019 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import exceptions
 from odoo.tests import common
 
 
-class TestCrmDeduplicateAcl(common.TransactionCase):
+class TestPartnerDeduplicateAcl(common.TransactionCase):
     def setUp(self):
-        super(TestCrmDeduplicateAcl, self).setUp()
+        super(TestPartnerDeduplicateAcl, self).setUp()
         self.partner_1 = self.env['res.partner'].create({
             'name': 'Partner 1',
             'email': 'partner1@example.org',
@@ -42,9 +43,3 @@ class TestCrmDeduplicateAcl(common.TransactionCase):
         ]
         # Now there shouldn't be error
         self.wizard.action_merge()
-        # Check that the posted message has correct user
-        resulting_partner = (self.partner_1 + self.partner_2).exists()
-        self.assertEqual(
-            resulting_partner.message_ids[0].author_id,
-            self.user.partner_id,
-        )
