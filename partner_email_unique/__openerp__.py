223c4bebb045ab9e59160719459af0055da1be4b
robin.keunen <robin@keunen.net>
2019-03-22 16:12:53 +0100

    [ADD] partner_email_unique: new module
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

---
+++ partner_email_unique/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Coop IT Easy SCRL fs
+#   Robin Keunen <robin@coopiteasy.be>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Partner unique email",
+    "summary": "Add an unique constraint to email field",
+    "version": "9.0.1.0.0",
+    "category": "Customer Relationship Management",
+    "website": "https://github.com/oca/partner-contact",
+    "author": "Coop IT Easy SCRL fs, "
+              "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "base",
+    ],
+}
---
+++ partner_email_unique/tests/test_res_partner_email.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Coop IT Easy SCRL fs
+#   Robin Keunen <robin@coopiteasy.be>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from openerp.tests import common
+from openerp.exceptions import ValidationError
+
+
+class TestResPartnerEmailUnique(common.SavepointCase):
+    @classmethod
+    def setUpClass(cls):
+        super(TestResPartnerEmailUnique, cls).setUpClass()
+        partner_obj = (
+            cls.env['res.partner']
+               .with_context({'test_partner_email_unique': True})
+        )
+        cls.partner1 = partner_obj.create({
+            'name': 'Partner1',
+        })
+        cls.partner2 = partner_obj.create({
+            'name': 'Partner2',
+        })
+
+    def test_check_email(self):
+
+        # Test can create/modify partners with different email
+        self.partner1.email = 'same_email@test.com'
+        self.partner2.email = 'different_email@test.com'
+        self.assertNotEqual(self.partner1.email, self.partner2.email)
+        self.partner2.ref = False
+
+        # Test can't create/modify partner with same email
+        with self.assertRaises(ValidationError):
+            self.partner2.email = 'same_email@test.com'
