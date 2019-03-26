1bc40c4673d9356141f820538ee28f0c5678bfbf
Timon Tschanz <timon.tschanz@camptocamp.com>
2019-03-26 17:36:29 +0100

    [12.0][Add] partner_company_group

---
+++ partner_company_group/models/res_partner.py
@@ -0,0 +1,17 @@
+# Copyright 2019 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models, fields
+
+
+class Contact(models.Model):
+    _inherit = 'res.partner'
+
+    company_group_id = fields.Many2one(
+        'res.partner',
+        'Company group',
+        domain=[('is_company', '=', True)]
+    )
+
+    def _commercial_fields(self):
+        return super(Contact, self)._commercial_fields() + ['company_group_id']
