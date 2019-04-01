e98d7fc93df6f90f3a23e0214b3b78878239a1b7
robin.keunen <robin@keunen.net>
2019-04-01 11:04:38 +0200

    fixup! [ADD] partner_email_unique: new module

--- partner_email_unique/__openerp__.py
+++ partner_email_unique/__openerp__.py
@@ -8,7 +8,7 @@
     "summary": "Add an unique constraint to email field",
     "version": "9.0.1.0.0",
     "category": "Customer Relationship Management",
-    "website": "https://odoo-community.org/",
+    "website": "https://github.com/oca/partner-contact",
     "author": "Coop IT Easy SCRL fs, "
               "Odoo Community Association (OCA)",
     "license": "AGPL-3",
