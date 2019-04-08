2dee7b2adeac5048e013217f8f1177f6254a75bf
Darío Lodeiros <dario@aloxa.eu>
2019-04-08 22:31:05 +0200

    [FIX] name param required to search name

--- partner_phone_search/models/res_partner.py
+++ partner_phone_search/models/res_partner.py
@@ -11,18 +11,17 @@ class ResPartner(models.Model):
     def name_search(self, name, args=None, operator='ilike', limit=100):
         if not args:
             args = []
-        if name:
-            domain = ['|', '|', ('phone', operator, name),
-                      ('mobile', operator, name), ('email', operator, name)
-                      ]
-            partners = self.search(domain + args, limit=limit,)
-            res = partners.name_get()
-            if limit:
-                limit_rest = limit - len(partners)
-            else:
-                limit_rest = limit
-            if limit_rest or not limit:
-                args += [('id', 'not in', partners.ids)]
-                res += super(ResPartner, self).name_search(
-                    name, args=args, operator=operator, limit=limit_rest)
-            return res
+        domain = ['|', '|', ('phone', operator, name),
+                  ('mobile', operator, name), ('email', operator, name)
+                  ]
+        partners = self.search(domain + args, limit=limit,)
+        res = partners.name_get()
+        if limit:
+            limit_rest = limit - len(partners)
+        else:
+            limit_rest = limit
+        if limit_rest or not limit:
+            args += [('id', 'not in', partners.ids)]
+            res += super(ResPartner, self).name_search(
+                name, args=args, operator=operator, limit=limit_rest)
+        return res
