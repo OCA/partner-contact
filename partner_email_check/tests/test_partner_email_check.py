f2ebd8015033690c8ff6700748bbd3c51c7ef0a4
dat.vh <dat.vh@komit-consulting.com>
2019-04-23 10:50:08 +0700

    Remove copyright in init + ref code.

--- partner_email_check/tests/test_partner_email_check.py
+++ partner_email_check/tests/test_partner_email_check.py
@@ -1,5 +1,5 @@
-# Copyright 2017 Komit <http://komit-consulting.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright 2019 Komit <https://komit-consulting.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo.exceptions import ValidationError
 from odoo.tests.common import TransactionCase
