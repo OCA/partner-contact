0cf36df35d4ffd53110c95d0f23597a4b0b52932
dat.vh <dat.vh@komit-consulting.com>
2019-04-23 10:50:08 +0700

    Remove copyright in init + ref code.

--- partner_email_check/__manifest__.py
+++ partner_email_check/__manifest__.py
@@ -1,12 +1,12 @@
-# Copyright 2017 Komit <http://komit-consulting.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright 2019 Komit <https://komit-consulting.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Email Format Checker',
     'version': '11.0.1.0.0',
     'summary': 'Validate email address field',
     'author': "Komit, Odoo Community Association (OCA)",
-    'website': 'http://komit-consulting.com',
+    'website': 'https://github.com/OCA/partner-contact',
     'category': 'Tools',
     'depends': ['base'],
     'installable': True,
