5e1b36775d52593454b48c8b19e6a1ca941f3cee
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2019-05-14 14:21:40 +0200

    [MIG] base_location_nuts: Migration to v12.0

--- base_location_nuts/models/res_partner_nuts.py
+++ base_location_nuts/models/res_partner_nuts.py
@@ -7,7 +7,7 @@ from odoo import fields, models
 
 class ResPartnerNuts(models.Model):
     _name = 'res.partner.nuts'
-    _order = 'parent_left'
+    _order = 'parent_path'
     _parent_order = 'name'
     _parent_store = True
     _description = 'NUTS Item'
@@ -24,8 +24,7 @@ class ResPartnerNuts(models.Model):
     # Parent hierarchy
     parent_id = fields.Many2one(comodel_name='res.partner.nuts',
                                 ondelete='restrict')
+    parent_path = fields.Char(index=True)
     child_ids = fields.One2many(comodel_name='res.partner.nuts',
                                 inverse_name='parent_id', string='Children',
                                 oldname='children')
-    parent_left = fields.Integer('Parent Left', index=True)
-    parent_right = fields.Integer('Parent Right', index=True)
