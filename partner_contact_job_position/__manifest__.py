45ca84b4737d360edc0544d63b20428ae0a6001d
ernesto <ernesto.tejeda87@gmail.com>
2019-05-23 10:16:59 -0400

    [MIG] partner_contact_job_position: Migration to 12.0
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>
    
    [UPD] Update partner_contact_job_position.pot
    
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>
    
    [UPD] README.rst
    
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

--- partner_contact_job_position/__manifest__.py
+++ partner_contact_job_position/__manifest__.py
@@ -7,7 +7,7 @@
 {
     "name": "Partner Job Position",
     "summary": "Categorize job positions for contacts",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     'category': 'Customer Relationship Management',
     "website": "https://github.com/OCA/partner-contact",
     'author': 'Tecnativa,'
