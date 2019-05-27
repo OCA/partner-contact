e75acd98e96134320c6286a8dc5f1eba7590812d
David <david.vidal@tecnativa.com>
2019-05-27 17:25:03 +0200

    [MIG] portal_partner_select_all: Migration to 12.0

--- portal_partner_select_all/__manifest__.py
+++ portal_partner_select_all/__manifest__.py
@@ -1,10 +1,8 @@
-# -*- coding: utf-8 -*-
-# Copyright 2018 Tecnativa S.L. - David Vidal
+# Copyright 2018 Tecnativa - David Vidal
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
 {
     'name': 'Portal Partner Select All',
-    'version': '10.0.1.0.1',
+    'version': '12.0.1.0.1',
     'category': 'Custom',
     'author': 'Tecnativa,'
               'Odoo Community Association (OCA)',
