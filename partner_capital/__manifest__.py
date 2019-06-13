a641414f8a00c36aa0f81ef4e5aaa76ffff6bb42
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2019-06-13 13:32:26 +0200

    [MIG] partner_capital: Migration to 12.0

--- partner_capital/__manifest__.py
+++ partner_capital/__manifest__.py
@@ -1,19 +1,19 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Antonio Espinosa <antonio.espinosa@tecnativa.com>
 # Copyright 2015 Jairo Llopis <jairo.llopis@tecnativa.com>
 # Copyright 2017 David Vidal <david.vidal@tecnativa.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Partners Capital',
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Customer Relationship Management',
+    'license': 'AGPL-3',
     'author': 'Antiun Ingeniería S.L., '
               'Tecnativa, '
               'Odoo Community Association (OCA)',
-    'website': 'https://www.tecnativa.com',
+    'website': 'https://github.com/OCA/partner-contact',
     'depends': [
-        'sale',
+        'contacts',
     ],
     'data': [
         'views/res_partner_turnover_range_view.xml',
