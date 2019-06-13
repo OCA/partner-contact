23a4ec7ec3d5536a5a02b2ea17fcb930dc9be414
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2019-06-13 13:32:26 +0200

    [MIG] partner_capital: Migration to 12.0

--- partner_capital/models/res_partner.py
+++ partner_capital/models/res_partner.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Antonio Espinosa <antonio.espinosa@tecnativa.com>
 # Copyright 2015 Jairo Llopis <jairo.llopis@tecnativa.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
 
@@ -12,21 +11,18 @@ class ResPartner(models.Model):
     capital_country_id = fields.Many2one(
         'res.country',
         "Capital country",
-        oldname="capital_country",
         help="Country of origin of this company's capital.")
-    capital_amount = fields.Float(
+    capital_amount = fields.Monetary(
         "Capital amount",
-        oldname="capital_registered",
+        currency_field='capital_currency_id',
         help="Publicly registered capital amount.")
     capital_currency_id = fields.Many2one(
         "res.currency",
         string="Capital currency")
     turnover_range_id = fields.Many2one(
         'res.partner.turnover_range',
-        "Turnover range",
-        oldname="turnover_range")
-    turnover_amount = fields.Float(
-        oldname="turnover_number")
+        "Turnover range")
+    turnover_amount = fields.Float()
     company_size = fields.Selection(
         string="Company size",
         selection=[('micro', 'Micro'), ('small', 'Small'),
