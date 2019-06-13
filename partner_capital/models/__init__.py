c04f17d383e7e99aee6bede8d585f99a7fa4c440
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2019-06-13 13:32:26 +0200

    [MIG] partner_capital: Migration to 12.0

--- partner_capital/models/__init__.py
+++ partner_capital/models/__init__.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-# © 2015 Antiun Ingeniería S.L. - Antonio Espinosa
-# © 2015 Antiun Ingeniería S.L. - Jairo Llopis
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import (
-    res_partner_turnover_range,
-    res_partner,
-)
+from . import res_partner_turnover_range
+from . import res_partner
