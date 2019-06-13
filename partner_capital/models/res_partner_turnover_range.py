e9d6537b3ff9214fd158888c766bd56f26e40c94
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2019-06-13 13:32:26 +0200

    [MIG] partner_capital: Migration to 12.0

--- partner_capital/models/res_partner_turnover_range.py
+++ partner_capital/models/res_partner_turnover_range.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Antonio Espinosa <antonio.espinosa@tecnativa.com>
 # Copyright 2015 Jairo Llopis <jairo.llopis@tecnativa.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
 
