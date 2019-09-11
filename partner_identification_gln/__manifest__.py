3e9a201b6b50f7c5675e88943b1a4b57943ce901
BT-aestebanez <alvaro.estebanez@braintec-group.com>
2019-09-11 14:55:53 +0200

    [MIG] partner_identification_gln: Migration to 12.0

--- partner_identification_gln/__manifest__.py
+++ partner_identification_gln/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016-2017 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
@@ -7,17 +6,20 @@
     'summary': """
         This addon extends "Partner Identification Numbers"
         to provide a number category for GLN registration""",
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'Acsone S.A.,Odoo Community Association (OCA)',
-    'website': 'https://www.acsone.eu',
+    'website': 'https://github.com/OCA/partner-contact',
     'external_dependencies': {
-        'python': ['stdnum'
-                   ],
+        'python': [
+            'stdnum',
+        ],
     },
-    'depends': ['partner_identification'
-                ],
-    'data': ['data/partner_identification_gln.xml'
-             ],
+    'depends': [
+        'partner_identification',
+    ],
+    'data': [
+        'data/partner_identification_gln.xml',
+    ],
     'installable': True,
 }
