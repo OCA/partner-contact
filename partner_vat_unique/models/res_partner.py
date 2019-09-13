2f177543e1e68fc9d7efa790f6de452f8b257174
Sergio Teruel <sergio.teruel@tecnativa.com>
2019-09-13 11:02:44 +0200

    [FIX] partner_vat_unique: Do not copy vat field

--- partner_vat_unique/models/res_partner.py
+++ partner_vat_unique/models/res_partner.py
@@ -1,13 +1,15 @@
 # Copyright 2017 Grant Thornton Spain - Ismael Calvo <ismael.calvo@es.gt.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, models, _
+from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 from odoo.tools import config
 
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
+
+    vat = fields.Char(copy=False)
 
     @api.constrains('vat')
     def _check_vat_unique(self):
