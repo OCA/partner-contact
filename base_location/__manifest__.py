15daa6741e99f12c5d75da4638dd38a32721bfdd
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2019-09-30 16:00:32 +0200

    [MIG] base_location: Migration to 13.0
    * Standard procedure
    * Change v13 specifics
    * Adapt tests + correct some practices

--- base_location/__manifest__.py
+++ base_location/__manifest__.py
@@ -1,10 +1,10 @@
 # Copyright 2016 Nicolas Bessi, Camptocamp SA
-# Copyright 2018 Tecnativa - Pedro M. Baeza
+# Copyright 2018-2019 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Location management (aka Better ZIP)',
-    'version': '12.0.1.0.1',
+    'version': '13.0.1.0.0',
     'depends': [
         'base_address_city',
         'contacts',
