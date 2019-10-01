513561afbd7ee420673004f658bde6439249ad7e
ps-tubtim <pimolnat.s@outlook.com>
2019-10-01 15:35:27 +0700

    [MIG] partner_fax: Migration to 13.0

--- partner_fax/__manifest__.py
+++ partner_fax/__manifest__.py
@@ -1,19 +1,15 @@
 # Copyright 2018 Apruzzese Francesco <f.apruzzese@apuliasoftware.it>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Partner fax',
-    'category': 'Extra Tools',
-    'summary': 'Add fax number on partner',
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'author':  'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/partner-contact',
-    'depends': [
-        'base_setup'
-        ],
-    'data': [
-        'views/res_partner.xml',
-        ],
-    'installable': True,
+    "name": "Partner fax",
+    "category": "Extra Tools",
+    "summary": "Add fax number on partner",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/partner-contact",
+    "depends": ["base_setup"],
+    "data": ["views/res_partner.xml"],
+    "installable": True,
 }
