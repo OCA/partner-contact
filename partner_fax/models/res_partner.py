82e258b71031d9125be8f9125d51b40a461a748f
ps-tubtim <pimolnat.s@outlook.com>
2019-10-01 15:35:27 +0700

    [MIG] partner_fax: Migration to 13.0

--- partner_fax/models/res_partner.py
+++ partner_fax/models/res_partner.py
@@ -1,11 +1,10 @@
 # Copyright 2018 Apruzzese Francesco <f.apruzzese@apuliasoftware.it>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
 
 
 class ResPartner(models.Model):
-
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
     fax = fields.Char()
