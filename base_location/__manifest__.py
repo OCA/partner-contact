3b76aa11ab6fa48110dd4106e681aa21ba34e9d9
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2019-10-15 19:03:49 +0200

    [FIX] base_location: Don't apply domain when cities are not enforced
    And also reset city_id for avoiding incoherent information in that case.
    
    Fixes #762

--- base_location/__manifest__.py
+++ base_location/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Location management (aka Better ZIP)",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.0.1",
     "depends": ["base_address_city", "contacts"],
     "author": (
         "Camptocamp,"
