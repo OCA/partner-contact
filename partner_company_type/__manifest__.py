ad59b6409d11373bc00e7d36fa706691d77ce7da
laurent.corron <laurent.corron@acsone.eu>
2019-11-05 17:41:42 +0100

    [REF] partner_company_type: Black python code

--- partner_company_type/__manifest__.py
+++ partner_company_type/__manifest__.py
@@ -2,22 +2,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Partner Company Type',
-    'summary': 'Adds a company type to partner that are companies',
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/partner-contact',
-    'depends': [
-        'base',
-        'contacts',
+    "name": "Partner Company Type",
+    "summary": "Adds a company type to partner that are companies",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/partner-contact",
+    "depends": ["base", "contacts"],
+    "data": [
+        "security/res_partner_company_type.xml",
+        "views/res_partner_company_type.xml",
+        "views/res_partner.xml",
     ],
-    'data': [
-        'security/res_partner_company_type.xml',
-        'views/res_partner_company_type.xml',
-        'views/res_partner.xml',
-    ],
-    'demo': [
-        'demo/res_partner_company_type.xml',
-    ],
+    "demo": ["demo/res_partner_company_type.xml"],
+    "installable": True,
 }
