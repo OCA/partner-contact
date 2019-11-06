248463390c3daea6c9eae96860e47848250e1f9d
laurent.corron <laurent.corron@acsone.eu>
2019-11-06 10:09:52 +0100

    [MIG] partner_contact_configuration: Migration to 13.0

--- partner_contact_configuration/__manifest__.py
+++ partner_contact_configuration/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -7,7 +6,7 @@
     "summary": """
         Adds menu configuration access through the 'contacts' module main menu
         """,
-    "version": "10.0.1.0.0",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://acsone.eu",
