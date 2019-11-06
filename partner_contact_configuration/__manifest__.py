1a853ddabf7c5e4a0e67a8635d0289c9b279cd43
laurent.corron <laurent.corron@acsone.eu>
2019-11-06 10:09:52 +0100

    [REF] partner_contact_configuration: Black python code

--- partner_contact_configuration/__manifest__.py
+++ partner_contact_configuration/__manifest__.py
@@ -3,19 +3,15 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Partner Contact Configuration',
-    'summary': """
+    "name": "Partner Contact Configuration",
+    "summary": """
         Adds menu configuration access through the 'contacts' module main menu
         """,
-    'version': '10.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
-    'website': 'https://acsone.eu',
-    'depends': ['base',
-                'contacts'
-                ],
-    'data': ['views/partner_contact.xml'
-             ],
-    'demo': [
-    ],
+    "version": "10.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://acsone.eu",
+    "depends": ["base", "contacts"],
+    "data": ["views/partner_contact.xml"],
+    "demo": [],
 }
