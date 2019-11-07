4ff05e8af70f7eb749cebfb25db86b868e63feff
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2019-11-07 15:41:32 +0100

    [MIG] partner_firstname: Black, isort

--- partner_firstname/models/base_config_settings.py
+++ partner_firstname/models/base_config_settings.py
@@ -2,12 +2,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 import logging
+
 from odoo import api, fields, models
+
 _logger = logging.getLogger(__name__)
 
 
 class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
+    _inherit = "res.config.settings"
 
     partner_names_order = fields.Selection(
         string="Partner names order",
@@ -15,36 +17,41 @@ class ResConfigSettings(models.TransientModel):
         help="Order to compose partner fullname",
         required=True,
     )
-    partner_names_order_changed = fields.Boolean(
-        compute="_compute_names_order_changed",
-    )
+    partner_names_order_changed = fields.Boolean(compute="_compute_names_order_changed")
 
     def _partner_names_order_selection(self):
         return [
-            ('last_first', 'Lastname Firstname'),
-            ('last_first_comma', 'Lastname, Firstname'),
-            ('first_last', 'Firstname Lastname'),
+            ("last_first", "Lastname Firstname"),
+            ("last_first_comma", "Lastname, Firstname"),
+            ("first_last", "Firstname Lastname"),
         ]
 
     @api.multi
     def _partner_names_order_default(self):
-        return self.env['res.partner']._names_order_default()
+        return self.env["res.partner"]._names_order_default()
 
     @api.model
     def get_values(self):
         res = super(ResConfigSettings, self).get_values()
-        partner_names_order = self.env['ir.config_parameter'].sudo().get_param(
-            'partner_names_order',
-            default=self._partner_names_order_default()
+        partner_names_order = (
+            self.env["ir.config_parameter"]
+            .sudo()
+            .get_param(
+                "partner_names_order", default=self._partner_names_order_default()
+            )
         )
         res.update(partner_names_order=partner_names_order)
         return res
 
     @api.multi
-    @api.depends('partner_names_order')
+    @api.depends("partner_names_order")
     def _compute_names_order_changed(self):
-        current = self.env['ir.config_parameter'].sudo().get_param(
-            'partner_names_order', default=self._partner_names_order_default()
+        current = (
+            self.env["ir.config_parameter"]
+            .sudo()
+            .get_param(
+                "partner_names_order", default=self._partner_names_order_default()
+            )
         )
         for record in self:
             record.partner_names_order_changed = bool(
@@ -52,28 +59,31 @@ class ResConfigSettings(models.TransientModel):
             )
 
     @api.multi
-    @api.onchange('partner_names_order')
+    @api.onchange("partner_names_order")
     def _onchange_partner_names_order(self):
         self._compute_names_order_changed()
 
     @api.multi
     def set_values(self):
         super(ResConfigSettings, self).set_values()
-        self.env['ir.config_parameter'].sudo().set_param(
-            'partner_names_order', self.partner_names_order
+        self.env["ir.config_parameter"].sudo().set_param(
+            "partner_names_order", self.partner_names_order
         )
 
     @api.multi
     def _partners_for_recalculating(self):
-        return self.env['res.partner'].search([
-            ('is_company', '=', False),
-            ('firstname', '!=', False), ('lastname', '!=', False),
-        ])
+        return self.env["res.partner"].search(
+            [
+                ("is_company", "=", False),
+                ("firstname", "!=", False),
+                ("lastname", "!=", False),
+            ]
+        )
 
     @api.multi
     def action_recalculate_partners_name(self):
-        self.env['ir.config_parameter'].sudo().set_param(
-            'partner_names_order', self.partner_names_order
+        self.env["ir.config_parameter"].sudo().set_param(
+            "partner_names_order", self.partner_names_order
         )
         partners = self._partners_for_recalculating()
         _logger.info("Recalculating names for %d partners.", len(partners))
