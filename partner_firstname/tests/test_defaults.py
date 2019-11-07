cb381a52d123278ebdf4b354812e85e30e2c7dca
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2019-11-07 15:41:32 +0100

    [MIG] partner_firstname: Black, isort

--- partner_firstname/tests/test_defaults.py
+++ partner_firstname/tests/test_defaults.py
@@ -4,44 +4,43 @@
 """Test default values for models."""
 
 from odoo.tests.common import TransactionCase
+
 from .base import MailInstalled
 
 
 class PersonCase(TransactionCase):
     """Test ``res.partner`` when it is a person."""
+
     context = {"default_is_company": False}
     model = "res.partner"
 
     def setUp(self):
         super(PersonCase, self).setUp()
-        self.values = {
-            "firstname": "Núñez",
-            "lastname": "Fernán",
-        }
-        self.values["name"] = "%s %s" % (self.values["firstname"],
-                                         self.values["lastname"])
+        self.values = {"firstname": "Núñez", "lastname": "Fernán"}
+        self.values["name"] = "{} {}".format(
+            self.values["firstname"], self.values["lastname"]
+        )
         if "default_is_company" in self.context:
             self.values["is_company"] = self.context["default_is_company"]
 
     def tearDown(self):
         for key, value in self.values.items():
-            self.assertEqual(
-                self.defaults.get(key),
-                value,
-                "Checking key %s" % key)
+            self.assertEqual(self.defaults.get(key), value, "Checking key %s" % key)
 
         return super(PersonCase, self).tearDown()
 
     def test_default_get(self):
         """Getting default values for fields includes new fields."""
-        self.defaults = (self.env[self.model]
-                         .with_context(self.context,
-                                       default_name=self.values["name"])
-                         .default_get(list(self.values.keys())))
+        self.defaults = (
+            self.env[self.model]
+            .with_context(self.context, default_name=self.values["name"])
+            .default_get(list(self.values.keys()))
+        )
 
 
 class CompanyCase(PersonCase):
     """Test ``res.partner`` when it is a company."""
+
     context = {"default_is_company": True}
 
     def tearDown(self):
@@ -51,6 +50,7 @@ class CompanyCase(PersonCase):
 
 class UserCase(PersonCase, MailInstalled):
     """Test ``res.users``."""
+
     model = "res.users"
     context = {"default_login": "user@example.com"}
 
