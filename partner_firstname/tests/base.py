a614a19144979a73a2c4eb7514d7ed295a426124
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2019-11-07 15:41:32 +0100

    [MIG] partner_firstname: Black, isort

--- partner_firstname/tests/base.py
+++ partner_firstname/tests/base.py
@@ -1,17 +1,18 @@
-
 # Copyright 2014 Nemry Jonathan (Acsone SA/NV) (http://www.acsone.eu)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo.tests.common import TransactionCase
+
 from .. import exceptions as ex
 
 
-class MailInstalled():
+class MailInstalled:
     def mail_installed(self):
         """Check if ``mail`` module is installed.``"""
-        return (self.env["ir.module.module"]
-                .search([("name", "=", "mail")])
-                .state == "installed")
+        return (
+            self.env["ir.module.module"].search([("name", "=", "mail")]).state
+            == "installed"
+        )
 
 
 class BaseCase(TransactionCase, MailInstalled):
@@ -22,15 +23,15 @@ class BaseCase(TransactionCase, MailInstalled):
         self.create_original()
 
     def create_original(self):
-        self.original = self.env["res.partner"].create({
-            "firstname": self.firstname,
-            "lastname": self.lastname})
+        self.original = self.env["res.partner"].create(
+            {"firstname": self.firstname, "lastname": self.lastname}
+        )
 
     def expect(self, lastname, firstname, name=None):
         """Define what is expected in each field when ending."""
         self.lastname = lastname
         self.firstname = firstname
-        self.name = name or "%s %s" % (firstname, lastname)
+        self.name = name or "{} {}".format(firstname, lastname)
 
     def tearDown(self):
         if self.check_fields:
@@ -41,15 +42,15 @@ class BaseCase(TransactionCase, MailInstalled):
                 self.assertEqual(
                     getattr(self.changed, field),
                     getattr(self, field),
-                    "Test failed with wrong %s" % field)
+                    "Test failed with wrong %s" % field,
+                )
 
         super(BaseCase, self).tearDown()
 
     def test_copy(self):
         """Copy the partner and compare the result."""
         self.expect("%s (copy)" % self.lastname, self.firstname)
-        self.changed = (self.original.with_context(copy=True, lang="en_US")
-                        .copy())
+        self.changed = self.original.with_context(copy=True, lang="en_US").copy()
 
     def test_one_name(self):
         """Test what happens when only one name is given."""
