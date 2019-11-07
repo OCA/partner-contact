19b2f41e1aea7b28569206ceccd48177f718b7eb
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2019-11-07 15:41:32 +0100

    [MIG] partner_firstname: Black, isort

--- partner_firstname/migrations/12.0.1.0.0/pre-ir_config_param.py
+++ partner_firstname/migrations/12.0.1.0.0/pre-ir_config_param.py
@@ -3,11 +3,12 @@ def store_ir_config_param(cr):
     names was last_first.  In order to retain this behaviour we
     store the config parameter if it is not present.
     """
-    cr.execute("SELECT 1 FROM ir_config_parameter "
-               "WHERE key = 'partner_names_order'")
+    cr.execute("SELECT 1 FROM ir_config_parameter " "WHERE key = 'partner_names_order'")
     if not cr.fetchone():
-        cr.execute("INSERT INTO ir_config_parameter (key, value) VALUES "
-                   "('partner_names_order', 'last_first')")
+        cr.execute(
+            "INSERT INTO ir_config_parameter (key, value) VALUES "
+            "('partner_names_order', 'last_first')"
+        )
 
 
 def migrate(cr, version):
