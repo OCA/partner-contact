3a1375702ae478caab63ce071f2715b651d2fbda
ernestotejeda <ernesto.tejeda87@gmail.com>
2019-11-22 15:56:03 -0500

    [IMP] partner_company_group: black, isort

--- partner_company_group/models/account_invoice.py
+++ partner_company_group/models/account_invoice.py
@@ -1,13 +1,12 @@
 # Copyright 2019 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class AccountInvoice(models.Model):
-    _inherit = 'account.invoice'
+    _inherit = "account.invoice"
 
     company_group_id = fields.Many2one(
-        related='partner_id.company_group_id',
-        store=True
+        related="partner_id.company_group_id", store=True
     )
