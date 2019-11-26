cc11e93e2b69e769347c7e3b5fa7fa1d85f0428b
Kevin Khao <kevinkhao@gmail.com>
2019-11-26 11:53:09 +0100

    [MIG] partner_deduplicate_acl: Migration to 13.0

--- partner_deduplicate_acl/wizards/partner_merge.py
+++ partner_deduplicate_acl/wizards/partner_merge.py
@@ -1,7 +1,7 @@
 # Copyright 2016 Tecnativa - Jairo Llopis
 # Copyright 2016 Tecnativa - Vicent Cubells
 # Copyright 2017-2019 Tecnativa - Pedro M. Baeza
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import models
 
