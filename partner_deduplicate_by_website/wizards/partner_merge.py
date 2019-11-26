ee047ed1b9fa5f9c477cc210e333fb4217f91be7
Kevin Khao <kevinkhao@gmail.com>
2019-11-26 18:53:49 +0100

    [IMP] partner_deduplicate_by_website: black, isort

--- partner_deduplicate_by_website/wizards/partner_merge.py
+++ partner_deduplicate_by_website/wizards/partner_merge.py
@@ -7,7 +7,7 @@ from odoo import fields, models
 class BasePartnerMergeAutomaticWizard(models.TransientModel):
     _inherit = "base.partner.merge.automatic.wizard"
 
-    group_by_website = fields.Boolean('Website')
+    group_by_website = fields.Boolean("Website")
 
     def _generate_query(self, fields, maximum_group=100):
         """Inject the additional criteria 'website IS NOT NULL' when needed.
@@ -15,14 +15,15 @@ class BasePartnerMergeAutomaticWizard(models.TransientModel):
         this criteria regularly.
         """
         query = super(BasePartnerMergeAutomaticWizard, self)._generate_query(
-            fields, maximum_group=maximum_group)
-        if 'website' in fields:
-            if 'WHERE' in query:
-                index = query.find('WHERE')
-                query = (query[:index + 6] + "website IS NOT NULL AND " +
-                         query[index + 6:])
+            fields, maximum_group=maximum_group
+        )
+        if "website" in fields:
+            if "WHERE" in query:
+                index = query.find("WHERE")
+                query = (
+                    query[: index + 6] + "website IS NOT NULL AND " + query[index + 6 :]
+                )
             else:
-                index = query.find(' GROUP BY')
-                query = (query[:index] + " WHERE website IS NOT NULL" +
-                         query[index:])
+                index = query.find(" GROUP BY")
+                query = query[:index] + " WHERE website IS NOT NULL" + query[index:]
         return query
