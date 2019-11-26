c7583548f58f9c92f5cb15eafad8491adc4dabaa
laurent.corron <laurent.corron@acsone.eu>
2019-11-26 11:47:59 +0100

    [REF] partner_identification: Black python code

--- partner_identification/__manifest__.py
+++ partner_identification/__manifest__.py
@@ -7,26 +7,24 @@
 # Copyright 2016 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': 'Partner Identification Numbers',
-    'category': 'Customer Relationship Management',
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'depends': [
-        'contacts',
+    "name": "Partner Identification Numbers",
+    "category": "Customer Relationship Management",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["contacts",],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/res_partner_id_category_view.xml",
+        "views/res_partner_id_number_view.xml",
+        "views/res_partner_view.xml",
     ],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/res_partner_id_category_view.xml',
-        'views/res_partner_id_number_view.xml',
-        'views/res_partner_view.xml',
-    ],
-    'author': 'ChriCar Beteiligungs- und Beratungs- GmbH,'
-              'Tecnativa,'
-              'Camptocamp,'
-              'ACSONE SA/NV,'
-              'LasLabs,'
-              'Onestein,'
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/partner-contact',
-    'development_status': 'Production/Stable',
+    "author": "ChriCar Beteiligungs- und Beratungs- GmbH,"
+    "Tecnativa,"
+    "Camptocamp,"
+    "ACSONE SA/NV,"
+    "LasLabs,"
+    "Onestein,"
+    "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/partner-contact",
+    "development_status": "Production/Stable",
 }
