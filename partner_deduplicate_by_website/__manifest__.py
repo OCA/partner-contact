4f8c9d7ce8ee73a7e8eae1b75e57fb74c5ee443b
Kevin Khao <kevinkhao@gmail.com>
2019-11-26 20:01:32 +0100

    [MIG] partner_deduplicate_by_website: Migration to 13.0

--- partner_deduplicate_by_website/__manifest__.py
+++ partner_deduplicate_by_website/__manifest__.py
@@ -1,11 +1,11 @@
 # Copyright 2016 Tecnativa - Pedro M. Baeza
 # Copyright 2017 Tecnativa - Vicent Cubells
 # Copyright 2018 Tecnativa - Cristina Martin
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Deduplicate Contacts by Website",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Tools",
     "website": "https://github.com/OCA/crm",
     "author": "Tecnativa, " "Odoo Community Association (OCA)",
--- partner_deduplicate_by_website/wizards/partner_merge.py
+++ partner_deduplicate_by_website/wizards/partner_merge.py
@@ -1,5 +1,5 @@
 # Copyright 2016 Tecnativa - Pedro M. Baeza
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
 
