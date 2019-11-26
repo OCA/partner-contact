d0de282532f0b4d5aa10a737e6d908d0c3992430
laurent.corron <laurent.corron@acsone.eu>
2019-11-26 11:47:59 +0100

    [REF] partner_identification: Black python code

--- partner_identification/tests/fake_models.py
+++ partner_identification/tests/fake_models.py
@@ -14,8 +14,7 @@ def setup_test_model(env, model_cls):
     model_cls._build_model(env.registry, env.cr)
     env.registry.setup_models(env.cr)
     env.registry.init_models(
-        env.cr, [model_cls._name],
-        dict(env.context, update_custom_fields=True)
+        env.cr, [model_cls._name], dict(env.context, update_custom_fields=True)
     )
 
 
@@ -24,24 +23,18 @@ def teardown_test_model(env, model_cls):
 
     Courtesy of SBidoul from https://github.com/OCA/mis-builder :)
     """
-    if not getattr(model_cls, '_teardown_no_delete', False):
+    if not getattr(model_cls, "_teardown_no_delete", False):
         del env.registry.models[model_cls._name]
     env.registry.setup_models(env.cr)
 
 
 class ResPartner(models.Model):
-    _name = 'res.partner'
-    _inherit = 'res.partner'
+    _name = "res.partner"
+    _inherit = "res.partner"
     _teardown_no_delete = True
 
     social_security = fields.Char(
-        compute=lambda s: s._compute_identification(
-            'social_security', 'SSN',
-        ),
-        inverse=lambda s: s._inverse_identification(
-            'social_security', 'SSN',
-        ),
-        search=lambda s, *a: s._search_identification(
-            'SSN', *a
-        ),
+        compute=lambda s: s._compute_identification("social_security", "SSN",),
+        inverse=lambda s: s._inverse_identification("social_security", "SSN",),
+        search=lambda s, *a: s._search_identification("SSN", *a),
     )
