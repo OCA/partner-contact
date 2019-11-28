0c51ad290d8fd778f4f905b6f9bf6805b29bc95e
david <david.vidal@tecnativa.com>
2019-11-28 16:36:55 +0100

    [MOV] crm_deduplicate_by_ref -> partner_deduplicate_by_ref

--- partner_deduplicate_by_ref/__manifest__.py
+++ partner_deduplicate_by_ref/__manifest__.py
@@ -4,13 +4,13 @@
     "name": "Deduplicate Contacts by reference",
     "version": "11.0.1.0.0",
     "category": "Tools",
-    "website": "https://www.github.com/OCA/crm",
+    "website": "https://www.github.com/OCA/partner-contact",
     "author": "Tecnativa, "
               "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "installable": True,
     "depends": [
-        "crm_deduplicate_acl",
+        "partner_deduplicate_acl",
     ],
     "data": [
         'wizards/partner_merge_view.xml',
