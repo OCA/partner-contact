25239087fca0d93b50b05feb61ae2690f80e157e
Stephan Rozendaal <stephan@west.nl>
2019-12-05 12:24:12 +0100

    [MIG] partner_address_street3: Migration to 13.0

--- partner_address_street3/__manifest__.py
+++ partner_address_street3/__manifest__.py
@@ -6,7 +6,7 @@
     "name": "Street3 in addresses",
     "summary": "Add a third address line on partners",
     "license": "AGPL-3",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "author": "Camptocamp,Odoo Community Association (OCA)",
     "maintainer": "Camptocamp",
     "category": "Sales",
