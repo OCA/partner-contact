22b7feb3e42dce18a179634ae3842a02b55ffff8
Stephan Rozendaal <stephan@west.nl>
2019-12-05 12:10:23 +0100

    [IMP] : black, isort

--- partner_address_street3/model/partner.py
+++ partner_address_street3/model/partner.py
@@ -7,21 +7,21 @@ from odoo import api, fields, models
 
 class ResPartner(models.Model):
     """Add third field in address"""
+
     _inherit = "res.partner"
 
-    street3 = fields.Char('Street 3')
+    street3 = fields.Char("Street 3")
 
     @api.model
     def _address_fields(self):
         fields = super(ResPartner, self)._address_fields()
-        fields.append('street3')
+        fields.append("street3")
         return fields
 
     @api.multi
     def _display_address(self, without_company=False):
         """Remove empty lines which can happen when street3 field is empty."""
-        res = super(ResPartner, self)._display_address(
-            without_company=without_company)
-        while '\n\n' in res:
-            res = res.replace('\n\n', '\n')
+        res = super(ResPartner, self)._display_address(without_company=without_company)
+        while "\n\n" in res:
+            res = res.replace("\n\n", "\n")
         return res
