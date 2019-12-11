fcf78a370ddbf3066d505c4595b63e5b87d2fbd4
Stephan Rozendaal <stephan@west.nl>
2019-12-11 11:43:57 +0100

    [IMP] Add test for address format
    Add a test for the address format with the street3 part
    included.

--- partner_address_street3/tests/test_street_3.py
+++ partner_address_street3/tests/test_street_3.py
@@ -7,28 +7,40 @@ from odoo.tests.common import TransactionCase
 
 
 class TestStreet3(TransactionCase):
-    def test_partner(self):
-        # Test address_format has been updated on existing countries
-        us_country = self.env.ref("base.us")
-        self.assertTrue("%(street3)s" in us_country.address_format)
-
-        homer = self.env["res.partner"].create(
+    def setUp(self):
+        super().setUp()
+        self.us_country = self.env.ref("base.us")
+        self.homer = self.env["res.partner"].create(
             {
                 "name": "Homer Simpson",
                 "city": "Springfield",
                 "street": "742 Evergreen Terrace",
                 "street2": "Donut Lane",
                 "street3": "Tho",
-                "country_id": us_country.id,
+                "country_id": self.us_country.id,
             }
         )
 
+    def test_partner(self):
+        # Test address_format has been updated on existing countries
+        self.assertTrue("%(street3)s" in self.us_country.address_format)
         # test synchro of street3 on create
         bart = self.env["res.partner"].create(
-            {"name": "Bart Simpson", "parent_id": homer.id, "type": "contact"}
+            {"name": "Bart Simpson", "parent_id": self.homer.id, "type": "contact"}
         )
         self.assertEqual(bart.street3, "Tho")
 
         # test synchro of street3 on write
-        homer.write({"street3": "in OCA we trust"})
+        self.homer.write({"street3": "in OCA we trust"})
         self.assertEqual(bart.street3, "in OCA we trust")
+
+    def test_street3_in_address(self):
+        self.assertEqual(
+            self.homer.contact_address,
+            "742 Evergreen Terrace\nDonut Lane\nTho\nSpringfield  \nUnited States",
+        )
+        self.homer.street3 = False
+        self.assertEqual(
+            self.homer.contact_address,
+            "742 Evergreen Terrace\nDonut Lane\nSpringfield  \nUnited States",
+        )
