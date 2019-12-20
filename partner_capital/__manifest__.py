9035a9a15372af40103687fd44d5f91910cad9a7
kongrattapong <rattapong.chok@gmail.com>
2019-12-20 10:00:42 +0700

    [IMP] partner_capital: black, isort

--- partner_capital/__manifest__.py
+++ partner_capital/__manifest__.py
@@ -4,21 +4,19 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Partners Capital',
-    'version': '12.0.1.0.0',
-    'category': 'Customer Relationship Management',
-    'license': 'AGPL-3',
-    'author': 'Antiun Ingeniería S.L., '
-              'Tecnativa, '
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/partner-contact',
-    'depends': [
-        'contacts',
-    ],
-    'data': [
-        'views/res_partner_turnover_range_view.xml',
-        'views/res_partner_view.xml',
-        'security/ir.model.access.csv',
+    "name": "Partners Capital",
+    "version": "12.0.1.0.0",
+    "category": "Customer Relationship Management",
+    "license": "AGPL-3",
+    "author": "Antiun Ingeniería S.L., "
+    "Tecnativa, "
+    "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/partner-contact",
+    "depends": ["contacts"],
+    "data": [
+        "views/res_partner_turnover_range_view.xml",
+        "views/res_partner_view.xml",
+        "security/ir.model.access.csv",
     ],
     "installable": True,
 }
