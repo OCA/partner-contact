67de42de8ee1fa6386280005ac86c8dba8c122b4
newtratip <tharathip.chaweewongphan@gmail.com>
2019-12-21 00:02:04 +0700

    [13.0][MIG] partner_contact_birthdate
    [13.0][UPD] partner_contact_birthdate
    
    [13.0][UPD] Change xml id to view_partner_form
    
    [13.0][UPD] Remove copyright in init files
    
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>
    
    [UPD] Update partner_contact_birthdate.pot
    
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>
    
    [UPD] README.rst
    
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

--- partner_contact_birthdate/models/res_partner.py
+++ partner_contact_birthdate/models/res_partner.py
@@ -1,11 +1,13 @@
 # Copyright (C) 2014-2015  Grupo ESOC <www.grupoesoc.es>
 # Copyright 2017-Apertoso N.V. (<http://www.apertoso.be>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
 from odoo import fields, models
 
 
 class ResPartner(models.Model):
     """Partner with birth date in date format."""
+
     _inherit = "res.partner"
 
     birthdate_date = fields.Date("Birthdate")
