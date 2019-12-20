cb2f9856f3e8a687af16cc458f2192476f93a081
kongrattapong <rattapong.chok@gmail.com>
2019-12-20 10:00:42 +0700

    [IMP] partner_capital: black, isort

--- partner_capital/models/res_partner.py
+++ partner_capital/models/res_partner.py
@@ -6,24 +6,27 @@ from odoo import fields, models
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
     capital_country_id = fields.Many2one(
-        'res.country',
+        "res.country",
         "Capital country",
-        help="Country of origin of this company's capital.")
+        help="Country of origin of this company's capital.",
+    )
     capital_amount = fields.Monetary(
         "Capital amount",
-        currency_field='capital_currency_id',
-        help="Publicly registered capital amount.")
-    capital_currency_id = fields.Many2one(
-        "res.currency",
-        string="Capital currency")
-    turnover_range_id = fields.Many2one(
-        'res.partner.turnover_range',
-        "Turnover range")
+        currency_field="capital_currency_id",
+        help="Publicly registered capital amount.",
+    )
+    capital_currency_id = fields.Many2one("res.currency", string="Capital currency")
+    turnover_range_id = fields.Many2one("res.partner.turnover_range", "Turnover range")
     turnover_amount = fields.Float()
     company_size = fields.Selection(
         string="Company size",
-        selection=[('micro', 'Micro'), ('small', 'Small'),
-                   ('medium', 'Medium'), ('big', 'Big')])
+        selection=[
+            ("micro", "Micro"),
+            ("small", "Small"),
+            ("medium", "Medium"),
+            ("big", "Big"),
+        ],
+    )
