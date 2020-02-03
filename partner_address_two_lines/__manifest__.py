9a238217f294331f2f7bd542f9abf6642048f4e6
Tonow-c2c <thomas.nowicki@camptocamp.com>
2020-02-03 11:51:12 +0100

    [ADD][10.0] add partner_address_two_lines module

---
+++ partner_address_two_lines/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Partner address in two lines",
+    "version": "10.0.1.0.0",
+    "summary": "The company and the partner name are on two different lines",
+    "author": "Camptocamp," "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Partner",
+    "website": "http://github.com/OCA/partner-contact",
+    "depends": [
+        "base",
+        "report",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
---
+++ partner_address_two_lines/readme/INSTALL.rst
@@ -0,0 +1,2 @@
+To install this module, you need to:
+* Add https://github.com/OCA/OCB/pull/903 and https://github.com/OCA/OCB/pull/904
