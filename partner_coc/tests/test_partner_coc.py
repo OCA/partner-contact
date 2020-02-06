367d9128be740cf6948d9043e6bf57697653ac05
Nikos Tsirintanis <ntsirintanis@therp.nl>
2020-02-06 12:47:10 +0100

    [IMP] partner_coc: black, isort

--- partner_coc/tests/test_partner_coc.py
+++ partner_coc/tests/test_partner_coc.py
@@ -7,58 +7,49 @@ from odoo.tests.common import TransactionCase
 class TestPartnerCoC(TransactionCase):
     def setUp(self):
         super(TestPartnerCoC, self).setUp()
-        self.main_partner = self.env.ref('base.main_partner')
-        self.partner_id_category = self.env.ref('partner_coc.id_category_coc')
+        self.main_partner = self.env.ref("base.main_partner")
+        self.partner_id_category = self.env.ref("partner_coc.id_category_coc")
 
     def test_01_id_number_new(self):
         # Good CoC
-        vals = {
-            'name': '56048785',
-            'category_id': self.partner_id_category.id,
-        }
-        self.main_partner.write({'id_numbers': [(0, 0, vals)]})
+        vals = {"name": "56048785", "category_id": self.partner_id_category.id}
+        self.main_partner.write({"id_numbers": [(0, 0, vals)]})
         id_number = self.main_partner.id_numbers[0]
 
-        self.assertEqual(id_number.name, '56048785')
+        self.assertEqual(id_number.name, "56048785")
 
     def test_02_id_number_duplicate(self):
         # Duplicate CoC
-        vals = {
-            'name': '56048785',
-            'category_id': self.partner_id_category.id,
-        }
+        vals = {"name": "56048785", "category_id": self.partner_id_category.id}
 
-        self.main_partner.write({'id_numbers': [(0, 0, vals)]})
+        self.main_partner.write({"id_numbers": [(0, 0, vals)]})
         id_number_name = self.main_partner.id_numbers[0].name
-        self.assertEqual(id_number_name, '56048785')
+        self.assertEqual(id_number_name, "56048785")
 
-        new_partner = self.env['res.partner'].create({'name': 'Test Partner'})
+        new_partner = self.env["res.partner"].create({"name": "Test Partner"})
 
-        new_partner.write({'id_numbers': [(0, 0, vals)]})
+        new_partner.write({"id_numbers": [(0, 0, vals)]})
         id_number_name = new_partner.id_numbers[0].name
-        self.assertEqual(id_number_name, '56048785')
+        self.assertEqual(id_number_name, "56048785")
 
     def test_03_coc_registration_number_create(self):
-        new_partner = self.env['res.partner'].create({
-            'name': 'Test Partner',
-            'coc_registration_number': '56048785',
-        })
+        new_partner = self.env["res.partner"].create(
+            {"name": "Test Partner", "coc_registration_number": "56048785"}
+        )
 
-        self.assertEqual(new_partner.coc_registration_number, '56048785')
+        self.assertEqual(new_partner.coc_registration_number, "56048785")
 
         id_numbers = new_partner.id_numbers
         self.assertTrue(id_numbers)
         self.assertEqual(len(id_numbers), 1)
-        self.assertEqual(id_numbers.name, '56048785')
+        self.assertEqual(id_numbers.name, "56048785")
 
     def test_04_coc_registration_number_write(self):
-        self.main_partner.write({
-            'coc_registration_number': '56048785'
-        })
+        self.main_partner.write({"coc_registration_number": "56048785"})
         coc = self.main_partner.coc_registration_number
-        self.assertEqual(coc, '56048785')
+        self.assertEqual(coc, "56048785")
 
         id_numbers = self.main_partner.id_numbers
         self.assertTrue(id_numbers)
         self.assertEqual(len(id_numbers), 1)
-        self.assertEqual(id_numbers.name, '56048785')
+        self.assertEqual(id_numbers.name, "56048785")
