209a3566508884996d50a49a07a089b61c841e61
Manuel Calero <manuel.calero@tecnativa.com>
2020-02-19 17:01:17 +0100

    [MIG] partner_vat_unique: Migration to 13.0

--- partner_vat_unique/tests/test_vat_unique.py
+++ partner_vat_unique/tests/test_vat_unique.py
@@ -1,5 +1,5 @@
 # Copyright 2017 Grant Thornton Spain - Ismael Calvo <ismael.calvo@es.gt.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo.exceptions import ValidationError
 from odoo.tests.common import SavepointCase
