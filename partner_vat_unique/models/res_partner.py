0b8f3fd80471eb15a3243227e817cbbbb661c2e4
Manuel Calero <manuel.calero@tecnativa.com>
2020-02-19 16:59:37 +0100

    [IMP] partner_vat_unique: black, isort

--- partner_vat_unique/models/res_partner.py
+++ partner_vat_unique/models/res_partner.py
@@ -7,25 +7,28 @@ from odoo.tools import config
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
     vat = fields.Char(copy=False)
 
-    @api.constrains('vat')
+    @api.constrains("vat")
     def _check_vat_unique(self):
         for record in self:
             if record.parent_id or not record.vat:
                 continue
-            test_condition = (config['test_enable'] and
-                              not self.env.context.get('test_vat'))
+            test_condition = config["test_enable"] and not self.env.context.get(
+                "test_vat"
+            )
             if test_condition:
                 continue
-            results = self.env['res.partner'].search_count([
-                ('parent_id', '=', False),
-                ('vat', '=', record.vat),
-                ('id', '!=', record.id)
-            ])
+            results = self.env["res.partner"].search_count(
+                [
+                    ("parent_id", "=", False),
+                    ("vat", "=", record.vat),
+                    ("id", "!=", record.id),
+                ]
+            )
             if results:
-                raise ValidationError(_(
-                    "The VAT %s already exists in another "
-                    "partner.") % record.vat)
+                raise ValidationError(
+                    _("The VAT %s already exists in another " "partner.") % record.vat
+                )
