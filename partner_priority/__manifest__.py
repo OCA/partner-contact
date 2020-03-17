16f2f8ababef1f834add091d21bfea492f2080ec
newtratip <tharathip.chaweewongphan@gmail.com>
2020-03-17 18:32:47 +0700

    [IMP] partner_priority: black, isort

--- partner_priority/__manifest__.py
+++ partner_priority/__manifest__.py
@@ -2,23 +2,21 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Partner Priority',
-    'summary': "Adds priority to partners.",
-    'author': "Patrick Wilson, Odoo Community Association (OCA)",
-    'website': "https://github.com/OCA/partner-contact",
-    'category': 'Customer Relationship Management',
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'depends': [
-        'contacts',
-        ],
-    'data': [
-        'views/res_partner.xml',
-        'views/partner_priority.xml',
-        'security/ir.model.access.csv',
-        'data/partner_priority_data.xml',
-        'data/partner_priority_sequence_data.xml',
+    "name": "Partner Priority",
+    "summary": "Adds priority to partners.",
+    "author": "Patrick Wilson, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/partner-contact",
+    "category": "Customer Relationship Management",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["contacts"],
+    "data": [
+        "views/res_partner.xml",
+        "views/partner_priority.xml",
+        "security/ir.model.access.csv",
+        "data/partner_priority_data.xml",
+        "data/partner_priority_sequence_data.xml",
     ],
-    'development_status': 'Beta',
-    'maintainers': ['patrickrwilson'],
+    "development_status": "Beta",
+    "maintainers": ["patrickrwilson"],
 }
