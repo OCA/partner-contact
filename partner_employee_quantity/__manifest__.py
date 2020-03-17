301a3746cebd91dda96bac6e227c2bf95e2d6648
newtratip <tharathip.chaweewongphan@gmail.com>
2020-03-17 17:55:54 +0700

    [IMP] partner_employee_quantity: black, isort

--- partner_employee_quantity/__manifest__.py
+++ partner_employee_quantity/__manifest__.py
@@ -5,26 +5,21 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Employee quantity in partners',
-    'summary': 'Know how many employees a partner has',
-    'version': '12.0.1.0.0',
-    'category': 'Customer Relationship Management',
-    "author": 'Tecnativa, '
-              'Odoo Community Association (OCA)',
+    "name": "Employee quantity in partners",
+    "summary": "Know how many employees a partner has",
+    "version": "13.0.1.0.0",
+    "category": "Customer Relationship Management",
+    "author": "Tecnativa, " "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
-    'website': 'https://github.com/OCA/partner-contact',
-    'depends': [
-        'contacts',
+    "website": "https://github.com/OCA/partner-contact",
+    "depends": ["contacts"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/res_partner_employee_quantity_range_view.xml",
+        "views/res_partner_view.xml",
     ],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/res_partner_employee_quantity_range_view.xml',
-        'views/res_partner_view.xml',
-    ],
-    'development_status': 'Mature',
-    'maintainers': [
-        'pedrobaeza',
-    ],
+    "development_status": "Mature",
+    "maintainers": ["pedrobaeza"],
     "installable": True,
 }
