3505a80ffbf06e12bbbb9d8b101fba1eecded63c
newtratip <tharathip.chaweewongphan@gmail.com>
2020-03-17 18:32:47 +0700

    [IMP] partner_priority: black, isort

--- partner_priority/models/partner_priority.py
+++ partner_priority/models/partner_priority.py
@@ -1,15 +1,20 @@
 # Copyright 2019 Patrick Wilson <patrickraymondwilson@gmail.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class PartnerPriority(models.Model):
-    _name = 'partner.priority'
-    _order = 'sequence'
-    _description = 'Partner Priority'
+    _name = "partner.priority"
+    _order = "sequence"
+    _description = "Partner Priority"
 
     name = fields.Char(string="Priority", required=True)
     description = fields.Text(required=True)
-    sequence = fields.Integer(required=True, default=lambda self: self.env[
-        'ir.sequence'].next_by_code('res.partner.priority') or 0)
+    sequence = fields.Integer(
+        required=True,
+        default=lambda self: self.env["ir.sequence"].next_by_code(
+            "res.partner.priority"
+        )
+        or 0,
+    )
