82b4230f26045c0f98629a72849a6253a2875ae3
newtratip <tharathip.chaweewongphan@gmail.com>
2020-03-17 18:32:47 +0700

    [IMP] partner_priority: black, isort

--- partner_priority/models/res_partner.py
+++ partner_priority/models/res_partner.py
@@ -1,10 +1,10 @@
 # Copyright 2019 Patrick Wilson <patrickraymondwilson@gmail.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
-    priority_id = fields.Many2one('partner.priority')
+    priority_id = fields.Many2one("partner.priority")
