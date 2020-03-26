1c3f158c728fa1d2c219b0e3ff5ea8051b2e01fc
Manuel Calero <manuel.calero@tecnativa.com>
2020-03-26 13:42:58 +0100

    [MIG] partner_deduplicate_by_ref: Migration to 13.0

--- partner_deduplicate_by_ref/wizards/partner_merge.py
+++ partner_deduplicate_by_ref/wizards/partner_merge.py
@@ -1,7 +1,7 @@
 # Copyright 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import fields, models
+from odoo import fields, models
 
 
 class BasePartnerMergeAutomaticWizard(models.TransientModel):
