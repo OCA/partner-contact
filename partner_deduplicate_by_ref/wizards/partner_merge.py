485ba31174aeefa86a36b6ea4a6bb81c3394374c
Manuel Calero <manuel.calero@tecnativa.com>
2020-03-26 13:40:19 +0100

    [IMP] partner_deduplicate_by_ref: black, isort

--- partner_deduplicate_by_ref/wizards/partner_merge.py
+++ partner_deduplicate_by_ref/wizards/partner_merge.py
@@ -7,22 +7,19 @@ from openerp import fields, models
 class BasePartnerMergeAutomaticWizard(models.TransientModel):
     _inherit = "base.partner.merge.automatic.wizard"
 
-    group_by_ref = fields.Boolean('Reference')
+    group_by_ref = fields.Boolean("Reference")
 
     def _generate_query(self, fields, maximum_group=100):
         """Inject the additional criteria 'ref IS NOT NULL' when needed.
         There's no better way to do it, as there are no hooks for adding
         this criteria regularly.
         """
-        query = super()._generate_query(
-            fields, maximum_group=maximum_group)
-        if 'ref' in fields:
-            if 'WHERE' in query:
-                index = query.find('WHERE')
-                query = (query[:index + 6] + "ref IS NOT NULL AND " +
-                         query[index + 6:])
+        query = super()._generate_query(fields, maximum_group=maximum_group)
+        if "ref" in fields:
+            if "WHERE" in query:
+                index = query.find("WHERE")
+                query = query[: index + 6] + "ref IS NOT NULL AND " + query[index + 6 :]
             else:
-                index = query.find(' GROUP BY')
-                query = (query[:index] + " WHERE ref IS NOT NULL" +
-                         query[index:])
+                index = query.find(" GROUP BY")
+                query = query[:index] + " WHERE ref IS NOT NULL" + query[index:]
         return query
