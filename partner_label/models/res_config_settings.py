181414280e9338cf6756ef8da521409a439e63d4
Holger Brunn <hbrunn@therp.nl>
2020-03-30 14:15:15 +0200

    [MIG][12.0] partner_label

--- partner_label/models/res_config_settings.py
+++ partner_label/models/res_config_settings.py
@@ -1,36 +1,38 @@
-# -*- coding: utf-8 -*-
-# © 2017 Therp BV <http://therp.nl>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from openerp import api, fields, models
+# Copyright 2017-2019 Therp BV <https://therp.nl>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import api, fields, models
 
 
-class BaseConfigSettings(models.TransientModel):
-    _inherit = 'base.config.settings'
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
 
     partner_labels_width = fields.Float(
         related='company_id.partner_labels_width', required=True,
+        readonly=False,
     )
     partner_labels_height = fields.Float(
         related='company_id.partner_labels_height', required=True,
+        readonly=False,
     )
     partner_labels_padding = fields.Float(
         related='company_id.partner_labels_padding', required=True,
+        readonly=False,
     )
     partner_labels_margin_top = fields.Float(
         related='company_id.partner_labels_margin_top',
-        required=True,
+        required=True, readonly=False,
     )
     partner_labels_margin_bottom = fields.Float(
         related='company_id.partner_labels_margin_bottom',
-        required=True,
+        required=True, readonly=False,
     )
     partner_labels_margin_left = fields.Float(
         related='company_id.partner_labels_margin_left',
-        required=True,
+        required=True, readonly=False,
     )
     partner_labels_margin_right = fields.Float(
         related='company_id.partner_labels_margin_right',
-        required=True,
+        required=True, readonly=False,
     )
     partner_labels_paperformat_id = fields.Many2one(
         'report.paperformat', string='Paperformat', required=True,
@@ -57,7 +59,8 @@ class BaseConfigSettings(models.TransientModel):
 
     @api.multi
     def action_partner_labels_preview(self):
-        return self.env['report'].get_action(
+        return self.env.ref(
+            'partner_label.report_res_partner_label'
+        ).report_action(
             self.env['res.partner'].search([], limit=100),
-            'partner_label.view_res_partner_label',
         )
--- partner_label/tests/test_partner_label.py
+++ partner_label/tests/test_partner_label.py
@@ -1,12 +1,12 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 Therp BV <http://therp.nl>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from openerp.tests.common import TransactionCase
+# Copyright 2017 Therp BV <https://therp.nl>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo.tests.common import TransactionCase
 
 
 class TestPartnerLabel(TransactionCase):
     def test_partner_label(self):
-        settings = self.env['base.config.settings'].create({})
+        settings = self.env['res.config.settings'].create({})
+        settings.refresh()
         self.assertItemsEqual(
             settings.action_partner_labels_preview()['context']['active_ids'],
             self.env['res.partner'].search([], limit=100).ids,
@@ -17,10 +17,10 @@ class TestPartnerLabel(TransactionCase):
             .paperformat_id
         )
         settings.partner_labels_paperformat_id = self.env.ref(
-            'report.paperformat_us'
+            'base.paperformat_us'
         ).id,
         self.assertEqual(
             self.env.ref('partner_label.report_res_partner_label')
             .paperformat_id,
-            self.env.ref('report.paperformat_us')
+            self.env.ref('base.paperformat_us')
         )
