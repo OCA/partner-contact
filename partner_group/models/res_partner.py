0ec88e3949a5521f0424008999a3c52797cd79e7
Sergio Teruel <sergio.teruel@tecnativa.com>
2020-03-30 17:18:37 +0200

    [IMP] partner_group: black, isort

--- partner_group/models/res_partner.py
+++ partner_group/models/res_partner.py
@@ -4,15 +4,12 @@ from odoo import api, fields, models
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
-    group_id = fields.Many2one(
-        comodel_name='res.partner',
-        string='Group',
-    )
+    group_id = fields.Many2one(comodel_name="res.partner", string="Group",)
 
     @api.model
     def _commercial_fields(self):
         res = super()._commercial_fields()
-        res.append('group_id')
+        res.append("group_id")
         return res
