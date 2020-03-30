56fb4a9860b597204685fc15308217f68c86cebb
Holger Brunn <hbrunn@therp.nl>
2020-03-30 14:15:15 +0200

    [MIG][12.0] partner_label

--- partner_label/__manifest__.py
+++ partner_label/__manifest__.py
@@ -1,16 +1,14 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 Therp BV <http://therp.nl>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright 2019 Therp BV <https://therp.nl>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Partner labels",
-    "version": "10.0.1.0.0",
+    "version": "12.0.1.0.0",
     "author": "Therp BV,Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Base",
     "summary": "Print partner labels",
     "depends": [
         'base_setup',
-        'report',
     ],
     "data": [
         "views/base_config_settings.xml",
