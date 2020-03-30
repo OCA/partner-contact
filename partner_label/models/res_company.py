0f91c8057827f0257dd0627f1516089c1d402ce5
Holger Brunn <hbrunn@therp.nl>
2020-03-30 14:15:15 +0200

    [MIG][12.0] partner_label

--- partner_label/models/res_company.py
+++ partner_label/models/res_company.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
-# © 2017 Therp BV <http://therp.nl>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from openerp import fields, models
+# Copyright 2017-2019 Therp BV <https://therp.nl>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import fields, models
 
 
 class ResCompany(models.Model):
