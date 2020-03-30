5cd9493857a366767a7b868f49109e185b3648f8
Sergio Teruel <sergio.teruel@tecnativa.com>
2020-03-30 17:18:38 +0200

    [MIG] partner_group: Migration to v13.0

--- partner_group/__manifest__.py
+++ partner_group/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Partner Group",
     "summary": "Group partners by groups (other partner)",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "development_status": "Beta",
     "category": "Contact",
     "website": "https://github.com/OCA/partner-contact",
@@ -11,6 +11,6 @@
     "license": "AGPL-3",
     "application": False,
     "installable": True,
-    "depends": ["base",],
-    "data": ["views/res_partner_views.xml",],
+    "depends": ["base"],
+    "data": ["views/res_partner_views.xml"],
 }
