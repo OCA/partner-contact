9d900dac1dbaf1f65158869956c6501aa6d63f38
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-04-02 21:47:15 +0200

    [FIX+IMP] base_location: Re-import process
    * Performance was very poor due to several reasons:
    
      * City name translatable.
      * Lack on indexes when deleting obsolete records.
      * Some prefetching.
    * City was not being searched also by state, having the chance of returning
      incorrect matches if same city name on different states.
    * Previous code was removing all previous records of zip entries!
    * Remove possible obsolete FK keys after migrating from v11.

--- base_location_geonames_import/__manifest__.py
+++ base_location_geonames_import/__manifest__.py
@@ -1,14 +1,14 @@
 # Copyright 2014-2016 Akretion (Alexis de Lattre
 #                     <alexis.delattre@akretion.com>)
 # Copyright 2014 Lorenzo Battistini <lorenzo.battistini@agilebg.com>
-# Copyright 2016-2019 Tecnativa - Pedro M. Baeza
+# Copyright 2016-2020 Tecnativa - Pedro M. Baeza
 # Copyright 2017 Eficent Business and IT Consulting Services, S.L.
 #                <contact@eficent.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Base Location Geonames Import",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.0.1",
     "category": "Partner Management",
     "license": "AGPL-3",
     "summary": "Import zip entries from Geonames",
