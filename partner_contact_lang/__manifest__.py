2f903f6b735000f5403ce67ffb00987de97e2616
Pedro <pedro.gonzalez@redcor.ch>
2020-04-05 11:19:47 +0200

    [MIG] 13.0 partner_contact_lang

--- partner_contact_lang/__manifest__.py
+++ partner_contact_lang/__manifest__.py
@@ -1,14 +1,16 @@
 # Copyright 2016 Tecnativa - Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # Copyright 2017 Tecnativa - Vicent Cubells <vicent.cubells@tecnativa.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright 2020 Pesol - Pedro Evaristo Gonzalez Sanchez <pedro.gonzalez@pesol.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Manage language in contacts",
-    "version": "11.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Sales",
     "license": "AGPL-3",
     "author": "Tecnativa," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/partner-contact/",
-    "depends": ["base",],
+    "depends": ["base"],
+    "data": ["views/res_partner_view.xml"],
     "installable": True,
 }
