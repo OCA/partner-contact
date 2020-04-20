f2d990a9e0e88c436f0e1d60d71a704895500c2a
Manuel Claeys Bouuaert <manuel.claeys.b@gmail.com>
2020-04-20 11:59:31 +0200

    [MIG] portal_partner_select_all: reverse migration v12 to v11

--- portal_partner_select_all/__manifest__.py
+++ portal_partner_select_all/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Portal Partner Select All',
-    'version': '12.0.1.0.1',
+    'version': '11.0.1.0.1',
     'category': 'Custom',
     'author': 'Tecnativa,'
               'Odoo Community Association (OCA)',
