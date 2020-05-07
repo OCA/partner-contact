f82f218eefbd7d397914730f49251aea06e02193
Harald Panten <harald.panten@sygel.es>
2020-05-07 20:09:21 +0200

    [IMP] partner_contact_department: black, isort, prettier

--- partner_contact_department/__manifest__.py
+++ partner_contact_department/__manifest__.py
@@ -4,22 +4,19 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Partner Contact Department',
+    "name": "Partner Contact Department",
     "summary": "Assign contacts to departments",
-    'version': '12.0.1.0.0',
-    'category': 'Customer Relationship Management',
-    'author': 'Tecnativa, '
-              'Odoo Community Association (OCA)',
+    "version": "13.0.1.0.0",
+    "category": "Customer Relationship Management",
+    "author": "Tecnativa, " "Odoo Community Association (OCA)",
     "license": "AGPL-3",
-    'website': 'https://github.com/OCA/partner-contact',
+    "website": "https://github.com/OCA/partner-contact",
     "application": False,
-    'depends': [
-        'contacts',
-    ],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/res_partner_department_view.xml',
-        'views/res_partner_view.xml',
+    "depends": ["contacts"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/res_partner_department_view.xml",
+        "views/res_partner_view.xml",
     ],
     "installable": True,
 }
