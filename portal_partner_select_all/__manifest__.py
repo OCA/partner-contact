f595d7bf6d68655f6d5573caaeddb96cd0631866
Stephan Keller <MiStK@gmx.de>
2020-05-09 17:06:10 +0200

    [MIG] portal_partner_select_all: Migration to 13.0

--- portal_partner_select_all/__manifest__.py
+++ portal_partner_select_all/__manifest__.py
@@ -2,12 +2,12 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Portal Partner Select All",
-    "version": "12.0.1.0.2",
+    "version": "13.0.1.0.0",
     "category": "Custom",
-    "author": "Tecnativa," "Odoo Community Association (OCA)",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://www.github.com/OCA/partner-contact",
     "license": "AGPL-3",
-    "depends": ["portal",],
-    "data": ["wizard/portal_wizard.xml",],
+    "depends": ["portal"],
+    "data": ["wizard/portal_wizard.xml"],
     "installable": True,
 }
