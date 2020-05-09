670ccf6c47890448db84e06122e0c8aee6e2136c
Stephan Keller <MiStK@gmx.de>
2020-05-09 16:58:49 +0200

    [IMP] portal_partner_select_all: black, isort, prettier

--- portal_partner_select_all/wizard/portal_wizard.py
+++ portal_partner_select_all/wizard/portal_wizard.py
@@ -6,20 +6,16 @@ from odoo import api, fields, models
 class PortalWizard(models.TransientModel):
     _inherit = "portal.wizard"
 
-    set_all_users = fields.Boolean(
-        string="Invite all the contacts",
-        default=False,
-    )
+    set_all_users = fields.Boolean(string="Invite all the contacts", default=False,)
 
-    @api.onchange('set_all_users')
+    @api.onchange("set_all_users")
     def onchange_set_all_users(self):
         """Toggle between select all partners and the default"""
         if not self.set_all_users:
             for user in self.user_ids:
-                user.in_portal = (
-                    user.partner_id.user_ids and
-                    user.partner_id.user_ids[0].has_group("base.group_portal")
-                )
+                user.in_portal = user.partner_id.user_ids and user.partner_id.user_ids[
+                    0
+                ].has_group("base.group_portal")
         else:
             not_in_portal = self.user_ids.filtered(lambda x: not x.in_portal)
-            not_in_portal.update({'in_portal': True})
+            not_in_portal.update({"in_portal": True})
