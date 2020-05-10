3e1d00f9915eac7fad89e61c46f4712954cbe0a4
Stephan Keller <MiStK@gmx.de>
2020-05-10 09:41:31 +0200

    [IMP] partner_disable_gravatar: black, isort, prettier

--- partner_disable_gravatar/__manifest__.py
+++ partner_disable_gravatar/__manifest__.py
@@ -3,13 +3,13 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Partner Disable Gravatar',
-    'version': '12.0.1.0.0',
-    'category': 'Tools',
-    'license': 'AGPL-3',
-    'summary': 'Disable automatic connection to gravatar.com',
-    'author': 'Akretion,Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/partner-contact',
-    'depends': ['base'],
-    'installable': True,
+    "name": "Partner Disable Gravatar",
+    "version": "12.0.1.0.0",
+    "category": "Tools",
+    "license": "AGPL-3",
+    "summary": "Disable automatic connection to gravatar.com",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/partner-contact",
+    "depends": ["base"],
+    "installable": True,
 }
