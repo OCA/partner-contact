3d47bc01a9b7c547d8c1a1350caf91fb9dac2b2b
Ajay Javiya <javiyaajay1987@gmail.com>
2020-05-12 18:11:31 +0530

    [MIG][13.0] partner_label

--- partner_label/tests/test_partner_label.py
+++ partner_label/tests/test_partner_label.py
@@ -5,22 +5,22 @@ from odoo.tests.common import TransactionCase
 
 class TestPartnerLabel(TransactionCase):
     def test_partner_label(self):
-        settings = self.env['res.config.settings'].create({})
+        settings = self.env["res.config.settings"].create({})
         settings.refresh()
         self.assertItemsEqual(
-            settings.action_partner_labels_preview()['context']['active_ids'],
-            self.env['res.partner'].search([], limit=100).ids,
+            settings.action_partner_labels_preview()["context"]["report_action"][
+                "context"
+            ]["active_ids"],
+            self.env["res.partner"].search([], limit=100).ids,
         )
         self.assertEqual(
             settings.partner_labels_paperformat_id,
-            self.env.ref('partner_label.report_res_partner_label')
-            .paperformat_id
+            self.env.ref("partner_label.report_res_partner_label").paperformat_id,
         )
-        settings.partner_labels_paperformat_id = self.env.ref(
-            'base.paperformat_us'
-        ).id,
+        settings.partner_labels_paperformat_id = (
+            self.env.ref("base.paperformat_us").id,
+        )
         self.assertEqual(
-            self.env.ref('partner_label.report_res_partner_label')
-            .paperformat_id,
-            self.env.ref('base.paperformat_us')
+            self.env.ref("partner_label.report_res_partner_label").paperformat_id,
+            self.env.ref("base.paperformat_us"),
         )
