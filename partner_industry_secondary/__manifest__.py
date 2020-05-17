27871cac8291ac267deabe364bea8f761459ee38
Stephan Keller <MiStK@gmx.de>
2020-05-17 12:58:34 +0200

    [MIG] partner_industry_secondary: Migration to 13.0

--- partner_industry_secondary/__manifest__.py
+++ partner_industry_secondary/__manifest__.py
@@ -1,18 +1,18 @@
 # Copyright 2015 Antiun Ingenieria S.L. - Javier Iniesta
 # Copyright 2016-2017 Tecnativa S.L. - Vicent Cubells
 # Copyright 2019 Tecnativa S.L. - Cristina Martin R.
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Partner Industry Secondary",
     "summary": "Add secondary partner industries",
-    "version": "12.0.1.0.1",
+    "version": "13.0.1.0.0",
     "category": "Sales",
     "website": "https://github.com/OCA/partner-contact",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "installable": True,
-    "depends": ["base_setup", "contacts",],
+    "depends": ["base_setup", "contacts"],
     "data": [
         "security/ir.model.access.csv",
         "security/partner_industry_security.xml",
