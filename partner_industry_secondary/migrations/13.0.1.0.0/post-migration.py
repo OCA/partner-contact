603465058f25d0e9345d2e2e147df6e6ffbd749a
Stephan Keller <MiStK@gmx.de>
2020-05-17 12:58:34 +0200

    [MIG] partner_industry_secondary: Migration to 13.0

--- partner_industry_secondary/migrations/13.0.1.0.0/post-migration.py
+++ partner_industry_secondary/migrations/13.0.1.0.0/post-migration.py
@@ -1,5 +1,5 @@
 # Copyright 2019 Tecnativa - Cristina Martin R.
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from openupgradelib import openupgrade
 
--- partner_industry_secondary/tests/test_res_partner_industry.py
+++ partner_industry_secondary/tests/test_res_partner_industry.py
@@ -1,9 +1,9 @@
 # Copyright 2015 Antiun Ingenieria S.L. - Javier Iniesta
 # Copyright 2016 Tecnativa S.L. - Vicent Cubells
 # Copyright 2016 Tecnativa S.L. - Pedro M. Baeza
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo.exceptions import ValidationError
+from odoo.exceptions import UserError, ValidationError
 from odoo.tests import common
 
 
@@ -12,9 +12,9 @@ class TestResPartnerIndustry(common.SavepointCase):
     def setUpClass(cls):
         super(TestResPartnerIndustry, cls).setUpClass()
         cls.industry_model = cls.env["res.partner.industry"]
-        cls.industry_main = cls.industry_model.create({"name": "Test",})
+        cls.industry_main = cls.industry_model.create({"name": "Test"})
         cls.industry_child = cls.industry_model.create(
-            {"name": "Test child", "parent_id": cls.industry_main.id,}
+            {"name": "Test child", "parent_id": cls.industry_main.id}
         )
 
     def test_check_industries(self):
@@ -27,11 +27,17 @@ class TestResPartnerIndustry(common.SavepointCase):
                 }
             )
 
-    def test_check_recursion(self):
+    def test_check_copy(self):
+        industry_copy = self.industry_child.copy()
+        self.assertEqual(industry_copy.name, "Test child 2")
+
+    def test_check_uniq_name(self):
         with self.assertRaises(ValidationError):
+            self.industry_model.create({"name": "Test"})
+
+    def test_check_recursion(self):
+        with self.assertRaises(UserError):
             self.industry_main.parent_id = self.industry_child.id
 
     def test_name(self):
-        self.assertEqual(
-            self.industry_child.name_get()[0][1], "Test / Test child",
-        )
+        self.assertEqual(self.industry_child.display_name, "Test / Test child")
