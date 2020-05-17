e34cfde42f3c7b5be61ab0e373643880b6b4ae63
Stephan Keller <MiStK@gmx.de>
2020-05-17 12:58:34 +0200

    [MIG] partner_industry_secondary: Migration to 13.0

--- partner_industry_secondary/models/res_partner.py
+++ partner_industry_secondary/models/res_partner.py
@@ -1,7 +1,7 @@
 # Copyright 2015 Antiun Ingenieria S.L. - Javier Iniesta
 # Copyright 2016 Tecnativa S.L. - Vicent Cubells
 # Copyright 2018 Eficent Business and IT Consulting Services, S.L.
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import _, api, exceptions, fields, models
 
