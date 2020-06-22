07f662cd3f3880586879cb6600396de4c3ec274a
sbejaoui <souheil.bejaoui@acsone.eu>
2020-06-22 14:04:47 +0200

    [13.0][MIG] - migration partner_identification_gln

--- partner_identification_gln/tests/test_gln.py
+++ partner_identification_gln/tests/test_gln.py
@@ -6,75 +6,65 @@ from odoo.tests.common import SavepointCase
 
 
 class TestGLN(SavepointCase):
+    def setUp(self):
+        super(TestGLN, self).setUp()
+        self.partner = self.env["res.partner"].create({"name": "TestGLN"})
+        self.partner2 = self.env["res.partner"].create({"name": "TestGLN2"})
+        pc = self.env.ref(
+            "partner_identification_gln." "partner_identification_gln_number_category"
+        )
+        self.partner_id_category = pc
 
-    def setUp(cls):
-        super(TestGLN, cls).setUp()
-        cls.partner = cls.env['res.partner'].create({'name': 'TestGLN'})
-        cls.partner2 = cls.env['res.partner'].create({'name': 'TestGLN2'})
-        pc = cls.env.ref('partner_identification_gln.'
-                         'partner_identification_gln_number_category')
-        cls.partner_id_category = pc
-
-        pc_gcp = cls.env.ref('partner_identification_gln.'
-                             'partner_identification_gcp_number_category')
-        cls.partner_id_gcp_category = pc_gcp
+        pc_gcp = self.env.ref(
+            "partner_identification_gln." "partner_identification_gcp_number_category"
+        )
+        self.partner_id_gcp_category = pc_gcp
 
     def test_gln(self):
         # Good GLN
-        vals = {'name': '5450534005852',
-                'category_id': self.partner_id_category.id
-                }
+        vals = {"name": "5450534005852", "category_id": self.partner_id_category.id}
 
-        self.partner.write({'id_numbers': [(0, 0, vals)]})
+        self.partner.write({"id_numbers": [(0, 0, vals)]})
         id_number = self.partner.id_numbers[0]
 
-        self.assertEqual(id_number.name, '5450534005852')
+        self.assertEqual(id_number.name, "5450534005852")
 
         # Duplicate GLN
-        vals = {'name': '5450534005852',
-                'category_id': self.partner_id_category.id
-                }
+        vals = {"name": "5450534005852", "category_id": self.partner_id_category.id}
 
         with self.assertRaises(ValidationError):
-            self.partner2.write({'id_numbers': [(0, 0, vals)]})
+            self.partner2.write({"id_numbers": [(0, 0, vals)]})
 
         # Bad GLN
-        vals = {'name': '5450534001716',
-                'category_id': self.partner_id_category.id
-                }
+        vals = {"name": "5450534001716", "category_id": self.partner_id_category.id}
         with self.assertRaises(ValidationError):
-            self.partner.write({'id_numbers': [(0, 0, vals)]})
+            self.partner.write({"id_numbers": [(0, 0, vals)]})
 
     def test_gln_with_no_number(self):
         self.assertFalse(
-            self.partner_id_category.validate_res_partner_gln(id_number=None))
+            self.partner_id_category.validate_res_partner_gln(id_number=None)
+        )
 
     def test_gcp(self):
         # Good GLN
-        vals = {'name': '545053',
-                'category_id': self.partner_id_gcp_category.id
-                }
-        self.partner.write({'id_numbers': [(0, 0, vals)]})
+        vals = {"name": "545053", "category_id": self.partner_id_gcp_category.id}
+        self.partner.write({"id_numbers": [(0, 0, vals)]})
         id_number = self.partner.id_numbers[0]
 
-        self.assertEqual(id_number.name, '545053')
+        self.assertEqual(id_number.name, "545053")
 
         # Duplicate GLN
-        vals = {'name': '545053',
-                'category_id': self.partner_id_gcp_category.id
-                }
+        vals = {"name": "545053", "category_id": self.partner_id_gcp_category.id}
 
         with self.assertRaises(ValidationError):
-            self.partner2.write({'id_numbers': [(0, 0, vals)]})
+            self.partner2.write({"id_numbers": [(0, 0, vals)]})
 
         # Bad GLN
-        vals = {'name': '5450534001716',
-                'category_id': self.partner_id_gcp_category.id
-                }
+        vals = {"name": "5450534001716", "category_id": self.partner_id_gcp_category.id}
         with self.assertRaises(ValidationError):
-            self.partner.write({'id_numbers': [(0, 0, vals)]})
+            self.partner.write({"id_numbers": [(0, 0, vals)]})
 
     def test_gcp_with_no_number(self):
         self.assertFalse(
-            self.partner_id_gcp_category.validate_res_partner_gln(
-                id_number=None))
+            self.partner_id_gcp_category.validate_res_partner_gln(id_number=None)
+        )
