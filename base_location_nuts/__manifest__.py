8365c0d600eb1338dbe1185d2054ec0f2d4346ba
Andrea <a.stirpe@onestein.nl>
2020-06-24 11:06:08 +0200

    Pre-commit

--- base_location_nuts/__manifest__.py
+++ base_location_nuts/__manifest__.py
@@ -4,26 +4,20 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'NUTS Regions',
-    'category': 'Localisation/Europe',
-    'version': '12.0.1.0.0',
-    'depends': [
-        'contacts',
+    "name": "NUTS Regions",
+    "category": "Localisation/Europe",
+    "version": "12.0.1.0.0",
+    "depends": ["contacts",],
+    "data": [
+        "views/res_country_view.xml",
+        "views/res_partner_nuts_view.xml",
+        "views/res_partner_view.xml",
+        "wizard/nuts_import_view.xml",
+        "security/ir.model.access.csv",
     ],
-    'data': [
-        'views/res_country_view.xml',
-        'views/res_partner_nuts_view.xml',
-        'views/res_partner_view.xml',
-        'wizard/nuts_import_view.xml',
-        'security/ir.model.access.csv',
-    ],
-    'images': [
-        'images/new_fields.png',
-    ],
-    'author': 'Tecnativa, '
-              'Agile Business Group, '
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/partner-contact/',
-    'license': 'AGPL-3',
-    'installable': True,
+    "images": ["images/new_fields.png",],
+    "author": "Tecnativa, " "Agile Business Group, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/partner-contact/",
+    "license": "AGPL-3",
+    "installable": True,
 }
