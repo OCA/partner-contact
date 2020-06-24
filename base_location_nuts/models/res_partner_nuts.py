21dabc526583ece249d52d7a6cf9709fe08b9c6b
Andrea <a.stirpe@onestein.nl>
2020-06-24 11:06:08 +0200

    Pre-commit

--- base_location_nuts/models/res_partner_nuts.py
+++ base_location_nuts/models/res_partner_nuts.py
@@ -6,25 +6,27 @@ from odoo import fields, models
 
 
 class ResPartnerNuts(models.Model):
-    _name = 'res.partner.nuts'
-    _order = 'parent_path'
-    _parent_order = 'name'
+    _name = "res.partner.nuts"
+    _order = "parent_path"
+    _parent_order = "name"
     _parent_store = True
-    _description = 'NUTS Item'
+    _description = "NUTS Item"
 
     # NUTS fields
     level = fields.Integer(required=True)
     code = fields.Char(required=True)
     name = fields.Char(required=True, translate=True)
-    country_id = fields.Many2one(comodel_name='res.country', string='Country',
-                                 required=True)
-    state_id = fields.Many2one(comodel_name='res.country.state',
-                               string='State')
+    country_id = fields.Many2one(
+        comodel_name="res.country", string="Country", required=True
+    )
+    state_id = fields.Many2one(comodel_name="res.country.state", string="State")
     not_updatable = fields.Boolean()
     # Parent hierarchy
-    parent_id = fields.Many2one(comodel_name='res.partner.nuts',
-                                ondelete='restrict')
+    parent_id = fields.Many2one(comodel_name="res.partner.nuts", ondelete="restrict")
     parent_path = fields.Char(index=True)
-    child_ids = fields.One2many(comodel_name='res.partner.nuts',
-                                inverse_name='parent_id', string='Children',
-                                oldname='children')
+    child_ids = fields.One2many(
+        comodel_name="res.partner.nuts",
+        inverse_name="parent_id",
+        string="Children",
+        oldname="children",
+    )
