62baeb15c8c54cdb52c994571de2fcc42834e0ce
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-07-08 17:22:15 +0200

    [FIX] partner_external_map: Resilient coordinate test
    As coordinates in partner are float, they are affected by the rounding problem we
    all know, so sometimes (depending on the Python interpreter used) the generated URL
    contains extra digits. Example:
    
    39.158370000000005 instead of 39.15837
    
    so with this change on the test, we make it resilient to this problem.

--- partner_external_map/__manifest__.py
+++ partner_external_map/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     'name': 'Partner External Maps',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.0.1',
     'category': 'Extra Tools',
     'license': 'AGPL-3',
     'summary': 'Add Map and Map Routing buttons on partner form to '
