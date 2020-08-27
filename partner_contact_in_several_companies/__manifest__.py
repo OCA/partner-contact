1a88fb0fca6bce6eb7f73ec8e277d1e2026a5210
Fábio Oliveira <foliveira@odoogap.com>
2020-08-27 14:44:54 +0100

    [MIG] partner_contact_in_several_companies: Migration to 13.0
    [IMP] pre-commit version downgrade
    
    [IMP] check travis failure_3
    
    [IMP] check travis failure_2
    
    [IMP] check travis failure
    
    [IMP] fix minor changes in manyfest and view

--- partner_contact_in_several_companies/__manifest__.py
+++ partner_contact_in_several_companies/__manifest__.py
@@ -3,20 +3,15 @@
 {
     "name": "Contacts in several partners",
     "summary": "Allow to have one contact in several partners",
-    "version": "12.0.2.0.0",
+    "version": "13.0.1.0.0",
     "category": "Customer Relationship Management",
     "website": "https://github.com/OCA/partner-contact",
     "author": "Nicolas JEUDY, Odoo Community Association (OCA),Odoo SA",
     "license": "AGPL-3",
-    "depends": [
-        "base",
-        "partner_contact_personal_information_page",
-    ],
-    "data": [
-        "views/res_partner.xml",
-    ],
-    "demo": [
-        "demo/res_partner.xml",
-        "demo/ir_actions.xml",
-    ],
+    "depends": ["base", "contacts", "partner_contact_personal_information_page"],
+    "data": ["views/res_partner.xml"],
+    "demo": ["demo/res_partner.xml", "demo/ir_actions.xml"],
+    "application": False,
+    "installable": True,
+    "auto_install": False,
 }
