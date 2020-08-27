21d20f2fc154236893913499b56a69155d182e8e
Fábio Oliveira <foliveira@odoogap.com>
2020-08-27 14:44:54 +0100

    [MIG] partner_contact_in_several_companies: Migration to 13.0

--- partner_contact_in_several_companies/__manifest__.py
+++ partner_contact_in_several_companies/__manifest__.py
@@ -1,15 +1,15 @@
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-
+# -*- coding: utf-8 -*-
 {
     "name": "Contacts in several partners",
     "summary": "Allow to have one contact in several partners",
-    "version": "12.0.2.0.0",
+    "version": "13.0.1.0.0",
     "category": "Customer Relationship Management",
     "website": "https://github.com/OCA/partner-contact",
-    "author": "Nicolas JEUDY, Odoo Community Association (OCA),Odoo SA",
+    "author": "Fabio Oliveira, Odoo Community Association (OCA),Odoo SA",
     "license": "AGPL-3",
     "depends": [
         "base",
+        "contacts",
         "partner_contact_personal_information_page",
     ],
     "data": [
@@ -19,4 +19,7 @@
         "demo/res_partner.xml",
         "demo/ir_actions.xml",
     ],
+    "application": False,
+    "installable": True,
+    "auto_install": False,
 }
