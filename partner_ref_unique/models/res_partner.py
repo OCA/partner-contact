42b2ef29ed8f6f6890a0343e07215040b747485d
João Marques <joao.marques@tecnativa.com>
2020-09-07 09:10:32 +0100

    [IMP] partner_ref_unique: black, isort, prettier

--- partner_ref_unique/models/res_partner.py
+++ partner_ref_unique/models/res_partner.py
@@ -11,28 +11,29 @@ class ResPartner(models.Model):
     # This related is needed in order to trigger the check when changing the
     # value on res.company
     partner_ref_unique = fields.Selection(
-        related='company_id.partner_ref_unique', store=True,
+        related="company_id.partner_ref_unique", store=True,
     )
 
     @api.multi
-    @api.constrains('ref', 'is_company', 'company_id', 'partner_ref_unique')
+    @api.constrains("ref", "is_company", "company_id", "partner_ref_unique")
     def _check_ref(self):
         for partner in self:
             mode = partner.partner_ref_unique
-            if (partner.ref and (
-                    mode == 'all' or
-                    (mode == 'companies' and partner.is_company))):
+            if partner.ref and (
+                mode == "all" or (mode == "companies" and partner.is_company)
+            ):
                 domain = [
-                    ('id', '!=', partner.id),
-                    ('ref', '=', partner.ref),
+                    ("id", "!=", partner.id),
+                    ("ref", "=", partner.ref),
                 ]
-                if mode == 'companies':
-                    domain.append(('is_company', '=', True))
+                if mode == "companies":
+                    domain.append(("is_company", "=", True))
                 other = self.search(domain)
 
                 # active_test is False when called from
                 # base.partner.merge.automatic.wizard
                 if other and self.env.context.get("active_test", True):
                     raise ValidationError(
-                        _("This reference is equal to partner '%s'") %
-                        other[0].display_name)
+                        _("This reference is equal to partner '%s'")
+                        % other[0].display_name
+                    )
