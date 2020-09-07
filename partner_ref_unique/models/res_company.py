1d10084576caa5fd71d535426fec516a23b329dd
João Marques <joao.marques@tecnativa.com>
2020-09-07 16:48:31 +0100

    [MIG] partner_ref_unique: Migration to 13.0

--- partner_ref_unique/models/res_company.py
+++ partner_ref_unique/models/res_company.py
@@ -1,4 +1,5 @@
 # Copyright 2016 Antonio Espinosa
+# Copyright 2020 Tecnativa - João Marques
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
@@ -16,3 +17,17 @@ class ResCompany(models.Model):
         string="Unique partner reference for",
         default="none",
     )
+
+    def write(self, vals):
+        """Launch manually the constraint check in partners as current ORM
+        doesn't trigger the constraint on related fields.
+        """
+        res = super().write(vals)
+        if "partner_ref_unique" in vals:
+            partners = (
+                self.env["res.partner"]
+                .with_context(active_test=False)
+                .search([("company_id", "in", [False] + self.ids)])
+            )
+            partners._check_ref()
+        return res
