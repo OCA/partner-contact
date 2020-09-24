fbe9854b669168ef8b0146ce3448806f2bc3f6d5
Fábio Oliveira <foliveira@odoogap.com>
2020-09-24 14:47:46 +0100

    [IMP] check travis failure

--- partner_contact_in_several_companies/__manifest__.py
+++ partner_contact_in_several_companies/__manifest__.py
@@ -8,18 +8,9 @@
     "website": "https://github.com/OCA/partner-contact",
     "author": "Nicolas JEUDY, Odoo Community Association (OCA),Odoo SA",
     "license": "AGPL-3",
-    "depends": [
-        "base",
-        "contacts",
-        "partner_contact_personal_information_page",
-    ],
-    "data": [
-        "views/res_partner.xml",
-    ],
-    "demo": [
-        "demo/res_partner.xml",
-        "demo/ir_actions.xml",
-    ],
+    "depends": ["base", "contacts", "partner_contact_personal_information_page"],
+    "data": ["views/res_partner.xml"],
+    "demo": ["demo/res_partner.xml", "demo/ir_actions.xml"],
     "application": False,
     "installable": True,
     "auto_install": False,
