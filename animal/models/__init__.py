8077211aa864cb1f162f3acb9a35ae7070810f29
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2020-10-06 15:36:58 -0500

    [ADD] animal

---
+++ animal/models/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (C) 2020 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from . import (
+    animal_species,
+    animal_breed,
+    animal_color,
+    animal,
+)
---
+++ animal/readme/USAGE.rst
@@ -0,0 +1,3 @@
+* Go to Animals
+* Create an animal by entering his name and selecting his gender, species, breed and
+  color.
