aa7c7e2c1e4de6c6f843668551fee3ad47e495a7
Capriatto <michu-1@hotmail.com>
2020-10-06 11:22:25 -0500

    [IMP] partner_other_names renamed -C

--- partner_multi_name/__manifest__.py
+++ partner_multi_name/__manifest__.py
@@ -2,14 +2,14 @@
 # Copyright 2015 Grupo ESOC Ingeniería de Servicios, S.L.U. - Jairo Llopis
 # Copyright 2015 Antiun Ingenieria S.L. - Antonio Espinosa
 # Copyright 2017 Tecnativa - Pedro M. Baeza
-# Copyright 2018 Guillermo Montoya <Github@guillermm>
-# Copyright 2018 Joan Marín <Github@JoanMarin>
+# Copyright 2018 EXA Auto Parts S.A.S Guillermo Montoya <Github@guillermm>
+# Copyright 2018 EXA Auto Parts S.A.S Joan Marín <Github@JoanMarin>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    "name": "Partner other names",
+    "name": "Partner Multi Name",
     "summary": "Have split first and other names",
-    "version": "11.0.1.0.0",
+    "version": "10.0.1.0.0",
     "license": "AGPL-3",
     "website": "https://github.com/OCA/partner-contact",
     "author": "EXA Auto Parts Github@exaap, "
