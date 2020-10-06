7758b04f1471a2fb2ee2b3c1a2b86675f46c9e54
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2020-10-06 15:51:38 -0500

    [ADD] animal_owner

---
+++ animal_owner/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright (C) 2020 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Animal Owner",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "summary": "Add owner to the animal",
+    "author": "Open Source Integrators, Odoo Community Association (OCA)",
+    "maintainer": "Open Source Integrators",
+    "website": "https://github.com/OCA/partner-contact",
+    "depends": ["animal"],
+    "data": ["views/animal.xml", "views/res_partner.xml"],
+    "application": False,
+    "development_status": "Beta",
+    "maintainers": ["max3903"],
+}
