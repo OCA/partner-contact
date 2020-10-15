825bca85ca56235f274bec5754dfeac65086cdda
newtratip <tharathip.chaweewongphan@gmail.com>
2020-10-16 02:53:53 +0700

    [MIG] partner_contact_lang: Migration to 14.0

--- partner_contact_lang/__manifest__.py
+++ partner_contact_lang/__manifest__.py
@@ -5,10 +5,10 @@
 
 {
     "name": "Manage language in contacts",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Sales",
     "license": "AGPL-3",
-    "author": "Tecnativa," "Odoo Community Association (OCA)",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/partner-contact/",
     "depends": ["base"],
     "data": ["views/res_partner_view.xml"],
