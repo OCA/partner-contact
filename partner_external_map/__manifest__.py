d3a706b36c1f628900c02e2a61348b2bb9967a30
João Marques <joao.marques@tecnativa.com>
2020-11-02 08:22:23 +0000

    [IMP] partner_external_map: black, isort, prettier

--- partner_external_map/__manifest__.py
+++ partner_external_map/__manifest__.py
@@ -4,26 +4,22 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Partner External Maps',
-    'version': '12.0.1.0.1',
-    'category': 'Extra Tools',
-    'license': 'AGPL-3',
-    'summary': 'Add Map and Map Routing buttons on partner form to '
-               'open GMaps, OSM, Bing and others',
-    'author': 'Akretion, '
-              'Tecnativa, '
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/partner-contact',
-    'depends': [
-        'base',
+    "name": "Partner External Maps",
+    "version": "12.0.1.0.1",
+    "category": "Extra Tools",
+    "license": "AGPL-3",
+    "summary": "Add Map and Map Routing buttons on partner form to "
+    "open GMaps, OSM, Bing and others",
+    "author": "Akretion, " "Tecnativa, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/partner-contact",
+    "depends": ["base",],
+    "data": [
+        "views/res_partner_view.xml",
+        "views/map_website_view.xml",
+        "data/map_website_data.xml",
+        "views/res_users_view.xml",
+        "security/ir.model.access.csv",
     ],
-    'data': [
-        'views/res_partner_view.xml',
-        'views/map_website_view.xml',
-        'data/map_website_data.xml',
-        'views/res_users_view.xml',
-        'security/ir.model.access.csv',
-    ],
-    'post_init_hook': 'set_default_map_settings',
-    'installable': True,
+    "post_init_hook": "set_default_map_settings",
+    "installable": True,
 }
--- partner_external_map/models/res_partner.py
+++ partner_external_map/models/res_partner.py
@@ -3,16 +3,16 @@
 # Copyright 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, api, _
-from odoo.exceptions import UserError
 import logging
 
+from odoo import _, api, models
+from odoo.exceptions import UserError
 
 logger = logging.getLogger(__name__)
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
     @api.multi
     def _address_as_string(self):
@@ -30,17 +30,17 @@ class ResPartner(models.Model):
             addr.append(self.country_id.name)
         if not addr:
             raise UserError(_("Address missing on partner '%s'.") % self.name)
-        return ' '.join(addr)
+        return " ".join(addr)
 
     @api.model
     def _prepare_url(self, url, replace):
-        assert url, 'Missing URL'
+        assert url, "Missing URL"
         for key, value in replace.items():
             if not isinstance(value, str):
                 # for latitude and longitude which are floats
                 value = str(value)
             url = url.replace(key, value)
-        logger.debug('Final URL: %s', url)
+        logger.debug("Final URL: %s", url)
         return url
 
     @api.multi
@@ -49,26 +49,37 @@ class ResPartner(models.Model):
         map_website = self.env.user.context_map_website_id
         if not map_website:
             raise UserError(
-                _('Missing map provider: '
-                  'you should set it in your preferences.'))
-        if (map_website.lat_lon_url and hasattr(self, 'partner_latitude') and
-                self.partner_latitude and self.partner_longitude):
+                _("Missing map provider: " "you should set it in your preferences.")
+            )
+        if (
+            map_website.lat_lon_url
+            and hasattr(self, "partner_latitude")
+            and self.partner_latitude
+            and self.partner_longitude
+        ):
             url = self._prepare_url(
-                map_website.lat_lon_url, {
-                    '{LATITUDE}': self.partner_latitude,
-                    '{LONGITUDE}': self.partner_longitude})
+                map_website.lat_lon_url,
+                {
+                    "{LATITUDE}": self.partner_latitude,
+                    "{LONGITUDE}": self.partner_longitude,
+                },
+            )
         else:
             if not map_website.address_url:
                 raise UserError(
-                    _("Missing parameter 'URL that uses the address' "
-                      "for map website '%s'.") % map_website.name)
+                    _(
+                        "Missing parameter 'URL that uses the address' "
+                        "for map website '%s'."
+                    )
+                    % map_website.name
+                )
             url = self._prepare_url(
-                map_website.address_url,
-                {'{ADDRESS}': self._address_as_string()})
+                map_website.address_url, {"{ADDRESS}": self._address_as_string()}
+            )
         return {
-            'type': 'ir.actions.act_url',
-            'url': url,
-            'target': 'new',
+            "type": "ir.actions.act_url",
+            "url": url,
+            "target": "new",
         }
 
     @api.multi
@@ -76,36 +87,55 @@ class ResPartner(models.Model):
         self.ensure_one()
         if not self.env.user.context_route_map_website_id:
             raise UserError(
-                _('Missing route map website: '
-                  'you should set it in your preferences.'))
+                _(
+                    "Missing route map website: "
+                    "you should set it in your preferences."
+                )
+            )
         map_website = self.env.user.context_route_map_website_id
         if not self.env.user.context_route_start_partner_id:
             raise UserError(
-                _('Missing start address for route map: '
-                  'you should set it in your preferences.'))
+                _(
+                    "Missing start address for route map: "
+                    "you should set it in your preferences."
+                )
+            )
         start_partner = self.env.user.context_route_start_partner_id
-        if (map_website.route_lat_lon_url and
-                hasattr(self, 'partner_latitude') and
-                self.partner_latitude and self.partner_longitude and
-                start_partner.partner_latitude and
-                start_partner.partner_longitude):
+        if (
+            map_website.route_lat_lon_url
+            and hasattr(self, "partner_latitude")
+            and self.partner_latitude
+            and self.partner_longitude
+            and start_partner.partner_latitude
+            and start_partner.partner_longitude
+        ):
             url = self._prepare_url(  # pragma: no cover
-                map_website.route_lat_lon_url, {
-                    '{START_LATITUDE}': start_partner.partner_latitude,
-                    '{START_LONGITUDE}': start_partner.partner_longitude,
-                    '{DEST_LATITUDE}': self.partner_latitude,
-                    '{DEST_LONGITUDE}': self.partner_longitude})
+                map_website.route_lat_lon_url,
+                {
+                    "{START_LATITUDE}": start_partner.partner_latitude,
+                    "{START_LONGITUDE}": start_partner.partner_longitude,
+                    "{DEST_LATITUDE}": self.partner_latitude,
+                    "{DEST_LONGITUDE}": self.partner_longitude,
+                },
+            )
         else:
             if not map_website.route_address_url:
                 raise UserError(
-                    _("Missing route URL that uses the addresses "
-                        "for the map website '%s'") % map_website.name)
+                    _(
+                        "Missing route URL that uses the addresses "
+                        "for the map website '%s'"
+                    )
+                    % map_website.name
+                )
             url = self._prepare_url(
-                map_website.route_address_url, {
-                    '{START_ADDRESS}': start_partner._address_as_string(),
-                    '{DEST_ADDRESS}': self._address_as_string()})
+                map_website.route_address_url,
+                {
+                    "{START_ADDRESS}": start_partner._address_as_string(),
+                    "{DEST_ADDRESS}": self._address_as_string(),
+                },
+            )
         return {
-            'type': 'ir.actions.act_url',
-            'url': url,
-            'target': 'new',
+            "type": "ir.actions.act_url",
+            "url": url,
+            "target": "new",
         }
