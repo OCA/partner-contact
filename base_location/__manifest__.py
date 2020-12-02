f57778db268c9d4fc1f9c3cee71c044ec680d992
Yann Papouin <y.papouin@dec-industrie.com>
2020-12-02 23:36:47 +0100

    [14.0][FIX] base_location: Update name when changing city's state or country

--- base_location/__manifest__.py
+++ base_location/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Location management (aka Better ZIP)",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "depends": ["base_address_city", "contacts"],
     "author": (
         "Camptocamp,"
