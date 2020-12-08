cc44e3e5c69c7b087d201654d9ff3b4db0c8ce4d
Florian da Costa <florian.dacosta@akretion.com>
2020-12-08 15:35:10 +0100

    [MIG] partner_helper: Migration to 14.0

--- partner_helper/__manifest__.py
+++ partner_helper/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Partner Helper",
-    "version": "12.0.0.1.0",
+    "version": "14.0.1.0.0",
     "author": "Akretion,Odoo Community Association (OCA)",
     "maintainer": "Akretion",
     "category": "Warehouse",
