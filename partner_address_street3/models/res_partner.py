2195af21df57d3735835d74054fa050836fb91f4
Alexis de Lattre <alexis.delattre@akretion.com>
2020-12-14 19:37:55 +0100

    Black, isort and other pre-commit stuff

--- partner_address_street3/models/res_partner.py
+++ partner_address_street3/models/res_partner.py
@@ -7,19 +7,20 @@ from odoo import api, fields, models
 
 class ResPartner(models.Model):
     """Add third field in address"""
+
     _inherit = "res.partner"
 
-    street3 = fields.Char('Street 3')
+    street3 = fields.Char("Street 3")
 
     @api.model
     def _address_fields(self):
         res = super()._address_fields()
-        res.append('street3')
+        res.append("street3")
         return res
 
     def _display_address(self, without_company=False):
         """Remove empty lines which can happen when street3 field is empty."""
         res = super()._display_address(without_company=without_company)
-        while '\n\n' in res:
-            res = res.replace('\n\n', '\n')
+        while "\n\n" in res:
+            res = res.replace("\n\n", "\n")
         return res
