d4a7509f35822e878ebef3f29887f9a57cc80d24
skeller1 <MiStK@gmx.de>
2020-12-30 17:47:02 +0100

    [MIG] portal_partner_select_all: Migration to 14.0

--- portal_partner_select_all/__manifest__.py
+++ portal_partner_select_all/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Portal Partner Select All",
-    "version": "13.0.1.0.1",
+    "version": "14.0.1.0.0",
     "category": "Custom",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/partner-contact",
