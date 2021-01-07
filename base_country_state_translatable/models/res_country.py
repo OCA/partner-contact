13c2e57e7c2d11bb8a73c2ce85fcbb70ae467541
dsolanki <dhara.solanki@initos.com>
2021-01-07 10:20:18 +0530

    [IMP] base_country_state_translatable: black, isort, prettier

--- base_country_state_translatable/models/res_country.py
+++ base_country_state_translatable/models/res_country.py
@@ -5,6 +5,6 @@ from odoo import fields, models
 
 
 class CountryState(models.Model):
-    _inherit = 'res.country.state'
+    _inherit = "res.country.state"
 
     name = fields.Char(translate=True)
