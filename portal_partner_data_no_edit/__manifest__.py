7acb9700a3b8c72378aa416b49d11f1855ec175a
david <david.vidal@tecnativa.com>
2021-02-03 17:55:59 +0100

    [ADD] portal_partner_data_no_edit: New module

---
+++ portal_partner_data_no_edit/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2021 Tecnativa - David Vidal
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Portal Partner Block Data Edit",
+    "version": "12.0.1.0.0",
+    "category": "Customer Relationship Management",
+    "author": "Tecnativa,"
+              "Odoo Community Association (OCA)",
+    "website": "https://www.github.com/OCA/partner-contact",
+    "license": "AGPL-3",
+    "depends": [
+        "portal",
+    ],
+    "data": [
+        "views/res_partner_views.xml",
+        "views/portal_template.xml",
+        "views/assets.xml",
+    ],
+    "installable": True,
+}
