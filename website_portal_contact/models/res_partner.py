855c530597e193b375dad291301be443a61b52a8
cpatel <cpatel@elegosoft.com>
2021-02-08 12:11:45 +0100

    [MIG] website_portal_contact: Migration to 14.0

---
+++ website_portal_contact/models/res_partner.py
@@ -0,0 +1,14 @@
+# Copyright 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import models
+
+
+class ResPartner(models.Model):
+    _name = "res.partner"
+    _inherit = "res.partner"
+
+    def _compute_website_url(self):
+        super(ResPartner, self)._compute_website_url()
+        for partner in self:
+            partner.website_url = "/my/contacts/%s" % partner.id
