6a508df9e98b1374e7643af61d762fb9a80cf2bb
Tran Thanh Phuc <phuc@trobz.com>
2021-02-12 23:24:59 +0700

    [MIG] partner_identification: Migration to 14.0

--- partner_identification/__manifest__.py
+++ partner_identification/__manifest__.py
@@ -9,7 +9,7 @@
 {
     "name": "Partner Identification Numbers",
     "category": "Customer Relationship Management",
-    "version": "13.0.1.2.0",
+    "version": "14.0.1.2.0",
     "license": "AGPL-3",
     "depends": ["contacts"],
     "data": [
