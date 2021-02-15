c8805914a6debe18edd6ae00f65a4c9e30187d1d
Carlos Roca <carlos.roca@tecnativa.com>
2021-02-15 08:24:51 +0100

    [MIG] portal_partner_data_no_edit: Migration to v13.0

--- portal_partner_data_no_edit/__manifest__.py
+++ portal_partner_data_no_edit/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Portal Partner Block Data Edit",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Customer Relationship Management",
     "author": "Tecnativa," "Odoo Community Association (OCA)",
     "website": "https://www.github.com/OCA/partner-contact",
@@ -11,7 +11,7 @@
     "data": [
         "views/res_partner_views.xml",
         "views/portal_template.xml",
-        "views/assets.xml",
+        "templates/assets.xml",
     ],
     "installable": True,
 }
