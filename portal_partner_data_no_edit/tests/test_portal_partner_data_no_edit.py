705f4e0c657f541440df53614e02fe6bb3beb55e
Carlos Roca <carlos.roca@tecnativa.com>
2021-02-15 08:24:51 +0100

    [MIG] portal_partner_data_no_edit: Migration to v13.0

--- portal_partner_data_no_edit/tests/test_portal_partner_data_no_edit.py
+++ portal_partner_data_no_edit/tests/test_portal_partner_data_no_edit.py
@@ -1,9 +1,8 @@
 # Copyright 2020 Tecnativa - David Vidal
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo.tests import common, tagged
+from odoo.tests import common
 
 
-@tagged("post_install", "-at_install")
 class PortalHttpCase(common.HttpCase):
     def setUp(self):
         super().setUp()
@@ -11,27 +10,11 @@ class PortalHttpCase(common.HttpCase):
 
     def test_portal_partner_default(self):
         """The default standard behavior"""
-        tour = (
-            "odoo.__DEBUG__.services['web_tour.tour']",
-            "portal_partner_data_no_edit_default_tour",
-        )
-        self.browser_js(
-            url_path="/my",
-            code="%s.run('%s')" % tour,
-            ready="%s.tours.%s.ready" % tour,
-            login="portal",
+        self.start_tour(
+            "/my", "portal_partner_data_no_edit_default_tour", login="portal"
         )
 
     def test_portal_partner_blocked(self):
         """There's no form fields anymore"""
         self.partner.block_portal_data_edit = True
-        tour = (
-            "odoo.__DEBUG__.services['web_tour.tour']",
-            "portal_partner_data_no_edit_block_tour",
-        )
-        self.browser_js(
-            url_path="/my",
-            code="%s.run('%s')" % tour,
-            ready="%s.tours.%s.ready" % tour,
-            login="portal",
-        )
+        self.start_tour("/my", "portal_partner_data_no_edit_block_tour", login="portal")
