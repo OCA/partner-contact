c65a040e0d9e75e15f6a0178a4cb8dc3230d3cf8
Freni-OSI <fpatel@opensourceintegrators.com>
2021-02-17 16:00:11 +0530

    [14.0][MIG]partner_tier_validation

--- partner_tier_validation/models/res_partner.py
+++ partner_tier_validation/models/res_partner.py
@@ -1,20 +1,15 @@
-# Copyright 2019 Open Source Integrators
+# Copyright 2021 Open Source Integrators
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
-class ResPartner(models.Model):
+class ResPartner11(models.Model):
     _name = "res.partner"
-    _inherit = ['res.partner', 'tier.validation']
-    _state_from = ['new', 'to approve']
-    _state_to = ['approved']
+    _inherit = ["res.partner", "tier.validation", "mail.activity.mixin"]
+    _state_from = ["new", "to approve"]
+    _state_to = ["approved"]
 
-    # override core odoo to set default value to False
-    customer = fields.Boolean(string='Is a Customer', default=False,
-                               help="Check this box if this contact is a customer. It can be selected in sales orders.")
-
-    state = fields.Selection(selection=[('new','New'),
-                                        ('approved','Approved'),],
-                             string='Status',
-                             default='new' )
+    state = fields.Selection(
+        [("new", "New"), ("approved", "Approved")], string="Status", default="new"
+    )
