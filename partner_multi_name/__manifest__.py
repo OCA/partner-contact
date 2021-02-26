f354bcf977312191554aedfd5b4ee0be90d83a43
Alejandro Olano <alejocode@gmail.com>
2021-02-26 15:22:19 -0500

    [IMP] Test Partner Multi Name

--- partner_multi_name/__manifest__.py
+++ partner_multi_name/__manifest__.py
@@ -4,6 +4,7 @@
 # Copyright 2018 EXA Auto Parts S.A.S Guillermo Montoya <Github@guillermm>
 # Copyright 2018 EXA Auto Parts S.A.S Joan Marín <Github@JoanMarin>
 # Copyright 2020 EXA Auto Parts S.A.S Juan Ocampo <Github@Capriatto>
+# Copyright 2021 Alejandro Olano <Github@alejo-code>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
