f839fc2c0aaecf25c8f06b536248b5ad20df9dae
joseph <josarmas94@hotmail.com>
2021-03-08 22:31:22 +0000

    [IMP] animal: black, isort, prettier

--- animal/__manifest__.py
+++ animal/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Animal",
-    "version": "12.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "summary": "Manage animals information",
     "author": "Open Source Integrators, Odoo Community Association (OCA)",
