3677658d4c88d2166a64b7de4b24415d9eb18df8
Kevin Khao <kevinkhao@gmail.com>
2021-03-16 17:06:46 +0100

    [CHG] partner_address_version: revamp functionality

--- partner_address_version/wizards/base_partner_merge.py
+++ partner_address_version/wizards/base_partner_merge.py
@@ -10,14 +10,5 @@ class MergePartnerAutomatic(models.TransientModel):
     def _get_fk_on(self, table):
         foreign_keys = super(MergePartnerAutomatic, self)._get_fk_on(table)
         if table == "res_partner" and self.env.context.get("address_version"):
-            models = self.env["res.partner"]._version_impacted_tables()
-            limited_fk = []
-            for fk in foreign_keys:
-                if fk[0] in models:
-                    ignore_col_dict = self.env["res.partner"]._version_exclude_keys()
-                    ignore_col = ignore_col_dict.get(fk[0], False)
-                    if ignore_col and fk[1] in ignore_col:
-                        continue
-                    limited_fk.append(fk)
-            return limited_fk
+            return self.env["res.partner"]._version_impacted_columns()
         return foreign_keys
