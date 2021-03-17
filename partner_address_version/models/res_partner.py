293011101c7f8698b837db36202bd3baedd5a83e
Kevin Khao <kevinkhao@gmail.com>
2021-03-17 17:39:44 +0100

    [IMP] Revert confusing _version_apply result, improve helper _version_exists

--- partner_address_version/models/res_partner.py
+++ partner_address_version/models/res_partner.py
@@ -86,8 +86,6 @@ class ResPartner(models.Model):
                 "base.partner.merge.automatic.wizard"
             ].with_context(address_version=True)
             partner_wizard._update_foreign_keys(self, version_p)
-            return version_p
-        return self
 
     def write(self, vals):
         version_fields = self._version_fields()
@@ -121,7 +119,7 @@ class ResPartner(models.Model):
         return self.copy(default=default)
 
     def _version_exists(self):
-        return bool(
+        return (
             self.env["res.partner"]
             .with_context(active_test=False)
             .search([("version_hash", "=", self._version_hash())])
