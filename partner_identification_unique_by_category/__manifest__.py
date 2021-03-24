66e7fbc222051405fbc513be8bd5db92d53c6698
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2021-03-24 08:35:08 +0100

    [13.0][ADD] partner_identification_unique_by_category
    This module extends the module `partner_identification` to enforce having
    unique id numbers by categories.
    This option can be set by category.
    
    [UPD] Update partner_identification_unique_by_category.pot
    
    [UPD] README.rst
    
    [ADD] icon.png

---
+++ partner_identification_unique_by_category/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Partner Identification Numbers Unique By Category",
+    "category": "Customer Relationship Management",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["partner_identification"],
+    "data": ["views/res_partner_id_category_view.xml"],
+    "author": "Camptocamp SA, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/partner-contact",
+    "development_status": "Production/Stable",
+}
