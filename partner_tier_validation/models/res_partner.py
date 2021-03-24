3134a0886e61599a93ad6bea907c406b87d4c027
Daniel Reis <dreis@opensourceintegrators.com>
2021-03-24 00:11:47 +0000

    [FIX] partner_tier_validation: workflow was not working

--- partner_tier_validation/models/res_partner.py
+++ partner_tier_validation/models/res_partner.py
@@ -1,15 +1,32 @@
 # Copyright 2019 Open Source Integrators
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ResPartner(models.Model):
     _name = "res.partner"
-    _inherit = ["res.partner", "tier.validation", "mail.activity.mixin"]
-    _state_from = ["new", "to approve"]
-    _state_to = ["approved"]
+    _inherit = ["res.partner", "tier.validation"]
+    _tier_validation_manual_config = False
 
     state = fields.Selection(
-        [("new", "New"), ("approved", "Approved")], string="Status", default="new"
+        [("draft", "Draft"), ("confirmed", "Active"), ("cancel", "Archived")],
+        string="Status",
+        default="draft",
     )
+
+    @api.model
+    def create(self, vals):
+        new = super().create(vals)
+        if new.need_validation and new.state != "confirmed":
+            new.active = False
+        return new
+
+    def write(self, vals):
+        """
+        Default `active` is False.
+        It is set to True when State changes to confirmed.
+        """
+        if "state" in vals:
+            vals["active"] = vals["state"] == "confirmed"
+        return super().write(vals)
