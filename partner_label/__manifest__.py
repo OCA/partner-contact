10187b8aed507063f10569b4de884be8b4f779f1
Alexei Rivera <alexeirivera87@gmail.com>
2021-03-28 18:34:27 -0400

    [MIG] partner_label: Migration to 14.0

--- partner_label/__manifest__.py
+++ partner_label/__manifest__.py
@@ -3,8 +3,9 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Partner labels",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "author": "Therp BV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/partner-contact",
     "license": "AGPL-3",
     "category": "Base",
     "summary": "Print partner labels",
---
+++ partner_label/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This module was written to allow users to print address labels for partners.
