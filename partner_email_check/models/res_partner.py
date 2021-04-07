992b4d641e563c141779d637f69849af07de8608
Carlos Manuel <cmanuelalvarez11>
2021-04-07 14:31:43 -0400

    [FIX] partner_email_check: Fixing Travis CI space in imports statements

--- partner_email_check/models/res_partner.py
+++ partner_email_check/models/res_partner.py
@@ -2,6 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 import logging
+
 from odoo import _, api, models
 from odoo.exceptions import UserError, ValidationError
 from odoo.tools import config
