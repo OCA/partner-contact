d0722b1d8079d393a4880511db747d2dc5dea26d
mreficent <miquel.raich@eficent.com>
2021-04-20 13:59:46 +0200

    [FIX] partner_company_group: assure no error in migration

--- partner_company_group/migrations/13.0.1.0.0/pre-migration.py
+++ partner_company_group/migrations/13.0.1.0.0/pre-migration.py
@@ -9,4 +9,14 @@ field_renames = [
 
 @openupgrade.migrate()
 def migrate(env, version):
-    openupgrade.rename_fields(env, field_renames)
+    if openupgrade.column_exists(env.cr, "res_partner", "group_id"):
+        if not openupgrade.column_exists(env.cr, "res_partner", "company_group_id"):
+            openupgrade.rename_fields(env, field_renames)
+        else:
+            openupgrade.logged_query(
+                env.cr,
+                """
+                UPDATE res_partner
+                SET company_group_id = group_id
+                WHERE company_group_id IS NULL AND group_id IS NOT NULL""",
+            )
