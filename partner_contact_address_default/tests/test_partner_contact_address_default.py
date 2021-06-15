d075437ec126d04f199172e0ac7d0119d0e7326e
david <david.vidal@tecnativa.com>
2021-06-15 10:12:14 +0200

    [IMP] partner_contact_address_default ux
    - Filter delivery and invoice addreses properly
    - Show it only in contact addresses
    - Default to the address the addresses set in the commercial partner if
    any.
    - If a contact is archived, unlink it from the default addresses
    
    TT30351

--- partner_contact_address_default/tests/test_partner_contact_address_default.py
+++ partner_contact_address_default/tests/test_partner_contact_address_default.py
@@ -31,7 +31,7 @@ class TestPartnerContactAddressDefault(common.TransactionCase):
     def test_contact_address_default(self):
         self.partner.partner_delivery_id = self.partner
         self.partner.partner_invoice_id = self.partner
-        res = self.partner.address_get()
+        res = self.partner.address_get(['delivery', 'invoice'])
         self.assertEqual(res['delivery'], self.partner.id)
         self.assertEqual(res['invoice'], self.partner.id)
 
@@ -39,6 +39,6 @@ class TestPartnerContactAddressDefault(common.TransactionCase):
             self.partner_child_delivery2
         self.partner_child_delivery2.partner_invoice_id =\
             self.partner_child_delivery2
-        res = self.partner_child_delivery2.address_get()
+        res = self.partner_child_delivery2.address_get(['delivery', 'invoice'])
         self.assertEqual(res['delivery'], self.partner_child_delivery2.id)
         self.assertEqual(res['invoice'], self.partner_child_delivery2.id)
