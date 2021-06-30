a275bf18b5defd1fac29838028ab41c74305aa6b
Alexis de Lattre <alexis.delattre@akretion.com>
2021-06-30 20:33:47 +0200

    [MIG] partner_external_map from v13 to v14
    Remove map website openstreetmap FR because its query URL doesn't work anymore
    Use street3 if the OCA module partner_address_street3 is installed
    Add sequence and active fields on map website object

--- partner_external_map/__manifest__.py
+++ partner_external_map/__manifest__.py
@@ -5,12 +5,12 @@
 
 {
     "name": "Partner External Maps",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Extra Tools",
     "license": "AGPL-3",
     "summary": "Add Map and Map Routing buttons on partner form to "
     "open GMaps, OSM, Bing and others",
-    "author": "Akretion, " "Tecnativa, " "Odoo Community Association (OCA)",
+    "author": "Akretion, Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/partner-contact",
     "depends": ["base"],
     "data": [
