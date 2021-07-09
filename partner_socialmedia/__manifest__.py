e009feed329474b3b33854e438ec8b7d9cbca165
nans <nans.lefebvre@acsone.eu>
2021-07-09 13:41:31 +0200

    [REF] partner_socialmedia: Black python code

--- partner_socialmedia/__manifest__.py
+++ partner_socialmedia/__manifest__.py
@@ -2,18 +2,18 @@
 # Copyright 2018 Humanitarian Logistics Organisation e.V. - Stefan Becker
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': 'Partner Socialmedia',
-    'summary': 'Add social media fields to contacts',
-    'version': '10.0.1.0.0',
-    'author': "humanilog, Odoo Community Association (OCA)",
-    'website': "https://github.com/OCA/partner-contact",
-    'category': 'CRM',
-    'license': 'AGPL-3',
-    'installable': True,
-    'depends': [
-        'base',
+    "name": "Partner Socialmedia",
+    "summary": "Add social media fields to contacts",
+    "version": "10.0.1.0.0",
+    "author": "humanilog, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/partner-contact",
+    "category": "CRM",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "base",
     ],
-    'data': [
-        'views/res_partner_view.xml',
+    "data": [
+        "views/res_partner_view.xml",
     ],
 }
