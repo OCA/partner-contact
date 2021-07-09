a2d45d9920ac0d4c9f324efded9e4624de3fa32e
nans <nans.lefebvre@acsone.eu>
2021-07-09 13:41:31 +0200

    [MIG] partner_socialmedia: Migration to 12.0

--- partner_socialmedia/__manifest__.py
+++ partner_socialmedia/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 Humanitarian Logistics Organisation e.V. - Stefan Becker
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Partner Socialmedia",
     "summary": "Add social media fields to contacts",
-    "version": "10.0.1.0.0",
+    "version": "12.0.1.0.0",
     "author": "humanilog, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/partner-contact",
     "category": "CRM",
