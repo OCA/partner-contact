a0df8324d0c365b1136fceb3b7838f928bba68b5
Zar21 <ivanperezfita@gmail.com>
2021-08-11 13:47:23 +0200

    partner_email_check: Migration to 14.0

--- partner_email_check/__manifest__.py
+++ partner_email_check/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Email Format Checker",
-    "version": "13.0.1.1.0",
+    "version": "14.0.1.1.0",
     "summary": "Validate email address field",
     "author": "Komit, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/partner-contact",
