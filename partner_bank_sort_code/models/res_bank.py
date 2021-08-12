755ef1029258b28943519b402d71c252d7017ac8
François Honoré <francois.honore@acsone.eu>
2021-08-12 14:28:55 +0200

    [IMP] partner_bank_sort_code: black, isort, prettier

--- partner_bank_sort_code/models/res_bank.py
+++ partner_bank_sort_code/models/res_bank.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -7,12 +6,14 @@ from odoo import fields, models
 
 class ResBank(models.Model):
 
-    _inherit = 'res.bank'
+    _inherit = "res.bank"
 
     sort_code = fields.Char()
 
-    _sql_constraints = [(
-        "sort_code_unique",
-        "EXCLUDE (sort_code WITH =) WHERE (active = True)",
-        "Sort Code has to be unique"),
+    _sql_constraints = [
+        (
+            "sort_code_unique",
+            "EXCLUDE (sort_code WITH =) WHERE (active = True)",
+            "Sort Code has to be unique",
+        ),
     ]
--- partner_bank_sort_code/tests/test_sort_code.py
+++ partner_bank_sort_code/tests/test_sort_code.py
@@ -1,36 +1,35 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.tests import common
 from psycopg2 import IntegrityError
+
+from odoo.tests import common
 
 
 class TestSortCode(common.TransactionCase):
-
     def setUp(self):
         super(TestSortCode, self).setUp()
         # Create two different active bank with different sort codes
 
-        self.bank_obj = self.env['res.bank']
+        self.bank_obj = self.env["res.bank"]
 
         vals = {
-            'name': 'BANK 1',
-            'sort_code': '95-01-32',
+            "name": "BANK 1",
+            "sort_code": "95-01-32",
         }
         self.bank_1 = self.bank_obj.create(vals)
 
         vals = {
-            'name': 'BANK 2',
-            'sort_code': '95-01-33',
+            "name": "BANK 2",
+            "sort_code": "95-01-33",
         }
         self.bank_2 = self.bank_obj.create(vals)
 
     def test_sort_code_duplicate(self):
         # Create a duplicate
         vals = {
-            'name': 'BANK 3',
-            'sort_code': '95-01-32',
+            "name": "BANK 3",
+            "sort_code": "95-01-32",
         }
         with self.assertRaises(IntegrityError):
             self.bank_obj.create(vals)
@@ -38,24 +37,24 @@ class TestSortCode(common.TransactionCase):
     def test_sort_code(self):
         # Create a new bank with new sort code
         vals = {
-            'name': 'BANK 3',
-            'sort_code': '95-01-34',
+            "name": "BANK 3",
+            "sort_code": "95-01-34",
         }
         self.bank_obj.create(vals)
 
     def test_sort_code_inactive(self):
         # Set several banks with same sort code to inactive and then create
         # a bank with that sort code
-        self.bank_1.write({'active': False})
+        self.bank_1.write({"active": False})
         vals = {
-            'name': 'BANK 3',
-            'sort_code': '95-01-32',
+            "name": "BANK 3",
+            "sort_code": "95-01-32",
         }
         self.bank_3 = self.bank_obj.create(vals)
         self.bank_3.active = False
 
         vals = {
-            'name': 'BANK 4',
-            'sort_code': '95-01-32',
+            "name": "BANK 4",
+            "sort_code": "95-01-32",
         }
         self.bank_obj.create(vals)
