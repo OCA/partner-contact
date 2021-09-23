448c092f20c0c1b2e8e22e6353b328eae034199d
Víctor Martínez <victor.martinez@tecnativa.com>
2021-09-23 17:33:11 +0200

    [MIG] partner_pricelist_search: Migration to 14.0
    [UPD] Update partner_pricelist_search.pot
    
    [UPD] README.rst
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: partner-contact-14.0/partner-contact-14.0-partner_pricelist_search
    Translate-URL: https://translation.odoo-community.org/projects/partner-contact-14-0/partner-contact-14-0-partner_pricelist_search/

--- partner_pricelist_search/__manifest__.py
+++ partner_pricelist_search/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Partner pricelist search",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "author": "Tecnativa,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/partner-contact",
     "category": "Partner Management",
