039d0af868774d472d3457ff6ecefcb6eba5ff02
Víctor Martínez <victor.martinez@tecnativa.com>
2021-09-24 14:49:55 +0200

    [MIG] partner_capital: Migration to 14.0

--- partner_capital/models/res_partner_turnover_range.py
+++ partner_capital/models/res_partner_turnover_range.py
@@ -1,5 +1,5 @@
-# Copyright 2015 Antonio Espinosa <antonio.espinosa@tecnativa.com>
-# Copyright 2015 Jairo Llopis <jairo.llopis@tecnativa.com>
+# Copyright 2015 Tecnativa - Antonio Espinosa
+# Copyright 2015 Tecnativa - Jairo Llopis
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
