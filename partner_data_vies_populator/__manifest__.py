d5143ebe31a2ebad07ff5e49026ba884bf68d6d2
Alexis de Lattre <alexis.delattre@akretion.com>
2021-10-17 11:32:45 +0200

    [MIG] partner_data_vies_populator from v13 to v14
    Improve error handling to avoid crash when Internet connectivity is down
    Read country code from VIES, not from VAT number
    No update when VAT number is invalid
    Improve tests

--- partner_data_vies_populator/__manifest__.py
+++ partner_data_vies_populator/__manifest__.py
@@ -6,16 +6,14 @@
     "name": "Partner Data VIES Populator",
     "summary": "Using VIES webservice, name and address information will "
     "be fetched and added to the partner.",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Customer Relationship Management",
     "author": "NextERP Romania,"
     "Forest and Biomass Romania,"
     "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/partner-contact",
     "license": "AGPL-3",
-    "application": False,
-    "installable": True,
-    "external_dependencies": {"python": ["python-stdnum"]},
     "depends": ["base_vat"],
-    "auto_install": False,
+    "external_dependencies": {"python": ["python-stdnum"]},
+    "installable": True,
 }
