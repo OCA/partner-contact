716df62c1cc6052328859cafe643c1451ecff1f8
Alexis de Lattre <alexis.delattre@akretion.com>
2021-10-17 11:32:45 +0200

    [MIG] partner_data_vies_populator from v13 to v14
    Improve error handling to avoid crash when Internet connectivity is down
    Read country code from VIES, not from VAT number
    No update when VAT number is invalid
    Improve tests

--- partner_data_vies_populator/tests/test_partner_data_vies_populator.py
+++ partner_data_vies_populator/tests/test_partner_data_vies_populator.py
@@ -9,49 +9,77 @@ class TestPartnerCreateByVAT(TransactionCase):
     def setUp(self):
         super().setUp()
         self.partner_model = self.env["res.partner"]
+        self.be_country_id = self.env.ref("base.be").id
         self.sample_1 = {
-            "valid": True,
-            "name": u"SA ODOO",
-            "address": u"Chaussée De Namur 40 1367 Ramillies",
+            "name": "SA ODOO",
+            "address": "Chaussée De Namur 40 1367 Ramillies",
+            "country_code": "BE",
         }
 
     def test_create_from_vat1(self):
         # Create an partner from VAT number field
-        with Form(self.env["res.partner"]) as partner_form:
+        with Form(self.partner_model) as partner_form:
             partner_form.is_company = True
             partner_form.vat = "be0477472701"
 
             # Check if the datas fetch correspond with the datas from VIES.
             self.assertEqual(partner_form.name, self.sample_1["name"])
             self.assertEqual(partner_form.street, self.sample_1["address"])
-            self.assertEqual(partner_form.country_id.name, "Belgium")
+            self.assertEqual(
+                partner_form.country_id.code, self.sample_1["country_code"]
+            )
             self.assertEqual(partner_form.vat, "BE0477472701")
 
-    def test_vat_change1(self):
+    def test_company_vat_change(self):
         # Change partner VAT number field
-        partner1 = self.partner_model.create({"name": "1", "is_company": True})
-        with Form(partner1) as partner_form:
+        partner = self.partner_model.create({"name": "1", "is_company": True})
+        with Form(partner) as partner_form:
             # Check if the datas fetch correspond with the datas from VIES.
             partner_form.vat = "be0477472701"
             self.assertEqual(partner_form.name, self.sample_1["name"])
             self.assertEqual(partner_form.street, self.sample_1["address"])
-            self.assertEqual(partner_form.country_id.name, "Belgium")
+            self.assertEqual(
+                partner_form.country_id.code, self.sample_1["country_code"]
+            )
             self.assertEqual(partner_form.vat, "BE0477472701")
 
-            # Empty VAT
+    def test_empty_vat_change(self):
+        partner = self.partner_model.create(
+            {
+                "name": "2",
+                "is_company": True,
+                "country_id": self.be_country_id,
+            }
+        )
+        with Form(partner) as partner_form:
             partner_form.vat = False
-            self.assertEqual(partner_form.name, self.sample_1["name"])
-            self.assertEqual(partner_form.street, self.sample_1["address"])
-            self.assertEqual(partner_form.country_id.name, "Belgium")
+            self.assertEqual(partner_form.name, partner.name)
+            self.assertEqual(partner_form.street, False)
+            self.assertEqual(partner_form.country_id.id, partner.country_id.id)
 
-            # Not company
-            partner_form.is_company = False
-            self.assertEqual(partner_form.name, self.sample_1["name"])
-            self.assertEqual(partner_form.street, self.sample_1["address"])
-            self.assertEqual(partner_form.country_id.name, "Belgium")
+    def test_individual_vat_change(self):
+        partner = self.partner_model.create(
+            {
+                "name": "3",
+                "is_company": False,
+                "country_id": self.be_country_id,
+            }
+        )
+        with Form(partner) as partner_form:
+            partner_form.vat = "BE0477472701"
+            self.assertEqual(partner_form.name, partner.name)
+            self.assertEqual(partner_form.street, False)
+            self.assertEqual(partner_form.country_id.id, partner.country_id.id)
 
-            # Not EU country
+    def test_non_eu_vat_change(self):
+        non_eu_country_id = self.env.ref("base.sc").id
+        partner = self.partner_model.create(
+            {
+                "name": "4",
+                "is_company": True,
+                "country_id": non_eu_country_id,
+            }
+        )
+        with Form(partner) as partner_form:
             partner_form.vat = "GT1234567 - 1"
-            self.assertEqual(partner_form.name, self.sample_1["name"])
-            self.assertEqual(partner_form.street, self.sample_1["address"])
-            self.assertEqual(partner_form.country_id.name, "Belgium")
+            self.assertEqual(partner_form.name, partner.name)
