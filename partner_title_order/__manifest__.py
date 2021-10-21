ba50e66fd2ab7d7237187d5c61075f26735d268f
Simone Orsi <simahawk@users.noreply.github.com>
2021-10-21 12:11:00 +0700

    [ADD] partner_title_order

---
+++ partner_title_order/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2020 Camptocamp SA
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Partner title order",
+    "summary": "Makes partner title sortable by sequence",
+    "version": "14.0.1.0.0",
+    "category": "Hidden",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+    ],
+    "website": "https://github.com/OCA/partner-contact",
+    "data": [
+        "views/res_partner_title_views.xml",
+    ],
+    "installable": True,
+}
