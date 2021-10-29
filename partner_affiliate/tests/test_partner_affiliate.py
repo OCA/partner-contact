44a87ce5ff4a549234d9713340cb458f99f84600
Ronald Portier <ronald@therp.nl>
2021-10-29 16:02:51 +0200

    [IMP] partner_affiliate: correct en extend tests

--- partner_affiliate/tests/test_partner_affiliate.py
+++ partner_affiliate/tests/test_partner_affiliate.py
@@ -1,12 +1,14 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+"""Tests for partner_affiliate."""
 from odoo.tests import common
 
 
 class TestPartnerAffiliate(common.TransactionCase):
-    def setUp(self):
-        super(TestPartnerAffiliate, self).setUp()
-        self.partner_obj = self.env["res.partner"]
+    """Tests for partner_affiliate."""
 
+    def setUp(self):
+        super().setUp()
+        self.partner_obj = self.env["res.partner"]
         self.first_company = self.partner_obj.create(
             {
                 "name": "MyFirstCompanyForTheAffiliate",
@@ -15,7 +17,6 @@ class TestPartnerAffiliate(common.TransactionCase):
                 "street": "first company street",
             }
         )
-
         self.second_company = self.partner_obj.create(
             {
                 "name": "MySecondCompanyForTheAffiliate",
@@ -25,9 +26,10 @@ class TestPartnerAffiliate(common.TransactionCase):
             }
         )
 
-    # Check data integrity of the objects when an affiliate is given a new
-    # parent. So both objects keeps their data.
     def test_change_parent_from_a_new_affiliate(self):
+        """Check data integrity of the objects when an affiliate is given a new
+        parent. So both objects keeps their data.
+        """
         my_affiliate = self.partner_obj.create(
             {
                 "name": "MyAffiliate",
@@ -37,48 +39,48 @@ class TestPartnerAffiliate(common.TransactionCase):
                 "street": "affiliate street",
             }
         )
-
         # Checks for data integrity in affiliate and his parent.
-        self.assertTrue(my_affiliate, "The new affiliate have been created.")
-
-        self.assertEquals(
+        self.assertTrue(my_affiliate, "Failed to create the new affiliate.")
+        self.assertEqual(
             my_affiliate.type, "affiliate", "Check type must be 'affiliate'"
         )
-        self.assertEquals(
+        self.assertEqual(
             my_affiliate.parent_id.id,
             self.first_company.id,
             "Must be child of the parent defined in the setup",
         )
-        self.assertEquals(
+        self.assertEqual(
             my_affiliate.street,
             "affiliate street",
-            "The street have been correctly set.",
+            "The street has not been correctly set.",
         )
-        self.assertEquals(
+        self.assertEqual(
             self.first_company.street,
             "first company street",
-            "The parent continues with his original street",
+            "Unexpected change in parent street",
         )
-
         # Change the parent of the affiliate for the second one in the set-up.
         my_affiliate.parent_id = self.second_company.id
         my_affiliate.onchange_parent_id()
-
-        # The parent have been changed. And is not the first one.
-        self.assertEquals(my_affiliate.parent_id.id, self.second_company.id)
-
+        # The parent has been changed. And is not the first one.
+        self.assertEqual(my_affiliate.parent_id.id, self.second_company.id)
         # The affiliate keeps its data for the street (address). Not modified.
-        self.assertEquals(
-            my_affiliate.street, "affiliate street", "keeps the same street"
+        self.assertEqual(
+            my_affiliate.street,
+            "affiliate street",
+            "Affilliate street should not have changed",
         )
         # The data for the street of the first parent have not been changed.
-        self.assertEquals(
-            self.first_company.street, "first company street", "keeps the same street"
+        self.assertEqual(
+            self.first_company.street,
+            "first company street",
+            "First company street should not have changed",
         )
 
-    # Check that the default value for 'type' defined by default in the view
-    # is set correctly when a new affiliate is created.
     def test_new_affiliate_is_created_with_type_affiliate_by_default(self):
+        """Check that the default value for 'type' defined by default in the view
+        is set correctly when a new affiliate is created.
+        """
         new_affiliate = self.partner_obj.with_context(
             {
                 "default_parent_id": self.first_company.id,
@@ -86,12 +88,12 @@ class TestPartnerAffiliate(common.TransactionCase):
                 "default_type": "affiliate",
             }
         ).create({"name": "MyTestAffiliate", "street": "affiliate street"})
+        self.assertEqual(new_affiliate.type, "affiliate")
 
-        self.assertEquals(new_affiliate.type, "affiliate")
-
-    # Check that when changing the parent from an individual, it changes also
-    # the address keeping the expected behaviour.
     def test_individual_changes_address_when_changing_parent_id(self):
+        """Check that when changing the parent from an individual, it changes also
+        the address keeping the expected behaviour.
+        """
         my_individual = self.partner_obj.create(
             {
                 "name": "MyIndividual",
@@ -101,14 +103,41 @@ class TestPartnerAffiliate(common.TransactionCase):
                 "street": "individual street",
             }
         )
-
         my_individual.parent_id = self.second_company.id
         my_individual.onchange_parent_id()
-
-        # The parent have been changed.
-        self.assertEquals(my_individual.parent_id.id, self.second_company.id)
-
+        # The parent has been changed.
+        self.assertEqual(my_individual.parent_id.id, self.second_company.id)
         # The affiliate gets the address from the new parent.
-        self.assertEquals(
-            my_individual.street, "second company street", "keeps the same street"
+        self.assertEqual(
+            my_individual.street,
+            "second company street",
+            "Street of affiliate individual should heve been set to company street",
+        )
+
+    def test_company_keeps_address_when_changing_parent_id(self):
+        """Check that when changing the parent from a company, it keeps its own
+        address.
+        """
+        my_affiliate_company = self.partner_obj.create(
+            {
+                "name": "My Affiliate Company",
+                "parent_id": self.first_company.id,
+                "type": "contact",
+                "is_company": True,
+                "street": "affiliate_company street",
+            }
+        )
+        my_affiliate_company.parent_id = self.second_company.id
+        my_affiliate_company.onchange_parent_id()
+        # The parent has been changed.
+        self.assertEqual(my_affiliate_company.parent_id.id, self.second_company.id)
+        # The affiliate should keep its own address.
+        self.assertEqual(
+            my_affiliate_company.street,
+            "affiliate_company street",
+            "Street of affiliate company should not have changed",
+        )
+        # Change of parent should not affect the type.
+        self.assertEqual(
+            my_affiliate_company.type, "affiliate", "Check type must remain 'affiliate'"
         )
