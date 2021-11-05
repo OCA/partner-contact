d179d7d22754749755be2f890149075a986d9f47
Simone Orsi <simahawk@gmail.com>
2021-11-05 15:46:55 +0100

    partner_identification_gln: migrate to v14

--- partner_identification_gln/__manifest__.py
+++ partner_identification_gln/__manifest__.py
@@ -2,11 +2,11 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    "name": "Partner Identification Gln",
+    "name": "Partner Identification GLN",
     "summary": """
         This addon extends "Partner Identification Numbers"
         to provide a number category for GLN registration""",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "Acsone S.A.,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/partner-contact",
