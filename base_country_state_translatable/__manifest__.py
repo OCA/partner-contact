3bfdc53fae3850bd409607bfa94f682907cd512f
moaadbourhim <moaad.bourhim@gmail.com>
2021-11-07 18:33:01 +0100

    [IMP] base_country_state_translatable: black, isort, prettier

--- base_country_state_translatable/__manifest__.py
+++ base_country_state_translatable/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Translate Country States",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "depends": ["base"],
     "website": "https://github.com/OCA/partner-contact",
     "summary": "Translate Country States",
