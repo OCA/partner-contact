0a187c33b43ce158af494dfbbbb1a4b59be0d000
David Beal <david.beal@akretion.com>
2021-11-10 16:06:40 +0100

    MIGR 14.0 animal_owner

--- animal_owner/__manifest__.py
+++ animal_owner/__manifest__.py
@@ -1,8 +1,8 @@
 # Copyright (C) 2020 Open Source Integrators
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Animal Owner",
-    "version": "12.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "summary": "Add owner to the animal",
     "author": "Open Source Integrators, Odoo Community Association (OCA)",
@@ -10,7 +10,6 @@
     "website": "https://github.com/OCA/partner-contact",
     "depends": ["animal"],
     "data": ["views/animal.xml", "views/res_partner.xml"],
-    "application": False,
     "development_status": "Beta",
     "maintainers": ["max3903"],
 }
--- animal_owner/models/animal.py
+++ animal_owner/models/animal.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2020 Open Source Integrators
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from odoo import fields, models
 
 
