d0d87116fc4e97e2e24ff7d2b2b6cf2329e7eba1
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2021-11-16 18:56:48 +0100

    [MIG] base_location_geonames_import: Migration to 15.0
    - Standard procedure
    
    TT32981

--- base_location_geonames_import/models/res_country.py
+++ base_location_geonames_import/models/res_country.py
@@ -8,5 +8,5 @@ class ResCountry(models.Model):
 
     _inherit = "res.country"
 
-    geonames_state_name_column = fields.Integer("Geonames State Name Column")
-    geonames_state_code_column = fields.Integer("Geonames State Code Column")
+    geonames_state_name_column = fields.Integer()
+    geonames_state_code_column = fields.Integer()
