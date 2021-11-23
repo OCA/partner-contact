864dd5951891e8937c2ff11d83bb74d3f72ba770
Víctor Martínez <victor.martinez@tecnativa.com>
2021-11-23 08:26:13 +0100

    [FIX] base_location: address field sync city_id TT33047

--- base_location/__manifest__.py
+++ base_location/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Location management (aka Better ZIP)",
-    "version": "15.0.1.0.0",
+    "version": "15.0.1.0.1",
     "depends": ["base_address_city", "contacts"],
     "author": (
         "Camptocamp,"
