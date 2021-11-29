9f8052c3409028c2ea64acadfa249c923e42b15f
GuillemCForgeFlow <guillem.casassas@forgeflow.com>
2021-11-29 11:01:38 +0100

    [14.0][MIG]purchase_supplier_rank: Migration to 14.0

--- purchase_supplier_rank/__init__.py
+++ purchase_supplier_rank/__init__.py
@@ -8,4 +8,5 @@ def post_init_hook(cr, registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
     partners = env["purchase.order"].search([]).mapped("partner_id")
     partners |= partners.mapped("commercial_partner_id")
-    partners._increase_rank("supplier_rank")
+    for partner in partners:
+        partner._increase_rank("supplier_rank", partner.purchase_order_count)
