1e270f8887327fd3c930f3bf39490172b15af638
Sergio Zanchetta <primes2h@gmail.com>
2021-12-12 09:54:07 +0100

    [IMP] : black, isort, prettier

--- partner_contact_birthplace/__manifest__.py
+++ partner_contact_birthplace/__manifest__.py
@@ -1,20 +1,17 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Partner contact birthplace',
-    'summary': 'This module allows to define a birthplace for partners.',
-    'version': '12.0.1.0.0',
-    'category': 'Customer Relationship Management',
-    'website': 'https://github.com/OCA/partner-contact/tree/12.0/'
-               'partner_contact_birthplace',
-    'author': 'Agile Business Group, Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'application': False,
-    'installable': True,
-    'depends': [
-        'partner_contact_personal_information_page',
+    "name": "Partner contact birthplace",
+    "summary": "This module allows to define a birthplace for partners.",
+    "version": "12.0.1.0.0",
+    "category": "Customer Relationship Management",
+    "website": "https://github.com/OCA/partner-contact" "partner_contact_birthplace",
+    "author": "Agile Business Group, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "partner_contact_personal_information_page",
     ],
-    'data': [
-        'views/res_partner.xml'
-    ],
+    "data": ["views/res_partner.xml"],
 }
