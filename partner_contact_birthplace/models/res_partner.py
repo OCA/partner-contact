4e9609b0cdd8059ca6891092037b242cad3e8215
Sergio Zanchetta <primes2h@gmail.com>
2021-12-12 09:54:07 +0100

    [IMP] : black, isort, prettier

--- partner_contact_birthplace/models/res_partner.py
+++ partner_contact_birthplace/models/res_partner.py
@@ -5,12 +5,12 @@ from odoo import fields, models
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
     birth_city = fields.Char()
     birth_state_id = fields.Many2one(
-        comodel_name='res.country.state', string='Birth state',
-        ondelete='restrict')
+        comodel_name="res.country.state", string="Birth state", ondelete="restrict"
+    )
     birth_country_id = fields.Many2one(
-        comodel_name='res.country', string='Birth country',
-        ondelete='restrict')
+        comodel_name="res.country", string="Birth country", ondelete="restrict"
+    )
