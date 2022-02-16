746054151bd7468baf8bb748c12aca317e8072c0
BT-rfresco <riccardo.fresco@bt-group.com>
2022-02-16 15:02:45 +0100

    [IMP] partner_contact_age_range: black, isort, prettier

--- partner_contact_age_range/__manifest__.py
+++ partner_contact_age_range/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Contact's Age Range",
-    "version": "14.0.1.0.1",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "author": "Druidoo, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/partner-contact",
