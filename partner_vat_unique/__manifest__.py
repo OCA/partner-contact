e09aa264937efb05001e3373dfd87f301298b95c
Andrea Piovesana <andrea.m.piovesana@gmail.com>
2022-03-10 07:39:45 +0100

    [14.0][FIX] partner_vat_unique set application to false
    fix to set application to false

--- partner_vat_unique/__manifest__.py
+++ partner_vat_unique/__manifest__.py
@@ -9,7 +9,7 @@
     "website": "https://github.com/OCA/partner-contact",
     "author": "Grant Thornton S.L.P, Odoo Community Association (OCA)",
     "license": "AGPL-3",
-    "application": True,
+    "application": False,
     "installable": True,
     "depends": ["base"],
 }
