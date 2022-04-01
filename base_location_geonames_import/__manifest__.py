a8d791fd5cca225615af638c2df216f77a418692
Jairo Llopis <yajo.sk8@gmail.com>
2022-04-01 10:58:13 +0100

    [FIX] base_location_geonames_import: some bottlenecks
    - Instead of doing one `.search()` per state, do one per batch.
    - Instead of searching in lists with `in` operator, use a set.
    
    Thousands of search queries and useless list loop lookups saved. Importing Spain states went down from 4:00 minutes to 2:30.
    
    Still having a big bottleneck when creating the states, but [that is explained in odoo][1] and outside of this fix scope.
    
    [1]: https://github.com/odoo/odoo/blob/c80d7a99236dfb6b99cca724d76632a82f6b605e/odoo/models.py#L4141-L4161
    
    @moduon MT-426

--- base_location_geonames_import/__manifest__.py
+++ base_location_geonames_import/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     "name": "Base Location Geonames Import",
-    "version": "15.0.1.0.1",
+    "version": "15.0.1.0.2",
     "development_status": "Mature",
     "category": "Partner Management",
     "license": "AGPL-3",
