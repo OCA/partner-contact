071392471b012b1628815a991501a0a405c010d2
bosd <evbokhoven@gmail.com>
2022-04-15 21:24:45 +0200

    [IMP] partner_coc: black, isort, prettier

--- partner_coc/__manifest__.py
+++ partner_coc/__manifest__.py
@@ -7,7 +7,7 @@
     "version": "13.0.1.0.0",
     "category": "Web",
     "author": "Onestein,Odoo Community Association (OCA)",
-    "website": "https://github.com/OCA/partner-contact/",
+    "website": "https://github.com/OCA/partner-contact",
     "license": "AGPL-3",
     "depends": ["partner_identification"],
     "data": ["data/res_partner_id_category_data.xml", "views/res_partner_view.xml"],
