95241c3297e9d1c18615f737633bce9f6d877841
bosd <evbokhoven@gmail.com>
2022-04-15 22:14:14 +0200

    [MIG] partner_coc: Migration to 14.0

--- partner_coc/__manifest__.py
+++ partner_coc/__manifest__.py
@@ -4,12 +4,14 @@
 {
     "name": "Partner CoC",
     "summary": "Adds field 'Chamber Of Commerce Registration Number'",
-    "version": "13.0.1.0.0",
-    "category": "Web",
+    "version": "14.0.1.0.0",
+    "category": "Customer Relationship Management",
     "author": "Onestein,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/partner-contact",
     "license": "AGPL-3",
     "depends": ["partner_identification"],
     "data": ["data/res_partner_id_category_data.xml", "views/res_partner_view.xml"],
+    "maintainers": ["bosd"],
+    "development_status": "Production/Stable",
     "installable": True,
 }
