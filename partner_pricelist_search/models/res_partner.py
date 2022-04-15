95606cff7cd47d0104b4583457801c8f8a40427b
Vince Lieckens <vince.lieckens@dynapps.be>
2022-04-15 09:33:58 +0200

    [MIG] partner_pricelist_search: Migration to 15.0
    TT37297

--- partner_pricelist_search/models/res_partner.py
+++ partner_pricelist_search/models/res_partner.py
@@ -1,5 +1,5 @@
 # Copyright 2021 Tecnativa - Carlos Dauden
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
 
