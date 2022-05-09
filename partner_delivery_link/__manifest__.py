c49e3bac1af16df0832d9639df27656548a50cb2
dsolanki <dhara.solanki@initos.com>
2022-05-09 16:15:10 +0530

    [15.0][ADD] partner_delivery_link

---
+++ partner_delivery_link/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Partner Delivery Link",
+    "version": "15.0.1.0.0",
+    "category": "Website",
+    "author": "ERP Harbor Consulting Services,"
+    "Odoo Community Association (OCA),"
+    "Nitrokey GmbH",
+    "website": "https://github.com/OCA/partner-contact",
+    "license": "AGPL-3",
+    "summary": """
+    Partner Delivery Link
+    This module adds a smart button in Partner to open the Delivery Orders of the
+     related partner""",
+    "depends": ["stock"],
+    "data": [
+        "views/res_partner_view.xml",
+    ],
+    "installable": True,
+}
---
+++ partner_delivery_link/readme/USAGE.rst
@@ -0,0 +1,4 @@
+To check functionality this module,
+
+You need to create a delivery(picking) order and then go to that delivery(picking) order
+partner form where you can see the smart button which holds the delivery related data of a partner
