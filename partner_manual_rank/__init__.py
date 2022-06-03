3282fc8e9ca85170b9fb5002204063d8bb2ac58e
Francisco Javier Luna Vazquez <fluna@vauxoo.com>
2022-06-03 17:32:21 -0500

    [MIG] partner_manual_rank: Migration to V15
    Fields are not stored anymore to avoid computed fields getting outdated and concurrency issues.

--- partner_manual_rank/__init__.py
+++ partner_manual_rank/__init__.py
@@ -2,30 +2,3 @@
 
 from . import models
 from odoo.tools.sql import column_exists
-
-
-def pre_init_hook(cr):
-    if not column_exists(cr, "res_partner", "is_customer"):
-        cr.execute(
-            """
-            ALTER TABLE res_partner
-            ADD COLUMN is_customer boolean""",
-        )
-        cr.execute(
-            """
-            UPDATE res_partner
-            SET is_customer = customer_rank::boolean
-            """
-        )
-    if not column_exists(cr, "res_partner", "is_supplier"):
-        cr.execute(
-            """
-            ALTER TABLE res_partner
-            ADD COLUMN is_supplier boolean""",
-        )
-        cr.execute(
-            """
-            UPDATE res_partner
-            SET is_supplier = supplier_rank::boolean
-            """
-        )
