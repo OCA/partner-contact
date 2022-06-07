9a6a2227aeff1bc2d852f7ef352cca943f0dae54
Victor Champonnois <victor@coopiteasy.be>
2022-06-07 11:09:30 +0200

    [ADD] search method for partner age

--- partner_contact_birthdate/models/res_partner.py
+++ partner_contact_birthdate/models/res_partner.py
@@ -14,7 +14,9 @@ class ResPartner(models.Model):
     _inherit = "res.partner"
 
     birthdate_date = fields.Date("Birthdate")
-    age = fields.Integer(string="Age", readonly=True, compute="_compute_age")
+    age = fields.Integer(
+        string="Age", readonly=True, compute="_compute_age", search="_search_age"
+    )
 
     @api.depends("birthdate_date")
     def _compute_age(self):
@@ -23,3 +25,20 @@ class ResPartner(models.Model):
             if record.birthdate_date:
                 age = relativedelta(fields.Date.today(), record.birthdate_date).years
             record.age = age
+
+    def _search_age(self, operator, value):
+        if operator not in ("=", "!=", "<", "<=", ">", ">=", "in", "not in"):
+            return []
+        # pylint: disable=sql-injection
+        # the value of operator is checked, no risk of injection
+        query = """
+            SELECT id
+            FROM res_partner
+            WHERE extract(year from age(CURRENT_DATE, birthdate_date))
+              {operator} %s
+            """.format(
+            operator=operator
+        )
+        self.env.cr.execute(query, (value,))
+        ids = [t[0] for t in self.env.cr.fetchall()]
+        return [("id", "in", ids)]
