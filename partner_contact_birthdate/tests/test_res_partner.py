b32f2fcfebd872e839baa5c5fdbc44a58f6d914e
Victor Champonnois <victor@coopiteasy.be>
2022-06-08 14:07:40 +0200

    [ADD] tests for _search_age

--- partner_contact_birthdate/tests/test_res_partner.py
+++ partner_contact_birthdate/tests/test_res_partner.py
@@ -13,9 +13,38 @@ class TestResPartner(common.TransactionCase):
         self.partner_admin = self.env.ref("base.partner_admin")
         self.partner_admin.write({"birthdate_date": "1991-09-05"})
 
+        self.partner_demo = self.env.ref("base.partner_demo")
+        self.partner_demo.write({"birthdate_date": "2100-01-01"})
+
     def test_compute_age(self):
         self.partner_admin._compute_age()
         age = relativedelta(
             fields.Date.today(), self.partner_admin.birthdate_date
         ).years
         self.assertEqual(self.partner_admin.age, age)
+
+    def test_search_age_unexpected_operator(self):
+        self.partner_admin._compute_age()
+        self.assertEqual(self.partner_admin._search_age("like", "0"), [])
+
+    def test_search_age_correct_result(self):
+        self.partner_admin._compute_age()
+        self.assertEqual(
+            self.partner_admin._search_age(">=", "1"),
+            [("id", "in", [self.env.ref("base.partner_admin").id])],
+        )
+
+    def test_search_age_same_as_compute_age(self):
+        self.partner_admin._compute_age()
+        self.assertEqual(
+            self.partner_admin._search_age("=", self.partner_admin.age),
+            [("id", "in", [self.env.ref("base.partner_admin").id])],
+        )
+
+    def test_search_age_same_as_compute_age_for_negative_age(self):
+        self.partner_admin._compute_age()
+        self.partner_demo._compute_age()
+        self.assertEqual(
+            self.partner_demo._search_age("=", self.partner_demo.age),
+            [("id", "in", [self.env.ref("base.partner_demo").id])],
+        )
