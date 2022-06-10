92770daacacfa4eb216b6a8780f84d6ffc1ab927
dhx <dh.tx.dev@dhx.at>
2022-06-10 16:08:29 +0200

    [MIG] partner_contact_job_position: Migration to 15.0
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

--- partner_contact_job_position/__manifest__.py
+++ partner_contact_job_position/__manifest__.py
@@ -7,7 +7,7 @@
 {
     "name": "Partner Job Position",
     "summary": "Categorize job positions for contacts",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Customer Relationship Management",
     "website": "https://github.com/OCA/partner-contact",
     "author": "Tecnativa, Odoo Community Association (OCA)",
