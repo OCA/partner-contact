0f8633bfa63f80f523bd573eed7df250274b6ea8
Carmen Bianca Bakker <carmen@coopiteasy.be>
2022-07-12 16:40:46 +0200

    [MIG] partner_email_unique: Migration to 11.0
    Signed-off-by: Carmen Bianca Bakker <carmen@coopiteasy.be>

--- partner_email_unique/__manifest__.py
+++ partner_email_unique/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019 Coop IT Easy SCRL fs
 #   Robin Keunen <robin@coopiteasy.be>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
@@ -6,7 +5,7 @@
 {
     "name": "Partner unique email",
     "summary": "Add an unique constraint to email field",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "category": "Customer Relationship Management",
     "website": "https://github.com/oca/partner-contact",
     "author": "Coop IT Easy SCRL fs, " "Odoo Community Association (OCA)",
