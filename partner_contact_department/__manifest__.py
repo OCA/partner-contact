445a501c15c77120c535a9b1d990406272f355f7
Galoperd <jvcsan@gmail.com>
2022-07-20 09:05:05 +0200

    Adding partner_contact_department from 14.0 branch

---
+++ partner_contact_department/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2014-2015 Tecnativa S.L. - Jairo Llopis
+# Copyright 2016 Tecnativa S.L. - Vicent Cubells
+# Copyright 2017 Tecnativa S.L. - David Vidal
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Partner Contact Department",
+    "summary": "Assign contacts to departments",
+    "version": "14.0.1.0.1",
+    "category": "Customer Relationship Management",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/partner-contact",
+    "application": False,
+    "depends": ["contacts"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/res_partner_department_view.xml",
+        "views/res_partner_view.xml",
+    ],
+    "installable": True,
+}
