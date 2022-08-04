11be38e9b83eb3c9fd26424a6bf6c7c3d66e7d5e
Víctor Martínez <victor.martinez@tecnativa.com>
2022-08-04 18:04:33 +0200

    [MIG] partner_company_group: Migration to 15.0 TT36476

--- partner_company_group/tests/test_partner_company_group.py
+++ partner_company_group/tests/test_partner_company_group.py
@@ -1,10 +1,10 @@
 # Copyright 2020 Ecosoft Co., Ltd (http://ecosoft.co.th/)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo.tests.common import SavepointCase
+from odoo.tests.common import TransactionCase
 
 
-class TestPartnerCompanyGroup(SavepointCase):
+class TestPartnerCompanyGroup(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
