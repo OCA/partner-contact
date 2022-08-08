6043f421d660bdda630bb14679b72c681ebcd5b3
Simone Orsi <simahawk@users.noreply.github.com>
2022-08-08 11:28:32 +0200

    partner_tz: dev status Alpha -> Beta

--- partner_tz/__manifest__.py
+++ partner_tz/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "Partner timezone",
     "summary": "Remove partner timezone default value and display on form",
     "version": "14.0.1.0.0",
-    "development_status": "Alpha",
+    "development_status": "Beta",
     "category": "Uncategorized",
     "website": "https://github.com/OCA/partner-contact",
     "author": "Camptocamp, Odoo Community Association (OCA)",
