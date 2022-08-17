974063823613e54d06fe1356e74779673ddca418
Víctor Martínez <victor.martinez@tecnativa.com>
2022-08-17 12:36:04 +0200

    [DCK] Pre-commit fixes

--- partner_pricelist_tracking/__manifest__.py
+++ partner_pricelist_tracking/__manifest__.py
@@ -2,10 +2,10 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Track partner pricelist changes",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.0.1",
     "category": "Partner Management",
     "author": "Tecnativa, Odoo Community Association (OCA)",
-    "website": "https://www.github.com/OCA/partner-contact",
+    "website": "https://github.com/OCA/partner-contact",
     "license": "AGPL-3",
     "depends": ["product"],
     "data": [],
