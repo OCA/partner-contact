7c5733ec7716d2ef441f3e2994c2895716d1d408
Víctor Martínez <victor.martinez@tecnativa.com>
2022-08-30 16:47:24 +0200

    [FIX] partner_category_security: Allow to correctly edit tags when the user has permissions. TT38576

--- partner_category_security/tests/test_partner_category_security.py
+++ partner_category_security/tests/test_partner_category_security.py
@@ -1,5 +1,6 @@
 # Copyright 2022 Tecnativa - Víctor Martínez
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from lxml import etree
 
 from odoo.tests import common, new_test_user
 
@@ -22,6 +23,7 @@ class TestPartnerCategorySecurity(common.SavepointCase):
             login="Test partner category manager",
             groups="partner_category_security.group_partner_category_manager",
         )
+        cls.partner_model = cls.env["res.partner"]
         cls.partner_category_model = cls.env["res.partner.category"]
 
     def test_check_access_rights_basic_user(self):
@@ -44,3 +46,20 @@ class TestPartnerCategorySecurity(common.SavepointCase):
         self.assertTrue(model.check_access_rights("write", False))
         self.assertTrue(model.check_access_rights("create", False))
         self.assertTrue(model.check_access_rights("unlink", False))
+
+    def test_partner_model_fields_view_get(self):
+        res = self.partner_model.with_user(self.basic_user).fields_view_get(
+            view_type="form"
+        )
+        node = etree.XML(res["arch"]).xpath("//field[@name='category_id']")[0]
+        self.assertTrue(res["fields"]["category_id"]["readonly"])
+        self.assertEqual(node.get("readonly"), "1")
+        self.assertEqual(node.get("force_save"), "1")
+        res = self.partner_model.with_user(self.partner_category_user).fields_view_get(
+            view_type="form"
+        )
+        self.assertFalse(res["fields"]["category_id"]["readonly"])
+        res = self.partner_model.with_user(
+            self.partner_category_manager
+        ).fields_view_get(view_type="form")
+        self.assertFalse(res["fields"]["category_id"]["readonly"])
