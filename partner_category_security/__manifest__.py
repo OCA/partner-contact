772206c749edcc1934b73f36bbdd7bd409ab9730
Víctor Martínez <victor.martinez@tecnativa.com>
2022-08-30 18:25:58 +0200

    [FIX] partner_category_security: Update ACL to prevent users with Publisher permission from write contact tags. TT38576
    [UPD] Update partner_category_security.pot
    
    partner_category_security 14.0.1.0.1
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: partner-contact-14.0/partner-contact-14.0-partner_category_security
    Translate-URL: https://translation.odoo-community.org/projects/partner-contact-14-0/partner-contact-14-0-partner_category_security/
    
    [UPD] README.rst

--- partner_category_security/__manifest__.py
+++ partner_category_security/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Partner category security",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "category": "Customer Relationship Management",
     "website": "https://github.com/OCA/partner-contact",
     "author": "Tecnativa, Odoo Community Association (OCA)",
