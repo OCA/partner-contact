d7ca8d83cf0538648f1229285566d4316b0d2d65
Aungkokolin1997 <aungkokolin23397@gmail.com>
2022-09-14 11:40:57 +0630

    [ADD] partner_name_hide_parent

---
+++ partner_name_hide_parent/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2020-2022 Quartile Limited
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Partner Name Hide Parent",
+    "version": "15.0.1.0.0",
+    "author": "Quartile Limited,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/partner-contact",
+    "category": "Extra Tools",
+    "license": "AGPL-3",
+    "depends": ["base"],
+    "data": ["views/res_partner_views.xml"],
+    "installable": True,
+}
