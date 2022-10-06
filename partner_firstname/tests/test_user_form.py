5ebcd2a569b2d0d22e4f414e251d836d275c7706
Benoit Aimont <benoit.aimont@acsone.eu>
2022-10-06 10:41:55 +0200

    [FIX] partner_firstname - fix form tests

--- partner_firstname/tests/test_user_form.py
+++ partner_firstname/tests/test_user_form.py
@@ -9,7 +9,7 @@ class UserOnchangeCase(TransactionCase):
         """In a new users form, a user set only the firstname."""
         login = "Zoë"
         firstname = "Zoë"
-        with self.get_clean_user_form() as user_form:
+        with self.get_user_form() as user_form:
             user_form.login = login
             # Changes firstname, which triggers onchanges
             user_form.firstname = firstname
@@ -22,7 +22,7 @@ class UserOnchangeCase(TransactionCase):
         """In a new user form, a user set only the lastname."""
         login = "Żywioł"
         lastname = "Żywioł"
-        with self.get_clean_user_form() as user_form:
+        with self.get_user_form() as user_form:
             user_form.login = login
             # Changes lastname, which triggers onchanges
             user_form.lastname = lastname
@@ -36,7 +36,7 @@ class UserOnchangeCase(TransactionCase):
         login = "Zoë.Żywioł"
         firstname = "Zoë"
         lastname = "Żywioł"
-        with self.get_clean_user_form() as user_form:
+        with self.get_user_form() as user_form:
             user_form.login = login
             # Changes firstname, which triggers onchanges
             user_form.firstname = firstname
@@ -47,18 +47,5 @@ class UserOnchangeCase(TransactionCase):
         self.assertEqual(user_form.firstname, firstname)
         self.assertEqual(user_form.name, " ".join((firstname, lastname)))
 
-    def get_clean_user_form(self):
-        """Get form without pseudo_fields.
-
-        This will be needed until Odoo Form does weed out these fields by itself.
-        """
-        form = Form(self.env["res.users"], view="partner_firstname.view_users_form")
-        pseudo_fields = [
-            fieldname
-            for fieldname in form._view["fields"].keys()
-            if fieldname[:11] == "sel_groups_" or fieldname[:9] == "in_group_"
-        ]
-        for fieldname in pseudo_fields:
-            del form._view["fields"][fieldname]
-            del form._values[fieldname]
-        return form
+    def get_user_form(self):
+        return Form(self.env["res.users"], view="partner_firstname.view_users_form")
