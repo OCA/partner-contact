6f4a4984662041df82d6c867c22a20504f16a85d
TheerayutEncoder <theerayutattajak@gmail.com>
2022-10-10 01:48:19 +0700

    [MIG] partner_helper: Migration to 15.0

--- partner_helper/__manifest__.py
+++ partner_helper/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Partner Helper",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "Akretion,Odoo Community Association (OCA)",
     "maintainer": "Akretion",
     "category": "Warehouse",
