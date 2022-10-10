899dce017d7b26114d395eaafa7fd6bcbf56d4dc
Pedro Evaristo Gonzalez Sanchez <pedro.gonzalez@pesol.es>
2022-10-10 08:14:58 -0400

    [MIG] partner_contact_lang: Migration to 16.0

--- partner_contact_lang/__manifest__.py
+++ partner_contact_lang/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Manage language in contacts",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Sales",
     "license": "AGPL-3",
     "author": "Tecnativa, Odoo Community Association (OCA)",
