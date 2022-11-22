571d7f25516d6144ae749b5534a12fc3261516d3
Jaume Bernaus <jaume.bernaus@forgeflow.com>
2022-11-22 13:55:40 +0100

    [ADD] partner_exception

---
+++ partner_exception/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2022 ForgeFlow, S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Partner Exception",
+    "summary": "Custom exceptions on partner contact",
+    "version": "13.0.1.0.0",
+    "category": "Generic Modules/Sale",
+    "author": "Forgeflow, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/partner-contact",
+    "depends": ["contacts", "base_exception"],
+    "license": "AGPL-3",
+    "data": ["views/contact_views.xml"],
+}
