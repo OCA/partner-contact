52a64f03f960b704abe6bd34ba501bc6079a1ca3
Aungkokolin1997 <aungkokolin23397@gmail.com>
2022-11-24 09:36:42 +0630

    [MIG] base_country_state_translatable: Migration to 16.0

--- base_country_state_translatable/__manifest__.py
+++ base_country_state_translatable/__manifest__.py
@@ -1,14 +1,14 @@
 # Copyright 2017 initOS GmbH
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
     "name": "Translate Country States",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "depends": ["base"],
     "website": "https://github.com/OCA/partner-contact",
     "summary": "Translate Country States",
     "author": "initOS GmbH, Odoo Community Association (OCA)",
-    "category": "Localisation",
+    "category": "Localization",
     "license": "AGPL-3",
     "installable": True,
 }
