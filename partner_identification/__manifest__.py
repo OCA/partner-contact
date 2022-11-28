288a1aa39eb6a13712eec7b3ef1fc0551253bd45
Victor Champonnois <victor@coopiteasy.be>
2022-11-28 14:31:09 +0100

    [MIG] partner_identification: Migration to 16.0

--- partner_identification/__manifest__.py
+++ partner_identification/__manifest__.py
@@ -9,7 +9,7 @@
 {
     "name": "Partner Identification Numbers",
     "category": "Customer Relationship Management",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["contacts"],
     "data": [
