ad1d260dbbf19bb73009ac2b7035a0cc93209d3e
FernandoRomera <fromera@astrohotel.es>
2022-12-16 12:07:13 +0100

    [16.0][MIG] partner_affiliate: Migration to 16.0

--- partner_affiliate/__manifest__.py
+++ partner_affiliate/__manifest__.py
@@ -5,7 +5,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Partner Affiliates",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Camptocamp, "
     "Tecnativa, "
     "brain-tec AG, "
