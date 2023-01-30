c1350af6c4b1024beef7f5a8b4816daa293cb82c
Aungkokolin1997 <aungkokolin23397@gmail.com>
2023-01-30 08:13:56 +0630

    [ADD] partner_company_default

---
+++ partner_company_default/__manifest__.py
@@ -0,0 +1,11 @@
+# Copyright 2023 Quartile Limited
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Partner Company Default",
+    "version": "15.0.1.0.0",
+    "author": "Quartile Limited, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/partner-contact",
+    "depends": ["base"],
+    "license": "AGPL-3",
+    "installable": True,
+}
