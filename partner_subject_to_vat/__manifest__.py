fca4c409c3d40ba08082cd154fe584c5adccc471
Victor Champonnois <victor@coopiteasy.be>
2023-02-03 10:02:36 +0100

    [ADD] partner_subject_to_vat

---
+++ partner_subject_to_vat/__manifest__.py
@@ -0,0 +1,17 @@
+# SPDX-FileCopyrightText: 2023 Coop IT Easy SC
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+{
+    "name": "Partner Subject to VAT",
+    "summary": """
+        Record whether a company is subject to VAT.""",
+    "version": "16.0.1.0.0",
+    "category": "Contact",
+    "website": "https://github.com/OCA/partner-contact",
+    "author": "Coop IT Easy SC, Odoo Community Association (OCA)",
+    "maintainers": ["victor-champonnois"],
+    "license": "AGPL-3",
+    "depends": ["base"],
+    "data": ["views/res_partner_view.xml"],
+}
