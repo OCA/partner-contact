137abf2ef9488042cea61b682707c0a981b3c417
Maksym Yankin <yankinmk@gmail.com>
2023-03-13 17:57:53 +0200

    [MIG] partner_industry_parent: Migration to 15.0

--- partner_industry_parent/__manifest__.py
+++ partner_industry_parent/__manifest__.py
@@ -2,14 +2,17 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Partner Industry Parent',
-    'summary': """
+    "name": "Partner Industry Parent",
+    "summary": """
         This module add a parent relation to the partner industry""",
-    'version': '12.0.2.0.0',
-    'license': 'AGPL-3',
-    'author': 'ACSONE SA/NV,' 'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/partner-contact',
-    'depends': ["base"],
-    'data': ["views/res_partner_industry.xml"],
-    'demo': [],
+    "version": "15.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV," "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/partner-contact",
+    "depends": ["base"],
+    "data": [
+        "views/res_partner_views.xml",
+        "views/res_partner_industry_views.xml",
+    ],
+    "demo": [],
 }
--- partner_industry_parent/tests/test_res_partner_industry.py
+++ partner_industry_parent/tests/test_res_partner_industry.py
@@ -5,22 +5,22 @@ from odoo.tests.common import TransactionCase
 
 
 class TestResPartnerIndustry(TransactionCase):
-    def setUp(self):
-        super(TestResPartnerIndustry, self).setUp()
-        self.industry_1 = self.env.ref('base.res_partner_industry_O')
-        self.industry_2 = self.env.ref('base.res_partner_industry_P')
-        self.industry_3 = self.env.ref('base.res_partner_industry_Q')
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
+        cls.industry_1 = cls.env.ref("base.res_partner_industry_O")
+        cls.industry_2 = cls.env.ref("base.res_partner_industry_P")
+        cls.industry_3 = cls.env.ref("base.res_partner_industry_Q")
 
     def test_res_partner_industry_complete_name(self):
-        self.assertEqual(
-            self.industry_1.complete_name, 'Public Administration'
-        )
+        self.assertEqual(self.industry_1.complete_name, "Public Administration")
         self.industry_1.parent_id = self.industry_2
         self.assertEqual(
-            self.industry_1.complete_name, 'Education / Public Administration'
+            self.industry_1.complete_name, "Education / Public Administration"
         )
         self.industry_2.parent_id = self.industry_3
         self.assertEqual(
             self.industry_1.complete_name,
-            'Health/Social / Education / Public Administration',
+            "Health/Social / Education / Public Administration",
         )
