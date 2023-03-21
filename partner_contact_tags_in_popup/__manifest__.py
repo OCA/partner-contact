cc7119233d40b89028382425e37a916e3b1f97e0
Carmen Bianca BAKKER <carmen@coopiteasy.be>
2023-03-21 11:38:34 +0100

    [ADD] partner_contact_tags_in_popup
    Signed-off-by: Carmen Bianca BAKKER <carmen@coopiteasy.be>

---
+++ partner_contact_tags_in_popup/__manifest__.py
@@ -0,0 +1,20 @@
+# SPDX-FileCopyrightText: 2023 Coop IT Easy SC
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+{
+    "name": "Partner Contact Tags in Contacts & Addresses Pop-up",
+    "summary": "Display a contact's tags in the 'Contacts & Addresses' pop-up form view.",
+    "version": "16.0.1.0.0",
+    "category": "Customer Relationship Management",
+    "website": "https://github.com/OCA/partner-contact",
+    "author": "Coop IT Easy SC, Odoo Community Association (OCA)",
+    "maintainers": ["carmenbianca"],
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "views/res_partner_views.xml",
+    ],
+}
