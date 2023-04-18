1ddd003214454d36e3fac5d6a0ebf4fc499a4785
Nikul-OSI <nchaudhary@opensourceintegrators.com>
2023-04-18 18:45:21 +0530

    [ADD] partner_middlename: Added in v16

---
+++ partner_middlename/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright (C) 2023 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Partner Middle Name",
+    "summary": "Have split Middle",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/partner-contact",
+    "author": "Open Source Integrators, " "Odoo Community Association (OCA)",
+    "category": "Partner Management",
+    "depends": ["partner_firstname"],
+    "data": ["views/res_partner.xml", "views/res_user.xml"],
+    "installable": True,
+}
