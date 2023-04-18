2d48a0322ba21c09f85ad3f131148d0f9e6bd904
Simon <simon.gonzalez@bluewin.ch>
2023-04-18 12:55:26 +0200

    [MIG] partner_tag_smart_assignation: Migration to 14.0

--- partner_tag_smart_assignation/__manifest__.py
+++ partner_tag_smart_assignation/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Smart Tagger",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Other",
     "summary": "Smart tagger, module to have smart tags who " "update themselves alone",
     "sequence": 150,
