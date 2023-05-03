0a5a70ee6082ea8ac3395aa70d2c154e9f191b06
Bruno Figueiredo <bfigueiredo@odoogap.com>
2023-05-03 17:13:54 +0100

    [IMP] Adding a condition to the unit tests

--- partner_contact_in_several_companies/tests/test_partner_contact_in_several_companies.py
+++ partner_contact_in_several_companies/tests/test_partner_contact_in_several_companies.py
@@ -122,6 +122,10 @@ class PartnerContactInSeveralCompaniesCase(common.TransactionCase):
         new_contact.write({"contact_id": False})
         self.assertEqual(new_contact.contact_type, "standalone")
 
+        # Reset contact to standalone
+        new_contact_contact_type = new_contact.read(['contact_type'])
+        self.assertEqual(new_contact_contact_type[0]['contact_type'], "standalone")
+
         # Reset contact to attached, and ensure only it is unlinked (i.e.
         # context is ignored).
         new_contact.write({"contact_id": self.bob_contact.id})
