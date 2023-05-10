e8e83a11a0ed5536cff20713b5c0356511d821b3
Andreas Hochuli <andreas.hochuli@camptocamp.com>
2023-05-10 15:48:49 +0200

    base_partner_company_group: fix author
    Co-authored-by: Simone Orsi <simahawk@users.noreply.github.com>

--- base_partner_company_group/__manifest__.py
+++ base_partner_company_group/__manifest__.py
@@ -5,7 +5,7 @@
     "summary": "Adds the possibility to add a company group to a company",
     "version": "16.0.1.0.0",
     "category": "Sales",
-    "author": "Camptocamp SA, Odoo Community Association (OCA)",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "depends": ["base"],
     "website": "https://github.com/OCA/partner-contact",
