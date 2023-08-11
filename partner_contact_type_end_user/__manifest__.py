ae739ea4b6cf621819aea35d38dfa674d3920593
François Honoré <francois.honore@acsone.eu>
2023-08-11 11:20:06 +0200

    [IMP] partner_contact_type_end_user: pre-commit stuff

--- partner_contact_type_end_user/__manifest__.py
+++ partner_contact_type_end_user/__manifest__.py
@@ -1,16 +1,13 @@
-# -*- coding: utf-8 -*-
 # Copyright 2020 ACSONE SA/NV
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Partner Contact Type End User',
-    'summary': """Adds a new contact type 'End User'""",
-    'version': '10.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/partner-contact',
-    'depends': [
-        'base'
-    ],
-    'maintainers': ['rousseldenis'],
+    "name": "Partner Contact Type End User",
+    "summary": """Adds a new contact type 'End User'""",
+    "version": "10.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/partner-contact",
+    "depends": ["base"],
+    "maintainers": ["rousseldenis"],
 }
