b3cc60f9fee10b59aeec5bdd6cd7008d06ea2234
François Honoré <francois.honore@acsone.eu>
2023-08-11 11:22:22 +0200

    [MIG] partner_contact_type_end_user: Migration to 16.0

--- partner_contact_type_end_user/__manifest__.py
+++ partner_contact_type_end_user/__manifest__.py
@@ -3,8 +3,8 @@
 
 {
     "name": "Partner Contact Type End User",
-    "summary": """Adds a new contact type 'End User'""",
-    "version": "10.0.1.0.0",
+    "summary": "Adds a new contact type 'End User'",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/partner-contact",
