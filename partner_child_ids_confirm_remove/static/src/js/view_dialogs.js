5c0a634bddae7259a419474b2a7eb580a6682a31
Francesco Ballerini <francescobl.lavoro@gmail.com>
2023-08-14 01:48:24 +0200

    Call _super(): preserve FormViewDialog _remove() inheritance
    Before this commit _remove() function was fully overridden.
    After this commit `super` is called on
    
    - `confirm_callback`
    - when model differ from `res.partner`
    
    The partner will now be removed by the parent _remove(), not
    from the override. Only the `cancel_callback` on confirm dialog
    will bypass _remove(), thus after this commit it will be possible
    to inherit _remove() while keeping module installed.

--- partner_child_ids_confirm_remove/static/src/js/view_dialogs.js
+++ partner_child_ids_confirm_remove/static/src/js/view_dialogs.js
@@ -6,14 +6,14 @@ odoo.define("partner_child_ids_confirm_remove.partner_child_dialog_remove", func
     var core = require("web.core");
     var Dialog = require("web.Dialog");
     var ViewDialogs = require("web.view_dialogs");
-
     var _t = core._t;
 
     ViewDialogs.FormViewDialog.include({
         _remove: function () {
             if (this.res_model === "res.partner") {
                 var self = this;
-
+                var args = arguments;
+                var _super = this._super;
                 var def = new Promise(function (resolve, reject) {
                     var message = _t(
                         "Confirming this will delete child contact from database. Do you still want to proceed?"
@@ -21,16 +21,14 @@ odoo.define("partner_child_ids_confirm_remove.partner_child_dialog_remove", func
                     Dialog.confirm(self, message, {
                         title: _t("Warning"),
                         confirm_callback: function () {
-                            resolve(self.on_remove());
-                            self.close();
+                            resolve(_super.apply(self, args));
                         },
                         cancel_callback: reject,
                     }).on("closed", self, reject);
                 });
                 return def;
             }
-            // Classic workflow if model is not res.partner
-            return Promise.resolve(this.on_remove());
+            return this._super.apply(this, arguments);
         },
     });
 });
