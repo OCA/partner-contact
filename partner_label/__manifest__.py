bf7197cf09018aef834c4d2aa0d443a385e28589
AlexPForgeFlow <alex.paris@forgeflow.com>
2023-09-01 17:09:35 +0200

    [MIG] partner_label: Migration to 16.0

--- partner_label/__manifest__.py
+++ partner_label/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Partner labels",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Therp BV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/partner-contact",
     "license": "AGPL-3",
