b0e895ce2ee0cc19394004516ceefa645edb7f34
JordiMForgeFlow <jordi.masvidal@forgeflow.com>
2023-10-24 11:42:38 +0200

    [ADD] partner_category_type

---
+++ partner_category_type/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2023 ForgeFlow S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Partner Category Type",
+    "summary": "Add a selection field 'Type' to classify Contact Tags.",
+    "version": "16.0.1.0.0",
+    "category": "Partner Management",
+    "website": "https://github.com/OCA/partner-contact",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "maintainers": ["JordiMForgeFlow"],
+    "depends": ["contacts"],
+    "data": ["views/res_partner_category_views.xml"],
+}
