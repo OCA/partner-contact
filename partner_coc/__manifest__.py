068b5c7135b9967ebc291f2f7f7fedfdd288c741
Danny de Jong <ddejong@therp.nl>
2023-11-06 15:52:39 +0100

    [MIG] partner_coc: Dummy module to move old data into new `company_registry` field part of odoo core.

---
+++ partner_coc/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2017-2018 Onestein (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Partner CoC",
+    "summary": "Adds field 'Chamber Of Commerce Registration Number'",
+    "version": "16.0.1.0.0",
+    "category": "Customer Relationship Management",
+    "author": "Onestein,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/partner-contact",
+    "license": "AGPL-3",
+    "depends": ["base"],
+    "maintainers": ["bosd"],
+    "development_status": "Production/Stable",
+    "installable": True,
+}
---
+++ partner_coc/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This module migrates the field *Chamber Of Commerce Registration Number* to the field *Company ID* on partners.
