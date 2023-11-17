373b86b1dfbf6eaa1e42377f5823d003ac920e4e
Simone Rubino <daemo00@gmail.com>
2023-11-18 00:56:01 +0100

    [MIG] partner_contact_personal_information_page: Migration to 17.0 Also adopt the module

--- partner_contact_personal_information_page/__manifest__.py
+++ partner_contact_personal_information_page/__manifest__.py
@@ -1,12 +1,16 @@
+# Copyright 2023 Simone Rubino <daemo00@gmail.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Personal information page for contacts",
     "summary": "Add a page to contacts form to put personal information",
-    "version": "16.0.1.0.1",
+    "version": "17.0.1.0.0",
     "category": "Customer Relationship Management",
     "website": "https://github.com/OCA/partner-contact",
     "author": "Nicolas JEUDY,Odoo Community Association (OCA)",
+    "maintainers": [
+        "Daemo00",
+    ],
     "license": "AGPL-3",
     "application": False,
     "installable": True,
