63cbdd01f7da9415699fba896b41893be4326dbb
Quoc - Pham Ngoc <quoc-pn@komit-consulting.com>
2023-12-06 10:25:51 +0700

    [MIG] partner_contact_gender: Migration to 17.0

--- partner_contact_gender/hooks.py
+++ partner_contact_gender/hooks.py
@@ -1,10 +1,8 @@
 # Copyright 2016-2018 Therp BV <https://therp.nl>.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import SUPERUSER_ID, api
 
 
-def post_init_hook(cr, registry):
-    env = api.Environment(cr, SUPERUSER_ID, {})
+def post_init_hook(env):
     gender_mappings = {
         "female": env.ref("base.res_partner_title_madam")
         + env.ref("base.res_partner_title_miss"),
