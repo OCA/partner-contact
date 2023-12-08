2b617883289ed5f494b5d082a4e08448166d0db7
Tima <tima@invitu.com>
2023-12-08 15:20:52 +0700

    [MIG] partner_contact_nationality: Migration to 12.0

--- partner_contact_nationality/__manifest__.py
+++ partner_contact_nationality/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright (C) 2014-2015  Grupo ESOC <www.grupoesoc.es>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Contact nationality",
     "summary": "Add nationality field to contacts",
-    "version": "10.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "Customer Relationship Management",
     "website": "https://odoo-community.org/",
     "author": "Grupo ESOC, Odoo Community Association (OCA)",
