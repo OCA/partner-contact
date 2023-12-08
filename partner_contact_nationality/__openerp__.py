4533f2151128c493a1f4c3d26bc81cd8dde43f76
Richard deMeester <richard@willowit.com.au>
2023-12-08 15:20:52 +0700

    [9.0] Port partner_contact_nationality.

--- partner_contact_nationality/__openerp__.py
+++ partner_contact_nationality/__openerp__.py
@@ -1,32 +1,26 @@
 # -*- coding: utf-8 -*-
-
-# Odoo, Open Source Management Solution
 # Copyright (C) 2014-2015  Grupo ESOC <www.grupoesoc.es>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    "name": "Contact's nationality",
-    "version": "8.0.1.0.0",
-    "author": "Odoo Community Association (OCA)",
+    "name": "Contact nationality",
+    "summary": "Add nationality field to contacts",
+    "version": "9.0.1.0.0",
     "category": "Customer Relationship Management",
     "website": "https://odoo-community.org/",
+    "author": "Grupo ESOC, Odoo Community Association (OCA)",
+    "contributors": [
+        'Jairo Llopis <j.llopis@grupoesoc.es>',
+        'Richard deMeester <richard@willowit.com.au>',
+    ],
+    "license": "AGPL-3",
+    'application': False,
+    'installable': True,
+    'auto_install': False,
     "depends": [
         "partner_contact_personal_information_page",
     ],
     "data": [
         "views/res_partner.xml",
     ],
-    'installable': False,
 }
