55e1ab8eebd07e7a03204e075ed445482cf41986
Quoc - Pham Ngoc <quoc-pn@komit-consulting.com>
2023-12-08 15:20:52 +0700

    [MIG] partner_contact_nationality: Migration to 17.0

--- partner_contact_nationality/__manifest__.py
+++ partner_contact_nationality/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Contact nationality",
     "summary": "Add nationality field to contacts",
-    "version": "16.0.1.0.1",
+    "version": "17.0.1.0.0",
     "category": "Customer Relationship Management",
     "website": "https://github.com/OCA/partner-contact",
     "author": "Grupo ESOC, Odoo Community Association (OCA)",
