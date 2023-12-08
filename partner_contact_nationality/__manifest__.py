d2046f6245fe57bfd0ab21ab0ef13cedc70a29c6
Pedro Evaristo Gonzalez Sanchez <pedro.gonzalez@pesol.es>
2023-12-08 15:20:52 +0700

    [MIG] partner_contact_nationality: Migration to 15.0

--- partner_contact_nationality/__manifest__.py
+++ partner_contact_nationality/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Contact nationality",
     "summary": "Add nationality field to contacts",
-    "version": "14.0.1.0.1",
+    "version": "15.0.1.0.0",
     "category": "Customer Relationship Management",
     "website": "https://github.com/OCA/partner-contact",
     "author": "Grupo ESOC, Odoo Community Association (OCA)",
