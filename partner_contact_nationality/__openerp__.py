d31ce066404e5037d5da723f5e9aef8a7d69e275
Jairo Llopis <j.llopis@grupoesoc.es>
2023-12-08 15:20:51 +0700

    Create partner_contact_personal_information_page.
    Really I'm just renaming partner_contact_base.
    The new name is more self-explanatory.

--- partner_contact_nationality/__openerp__.py
+++ partner_contact_nationality/__openerp__.py
@@ -23,7 +23,7 @@
     "category": "Customer Relationship Management",
     "website": "https://odoo-community.org/",
     "depends": [
-        "partner_contact_base",
+        "partner_contact_personal_information_page",
     ],
     "data": [
         "views/res_partner.xml",
