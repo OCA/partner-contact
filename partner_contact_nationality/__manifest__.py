93650eb07b1994718fa994441fbee4c7ad5e4209
Marçal Isern <marsal.isern@qubiq.es>
2023-12-08 15:20:52 +0700

    [IMP] partner_contact_nationality: black, isort, prettier

--- partner_contact_nationality/__manifest__.py
+++ partner_contact_nationality/__manifest__.py
@@ -9,18 +9,14 @@
     "website": "https://odoo-community.org/",
     "author": "Grupo ESOC, Odoo Community Association (OCA)",
     "contributors": [
-        'Jairo Llopis <j.llopis@grupoesoc.es>',
-        'Richard deMeester <richard@willowit.com.au>',
-        'Denis Roussel <denis.roussel@acsone.eu>',
+        "Jairo Llopis <j.llopis@grupoesoc.es>",
+        "Richard deMeester <richard@willowit.com.au>",
+        "Denis Roussel <denis.roussel@acsone.eu>",
     ],
     "license": "AGPL-3",
-    'application': False,
-    'installable': True,
-    'auto_install': False,
-    "depends": [
-        "partner_contact_personal_information_page",
-    ],
-    "data": [
-        "views/res_partner.xml",
-    ],
+    "application": False,
+    "installable": True,
+    "auto_install": False,
+    "depends": ["partner_contact_personal_information_page"],
+    "data": ["views/res_partner.xml"],
 }
