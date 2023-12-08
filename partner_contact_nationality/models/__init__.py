a7f6bb9ce5b39259ec15f51f72e12963e23f27d4
Marçal Isern <marsal.isern@qubiq.es>
2023-12-08 15:20:52 +0700

    [MIG] partner_contact_nationality: Migration to 13.0

--- partner_contact_nationality/models/__init__.py
+++ partner_contact_nationality/models/__init__.py
@@ -1,4 +1,3 @@
-# Copyright (C) 2014-2015  Grupo ESOC <www.grupoesoc.es>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from . import res_partner
