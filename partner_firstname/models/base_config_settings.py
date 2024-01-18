528f53df626d4a3c0b234f69798a811275f6f869
payen000 <payen@vauxoo.com>
2024-01-18 16:29:06 +0000

    [FIX] partner_firstname: Avoid name fields rewrite on partner creation
    Previously, creating a non-company partner by passing the `name` field
    would trigger the `_inverse_name_after_cleaning_whitespace` method which,
    for values of `firstname` and `lastname` with multiple words, would result
    in most of those words being set to the `lastname` field, even if the
    correct values were explicitly passed to the `create` method. For example,
    duplicating a non-company partner with the following field values:
    
    firstname = Francisco Javier
    lastname = Garcia Cabeza De Vaca
    
    Would result in a partner with these values:
    
    firstname = Francisco
    lastname = Javier Garcia Cabeza De Vaca (copy)
    
    A similar issue can be reproduced when recalculating names: when
    explicitly calling `_compute_name`, the
    `_inverse_name_after_cleaning_whitespace` is called and the `firstname`
    and `lastname` fields are modified, if they contain more than one word.
    
    This commit solves the issue and prevents the `_inverse_name_after_cleaning_whitespace`
    from being called when creating a non-company partner while explicitly
    providing the `firstname` or `lastname` fields, and when recalculating
    names.
    
    In addition to the 16.0: Complying with E501 linting rule

--- partner_firstname/models/base_config_settings.py
+++ partner_firstname/models/base_config_settings.py
@@ -61,7 +61,10 @@ class ResConfigSettings(models.TransientModel):
         )
         partners = self._partners_for_recalculating()
         _logger.info("Recalculating names for %d partners.", len(partners))
-        partners._compute_name()
+        # Use add_to_compute instead of _compute_name to avoid triggering
+        # _inverse_name_after_cleaning_whitespace, which can
+        # modify a partner's firstname, lastname and lastname2
+        self.env.add_to_compute(self.env["res.partner"]._fields["name"], partners)
         self.partner_names_order_changed = False
         self.execute()
         _logger.info("%d partners updated.", len(partners))
