ff8e5fc4a056cab6f0a34f5810f7048e4ba45afd
Frédéric GRALL <fgr@apik.cloud>
2024-01-25 10:10:28 +0100

    [IMP]partner-contact: mig base_location

--- base_location_geonames_import/__manifest__.py
+++ base_location_geonames_import/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     "name": "Base Location Geonames Import",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "development_status": "Mature",
     "category": "Partner Management",
     "license": "AGPL-3",
