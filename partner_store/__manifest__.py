69404842c5079318bd68c6d5e4a4814dd2b685de
Hadrien Huvelle <hhu@baboum.be>
2024-02-06 07:15:43 +0100

    Init partner store

---
+++ partner_store/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2024 Camptocamp
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Partner Store",
+    "summary": "Add store type to Partners",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/partner-contact",
+    "category": "",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["contacts"],
+    "data": [],
+    "installable": True,
+    "maintainers": ["wouitmil"],
+}
