9a03424f0a02e5c571ee58d684159096ccc9b221
Maksym Yankin <yankinmk@gmail.com>
2024-03-20 14:50:00 +0200

    [MIG] partner_stage: Migration to 17.0

--- partner_stage/init_hook.py
+++ partner_stage/init_hook.py
@@ -3,20 +3,17 @@
 
 import logging
 
-from odoo import SUPERUSER_ID, api
-
 _logger = logging.getLogger(__name__)
 
 
-def post_init_hook(cr, registry):
+def post_init_hook(env):
     """Set default Stage on partners"""
-    env = api.Environment(cr, SUPERUSER_ID, {})
     Partner = env["res.partner"]
     default_stage = Partner._get_default_stage_id()
     missing_stages = Partner.search([("stage_id", "=", False)])
     if default_stage and missing_stages:
         _logger.info("Init stage_id for %d partner records...", len(missing_stages))
-        cr.execute(
+        env.cr.execute(
             """
             UPDATE res_partner
             SET stage_id = %(id)s, state = %(state)s
