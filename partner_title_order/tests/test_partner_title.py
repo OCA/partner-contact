831e4146897108cad5959c4ea82c8c65ee369cb1
Ruchir Shukla <ruchir@bizzappdev.com>
2024-03-28 11:20:15 +0530

    [MIG][15.0]partner_title_order:Migration to 15.0

--- partner_title_order/tests/test_partner_title.py
+++ partner_title_order/tests/test_partner_title.py
@@ -1,10 +1,10 @@
 # Copyright 2020 Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
 
-from odoo.tests.common import SavepointCase
+from odoo.tests.common import TransactionCase
 
 
-class TestPartnerTitle(SavepointCase):
+class TestPartnerTitle(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
