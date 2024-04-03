33850cab0ee58fd2f01fad7407ccdd8bc4d96f39
syera.bonneaux@akretion.com <syera.bonneaux@akretion.com>
2024-04-03 10:21:06 +0200

    [16.0][ADD] FIX pre_commit: add partner_title_active on partner_contact

--- partner_title_active/models/res_partner_title.py
+++ partner_title_active/models/res_partner_title.py
@@ -2,10 +2,10 @@
 # @author: Syera BONNEAUX <syera.bonneaux@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import _, models, fields, api
+from odoo import fields, models
 
 
 class PartnerTitle(models.Model):
-    _inherit = 'res.partner.title'
+    _inherit = "res.partner.title"
 
-    active = fields.Boolean(string='Active', default=True)
+    active = fields.Boolean(default=True)
