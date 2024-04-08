6c0a9bc35c99f890dd454901025eaa03b46c2f72
chien <chien@trobz.com>
2024-04-08 17:09:59 +0700

    [MIG] partner_pricelist_search: Migration to 17.0

--- partner_pricelist_search/__manifest__.py
+++ partner_pricelist_search/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Partner pricelist search",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "author": "Tecnativa,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/partner-contact",
     "category": "Partner Management",
