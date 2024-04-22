8a16be2c4f57a3c9c9deccb604be175ef1c1b3ea
Víctor Martínez <victor.martinez@tecnativa.com>
2024-04-22 18:29:40 +0200

    [IMP] partner_category_security: Add Contact Tags menu from Managers
    Add a menu to be able to access Contact Tags. The menu will not be displayed if the
    user has the base.group_system permission.
    
    TT48639

--- partner_category_security/__manifest__.py
+++ partner_category_security/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Partner category security",
-    "version": "16.0.1.0.0",
+    "version": "16.0.2.0.0",
     "category": "Customer Relationship Management",
     "website": "https://github.com/OCA/partner-contact",
     "author": "Tecnativa, Odoo Community Association (OCA)",
