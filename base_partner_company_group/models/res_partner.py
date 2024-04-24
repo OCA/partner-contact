a4859f20e652352bcbb6822fa60340d689bb82c8
Mohamed Osman <mohamed.osman3@yahoo.com>
2024-04-24 11:39:32 +0000

    [MIG] base_partner_company_group: Migration to 17.0

--- base_partner_company_group/models/res_partner.py
+++ base_partner_company_group/models/res_partner.py
@@ -9,7 +9,6 @@ class Contact(models.Model):
 
     company_group_id = fields.Many2one(
         "res.partner",
-        string="Company group",
         domain=[("is_company", "=", True)],
         recursive=True,
     )
@@ -21,3 +20,13 @@ class Contact(models.Model):
 
     def _commercial_fields(self):
         return super()._commercial_fields() + ["company_group_id"]
+
+    def action_view_company_group_members(self):
+        action = self.env["ir.actions.act_window"]._for_xml_id(
+            "base_partner_company_group.action_open_group_members"
+        )
+        all_child = self.with_context(active_test=False).search(
+            [("id", "child_of", self.ids)]
+        )
+        action["domain"] = [("company_group_id", "in", all_child.ids)]
+        return action
