d2b5911613ba02d1c06ce3132f4215ae6bd97739
sygel <alberto.martinez@sygel.es>
2024-05-21 09:11:22 +0200

    [MIG] partner_contact_access_link: Migration to 17.0

--- partner_contact_access_link/__manifest__.py
+++ partner_contact_access_link/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Partner contact access link",
     "summary": "Allow to visit the full contact form from a company",
-    "version": "16.0.1.1.0",
+    "version": "17.0.1.0.0",
     "development_status": "Production/Stable",
     "category": "Tools",
     "website": "https://github.com/OCA/partner-contact",
