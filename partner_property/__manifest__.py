6b7ac3695f68fd8b863456005848d79a9c53d986
Víctor Martínez <victor.martinez@tecnativa.com>
2024-06-05 11:37:26 +0200

    [ADD] partner_property: New module
    TT49480

---
+++ partner_property/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2024 Tecnativa - Víctor Martínez
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Partner Property",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/partner-contact",
+    "version": "16.0.1.0.0",
+    "depends": ["base"],
+    "license": "AGPL-3",
+    "category": "Partner Management",
+    "data": ["views/res_partner_views.xml"],
+    "installable": True,
+    "maintainers": ["victoralmau"],
+}
