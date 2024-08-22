7d54ca6b188f83252da299506b52d454fe9fae5e
Aungkokolin1997 <lin@quartile.co>
2024-08-22 02:33:52 +0000

    [ADD] partner_search_alias

---
+++ partner_search_alias/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2024 Quartile (https://www.quartile.co)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Partner Search Alias",
+    "version": "16.0.1.0.0",
+    "author": "Quartile, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/partner-contact",
+    "category": "Partner",
+    "license": "AGPL-3",
+    "depends": ["base"],
+    "data": [
+        "views/res_partner_views.xml",
+    ],
+    "installable": True,
+}
