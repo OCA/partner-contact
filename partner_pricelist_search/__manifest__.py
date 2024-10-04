e48816c4f0dcfd16cfc7420277840f46a1904773
Duy (Đỗ Anh) <doanhduyxavie@gmail.com>
2024-10-04 14:12:57 +0700

    [MIG] partner_pricelist_search: Migration to 18.0

--- partner_pricelist_search/__manifest__.py
+++ partner_pricelist_search/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Partner pricelist search",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Tecnativa,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/partner-contact",
     "category": "Partner Management",
