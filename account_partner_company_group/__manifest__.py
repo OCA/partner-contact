c4650301049b2dd43d1bed8c02710aa14e1df3bb
Nguyen Minh Chien <chien@trobz.com>
2024-10-09 17:44:08 +0700

    [MIG] account_partner_company_group: Migration to 18.0

--- account_partner_company_group/__manifest__.py
+++ account_partner_company_group/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Account Partner Company Group",
     "summary": "Adds the possibility to add a company group to a company",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Sales",
     "author": "Camptocamp SA, Odoo Community Association (OCA)",
     "license": "AGPL-3",
