95a2d3f3054265454a044557b9b68a5cbf187f83
Nguyen Minh Chien <chien@trobz.com>
2024-10-09 17:44:32 +0700

    [MIG] sale_partner_company_group: Migration to 18.0

--- sale_partner_company_group/tests/test_sale_partner_company_group.py
+++ sale_partner_company_group/tests/test_sale_partner_company_group.py
@@ -10,16 +10,6 @@ class TestSalePartnerCompanyGroup(TestBasePartnerCompanyGroup):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        # Remove this variable in v16 and put instead:
-        # from odoo.addons.base.tests.common import DISABLED_MAIL_CONTEXT
-        DISABLED_MAIL_CONTEXT = {
-            "tracking_disable": True,
-            "mail_create_nolog": True,
-            "mail_create_nosubscribe": True,
-            "mail_notrack": True,
-            "no_reset_password": True,
-        }
-        cls.env = cls.env(context=dict(cls.env.context, **DISABLED_MAIL_CONTEXT))
         currency = cls.env.ref("base.USD")
         cls.pricelist1 = cls.env["product.pricelist"].create(
             {"name": "Pricelist 01", "currency_id": currency.id}
