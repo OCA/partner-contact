618562f0794de179ec54da86f574b99cf57a2ea2
Joachim David <joachim.david@dynapps.be>
2024-10-14 16:59:51 +0200

    [MIG] partner_firstname: Migration to 18.0

--- partner_firstname/exceptions.py
+++ partner_firstname/exceptions.py
@@ -1,12 +1,12 @@
 # Copyright 2014-2015 Grupo ESOC (<http://www.grupoesoc.es>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from odoo import _, exceptions
+from odoo import exceptions
 
 
 class EmptyNamesError(exceptions.ValidationError):
-    def __init__(self, record, value=None):
-        value = value or _("No name is set.")
+    def __init__(self, record, env, value=None):
+        value = value or env._("No name is set.")
         self.record = record
         self._value = value
-        self._name = _("Error(s) with partner %d's name.") % record.id
+        self._name = env._("Error(s) with partner %d's name.") % record.id
         self.args = (self._name, value)
