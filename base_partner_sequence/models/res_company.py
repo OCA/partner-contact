2718d8147ccad7ffb7abccad321f90d50a73f0a8
Denis Roussel <denis.roussel@acsone.eu>
2024-10-16 16:50:57 +0200

    [IMP] base_partner_sequence: Allows to use unique generated references across all partners
    In some configurations, company strategy will use a unique reference for all its partners.
    A configuration parameter will allow to enable that behavior.

--- base_partner_sequence/models/res_company.py
+++ base_partner_sequence/models/res_company.py
@@ -11,3 +11,31 @@ class ResCompany(models.Model):
         string="Partner Reference Readonly",
         help="If marked, the Reference in partners will not be editable.",
     )
+    partner_generated_reference_unique = fields.Boolean(
+        compute="_compute_partner_generated_reference_unique",
+        inverse="_inverse_partner_generated_reference_unique",
+        groups="base.group_system",
+        help="This is a technical field that allows to get this from config parameter",
+    )
+
+    def _compute_partner_generated_reference_unique(self):
+        """
+        Compute the parameter to be accessible on company level
+        """
+        unique = (
+            self.env["ir.config_parameter"]
+            .sudo()
+            .get_param("base_partner_sequence.partner_generated_reference_unique")
+        )
+        self.partner_generated_reference_unique = unique
+
+    def _inverse_partner_generated_reference_unique(self):
+        """
+        Allows to change the parameter through company field.
+        """
+        unique = bool()
+        for company in self:
+            unique = company.partner_generated_reference_unique
+        self.env["ir.config_parameter"].sudo().set_param(
+            "base_partner_sequence.partner_generated_reference_unique", unique
+        )
--- base_partner_sequence/models/res_config_settings.py
+++ base_partner_sequence/models/res_config_settings.py
@@ -10,3 +10,10 @@ class ResConfigSettings(models.TransientModel):
     partner_ref_readonly = fields.Boolean(
         related="company_id.partner_ref_readonly", readonly=False
     )
+    # This as general parameter as multi-company behavior is quite impossible
+    # to manage correctly as 'ref' field is not company dependent.
+    partner_generated_reference_unique = fields.Boolean(
+        config_parameter="base_partner_sequence.partner_generated_reference_unique",
+        help="Check this if you want to have unique generated references "
+        "across all partners (companies, contacts, addresses,...)",
+    )
