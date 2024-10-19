cb61b086c2c00370db0b4e4a63c9b2ffe2b58ba5
Gil Arasa Verge <gav@wvbs.eu>
2024-10-19 13:27:01 +0200

    [MIG] partner_company_default: Migration to 17.0

--- partner_company_default/__manifest__.py
+++ partner_company_default/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Partner Company Default",
-    "version": "16.0.1.1.2",
+    "version": "17.0.1.0.0",
     "author": "Quartile Limited, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/partner-contact",
     "depends": ["base"],
