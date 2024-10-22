34dfd54a471baa2e2a9d2f991ff32c0de5b9dcd5
Bert Van Groenendael <bert.vangroenendael@dynapps.be>
2024-10-22 10:47:12 +0200

    [IMP] partner_stage: pre-commit stuff

--- partner_stage/__manifest__.py
+++ partner_stage/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "Open Source Integrators, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/partner-contact",
     "category": "Sales/CRM",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["contacts"],
     "data": [
