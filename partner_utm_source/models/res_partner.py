f63d09d3a980ba8551bdd86be8e1a882e7de45fb
natuan9 <tuan3032001@gmail.com>
2024-10-31 12:03:15 +0700

    [ADD] partner_utm_source

---
+++ partner_utm_source/models/res_partner.py
@@ -0,0 +1,9 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    utm_source_id = fields.Many2one("utm.source", string="UTM Source")
---
+++ partner_utm_source/readme/DESCRIPTION.md
@@ -0,0 +1,3 @@
+This module includes the following features:
+- Adds a source field to the partner form view.
+- Enables grouping by source in the partner tree view.
