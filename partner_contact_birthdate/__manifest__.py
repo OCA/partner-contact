49752fdd5cf31f592a8090b3cb1cb023faf798b7
Borruso <gborruso@dinamicheaziendali.it>
2024-11-27 09:24:34 +0100

    [MIG] partner_contact_birthdate: Migration to 18.0

--- partner_contact_birthdate/__manifest__.py
+++ partner_contact_birthdate/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     "name": "Contact's birthdate",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "maintainers": [
         "Daemo00",
