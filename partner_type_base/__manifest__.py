c93653fcbbefe8036c03e2875373ad0fb3874b72
Christopher Rogos <crogos@gmail.com>
2024-11-28 18:41:47 +0000

    [ADD] partner_type_base: improve partner type customizable

---
+++ partner_type_base/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2013 Nicolas Bessi (Camptocamp SA)
+# Copyright 2014 Agile Business Group (<http://www.agilebg.com>)
+# Copyright 2015 Grupo ESOC (<http://www.grupoesoc.es>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Partner Address Types",
+    "summary": "Base implementation to improve the address type customization.",
+    "version": "18.0.1.0.0",
+    "author": "glueckkanja AG, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Extra Tools",
+    "website": "https://github.com/OCA/partner-contact",
+    "depends": ["base_setup"],
+    "data": [
+        "views/res_partner.xml",
+    ],
+    "auto_install": False,
+    "installable": True,
+}
