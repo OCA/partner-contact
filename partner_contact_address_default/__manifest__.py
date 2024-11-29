ea0afaa854bb5ef7223dedad7809d4f31f635d2a
Borruso <gborruso@dinamicheaziendali.it>
2024-11-29 11:57:20 +0100

    [MIG] partner_contact_address_default: Migration to 18.0

--- partner_contact_address_default/__manifest__.py
+++ partner_contact_address_default/__manifest__.py
@@ -3,8 +3,9 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Partner Contact address default",
-    "summary": "Set a default delivery address, invoice address and contact for contacts",
-    "version": "16.0.1.2.0",
+    "summary": "Set a default delivery address, "
+    "invoice address and contact for contacts",
+    "version": "18.0.1.0.0",
     "development_status": "Beta",
     "category": "Generic Modules/Base",
     "website": "https://github.com/OCA/partner-contact",
