fcce2ce574fd9d9e5da0ea6843d5678b6176817c
Aungkokolin1997 <lin@quartile.co>
2025-01-17 08:42:14 +0000

    imp

--- partner_search_alias/models/res_partner.py
+++ partner_search_alias/models/res_partner.py
@@ -1,7 +1,9 @@
 # Copyright 2024 Quartile (https://www.quartile.co)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models
+from lxml import etree
+
+from odoo import api, fields, models
 
 
 class ResPartner(models.Model):
@@ -12,3 +14,19 @@ class ResPartner(models.Model):
     @property
     def _rec_names_search(self):
         return list(set(super()._rec_names_search + ["search_alias"]))
+
+    @api.model
+    def get_view(self, view_id=None, view_type="form", **options):
+        res = super().get_view(view_id, view_type, **options)
+        if view_type == "search":
+            xml = etree.XML(res["arch"])
+            name_field = xml.xpath("//field[@name='name']")
+            if name_field:
+                name = name_field[0]
+                filter_domain = name.get("filter_domain", "[]")
+                filter_domain = filter_domain.replace(
+                    "[", "['|', ('search_alias', 'ilike', self), "
+                )
+                name.set("filter_domain", filter_domain)
+                res["arch"] = etree.tostring(xml)
+        return res
