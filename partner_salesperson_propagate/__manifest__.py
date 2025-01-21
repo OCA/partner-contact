1f935ae1d37b263f75203d54b984eb37f3c072af
Marina Alapont <marina.alapont@forgeflow.com>
2025-01-21 14:02:39 +0100

    [ADD] partner_salesperson_propagate
    With this module the changes on the salesperson field (user_id) in a partner will be propagated to all of its contacts (childs).

---
+++ partner_salesperson_propagate/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2025 ForgeFlow S.L. (https://www.forgeflow.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Partner Salesperson Propagate",
+    "version": "16.0.1.0.0",
+    "author": "ForgeFlow,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/partner-contact",
+    "category": "Customer Relationship Management",
+    "summary": "Propagate any changes in the salesperson field "
+    "from the partner to its contacts.",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+    ],
+    "data": [],
+    "installable": True,
+    "application": False,
+}
---
+++ partner_salesperson_propagate/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+Propagate the changes made in a partner's salesperson field (user_id) from parent to child.
