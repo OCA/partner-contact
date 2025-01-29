e56d0ebe9b3cea0af13c76e0c99049d5e117932c
abdessamad salim <abdessamad.salim@smile-maroc.com>
2025-01-29 08:47:04 +0100

    [MIG] partner_company_type: Migration to 18.0

--- partner_company_type/tests/test_res_partner_company_type.py
+++ partner_company_type/tests/test_res_partner_company_type.py
@@ -17,6 +17,29 @@ class TestResPartnerCompanyType(TransactionCase):
 
     def test_00_duplicate(self):
         # Test Duplicate Company type
-
         with self.assertRaises(IntegrityError), tools.mute_logger("odoo.sql_db"):
             self.company_type.create(dict(name=self.company_type.name))
+
+    def test_create_company_type(self):
+        # Test creating a new company type
+        new_company_type = self.company_type.create(dict(name="New Company Type"))
+        self.assertTrue(new_company_type, "New company type should be created")
+
+    def test_update_company_type(self):
+        # Test updating an existing company type
+        self.company_type.write(dict(name="Updated Company Type"))
+        self.assertEqual(
+            self.company_type.name,
+            "Updated Company Type",
+            "Company type name should be updated",
+        )
+
+    def test_delete_company_type(self):
+        # Test deleting a company type
+        company_type_to_delete = self.company_type.create(
+            dict(name="Company Type to Delete")
+        )
+        company_type_to_delete.unlink()
+        self.assertFalse(
+            company_type_to_delete.exists(), "Company type should be deleted"
+        )
