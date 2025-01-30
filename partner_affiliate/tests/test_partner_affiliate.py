a1c77fc0de93f3bcf756fac79a99b8956579c1c6
abdessamad salim <abdessamad.salim@smile-maroc.com>
2025-01-30 16:52:49 +0100

    [MIG] partner_affiliate: Migration to 18.0

--- partner_affiliate/tests/test_partner_affiliate.py
+++ partner_affiliate/tests/test_partner_affiliate.py
@@ -26,3 +26,33 @@ class TestPartnerAffiliate(TransactionCase):
         self.assertEqual(res["res_id"], self.affiliate.id)
         self.assertEqual(res["view_mode"], "form")
         self.assertEqual(res["target"], "current")
+
+    def test_create_affiliate(self):
+        """Test creating a new affiliate"""
+        new_affiliate = self.partner_model.create(
+            {
+                "name": "New Affiliate",
+                "company_type": "company",
+                "parent_id": self.company.id,
+            }
+        )
+        self.assertTrue(new_affiliate, "New affiliate should be created")
+
+    def test_update_affiliate(self):
+        """Test updating an existing affiliate"""
+        self.affiliate.write({"name": "Updated Affiliate"})
+        self.assertEqual(
+            self.affiliate.name, "Updated Affiliate", "Affiliate name should be updated"
+        )
+
+    def test_delete_affiliate(self):
+        """Test deleting an affiliate"""
+        affiliate_to_delete = self.partner_model.create(
+            {
+                "name": "Affiliate to Delete",
+                "company_type": "company",
+                "parent_id": self.company.id,
+            }
+        )
+        affiliate_to_delete.unlink()
+        self.assertFalse(affiliate_to_delete.exists(), "Affiliate should be deleted")
