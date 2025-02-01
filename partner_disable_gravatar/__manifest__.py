9420956cfd9e9408e23e280e67c3358e8bb8cb88
Alexis de Lattre <alexis.delattre@akretion.com>
2025-02-01 15:20:44 +0100

    [MIG] partner_disable_gravatar to v18

--- partner_disable_gravatar/__manifest__.py
+++ partner_disable_gravatar/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Partner Disable Gravatar",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Tools",
     "license": "AGPL-3",
     "summary": "Disable automatic connection to gravatar.com",
