d54e635b7f68ebc659f5470018902fdb375b5519
Víctor Martínez <victor.martinez@tecnativa.com>
2025-02-05 17:42:02 +0100

    [MIG] partner_country_lang: Migration to 17.0
    TT54939

--- partner_country_lang/tests/test_partner_country_lang.py
+++ partner_country_lang/tests/test_partner_country_lang.py
@@ -1,10 +1,10 @@
-# Copyright 2022 Tecnativa - Víctor Martínez
+# Copyright 2022-2025 Tecnativa - Víctor Martínez
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.tests import Form, TransactionCase
+from odoo.addons.base.tests.common import BaseCommon
 
 
-class TestPartnerCountryLang(TransactionCase):
+class TestPartnerCountryLang(BaseCommon):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
@@ -13,11 +13,6 @@ class TestPartnerCountryLang(TransactionCase):
         cls.country_fr = cls.env.ref("base.fr")
         cls.country_fr.lang = cls.lang_fr.code
         cls.partner = cls.env["res.partner"].create({"name": "Mr Odoo"})
-
-    def test_partner_onchange(self):
-        partner_form = Form(self.partner)
-        partner_form.country_id = self.country_fr
-        self.assertEqual(partner_form.lang, self.lang_fr.code)
 
     def test_partner_create(self):
         partner = self.env["res.partner"].create(
