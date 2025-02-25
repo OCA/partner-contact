9afcd40c772680a153bd065e3c13ce3d4dfe882c
Bhavesh Heliconia <oca@heliconia.in>
2025-02-25 15:28:59 +0530

    [MIG] partner_store: Migration to 18.0

--- partner_store/__manifest__.py
+++ partner_store/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/partner-contact",
     "category": "",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["contacts"],
     "data": [],
