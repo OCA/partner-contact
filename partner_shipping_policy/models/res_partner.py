b1cdd0dbb41ee423be014846c733e2b874439bf0
Bhavesh Heliconia <oca@heliconia.in>
2025-02-25 15:22:24 +0530

    [IMP] partner_shipping_policy: pre-commit auto fixes

--- partner_shipping_policy/models/res_partner.py
+++ partner_shipping_policy/models/res_partner.py
@@ -10,9 +10,9 @@ class Partner(models.Model):
     picking_policy = fields.Selection(
         selection="_get_picking_policy_selection",
         string="Shipping Policy",
-        help="Shipping policy to use in this partner's sales orders. If you deliver all "
-        "products at once, the delivery order will be scheduled based on the greatest "
-        "product lead time. Otherwise, it will be based on the shortest.",
+        help="Shipping policy to use in this partner's sales orders. If you deliver"
+        "all products at once, the delivery order will be scheduled based on the"
+        "greatest product lead time. Otherwise, it will be based on the shortest.",
     )
 
     @api.model
