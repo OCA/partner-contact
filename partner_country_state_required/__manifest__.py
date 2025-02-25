2854161cb663c40cdc2edea4e51bb041c5c42003
Bhavesh Heliconia <oca@heliconia.in>
2025-02-25 15:08:28 +0530

    [MIG] partner_country_state_required: Migration to 18.0

--- partner_country_state_required/__manifest__.py
+++ partner_country_state_required/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Partner Country State Required",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/partner-contact",
     "depends": ["base"],
