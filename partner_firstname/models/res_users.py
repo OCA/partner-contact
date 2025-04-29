639113063f5f8c8e1dd23ae93a1056a44d32e652
Adam Heinz <adam.heinz@metricwise.com>
2025-04-29 16:09:04 -0400

    [FIX] partner_firstname: copy() multiple res.users records at once.
    This avoids a ValueError being raised to the user interface when
    a user attempts Actions > Duplicate for multiple users at once.

--- partner_firstname/models/res_users.py
+++ partner_firstname/models/res_users.py
@@ -2,7 +2,7 @@
 # Copyright 2014 Agile Business Group (<http://www.agilebg.com>)
 # Copyright 2015 Grupo ESOC (<http://www.grupoesoc.es>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from odoo import _, api, models
+from odoo import api, models
 
 
 class ResUser(models.Model):
@@ -32,18 +32,21 @@ class ResUser(models.Model):
             rec.name = rec.partner_id._get_computed_name(rec.lastname, rec.firstname)
 
     def copy(self, default=None):
-        self.ensure_one()
-        default = dict(default or {})
-        if ("name" not in default) and ("partner_id" not in default):
-            default["name"] = _("%(name)s (copy)", name=self.name)
-        if "login" not in default:
-            default["login"] = _("%(login)s (copy)", login=self.login)
-        if (
-            ("firstname" not in default)
-            and ("lastname" not in default)
-            and ("name" in default)
-        ):
-            default.update(
-                self.env["res.partner"]._get_inverse_name(default["name"], False)
-            )
-        return super().copy(default)
+        default_ = default or {}
+        records = self.browse()
+        for record in self:
+            default = dict(default_)
+            if ("name" not in default) and ("partner_id" not in default):
+                default["name"] = self.env._("%(name)s (copy)", name=record.name)
+            if "login" not in default:
+                default["login"] = self.env._("%(login)s (copy)", login=record.login)
+            if (
+                ("firstname" not in default)
+                and ("lastname" not in default)
+                and ("name" in default)
+            ):
+                default.update(
+                    self.env["res.partner"]._get_inverse_name(default["name"], False)
+                )
+            records |= super(ResUser, record).copy(default)
+        return records
