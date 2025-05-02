85bdff5faa2b38cc081ab0f69187e86ca1e7c6ee
Sergio Zanchetta <primes2h@gmail.com>
2025-05-02 11:02:15 +0200

    [FIX] base_location_nuts: fix category in manifest file

--- base_location_nuts/__manifest__.py
+++ base_location_nuts/__manifest__.py
@@ -6,8 +6,8 @@
 
 {
     "name": "NUTS Regions",
-    "category": "Localisation/Europe",
-    "version": "18.0.1.1.0",
+    "category": "Localization/Europe",
+    "version": "18.0.1.1.1",
     "depends": ["contacts"],
     "data": [
         "views/res_country_view.xml",
