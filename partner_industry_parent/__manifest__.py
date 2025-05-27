be5a17c0754c7f4269239e14673ac0f5cecc1ec7
Samir GUESMI <samir.guesmi@acsone.eu>
2025-05-27 15:39:43 +0200

    [IMP] partner_industry_parent: pre-commit auto fixes

--- partner_industry_parent/__manifest__.py
+++ partner_industry_parent/__manifest__.py
@@ -2,14 +2,14 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Partner Industry Parent',
-    'summary': """
+    "name": "Partner Industry Parent",
+    "summary": """
         This module add a parent relation to the partner industry""",
-    'version': '12.0.2.0.0',
-    'license': 'AGPL-3',
-    'author': 'ACSONE SA/NV,' 'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/partner-contact',
-    'depends': ["base"],
-    'data': ["views/res_partner_industry.xml"],
-    'demo': [],
+    "version": "12.0.2.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV," "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/partner-contact",
+    "depends": ["base"],
+    "data": ["views/res_partner_industry.xml"],
+    "demo": [],
 }
