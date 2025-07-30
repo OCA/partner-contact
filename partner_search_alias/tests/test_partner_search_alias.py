dc12340a6569f3ee32107899c0696f7a0e50ddaa
Ruchir Shukla <ruchir@bizzappdev.com>
2025-07-30 15:42:40 +0530

    [MIG] partner_search_alias: Migration to 18.0

--- partner_search_alias/tests/test_partner_search_alias.py
+++ partner_search_alias/tests/test_partner_search_alias.py
@@ -1,6 +1,8 @@
 # Copyright 2024 Quartile (https://www.quartile.co)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
+from lxml import etree
+
 from odoo.tests.common import TransactionCase
 
 
@@ -16,10 +18,22 @@ class TestPartnerAlias(TransactionCase):
         )
 
     def test_name_search_with_alias(self):
-        partner_ids = self.env["res.partner"]._name_search("alias")
+        partners = self.env["res.partner"].name_search("alias")
+        partner_ids = [partner[0] for partner in partners]
         # Ensure the search returns the created partner
         self.assertIn(
             self.partner.id,
             partner_ids,
             "The partner with search_alias 'alias' should be found.",
+        )
+
+    def test_get_view_search_filter_domain_modified(self):
+        # Ensure that search filter include the search_alias.
+        view = self.partner.get_view(view_type="search")
+        xml = etree.XML(view["arch"])
+        name_field = xml.xpath("//field[@name='name']")
+        self.assertTrue(name_field, "Search view must contain name field")
+        filter_domain = name_field[0].get("filter_domain", "")
+        self.assertIn(
+            "search_alias", filter_domain, "Filter domain must include 'search_alias'"
         )
