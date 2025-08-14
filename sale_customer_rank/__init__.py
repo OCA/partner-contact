2f30d6b1ae3a1e6cdca947eed8b1d9cb19c99984
Ruchir Shukla <ruchir@bizzappdev.com>
2025-08-14 11:02:40 +0530

    [MIG] sale_customer_rank: Migration to 17.0

--- sale_customer_rank/__init__.py
+++ sale_customer_rank/__init__.py
@@ -1,11 +1,9 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from . import models
-from odoo import api, SUPERUSER_ID
 
 
-def post_init_hook(cr, registry):
-    env = api.Environment(cr, SUPERUSER_ID, {})
+def post_init_hook(env):
     partners = env["sale.order"].search([]).mapped("partner_id")
     partners |= partners.mapped("commercial_partner_id")
     for partner in partners:
