fcd971e6ab500ffa54bab7dde8402e58ccd1dd1b
Ronald Portier <ronald@therp.nl>
2025-08-14 13:25:07 +0200

    [IMP] partner_mobile_duplicate_warn: make module extendable

--- partner_mobile_duplicate_warn/models/res_partner.py
+++ partner_mobile_duplicate_warn/models/res_partner.py
@@ -1,6 +1,7 @@
-# Copyright 2021-2022 Akretion France (http://www.akretion.com/)
-# @author: Alexis de Lattre <alexis.delattre@akretion.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright 2021-2022 Akretion France (https://www.akretion.com/).
+# Copyright 2025 Therp BV (https://therp.nl/).
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
 
@@ -15,26 +16,44 @@ class ResPartner(models.Model):
         compute_sudo=True,
     )
 
-    @api.depends("mobile", "company_id")
+    @api.depends(lambda x: x._get_same_mobile_depends())
     def _compute_same_mobile_partner_id(self):
+        empty_recordset = self.env[self._name]
+        for partner in self:
+            partner.same_mobile_partner_id = (
+                partner._find_same_mobile_partner()
+                if partner.mobile
+                else empty_recordset
+            )
+
+    def _find_same_mobile_partner(self):
+        """Find one partner with the same mobile."""
+        self.ensure_one()
+        domain = self._get_same_mobile_domain()
+        return self.with_context(active_test=False).search(domain, limit=1)
+
+    @api.model
+    def _get_same_mobile_depends(self):
+        """Return the fields on which same_mobile_partner_id depends.
+
+        Return the fields used in _get_same_mobile_domain function.
+        """
+        return ["mobile", "company_id"]
+
+    def _get_same_mobile_domain(self):
+        """Return domain to find partners with same mobile."""
+        self.ensure_one()
         # With phone_validation, the "mobile" field should be
         # clean in E.164 format, without any start/ending spaces
         # So we search on the 'mobile' field with '=' !
-        for partner in self:
-            same_mobile_partner_id = False
-            if partner.mobile:
-                domain = [("mobile", "=", partner.mobile)]
-                if partner.company_id:
-                    domain += [
-                        "|",
-                        ("company_id", "=", False),
-                        ("company_id", "=", partner.company_id.id),
-                    ]
-                partner_id = partner._origin.id
-                if partner_id:
-                    domain.append(("id", "!=", partner_id))
-                same_mobile_partner = self.with_context(active_test=False).search(
-                    domain, limit=1
-                )
-                same_mobile_partner_id = same_mobile_partner.id or False
-            partner.same_mobile_partner_id = same_mobile_partner_id
+        domain = [("mobile", "=", self.mobile)]
+        if self.company_id:
+            domain += [
+                "|",
+                ("company_id", "=", False),
+                ("company_id", "=", self.company_id.id),
+            ]
+        self_id = self._origin.id
+        if self_id:
+            domain.append(("id", "!=", self_id))
+        return domain
