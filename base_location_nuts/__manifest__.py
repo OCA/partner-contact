1416989471ea1273ac176f030d9a30e62ee318f9
Andrii Kompaniiets <andrii@moduon.team>
2025-09-03 09:23:43 +0200

    [FIX] base_location_nuts: Restrict permissions for creation and edition of Nuts
    Only users with administrator rights should be able to create and edit Nuts. Users without administrator permissions should only have read access to Nuts.

--- base_location_nuts/__manifest__.py
+++ base_location_nuts/__manifest__.py
@@ -7,7 +7,7 @@
 {
     "name": "NUTS Regions",
     "category": "Localisation/Europe",
-    "version": "18.0.1.0.1",
+    "version": "18.0.1.1.0",
     "depends": ["contacts"],
     "data": [
         "views/res_country_view.xml",
