cb7a5376acb21795584e691ab49743b87ee50f2e
Ruchir Shukla <ruchir@bizzappdev.com>
2025-09-04 12:42:27 +0530

    [IMP] partner_contact_in_several_companies: pre-commit auto fixes

--- partner_contact_in_several_companies/models/res_partner.py
+++ partner_contact_in_several_companies/models/res_partner.py
@@ -60,14 +60,14 @@ class ResPartner(models.Model):
             attached_contact_args = expression.AND(
                 (args, [("contact_type", "=", "attached")])
             )
-            attached_contacts = super(ResPartner, self).search(attached_contact_args)
+            attached_contacts = super().search(attached_contact_args)
             args = expression.OR(
                 (
                     expression.AND(([("contact_type", "=", "standalone")], args)),
                     [("other_contact_ids", "in", attached_contacts.ids)],
                 )
             )
-        return super(ResPartner, self).search(
+        return super().search(
             args, offset=offset, limit=limit, order=order, count=count
         )
 
@@ -99,7 +99,7 @@ class ResPartner(models.Model):
         """Returns the partner that is considered the commercial
         entity of this partner. The commercial entity holds the master data
         for all commercial fields (see :py:meth:`~_commercial_fields`)"""
-        result = super(ResPartner, self)._compute_commercial_partner()
+        result = super()._compute_commercial_partner()
         for partner in self:
             if partner.contact_type == "attached" and not partner.parent_id:
                 partner.commercial_partner_id = partner.contact_id
@@ -135,7 +135,7 @@ class ResPartner(models.Model):
         fields.related to the parent
         """
         self.ensure_one()
-        res = super(ResPartner, self)._fields_sync(update_values)
+        res = super()._fields_sync(update_values)
         contact_fields = self._contact_fields()
         # 1. From UPSTREAM: sync from parent contact
         if update_values.get("contact_id"):
