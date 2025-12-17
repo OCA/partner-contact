2a978b17f75166a48d6f5567b7cef3d6a5cd8263
yostashiro <tashiro@quartile.co>
2025-12-17 05:51:03 +0000

    [ADD] partner_bank_acc_holder_name

---
+++ partner_bank_acc_holder_name/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2025 Quartile (https://www.quartile.co)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Partner Bank Account Holder Name",
+    "summary": "Show the account holder name field on bank accounts",
+    "version": "18.0.1.0.0",
+    "website": "https://github.com/OCA/partner-contact",
+    "author": "Quartile, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": ["base"],
+    "data": ["views/res_partner_bank_views.xml"],
+}
