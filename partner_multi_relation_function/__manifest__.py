68c2a1b9b3bf4f23b2c732ab6d2f42286c0733b1
Ronald Portier <ronald@therp.nl>
2025-12-30 17:20:22 +0100

    [FIX] Adapt partner_multi_relation_function to simplification

--- partner_multi_relation_function/__manifest__.py
+++ partner_multi_relation_function/__manifest__.py
@@ -1,8 +1,8 @@
-# Copyright 2024 Therp BV <http://therp.nl>.
+# Copyright 2024-2025 Therp BV <http://therp.nl>.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Partner Relation Functions",
-    "version": "16.0.1.1.0",
+    "version": "16.0.2.0.0",
     "author": "Therp BV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/partner-contact",
     "maintainers": ["NL66278"],
@@ -17,7 +17,7 @@
     ],
     "data": [
         "views/res_partner_views.xml",
-        "views/res_partner_relation_all_views.xml",
+        "views/res_partner_relation_views.xml",
         "views/res_partner_relation_type_views.xml",
     ],
     "auto_install": False,
--- partner_multi_relation_function/models/res_partner_relation.py
+++ partner_multi_relation_function/models/res_partner_relation.py
@@ -1,4 +1,4 @@
-# Copyright 2024 Therp BV <http://therp.nl>
+# Copyright 2024-2025 Therp BV <http://therp.nl>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
@@ -9,13 +9,29 @@ class ResPartnerRelation(models.Model):
     _inherit = "res.partner.relation"
 
     function = fields.Char()
+    allow_function = fields.Boolean(
+        readonly=True,
+        related="type_id.allow_function",
+    )
 
     @api.constrains("function")
     def _check_function(self):
         """Function should only be filled when allowed on type."""
-        for record in self:
-            if record.function and not record.type_id.allow_function:
+        for this in self:
+            if this.function and not this.type_id.allow_function:
                 raise ValidationError(
                     _("You can not have a function on relations of type %(type)s."),
-                    {"type": record.type_id.display_name},
+                    {"type": this.type_id.display_name},
                 )
+
+    def name_get(self):
+        """Add function to name if present."""
+        wf = _(" with function ")  # Prevent repeated translation.
+        return [
+            (
+                this.id,
+                super(ResPartnerRelation, this).name_get()[0][1]
+                + (this.function and wf + this.function or ""),
+            )
+            for this in self
+        ]
