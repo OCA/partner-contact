a66baae400ccf05a4878e5faedcb93b42c6fed1b
Joerg Steffens <joerg.steffens@bareos.com>
2026-01-28 18:23:58 +0100

    [MIG] partner_contact_gender: Migration to 19.0

--- partner_contact_gender/tests/test_partner_contact_gender.py
+++ partner_contact_gender/tests/test_partner_contact_gender.py
@@ -1,17 +1,65 @@
 # Copyright 2016-2018 Therp BV <https://therp.nl>.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from unittest.mock import patch
+
 from odoo.tests.common import TransactionCase
 
 
 class TestPartnerContactGender(TransactionCase):
     def setUp(self):
         super().setUp()
-        self.testpartner = self.env["res.partner"].create(
-            {"name": "test", "title": self.env.ref("base.res_partner_title_madam").id}
+        self.testpartner_none = self.env["res.partner"].create(
+            {
+                "name": "test",
+            }
+        )
+        self.testpartner_male = self.env["res.partner"].create(
+            {
+                "name": "test",
+                "gender": "male",
+            }
+        )
+        self.testpartner_female = self.env["res.partner"].create(
+            {
+                "name": "test",
+                "gender": "female",
+            }
         )
 
     def test_partner_contact_gender(self):
-        from ..hooks import post_init_hook
+        self.assertFalse(self.testpartner_none.gender)
+        self.assertEqual(self.testpartner_male.gender, "male")
+        self.assertEqual(self.testpartner_female.gender, "female")
 
-        post_init_hook(self.env)
-        self.assertEqual(self.testpartner.gender, "female")
+    def test_partner_contact_gender_title(self):
+        from .. import hooks
+
+        if not hooks.has_module_partner_title(self.env):
+            self.skipTest("partner_title module not installed")
+
+        testpartner_title_madam = self.env["res.partner"].create(
+            {
+                "name": "test",
+                "title_id": self.env.ref("partner_title.res_partner_title_madam").id,
+            }
+        )
+        hooks.post_init_hook(self.env)
+        self.assertEqual(testpartner_title_madam.gender, "female")
+        self.assertFalse(self.testpartner_none.gender)
+
+    def test_partner_contact_gender_no_title_module(self):
+        testpartner = self.env["res.partner"].create(
+            {
+                "name": "test",
+            }
+        )
+
+        from .. import hooks
+
+        with patch(
+            "odoo.addons.partner_contact_gender.hooks.has_module_partner_title",
+            return_value=False,
+        ):
+            hooks.post_init_hook(self.env)
+
+        self.assertFalse(testpartner.gender)
