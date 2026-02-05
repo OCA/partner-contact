974f15f377590491493b2af1c4bf1d8be3430253
ngabsi <nihel.gabsi@acsone.eu>
2026-02-05 08:40:27 +0100

    [18.0][IMP] partner_street_city_search : replace town by city in module name

---
+++ partner_street_city_search/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2026 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Partner Street City Search",
+    "summary": """Enable partner search by street and city""",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/partner-contact",
+    "depends": [],
+    "data": [
+        "views/res_partner.xml",
+    ],
+    "demo": [],
+}
