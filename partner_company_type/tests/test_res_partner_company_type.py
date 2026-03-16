5cf40ee03695a5d69e12990a038b7e3030358b48
monthop-gmail <monthop-gmail@users.noreply.github.com>
2026-03-16 19:27:59 +0700

    [MIG] partner_company_type: Migration to 19.0

--- partner_company_type/tests/test_res_partner_company_type.py
+++ partner_company_type/tests/test_res_partner_company_type.py
@@ -1,7 +1,10 @@
 # Copyright 2017-2018 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from psycopg2 import IntegrityError
+try:
+    from psycopg import errors as psycopg_errors
+except ImportError:
+    from psycopg2 import errors as psycopg_errors
 
 from odoo import tools
 from odoo.tests.common import TransactionCase
@@ -11,12 +14,17 @@ class TestResPartnerCompanyType(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.company_type = cls.env.ref(
-            "partner_company_type.res_partner_company_type_sa"
+        cls.company_type = cls.env["res.partner.company.type"].create(
+            {"name": "SA Test", "shortcut": "SAT"}
         )
 
     def test_00_duplicate(self):
         # Test Duplicate Company type
 
-        with self.assertRaises(IntegrityError), tools.mute_logger("odoo.sql_db"):
-            self.company_type.create(dict(name=self.company_type.name))
+        with (
+            self.assertRaises(psycopg_errors.UniqueViolation),
+            tools.mute_logger("odoo.sql_db"),
+        ):
+            self.env["res.partner.company.type"].create(
+                {"name": self.company_type.name}
+            )
