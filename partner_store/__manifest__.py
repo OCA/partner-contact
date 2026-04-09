ba0418fa0435cec64aa3716ca1fc634acae5380b
Cédric Paradis <cparadis@impressfoods.com>
2026-04-09 19:12:26 +0000

    [MIG] partner_store: Migration to 19.0

--- partner_store/__manifest__.py
+++ partner_store/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/partner-contact",
     "category": "",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["contacts"],
     "data": [],
