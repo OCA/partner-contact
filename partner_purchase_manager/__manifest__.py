d2caa9fc9a0aa40b07c46ae0a111feaa5e6a4a38
uncannycs <uncannycs@gmail.com>
2026-05-27 11:44:15 +0530

    [19.0] [MIG] partner_purchase_manager: Migration to 19.0.

--- partner_purchase_manager/__manifest__.py
+++ partner_purchase_manager/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Partner Purchase Manager",
     "summary": "Add purchase manager field in partner",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "development_status": "Alpha",
     "category": "Purchase",
     "website": "https://github.com/OCA/partner-contact",
