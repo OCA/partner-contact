33e593bcb8d28563143c319b3d2b1af666d266e3
Don Kendall <dkendall@ledoweb.com>
2026-06-15 10:34:14 -0400

    [MIG] partner_street_city_search: Migration to 19.0

--- partner_street_city_search/__manifest__.py
+++ partner_street_city_search/__manifest__.py
@@ -3,8 +3,8 @@
 
 {
     "name": "Partner Street City Search",
-    "summary": """Enable partner search by street and city""",
-    "version": "18.0.1.0.0",
+    "summary": "Enable partner search by street and city",
+    "version": "19.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/partner-contact",
