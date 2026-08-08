b444bf25fce66afede22350ce437cee329a9d36f
Andrii Chychkan <mywork1728@gmail.com>
2026-08-08 18:32:57 +0300

    [IMP] partner_industry_parent: pre-commit auto fixes

--- partner_industry_parent/__manifest__.py
+++ partner_industry_parent/__manifest__.py
@@ -7,7 +7,7 @@
         This module add a parent relation to the partner industry""",
     "version": "18.0.1.0.0",
     "license": "AGPL-3",
-    "author": "ACSONE SA/NV," "Odoo Community Association (OCA)",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/partner-contact",
     "depends": ["base"],
     "data": ["views/res_partner_industry.xml"],
