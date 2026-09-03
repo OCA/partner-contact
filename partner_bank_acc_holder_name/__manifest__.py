9edc829a20df028933d6974995f88f82154287db
Andrii Chychkan <mywork1728@gmail.com>
2026-09-03 21:35:45 +0300

    [MIG] partner_bank_acc_holder_name: Migration to 19.0

--- partner_bank_acc_holder_name/__manifest__.py
+++ partner_bank_acc_holder_name/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Partner Bank Account Holder Name",
     "summary": "Show the account holder name field on bank accounts",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "website": "https://github.com/OCA/partner-contact",
     "author": "Quartile, Odoo Community Association (OCA)",
     "license": "AGPL-3",
