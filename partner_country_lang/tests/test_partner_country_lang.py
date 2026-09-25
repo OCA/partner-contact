9a7dedf7ec86262616d7a344861d959417c1fc22
Sudhir Arya <sudhir@erpharbor.com>
2026-09-25 18:42:49 +0530

    [MIG] partner_country_lang: Migration to 20.0

--- partner_country_lang/tests/test_partner_country_lang.py
+++ partner_country_lang/tests/test_partner_country_lang.py
@@ -12,14 +12,16 @@ class TestPartnerCountryLang(BaseCommon):
         cls.lang_fr.active = True
         cls.country_fr = cls.env.ref("base.fr")
         cls.country_fr.lang = cls.lang_fr.code
-        cls.partner = cls.env["res.partner"].create({"name": "Mr Odoo"})
+        cls.partner = cls.env["res.partner"].sudo().create({"name": "Mr Odoo"})
 
     def test_partner_create(self):
-        partner = self.env["res.partner"].create(
-            {"name": "Mrs Odoo", "country_id": self.country_fr.id}
+        partner = (
+            self.env["res.partner"]
+            .sudo()
+            .create({"name": "Mrs Odoo", "country_id": self.country_fr.id})
         )
         self.assertEqual(partner.lang, self.lang_fr.code)
 
     def test_partner_write(self):
-        self.partner.write({"country_id": self.country_fr.id})
+        self.partner.sudo().write({"country_id": self.country_fr.id})
         self.assertEqual(self.partner.lang, self.lang_fr.code)
